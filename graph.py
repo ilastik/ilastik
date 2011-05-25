3c34f7b077885981837bee104190a0a09f0f8371
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2011-05-25 13:43:28 +0200

    spelling

--- graph.py
+++ graph.py
@@ -585,7 +585,7 @@ class Operator(object):
             oo = o.getInstance(self)
             self.outputs[o.name] = oo         
             # output slots are connected
-            # when the correspondign input slots
+            # when the corresponding input slots
             # of the partner operators are created       
         self.graph.registerOperator(self)
          
@@ -654,13 +654,13 @@ class OperatorWrapper(Operator):
         self._inputSlots = []
         self._outputSlots = []
         
-        # replicate inputslot definitions
+        # replicate input slot definitions
         for islot in self.operator.inputSlots:
             level = islot.level + 1
             print "new level", level, "for slot", islot.name
             self._inputSlots.append(MultiInputSlot(islot.name, level = level))
 
-        # replicate outputslot definitions
+        # replicate output slot definitions
         for oslot in self.outputSlots:
             level = oslot.level + 1
             self._outputSlots.append(MultiOutputSlot(oslot.name, level = level))
@@ -686,7 +686,7 @@ class OperatorWrapper(Operator):
                 op = op.operator
             op.outputs[oslot.name] = oo
 
-        #connnect input slots
+        #connect input slots
         for islot in self.origInputs.values():
             ii = self.inputs[islot.name]
             partner = islot.partner
@@ -930,7 +930,7 @@ class Graph(object):
             event.set()
             if customClosure is not None:
                 customClosure()
-            # return something that can be handeled by the innnerWork function
+            # return something that can be handled by the innnerWork function
             ret = [None, gr, event, thread]
             return ret
         task = [runnerClosure, gr, event, thread]
