6e453c4a1ebb124d7153384c953419bcbe6a0bc7
Christoph Straehle <christoph.straehle@iwr.uni-heidelberg.de>
2011-06-10 13:59:09 +0200

    initial setuptools packagin

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+    name = "lazyflow",
+#    version = "0.1",
+    packages = find_packages('lazyflow'),
+    scripts = [],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['greenlet'],
+
+    package_data = {
+        '': ['*.txt', '*.py'],
+    },
+
+    include_package_data = True,    # include everything in source control
+
+
+    # metadata for upload to PyPI
+    author = "Christoph Straehle",
+    author_email = "christoph.straehle@iwr.uni-heidelberg.de",
+    description = "Lazyflow - graph based lazy numpy data flows",
+    license = "BSD",
+    keywords = "graph numpy dataflow",
+    url = "http://ilastik.org/lazyflow",
+)
