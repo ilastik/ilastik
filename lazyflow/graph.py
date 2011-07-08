bbfe7db8a341456c6dbc25b7b96e07870c889e2f
Christoph Straehle <christoph.straehle@iwr.uni-heidelberg.de>
2011-07-08 13:35:22 +0200

    lazyflow now has a settings directory (~/.lazyflow) and a config file (~/.lazyflow/config) also: extended settings reader

--- lazyflow/graph.py
+++ lazyflow/graph.py
@@ -36,6 +36,7 @@ import psutil
 import os
 import time
 import gc
+import ConfigParser
 
 from roi import sliceToRoi, roiToSlice
 from collections import deque
@@ -46,6 +47,57 @@ import weakref
 
 
 greenlet.GREENLET_USE_GC # use garbage collection
+
+
+class DefaltConfigParser(ConfigParser.SafeConfigParser):
+    """
+    Simple extension to the default SafeConfigParser that
+    accepts a default parameter in its .get method and
+    returns its valaue when the parameter cannot be found
+    in the config file instead of throwing an exception
+    """
+    def __init__(self):
+        ConfigParser.SafeConfigParser.__init__(self)
+        if not os.path.exists(CONFIG_DIR+"config"):
+            self.configfile = open(CONFIG_DIR+"config", "w+")
+        else:
+            self.configfile = open(CONFIG_DIR+"config", "r+")
+        self.readfp(self.configfile)
+        self.configfile.close()
+    
+    def get(self, section, option, default = None):
+        """
+        accepts a default parameter and returns its value
+        instead of throwing an exception when the section
+        or option is not found in the config file
+        """
+        try:
+            ans = ConfigParser.SafeConfigParser.get(self, section, option)
+            print ans
+            return ans
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError), e:
+            if default is not None:
+                if e == ConfigParser.NoSectionError:
+                    self.add_section(section)
+                    self.set(section, option, default)
+                if e == ConfigParser.NoOptionError:
+                    self.set(section, option, default)
+                return default
+            else:
+                raise e
+
+try:
+    if LAZYFLOW_LOADED == None:
+        pass
+except:
+    LAZYFLOW_LOADED = True
+    
+    CONFIG_DIR = os.path.expanduser("~/.lazyflow/")
+    if not os.path.exists(CONFIG_DIR):
+        os.mkdir(CONFIG_DIR)
+    CONFIG = DefaltConfigParser()
+
+
 
 def itersubclasses(cls, _seen=None):
     """
