90037bf03a008b3bf2f341fc714341b483e0e0ec
lfiaschi <luca.fiaschi@iwr.uni-heidelberg.de>
2011-07-19 16:54:41 +0200

    improved run tests

--- run_tests.py
+++ run_tests.py
@@ -9,8 +9,11 @@ def do(testname):
     if testname not in excludeList:
         cmd = "cd tests && python " + test
         if os.system(cmd):
-            raise
+            raise RuntimeError, "fail to execute " + testname
+    else:
+        print "#####################skipping test: " + testname
 
 for test in tests:
+    print "#######################executing test: " + test
     folder,test=os.path.split(test)
     do(test)
