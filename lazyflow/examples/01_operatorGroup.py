3fa11e7c15adf834ec998190912417ffd00b2625
lfiaschi <luca.fiaschi@iwr.uni-heidelberg.de>
2011-08-24 11:16:06 +0200

    added examples of usage OpPixelFeatures

---
+++ lazyflow/examples/01_operatorGroup.py
@@ -0,0 +1,39 @@
+import vigra
+import threading
+from lazyflow.graph import *
+import copy
+
+from lazyflow.operators.operators import OpArrayPiper 
+from lazyflow.operators.vigraOperators import *
+from lazyflow.operators.valueProviders import *
+from lazyflow.operators.classifierOperators import *
+from lazyflow.operators.generic import *
+from lazyflow import operators
+import numpy
+
+g = Graph(numThreads = 1, softMaxMem = 2000*1024**2)
+
+
+OpG = operators.OpPixelFeatures(g)
+
+inputImage = vigra.impex.readImage("../../tests/ostrich.jpg")
+
+#OpG.inputs["Input"].setValue(inputImage)
+OpG.inputs["Input"].setValue(numpy.random.rand(200,200,200).astype(numpy.float32))
+OpG.inputs["Matrix"].setValue([[1,1,0,0],[0,1,0,0],[0,1,0,0],[1,0,0,1]])
+OpG.inputs["Scales"].setValue([1,20,0.30,0.40])
+
+print OpG.outputs["Output"].shape
+
+dest1 = OpG.outputs["Output"][:,:,0:3].allocate().wait()
+dest2= OpG.outputs["Output"][:,:,3:6].allocate().wait()
+dest3 = OpG.outputs["Output"][:,:,6:9].allocate().wait()
+
+vigra.impex.writeImage(dest1,"resultOpG1.jpg") 
+vigra.impex.writeImage(dest2,"resultOpG2.jpg") 
+vigra.impex.writeImage(dest3,"resultOpG3.jpg") 
+
+
+
+#write shifted image on disk
+g.finalize()
