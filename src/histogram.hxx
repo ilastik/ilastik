2e87c6556bf14ac1ebf5faaab8c255fe878bf0c3
Claus Ripp <claus.ripp@iwr.uni-heidelberg.de>
2011-09-29 15:18:13 +0200

    new integraloverlapping functions with different weightings

--- src/histogram.hxx
+++ src/histogram.hxx
@@ -95,7 +95,7 @@ void histogram2D(const MultiArrayView<3, T, S1>& image, int nbins,
 // NB Assumes that the values of each channel in the original image are between 0,1
 template<class T, class S1, class S2>
 void overlappingHistogram2D(MultiArrayView<3, T, S1>& image, int nbins,
-		float frac_overlap, MultiArrayView<3, float, S2>& Hist) {
+                float frac_overlap, MultiArrayView<3, T, S2>& Hist) {
 
 	vigra_precondition(frac_overlap>=0 && frac_overlap<1,"fractionary overlap between [0,1)");
 
@@ -123,19 +123,19 @@ void overlappingHistogram2D(MultiArrayView<3, T, S1>& image, int nbins,
 				double t = 0;
 
 				T val = image(x, y, c);
-				if (val <= dt + 2 * ddt)
+                                if (val < dt + ddt)
 					++Hist(x, y, shift+0);
 				t += dt;
 
 				for (int k = 1; k < nbins - 1; k++)
-				{
-					if (val > t - ddt && val <= t + dt + ddt)
+                                {
+                                        if (val >= t - ddt && val < t + dt + ddt)
 						++Hist(x, y, shift + k);
 					t += dt;
 				}
 
 				if (nbins - 2 >= 0)
-					if (val <= 1 && val > 1 - dt - 2 * ddt)
+                                        if (val <= 1 && val >= 1 - dt - ddt)
 						++Hist(x, y, shift + (nbins - 1));
 
 			}
@@ -147,34 +147,31 @@ void overlappingHistogram2D(MultiArrayView<3, T, S1>& image, int nbins,
 //compute integral histogram of a given image
 //for multichannel images, compute channelwise
 // we can choose an amount of overlap beteeen different pixels
-// The imput shoudl be a multiarray view last index indicating the channel
+// The imput shoudl be a multiarray view last index indicating the channelstd::endl;
 // The result is a Histogram where last index loops through the bins
 // NB Assumes that the values of each channel in the original image are between 0,1
 template<class T1,class T2, class S1, class S2>
-void integralHistogram2D(MultiArrayView<3, T1, S1>& image, int nbins,
+void
+integralHistogram2D(MultiArrayView<3, T1, S1>& image, int nbins,
 		MultiArrayView<3, T2, S2>& H) {
 
 	//FIXME you are imposing between zero and 1
-    //The order in the output array is all bins of the first channel,
-    //then all bins of the second channel
-    
-    
 	int height = image.shape()[0];
 	int width = image.shape()[1];
 	int nc = image.shape()[2];
 	int x,y,c,index;
 	MultiArrayShape<3>::type resShp(width, height, nc * nbins);
 	vigra_precondition(resShp[2]==H.shape(2),"wrong shape");
-	//std::cerr << "inside c++" << std::endl;
-    //std::cout << "processing image with width="<<width<<", height="<<height<<", channels="<<nc<<std::endl;
-    //std::cout<<"number of bins="<<nbins<<std::endl;
-    
-    
-    //For all the channels do the same
+
+
+
+
+	std::cerr << "inside c++" << std::endl;
+	//For all the channels do the same
 	for (c=0;c<nc;c++)
 	{
 		int shift=c*nbins;
-		//std::cerr << "channel " << c<<std::endl ;
+		//std::cerr << "here" << std::endl ;
 
 		//initialize the histogram
 		for(int j=shift; j<nbins+shift;j++)
@@ -222,6 +219,334 @@ void integralHistogram2D(MultiArrayView<3, T1, S1>& image, int nbins,
 
 }
 
+
+template<class T1,class T2, class S2>
+void
+integratePixel(T1& val, int y, int x, int nbins,
+               float frac_overlap, int shift, MultiArrayView<3, T2, S2>& Hist) {
+
+
+    double dt = 1 / double(nbins);
+    double ddt = dt * frac_overlap;
+
+    double t = 0;
+
+    if (val < dt + ddt)
+            Hist(y, x, shift+0)++;
+    t += dt;
+
+    for (int k = 1; k < nbins - 1; k++)
+    {
+            if (val >= t - ddt && val < t + dt + ddt)
+                    Hist(y, x, shift + k)++;
+            t += dt;
+    }
+
+    if (nbins - 2 >= 0)
+            if (val <= 1 && val >= 1 - dt - 1 * ddt)
+                    Hist(y, x, shift + (nbins - 1))++;
+}
+
+double wfunc(double w){
+
+    return w;
+
+}
+
+template<class T>
+double overlapWeighting(double t, double dt, double ddt, T& val ) {
+
+    double w;
+    if (val >= t + ddt && val <= t + dt - ddt) return 1;
+    else if (val < t + ddt ) w = (val - (t - ddt))/(2*ddt);
+    else if (val > t + dt - ddt) w = ((t + dt + ddt - val))/(2*ddt);
+    return wfunc(w);
+}
+
+
+template<class T1,class T2, class S2>
+void
+integratePixelWeightLin(T1& val, int x, int y, int nbins,
+               float frac_overlap, int shift, MultiArrayView<3, T2, S2>& Hist) {
+
+
+    double dt = 1 / double(nbins);
+    double ddt = dt * frac_overlap;
+
+    double t = 0;
+
+    if (val < dt + ddt)
+    {
+            if (val< dt- ddt) Hist(x,y,shift+0) ++;
+            else Hist(x, y, shift+0) += overlapWeighting(t,dt,ddt,val);
+    }
+    t += dt;
+
+    for (int k = 1; k < nbins - 1; k++)
+    {
+            if (val >= t - ddt && val < t + dt + ddt)
+                    Hist(x, y, shift + k) += overlapWeighting(t,dt,ddt,val);
+            t += dt;
+    }
+
+    if (nbins - 2 >= 0)
+            if (val <= 1 && val >= 1 - dt - ddt)
+            {
+                    if (val > 1 - dt + ddt) Hist(x,y,shift + (nbins - 1)) ++;
+                    else Hist(x, y, shift + (nbins - 1)) += overlapWeighting(t,dt,ddt,val);
+            }
+}
+
+
+
+template<class T1,class T2, class S2>
+void
+integratePixelWeightGauss(T1& val, int x, int y, int nbins,
+               float frac_overlap, int shift, MultiArrayView<3, T2, S2>& Hist) {
+
+
+
+}
+
+
+
+
+template<class T1,class T2, class S1, class S2>
+void
+integralOverlappingHistogram2D(MultiArrayView<3, T1, S1>& image, int nbins,
+               float frac_overlap, MultiArrayView<3, T2, S2>& H) {
+
+
+
+
+        //FIXME you are imposing between zero and 1
+        int width = image.shape()[0];
+        int height = image.shape()[1];
+        int nc = image.shape()[2];
+        int x,y,c,index;
+        MultiArrayShape<3>::type resShp(width, height, nc * nbins);
+        vigra_precondition(resShp[2]==H.shape(2),"wrong shape");
+        vigra_precondition(frac_overlap>=0 && frac_overlap<1,"fractionary overlap between [0,1)");
+
+
+        std::cerr << "inside c++" << std::endl;
+        //For all the channels do the same
+        for (c=0;c<nc;c++)
+        {
+                int shift=c*nbins;
+                //std::cerr << "here" << std::endl ;
+
+                //initialize the histogram
+                for(int j=shift; j<nbins+shift;j++)  H(0,0,j)=0;
+
+                integratePixel(image(0,0,c),0,0, nbins,frac_overlap,shift,H);
+
+
+                for (y=1;y<height;y++)
+                {
+                        for(int j=shift; j<nbins+shift;j++)  H(0,y,j)=H(0,y-1,j);
+
+                        integratePixel(image(0,y,c),0,y, nbins,frac_overlap,shift,H);
+                }
+
+                for (x=1;x<width;x++)
+                {                       
+                        for(int j=shift; j<nbins+shift;j++)   H(x,0,j)=H(x-1,0,j);
+
+                        integratePixel(image(x,0,c),x,0, nbins,frac_overlap,shift,H);
+                }
+
+                for (y=1;y<height;y++)
+                        for(x=1;x<width;x++)
+                        {
+                                for(int j=shift; j<nbins+shift;j++)  H(x,y,j)=H(x,y-1,j)+H(x-1,y,j)-H(x-1,y-1,j);
+
+                                integratePixel(image(x,y,c),x,y, nbins,frac_overlap,shift,H);
+                        }
+
+        }
+
+}
+
+
+template<class T1,class T2, class S1, class S2>
+void
+integralOverlappingWeightLinHistogram2D(MultiArrayView<3, T1, S1>& image, int nbins,
+               float frac_overlap, MultiArrayView<3, T2, S2>& H) {
+
+
+
+
+        //FIXME you are imposing between zero and 1
+        int width = image.shape()[0];
+        int height = image.shape()[1];
+        int nc = image.shape()[2];
+        int x,y,c,index;
+        MultiArrayShape<3>::type resShp(width, height, nc * nbins);
+        vigra_precondition(resShp[2]==H.shape(2),"wrong shape");
+        vigra_precondition(frac_overlap>=0 && frac_overlap<1,"fractionary overlap between [0,1)");
+
+
+        std::cerr << "inside c++" << std::endl;
+        //For all the channels do the same
+        for (c=0;c<nc;c++)
+        {
+                int shift=c*nbins;
+                //std::cerr << "here" << std::endl ;
+
+                //initialize the histogram
+                for(int j=shift; j<nbins+shift;j++)  H(0,0,j)=0;
+
+                integratePixelWeightLin(image(0,0,c),0,0, nbins,frac_overlap,shift,H);
+
+
+                for (y=1;y<height;y++)
+                {
+                        for(int j=shift; j<nbins+shift;j++)  H(0,y,j)=H(0,y-1,j);
+
+                        integratePixelWeightLin(image(0,y,c),0,y, nbins,frac_overlap,shift,H);
+                }
+
+                for (x=1;x<width;x++)
+                {
+                        for(int j=shift; j<nbins+shift;j++)   H(x,0,j)=H(x-1,0,j);
+
+                        integratePixelWeightLin(image(x,0,c),x,0, nbins,frac_overlap,shift,H);
+                }
+
+                for (y=1;y<height;y++)
+                        for(x=1;x<width;x++)
+                        {
+                                for(int j=shift; j<nbins+shift;j++)  H(x,y,j)=H(x-1,y,j)+H(x,y-1,j)-H(x-1,y-1,j);
+
+                                integratePixelWeightLin(image(x,y,c),x,y, nbins,frac_overlap,shift,H);
+                        }
+
+        }
+
+}
+
+
+
+template<class T1,class T2, class S1, class S2>
+void
+integralOverlappingWeightGaussHistogram2D(MultiArrayView<3, T1, S1>& image, int nbins,
+               float sigma, MultiArrayView<3, T2, S2>& H) {
+
+
+
+
+        //FIXME you are imposing between zero and 1
+        int width = image.shape()[0];
+        int height = image.shape()[1];
+        int nc = image.shape()[2];
+        int x,y,c,index;
+        MultiArrayShape<3>::type resShp(width, height, nc * nbins);
+        vigra_precondition(resShp[2]==H.shape(2),"wrong shape");
+        vigra_precondition(sigma>0 ,"sigam greater should be than 0");
+
+        std::cerr << "inside c++" << std::endl;
+        //For all the channels do the same
+        for (c=0;c<nc;c++)
+        {
+                int shift=c*nbins;
+                //std::cerr << "here" << std::endl ;
+
+                //initialize the histogram
+                for(int j=shift; j<nbins+shift;j++)  H(0,0,j)=0;
+
+                integratePixelWeightGauss(image(0,0,c),0,0, nbins,sigma,shift,H);
+
+
+                for (y=1;y<height;y++)
+                {
+                        for(int j=shift; j<nbins+shift;j++)  H(0,y,j)=H(0,y-1,j);
+
+                        integratePixelWeightGauss(image(0,y,c),0,y, nbins,sigma,shift,H);
+                }
+
+                for (x=1;x<width;x++)
+                {
+                        for(int j=shift; j<nbins+shift;j++)   H(x,0,j)=H(x-1,0,j);
+
+                        integratePixelWeightGauss(image(x,0,c),x,0, nbins,sigma,shift,H);
+                }
+
+                for (y=1;y<height;y++)
+                        for(x=1;x<width;x++)
+                        {
+                                for(int j=shift; j<nbins+shift;j++)  H(x,y,j)=H(x-1,y,j)+H(x,y-1,j)-H(x-1,y-1,j);
+
+                                integratePixelWeightGauss(image(x,y,c),x,y, nbins,sigma,shift,H);
+                        }
+
+        }
+
+}
+
+template<class T, class S1, class S2>
+void overlappingWeightLinHistogram2D(MultiArrayView<3, T, S1>& image, int nbins,
+                float frac_overlap, MultiArrayView<3, T, S2>& Hist) {
+
+        vigra_precondition(frac_overlap>=0 && frac_overlap<1,"fractionary overlap between [0,1)");
+
+        int width = image.shape()[0];
+        int height = image.shape()[1];
+        int nc = image.shape()[2];
+
+        MultiArrayShape<3>::type resShp(width, height, nc * nbins);
+        vigra_precondition(resShp[2]==Hist.shape(2),"wrong shape");
+
+        int x, y, c, index;
+
+
+        //Do this for all the channels
+        for (c = 0; c < nc; c++)
+        {
+            std::cout <<"EEE" << std::endl;
+                int shift=nbins*c; //shift to write the result to corret bin
+
+                for (y = 0; y < height; y++)
+                {
+                        for (x = 0; x < width; x++)  integratePixelWeightLin(image(x,y,c),x,y,nbins,frac_overlap,shift,Hist);
+
+                }
+        }
+
+}
+
+
+template<class T, class S1, class S2>
+void overlappingWeightGaussHistogram2D(MultiArrayView<3, T, S1>& image, int nbins,
+                float sigma, MultiArrayView<3, T, S2>& Hist) {
+
+        vigra_precondition(sigma>0 ,"sigam greater should be than 0");
+
+        int width = image.shape()[0];
+        int height = image.shape()[1];
+        int nc = image.shape()[2];
+
+        MultiArrayShape<3>::type resShp(width, height, nc * nbins);
+        vigra_precondition(resShp[2]==Hist.shape(2),"wrong shape");
+
+        int x, y, c, index;
+
+        //Do this for all the channels
+        for (c = 0; c < nc; c++)
+        {
+            std::cout <<"EEE" << std::endl;
+                int shift=nbins*c; //shift to write the result to corret bin
+
+                for (y = 0; y < height; y++)
+                {
+                        for (x = 0; x < width; x++)  integratePixelWeightGauss(image(x,y,c),x,y,nbins,sigma,shift,Hist);
+
+                }
+        }
+
+}
+
+
 template<class IND, class T1,class T2, class S1, class S2>
 void contextHistogram2D(MultiArrayView<1, IND, S1>&radii, int nbins, 
                         MultiArrayView<3, T1, S1>& image, 
@@ -232,8 +557,6 @@ void contextHistogram2D(MultiArrayView<1, IND, S1>&radii, int nbins,
     //where  for each r_i the middle of the square of size r_i-1 is removed
     //for multichannel images, histogramming is done channel-wise
     
-    //std::cout<<"contextHistogram2D in c++"<<std::endl;
-    
     int nx = image.shape()[0];
     int ny = image.shape()[1];
     int nchannels = image.shape()[2];
@@ -243,8 +566,6 @@ void contextHistogram2D(MultiArrayView<1, IND, S1>&radii, int nbins,
     int nnewfeatures = nr*nctb;
     MultiArrayShape<3>::type resShp(nx, ny, nctb);
     MultiArray<3, T1> integral(resShp);
-    
-    //std::cout<<"computing the integral histogram"<<std::endl;
     integralHistogram2D(image, nbins, integral);
     
     std::vector<T1> flathisto(nnewfeatures);
@@ -256,9 +577,7 @@ void contextHistogram2D(MultiArrayView<1, IND, S1>&radii, int nbins,
     
     for (int ix=0; ix<nx; ++ix){
         for (int iy=0; iy<ny; ++iy){
-            //std::cout<<"x="<<ix<<", y="<<iy<<std::endl;
             contextHistogramFeatures(ix, iy, nchannels, lr, ll, ur, ul, radii, integral, flathisto);
-            //std::cout<<"computed, writing"<<std::endl;
             for (int ii=0; ii<nnewfeatures; ++ii){
                 res(ix, iy, ii) = flathisto[ii];
             }
@@ -274,7 +593,7 @@ void contextHistogramFeatures(int x, int y, int nclasses,
                               MultiArrayView<3, T1, S2>& integral,
                               std::vector<T1>& flathisto)
 {
-    //The order in the output array is first radius, then channel, then bins.
+
     
     int nr = radii.shape()[0];
     int nctb = integral.shape()[2];
