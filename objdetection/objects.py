d78cc1078d20ed65fc1b2e456711a9e0b96a0e76
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-01-06 21:48:42 +0100

    OpObjectExtractor stub

---
+++ objdetection/objects.py
@@ -0,0 +1,45 @@
+import numpy
+from lazyflow.graph import Graph, Operator, InputSlot, OutputSlot
+
+class Object:
+    def __repr__( self ):
+        s = "Object:\n"
+        for attr in dir(self):
+            if not attr.startswith('__'):
+                s += attr + ': '+str(getattr(self,attr)) + "\n"
+        return s
+
+def objects_from_connected_components( cc, background=None ):
+    labels = numpy.unique(cc)
+    labels = labels[labels != background] if background else labels
+    
+    ret = []
+    for label in labels:
+        obj = Object()
+        obj.location = numpy.mean(numpy.vstack(numpy.where(cc == label)), axis=1)
+        ret.append(obj)
+    return ret    
+
+class OpObjectExtractor( Operator ):
+    name = "OpObjectExtractor"
+
+    inputSlots = [InputSlot('Input')]
+    outputSlots = [OutputSlot('Output')]
+
+    def notifyConnectAll(self):
+        self.outputs["Output"]._shape = self.Input.shape
+
+    def getOutSlot( self, slot, key, result ):
+        cc = self.Input[key].allocate().wait()
+        objs = objects_from_connected_components(cc, background = 0)
+        result = objs
+        
+if __name__ == '__main__':
+    cc = numpy.load("cc.npy")    
+    g = Graph()
+    extractor = OpObjectExtractor( g )
+    extractor.Input.setValue(cc)
+    objs = []
+    extractor.Output[:].writeInto(objs).wait()
+    print objs, "sad"
+
