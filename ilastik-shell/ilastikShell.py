311d813e35b34b8492ca08d5d08e7d4b6bd06567
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-02-02 17:12:53 +0100

    first attempt at a real IlastikShell

---
+++ ilastik-shell/ilastikShell.py
@@ -0,0 +1,49 @@
+from PyQt4 import uic
+from PyQt4 import Qt
+from PyQt4.QtGui import QMainWindow, QLabel
+from PyQt4 import QtCore
+
+
+class Applet( object ):
+    def __init__( self, name = "Example Applet" ):
+        self.name = name
+        
+        self.__centralWidget = QLabel(name + " Central Widget")
+        self.__controlWidget = QLabel(name + " Control Widget")
+        
+    def centralWidget( self ):
+        return self.__centralWidget
+
+    def controlWidget( self ):
+        return self.__controlWidget
+
+
+
+class IlastikShell( QMainWindow ):
+    def __init__( self, workflow = [], parent = None, flags = QtCore.Qt.WindowFlags(0) ):
+        QMainWindow.__init__(self, parent = parent, flags = flags )
+        uic.loadUi( "ui/ilastikShell.ui", self )
+        self._applets = []
+
+        for applet in workflow:
+            self.addApplet(applet)
+
+    def addApplet( self, applet ):
+        self._applets.append(applet)
+        self.appletBar.addItem( applet.controlWidget() , applet.name )
+
+    def setCurrentIndex( self, index ):
+        
+
+
+if __name__ == "__main__":
+    #make the program quit on Ctrl+C
+    import signal
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    from PyQt4.QtGui import QApplication
+    import sys
+
+    qapp = QApplication(sys.argv)
+    shell = IlastikShell( [Applet(), Applet("Tracking")])
+    shell.show()
+    qapp.exec_()
