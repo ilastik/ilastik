9366057f2e2acfb48ae00446657d5caaefd248b4
Stuart Berg <bergs@janelia.hhmi.org>
2012-04-07 13:37:48 +0200

    Moved data import functions into a separate class.

--- ilastik-shell/applets/pixelClassification.py
+++ ilastik-shell/applets/pixelClassification.py
@@ -32,6 +32,8 @@ from ilastikshell.applet import Applet
 
 import vigra
 
+from dataImporter import DataImporter
+
 class PixelClassificationGui(QMainWindow):        
     def __init__(self, pipeline = None, graph = None ):
         QMainWindow.__init__(self)
@@ -66,7 +68,13 @@ class PixelClassificationGui(QMainWindow):
         
         #if the filename was specified on command line, load it
         def loadFile():
-            self._openFile(sys.argv[1:])
+            # Put the data into an operator
+            importer = DataImporter( self.g )
+            inputProvider = importer.openFile(sys.argv[1:])
+            
+            # Connect the new input operator to our pipeline
+            self.pipeline.setInputData(inputProvider)
+
         if len(sys.argv) > 1:
             QTimer.singleShot(0, loadFile)
         
@@ -344,10 +352,17 @@ class PixelClassificationGui(QMainWindow):
         self.stackLoader.loadButton.clicked.connect(self._stackLoad)
 
     def openFile(self):
-        fileNames = QFileDialog.getOpenFileNames(self, "Open Image", os.path.abspath(__file__), "Numpy and h5 files (*.npy *.h5)")
+        fileNames = QFileDialog.getOpenFileNames(
+            self, "Open Image", os.path.abspath(__file__), "Numpy and h5 files (*.npy *.h5)")
         if fileNames.count() == 0:
             return
-        self._openFile(fileNames)
+        
+        # Put the data into an operator
+        importer = DataImporter( self.g )
+        inputProvider = importer.openFile(fileNames)
+        
+        # Connect the operator to the pipeline input
+        self.pipeline.setInputData(inputProvider)
     
     def _stackLoad(self):
         inputProvider = OpArrayPiper(self.g)
@@ -366,57 +381,7 @@ class PixelClassificationGui(QMainWindow):
         self.pipeline.setInputData(inputProvider)
         self.stackLoader.close()
             
-    def _openFile(self, fileNames):
-        """Open the given image file(s) and load them into our pipeline."""
-        inputProvider = None
-        fName, fExt = os.path.splitext(str(fileNames[0]))
-        print "Opening Files %r" % fileNames
-        if fExt=='.npy':
-            inputProvider = self.createArrayPiperFromNpyFile(fileNames)
-        elif fExt=='.h5':
-            inputProvider = createArrayPiperFromHdf5File(fileNames)
-        else:
-            raise RuntimeError("opening filenames=%r not supported yet" % fileNames)
 
-        self.pipeline.setInputData(inputProvider)
-    
-    def createArrayPiperFromNpyFile(self, fileNames):
-        """Open given .npy file(s) and produce an array piper operator with the data."""
-        fileName = fileNames[0]
-        if len(fileNames)>1:
-            print "WARNING: only the first file will be read, multiple file prediction not supported yet"
-        fName, fExt = os.path.splitext(str(fileName))
-        self.raw = numpy.load(str(fileName))
-        self.min, self.max = numpy.min(self.raw), numpy.max(self.raw)
-        inputProvider = OpArrayPiper(self.g)
-        self.raw = self.raw.view(vigra.VigraArray)
-        self.raw.axistags =  vigra.AxisTags(
-            vigra.AxisInfo('t',vigra.AxisType.Time),
-            vigra.AxisInfo('x',vigra.AxisType.Space),
-            vigra.AxisInfo('y',vigra.AxisType.Space),
-            vigra.AxisInfo('z',vigra.AxisType.Space),
-            vigra.AxisInfo('c',vigra.AxisType.Channels))
-        inputProvider.inputs["Input"].setValue(self.raw)
-        
-        return inputProvider
-
-    def createArrayPiperFromHdf5File(self, fileNames):
-        """Open given .h5 file(s) and produce an array piper operator with the data."""
-        readerNew=OpH5ReaderBigDataset(self.g)
-        
-        readerNew.inputs["Filenames"].setValue(fileNames)
-        readerNew.inputs["hdf5Path"].setValue("volume/data")
-
-        readerCache =  OpSlicedBlockedArrayCache(self.g)
-        readerCache.inputs["fixAtCurrent"].setValue(False)
-        readerCache.inputs["innerBlockShape"].setValue(((1,256,256,1,2),(1,256,1,256,2),(1,1,256,256,2)))
-        readerCache.inputs["outerBlockShape"].setValue(((1,256,256,4,2),(1,256,4,256,2),(1,4,256,256,2)))
-        readerCache.inputs["Input"].connect(readerNew.outputs["Output"])
-
-        inputProvider = OpArrayPiper(self.g)
-        inputProvider.inputs["Input"].connect(readerCache.outputs["Output"])
-        return inputProvider
-    
     def handleGraphInputChanged(self, newInputProvider):
         """Update our view of the data with the new dataset, as provided in the newInputProvider operator.""" 
         shape = newInputProvider.outputs["Output"].shape
@@ -487,7 +452,7 @@ class PixelClassificationGui(QMainWindow):
         self.labellayer = ColortableLayer(self.labelsrc, colorTable = [transparent.rgba()] )
         self.labellayer.name = "Labels"
         self.labellayer.ref_object = None
-        self.layerstack.append(self.labellayer)    
+        self.layerstack.append(self.labellayer)
     
     def initEditor(self, newInputProvider):
         shape = newInputProvider.outputs["Output"].shape
