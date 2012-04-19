3f46415b9b4d414fdbcb6971d2cca6a4a6392b11
Stuart Berg <bergs@janelia.hhmi.org>
2012-04-19 21:09:48 +0200

    Applet: Expanded the Applet base class interface.

--- ilastik-shell/ilastikshell/applet.py
+++ ilastik-shell/ilastikshell/applet.py
@@ -1,6 +1,88 @@
 from PyQt4.QtGui import QLabel, QApplication
 
-from test.serializationTestItems import ExampleSerializableItem
+class Applet( object ):
+    def __init__( self ):
+        self.name = None
+
+        # Status signal.
+        # Shell uses it for the status bar.
+        self.statusSignal = SimpleSignal() # Signature: emit(statusText)
+        
+        # Progress signal.
+        # When the applet is doing something time-consuming, this signal tells the shell.
+        self.progressSignal = SimpleSignal() # Signature: emit(percentComplete, canceled=false)
+
+    ###
+    ### Outputs provided by the Applet to the Shell or Workflow Manager
+    ###
+
+    @property
+    def topLevelOperator(self):
+        """
+        Return the applet's Top Level Operator, which is a single operator for all computation performed by the applet.
+        Each applet has exactly one top-level operator for performing computations.
+        Workflow managers can connect the top-level operator of one applet to others.
+        """
+        return None
+
+    @property
+    def centralWidget( self ):
+        """
+        Return the applet's central widget to be displayed on the right side of the window (if any).
+        """
+        return None
+
+    @property
+    def appletDrawers(self):
+        """
+        Return a list of the control widgets to be displayed in the left-hand side bar for this applet.
+        Only one will be open at any particular time.
+        """
+        return []
+    
+    @property
+    def menuWidget( self ):
+        return None
+
+    @property
+    def dataSerializers(self):
+        """
+        Return a list of dataSerializer objects for loading/saving any project data the applet is responsible for.
+        """ 
+        return []
+    
+    @property
+    def appletPreferencesManager(self):
+        """
+        Return the applet's preferences manager (if any).
+        
+        Applets with preferences or "last state" items that are independent 
+        of project saved data may save/load that information via a preferences manager.
+        """
+        return None
+
+    ###
+    ### Inputs provided to the Applet from the shell or workflow manager
+    ###
+
+    def setShellActions(self, shellActions):
+        """
+        Accept the given shell actions object and connect callbacks to it as needed.
+        
+        The shell provides a few actions that applets may choose to react to (load, save, etc.).
+        This function is called when the applet is added to the shell.
+        Applets can retain access to the shell actions if they need to trigger or respond to them.
+        """
+        pass
+          
+    def setVolumeEditor(self, editor):
+        """
+        Accept the provided volume editor object for display within the applet's central widget (if any).
+        Applets may share volume editors.
+        The volume editor is instantiated externally by the workflow manager, and passed to the applet via this function.
+        Applets may add and remove layers to the editor via the editor's layerstack member.
+        """
+        pass
 
 def run_applet( applet_type, *args, **kwargs):
     '''Run applet standalone.'''
@@ -12,44 +94,6 @@ def run_applet( applet_type, *args, **kwargs):
     applet.centralWidget.show()
     applet.controlWidget.show()
     qapp.exec_()
-
-class Applet( object ):
-    def __init__( self, name = "Example Applet" ):
-        """ Example constructor for testing purposes only.
-            No need to be called from subclasses."""
-        self.name = name
-        
-        self._centralWidget = QLabel(name + " Central Widget")
-        self._controlWidget = QLabel(name + " Control Widget")
-        self._menuWidget = QLabel(name + " Menu Widget")
-        
-        # Only a single serializable item for this example applet
-        self._serializableItems = [ExampleSerializableItem(self.name)]
-
-    @property
-    def centralWidget( self ):
-        return self._centralWidget
-
-    @property
-    def controlWidgets( self ):
-        """ A list of the control widgets to be displayed in the left-hand side bar for this applet"""
-        return self._controlWidgets
-
-    @property
-    def menuWidget( self ):
-        return self._menuWidget
-
-    @property
-    def serializableItems(self):
-        """ Return a list of serialization.SerializableItem objects for loading/saving """ 
-        return self._serializableItems
-    
-    def acceptShellActions(self, shellActions):
-        """
-        Called when the applet is added to the shell.
-        Applets can retain access to the shell actions if they need to trigger or respond to them.
-        """
-        pass
 
 if __name__ == '__main__':
     run_applet(Applet)
