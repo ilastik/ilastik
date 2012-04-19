b8e7dc5dcbf9f2dabfa96bbcb4ce75fbad2da0c7
Stuart Berg <bergs@janelia.hhmi.org>
2012-04-19 21:13:00 +0200

    oldIlastikInNewShoes: Deleted junk commented code.

--- ilastik-shell/workflows/oldIlastikInNewShoes.py
+++ ilastik-shell/workflows/oldIlastikInNewShoes.py
@@ -13,65 +13,16 @@ app = QApplication([])
 
 # Splash Screen
 splashImage = QPixmap("ilastik-splash.png")
-#painter = QtGui.QPainter()
-#painter.begin(splashImage)
-#painter.drawText(QtCore.QPointF(270,110), ilastik.core.readInBuildInfo())
-#painter.end()
-
 splashScreen = QSplashScreen(splashImage)
 splashScreen.show()
-
-
-#g = Graph()
-#pipeline = PixelClassificationPipeline( g )
-#pig = PixelClassificationGui( pipeline, graph = g)
-#pig.show()
-
-#inputDataSelectionApplet
-
 splashScreen = QSplashScreen(splashImage)
 splashScreen.show()
-
-
-#g = Graph()
-#pipeline = PixelClassificationPipeline( g )
-#pig = PixelClassificationGui( pipeline, graph = g)
-#pig.show()
-
-
-#from ilastikshell.applet import Applet
-#from PyQt4.QtGui import QMenuBar, QMenu
-#defaultApplet = Applet()
-## Normally applets would provide their own menu items,
-## but for this test we'll add them here (i.e. from the outside).
-#defaultApplet._menuWidget = QMenuBar()
-#defaultApplet._menuWidget.setNativeMenuBar( False ) # Native menus are broken on Ubuntu at the moment
-#defaultMenu = QMenu("Default Applet", defaultApplet._menuWidget)
-#defaultMenu.addAction("Default Action 1")
-#defaultMenu.addAction("Default Action 2")
-#defaultApplet._menuWidget.addMenu(defaultMenu)
-#defaultApplet._serializableItems = []
-
 inputDataSelectionApplet = InputDataSelectionApplet()
 pc = PixelClassificationApplet()
-
-#from ilastikshell.applet import Applet
-#from PyQt4.QtGui import QMenuBar, QMenu
-#defaultApplet = Applet()
-## Normally applets would provide their own menu items,
-## but for this test we'll add them here (i.e. from the outside).
-#defaultApplet._menuWidget = QMenuBar()
-#defaultApplet._menuWidget.setNativeMenuBar( False ) # Native menus are broken on Ubuntu at the moment
-#defaultMenu = QMenu("Default Applet", defaultApplet._menuWidget)
-#defaultMenu.addAction("Default Action 1")
-#defaultMenu.addAction("Default Action 2")
-#defaultApplet._menuWidget.addMenu(defaultMenu)
-#defaultApplet._serializableItems = []
 
 shell = IlastikShell()
 shell.addApplet(inputDataSelectionApplet)
 shell.addApplet(pc)
-#shell.addApplet(defaultApplet)
 shell.show()
 
 # Hide the splash screen
