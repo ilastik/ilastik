f2e76ec720434cac8eff0ab9000cdbe422224e76
Stuart Berg <bergs@janelia.hhmi.org>
2012-04-25 14:27:18 -0400

    OpNpyFileReader now attempts to guess the interpretation of the image dimensions:  - Reshapes 2D arrays to be 3D (with a single channel)  - Reshapes 3D arrays with a large 3rd dimension as 4D (with a single channel)  - Interprets 3D arrays with a small 3rd dimension as a 2D image with channels

--- ilastik-shell/applets/dataSelection/opNpyFileReader.py
+++ ilastik-shell/applets/dataSelection/opNpyFileReader.py
@@ -24,14 +24,40 @@ class OpNpyFileReader(Operator):
         # Load the file in read-only "memmap" mode to avoid reading it from disk all at once.
         rawNumpyArray = numpy.load(str(fileName), 'r')
 
+        # We assume that a 2D array should be treated as a single-channel image
+        if len(rawNumpyArray.shape) == 2:
+            rawNumpyArray = rawNumpyArray.reshape(rawNumpyArray.shape + (1,))
+
+        # For a 3D image with a large third dimension, we'll assume that 
+        #  it's a 3D grayscale image, not a 2D multi-channel image
+        if len(rawNumpyArray.shape) == 3 and rawNumpyArray.shape[2] > 3:
+            rawNumpyArray = rawNumpyArray.reshape(rawNumpyArray.shape + (1,))
+
         # Cast to vigra array
         self.rawVigraArray = rawNumpyArray.view(vigra.VigraArray)
-        self.rawVigraArray.axistags =  vigra.AxisTags(
-            vigra.AxisInfo('t',vigra.AxisType.Time),
-            vigra.AxisInfo('x',vigra.AxisType.Space),
-            vigra.AxisInfo('y',vigra.AxisType.Space),
-            vigra.AxisInfo('z',vigra.AxisType.Space),
-            vigra.AxisInfo('c',vigra.AxisType.Channels))
+
+        # Guess what the axistags should be based on the number of channels
+        numDimensions = len(self.rawVigraArray.shape) 
+        assert numDimensions != 1, "OpNpyFileReader: Support for 1-D data not yet supported"
+        assert numDimensions != 2, "OpNpyFileReader: BUG: 2-D was supposed to be reshaped above."
+        if numDimensions == 3:
+            self.rawVigraArray.axistags = vigra.AxisTags(
+                vigra.AxisInfo('x',vigra.AxisType.Space),
+                vigra.AxisInfo('y',vigra.AxisType.Space),
+                vigra.AxisInfo('c',vigra.AxisType.Channels))
+        if numDimensions == 4:
+            self.rawVigraArray.axistags = vigra.AxisTags(
+                vigra.AxisInfo('x',vigra.AxisType.Space),
+                vigra.AxisInfo('y',vigra.AxisType.Space),
+                vigra.AxisInfo('z',vigra.AxisType.Space),
+                vigra.AxisInfo('c',vigra.AxisType.Channels))
+        if numDimensions == 5:
+            self.rawVigraArray.axistags =  vigra.AxisTags(
+                vigra.AxisInfo('t',vigra.AxisType.Time),
+                vigra.AxisInfo('x',vigra.AxisType.Space),
+                vigra.AxisInfo('y',vigra.AxisType.Space),
+                vigra.AxisInfo('z',vigra.AxisType.Space),
+                vigra.AxisInfo('c',vigra.AxisType.Channels))
  
         # Our output slot should match the shape of the array on disk
         self.Output.meta.dtype = self.rawVigraArray.dtype
@@ -65,7 +91,7 @@ if __name__ == "__main__":
 
     # Read the entire file and verify the contents
     a = npyReader.Output[:].wait()
-    assert a.shape == (10,11)
+    assert a.shape == (10,11,1)
     for x in range(0,10):
         for y in range(0,11):
             assert a[x,y] == x+y
