b41c68f0e22da7db71d452b4cad9c2c275a00e52
Stuart Berg <bergs@janelia.hhmi.org>
2012-04-27 11:36:47 -0400

    Added feature selection applet.  Mostly works, with odd bugs and crashes in the viewer sometimes.

---
+++ ilastik-shell/applets/featureSelection/featureSelectionGui.py
@@ -0,0 +1,264 @@
+from PyQt4.QtCore import pyqtSignal, QTimer, QRectF, Qt, SIGNAL, QObject
+from PyQt4.QtGui import *
+from PyQt4 import uic
+
+from volumina.api import ArraySource, LazyflowSource, GrayscaleLayer, RGBALayer, ColortableLayer, \
+                         AlphaModulatedLayer, LayerStackModel, VolumeEditor, LazyflowSinkSource
+
+import labelListModel
+from labelListModel import LabelListModel
+
+from lazyflow.operators import OpSingleChannelSelector
+
+from opFeatureSelection import OpFeatureSelection
+
+from igms.featureTableWidget import FeatureEntry
+from igms.featureDlg import FeatureDlg
+
+from functools import partial
+import os
+import utility # This is the ilastik shell utility module
+import numpy
+
+class FeatureSelectionGui(QMainWindow):
+    """
+    Manages all GUI elements in the data selection applet.
+    This class itself is the central widget and also owns/manages the applet drawer widgets.
+    """
+    
+    ScalesList = [0.3, 0.7, 1, 1.6, 3.5, 5.0, 10.0]
+    DefaultColorTable = None
+
+    def __init__(self, topLevelOperator):
+        super(FeatureSelectionGui, self).__init__()
+
+        # Constants
+        FeatureSelectionGui.DefaultColorTable = self.createDefault16ColorColorTable()
+
+        self.mainOperator = topLevelOperator
+        self.menuBar = QMenuBar()
+        
+        self.drawer = None
+        self.initAppletDrawerUic()
+        self.initCentralUic()
+        self.initFeatureDlg()
+        
+        self.editor = None
+        self.layerstack = LayerStackModel()
+        self.initEditor()
+
+        self.mainOperator.Scales.setValue( self.ScalesList )
+        
+    def initAppletDrawerUic(self):
+        """
+        Load the ui file for the applet drawer, which we own.
+        """
+        localDir = utility.getPathToLocalDirectory(__file__)
+        # (We don't pass self here because we keep the drawer ui in a separate object.)
+        self.drawer = uic.loadUi(localDir+"/featureSelectionDrawer.ui")
+        self.drawer.SelectFeaturesButton.clicked.connect(self.onFeatureButtonClicked)
+
+        # Subscribe to feature selection changes directly from the graph.
+        self.mainOperator.Matrix.notifyConnect( self.onFeaturesSelectionsChanged ) # In case of setValue
+        self.mainOperator.Matrix.notifyDirty( self.onFeaturesSelectionsChanged ) # In case of dirty data from the partner operator
+    
+    def initCentralUic(self):
+        """
+        Load the GUI from the ui file into this class and connect it with event handlers.
+        """
+        # Load the ui file into this class (find it in our own directory)
+        localDir = utility.getPathToLocalDirectory(__file__)
+        uic.loadUi(localDir+"/centralWidget.ui", self)
+            
+    def initFeatureDlg(self):
+        """
+        Initialize the feature selection widget.
+        """
+        self.featureDlg = FeatureDlg()
+        self.featureDlg.setWindowTitle("Features")
+        self.featureDlg.createFeatureTable({"Features":
+                                                [FeatureEntry("Gaussian smoothing"), \
+                                                 FeatureEntry("Laplacian of Gaussian"), \
+                                                 FeatureEntry("Structure Tensor Eigenvalues"), \
+                                                 FeatureEntry("Hessian of Gaussian EV"),  \
+                                                 FeatureEntry("Gaussian Gradient Magnitude"), \
+                                                 FeatureEntry("Difference Of Gaussian")]}, \
+                                           self.ScalesList)
+        self.featureDlg.setImageToPreView(None)
+
+        # Create a matrix of False values
+        defaultFeatures = numpy.zeros((6,7), dtype=bool)
+
+        # Select some default features.
+        defaultFeatures[0,0] = True
+        defaultFeatures[1,0] = True
+        defaultFeatures[3,0] = True
+        defaultFeatures[4,0] = True
+        defaultFeatures[5,0] = True
+
+        self.featureDlg.selectedFeatureBoolMatrix = defaultFeatures
+        self.featureDlg.accepted.connect(self.onNewFeaturesFromFeatureDlg)
+
+    def onFeatureButtonClicked(self):
+        # Refresh the feature matrix in case it has changed since the last time we were opened
+        # (e.g. if the user loaded a project from disk)
+        if self.mainOperator.Matrix.configured():
+            self.featureDlg.selectedFeatureBoolMatrix = self.mainOperator.Matrix.value
+        
+        # Now open the feature selection dialog
+        self.featureDlg.show()
+    
+    def onNewFeaturesFromFeatureDlg(self):
+        # Give the new features to the pipeline 
+        featureMatrix = numpy.asarray(self.featureDlg.selectedFeatureBoolMatrix)
+        self.mainOperator.Matrix.setValue( featureMatrix )
+    
+    def onFeaturesSelectionsChanged(self, slot):
+        """
+        Handles changes to our top-level operator's matrix of feature selections.
+        """
+        # Update the drawer caption
+        self.drawer.caption.setText( "(Selected %d features)" % numpy.sum(self.mainOperator.Matrix.value) )
+
+    def initEditor(self):
+        """
+        Initialize the Volume Editor GUI.
+        """
+        self.editor = VolumeEditor(self.layerstack)
+
+        self.editor.newImageView2DFocus.connect(self.setIconToViewMenu)
+        self.editor.setInteractionMode( 'navigation' )
+        self.volumeEditorWidget.init(self.editor)
+        
+        # The editor's layerstack is in charge of which layer movement buttons are enabled
+        model = self.editor.layerStack
+        model.canMoveSelectedUp.connect(self.UpButton.setEnabled)
+        model.canMoveSelectedDown.connect(self.DownButton.setEnabled)
+        model.canDeleteSelected.connect(self.DeleteButton.setEnabled)     
+
+        # Connect our layer movement buttons to the appropriate layerstack actions
+        self.layerWidget.init(model)
+        self.UpButton.clicked.connect(model.moveSelectedUp)
+        self.DownButton.clicked.connect(model.moveSelectedDown)
+        self.DeleteButton.clicked.connect(model.deleteSelected)
+        
+        # No brushing model necessary (we're using the editor as a viewer only)
+        #self.pipeline.labels.inputs["eraser"].setValue(self.editor.brushingModel.erasingNumber)
+
+        self.mainOperator.notifyConfigured( self.handleFeaturesChanged )
+
+    def setIconToViewMenu(self):
+        """
+        In the "Only for Current View" menu item of the View menu, 
+        show the user which axis is the current one by changing the menu item icon.
+        """
+        self.actionOnly_for_current_view.setIcon(QIcon(self.editor.imageViews[self.editor._lastImageViewFocus]._hud.axisLabel.pixmap()))
+
+    def handleFeaturesChanged(self):
+        # Start by removing all layers
+        # TODO: We can do better than this.  We should try to determine if some feature layers can be kept.
+        for il, layer in enumerate(self.layerstack):
+            self.layerstack.removeRows(il, 1)
+
+        # Update the editor data shape
+        # TODO: This assumes only one input image for now.
+        shape = self.mainOperator.InputImages[0].shape
+        self.editor.dataShape = shape
+        
+        # First add a black layer on the bottom of the image
+        # TODO: Optimize: Replace this ArraySource with a special operator that always returns zero
+        singleChannelShape = shape[:-1] + (1,)
+        blackSource = ArraySource( numpy.zeros(singleChannelShape, dtype=numpy.float32) )
+        blackLayer = GrayscaleLayer(blackSource)
+        blackLayer.name = "Black background"
+        self.layerstack.insert(0, blackLayer)
+
+        # Now add a layer for each feature
+        # TODO: This assumes only one input image for now.
+        # TODO: This assumes the channel is the last axis 
+        numChannels = self.mainOperator.CachedOutputImages[0].shape[-1]
+        for channelIndex in range(0, numChannels):
+            if channelIndex < len(self.DefaultColorTable):
+                # Choose the next color from our default color table
+                color = self.DefaultColorTable[channelIndex]
+            else:
+                # Choose a random color
+                color = QColor(numpy.random.randint(0,255), numpy.random.randint(0,255), numpy.random.randint(0,255))
+            
+            label = labelListModel.Label( str(channelIndex), color )
+            self.addFeatureLayer(channelIndex, label)
+
+    def addFeatureLayer(self, channelIndex, ref_label):
+        """
+        Display a feature in the layer editor.
+        """
+        # Create an operator to select the channel (feature) we're interested in
+        # TODO: This assumes only one input image for now.
+        selector=OpSingleChannelSelector(self.mainOperator.graph)
+        selector.Input.connect(self.mainOperator.CachedOutputImages[0])
+        selector.Index.setValue(channelIndex)
+        
+        featureSource = LazyflowSource(selector.Output)
+        featureSource.setObjectName("Feature %s" % ref_label.name)
+        featureLayer = AlphaModulatedLayer(featureSource, tintColor=ref_label.color, normalize = None )
+        featureLayer.name = featureSource.objectName()
+
+        # By default, only the first feature is visible
+        featureLayer.opacity = 1.0
+        featureLayer.visible = (channelIndex == 0)
+        self.layerstack.insert(0, featureLayer )
+
+    def createDefault16ColorColorTable(self):
+        c = []
+        c.append(QColor(0, 0, 255))
+        c.append(QColor(255, 255, 0))
+        c.append(QColor(255, 0, 0))
+        c.append(QColor(0, 255, 0))
+        c.append(QColor(0, 255, 255))
+        c.append(QColor(255, 0, 255))
+        c.append(QColor(255, 105, 180)) #hot pink
+        c.append(QColor(102, 205, 170)) #dark aquamarine
+        c.append(QColor(165,  42,  42)) #brown        
+        c.append(QColor(0, 0, 128))     #navy
+        c.append(QColor(255, 165, 0))   #orange
+        c.append(QColor(173, 255,  47)) #green-yellow
+        c.append(QColor(128,0, 128))    #purple
+        c.append(QColor(192, 192, 192)) #silver
+        c.append(QColor(240, 230, 140)) #khaki
+        c.append(QColor(69, 69, 69))    # dark grey
+        return c
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ilastik-shell/applets/featureSelection/featureSelectionSerializer.py
@@ -0,0 +1,38 @@
+class FeatureSelectionSerializer(object):
+    """
+    Serializes the user's pixel feature selections to an ilastik v0.6 project file.
+    """
+    CurrentVersion = 0.1
+    OldestReadableVersion = 0.1
+    GroupName = "FeatureSelections"
+    
+    def __init__(self, mainOperator):
+        self.mainOperator = mainOperator
+    
+    def serializeToHdf5(self, hdf5File):
+        # Check the overall version.
+        # We only support v0.6 at the moment.
+        ilastikVersion = hdf5File["ilastikVersion"].value
+        if ilastikVersion != 0.6:
+            return        
+    
+    def deserializeFromHdf5(self, hdf5File):
+        # Check the overall version.
+        # We only support v0.6 at the moment.
+        ilastikVersion = hdf5File["ilastikVersion"].value
+        if ilastikVersion != 0.6:
+            return
+
+    def isDirty(self):
+        """ Return true if the current state of this item 
+            (in memory) does not match the state of the HDF5 group on disk.
+            SerializableItems are responsible for tracking their own dirty/notdirty state."""
+        pass
+
+    def unload(self):
+        """ Called if either
+            (1) the user closed the project or
+            (2) the project opening process needs to be aborted for some reason
+                (e.g. not all items could be deserialized properly due to a corrupted ilp)
+            This way we can avoid invalid state due to a partially loaded project. """ 
+        pass
