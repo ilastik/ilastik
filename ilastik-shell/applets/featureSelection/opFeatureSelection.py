8f2cf29f95835f56e05af92c38731b756be531ba
Stuart Berg <bergs@janelia.hhmi.org>
2012-05-10 14:35:23 -0400

    Added basic unit test for OpFeatureSelection

--- ilastik-shell/applets/featureSelection/opFeatureSelection.py
+++ ilastik-shell/applets/featureSelection/opFeatureSelection.py
@@ -97,66 +97,10 @@ class OpFeatureSelection(Operator):
             res = req.wait()
             return res
 
-#
-# Simple test
-#
-if __name__ == "__main__":
-    import numpy
-    from lazyflow.operators.ioOperators import OpInputDataReader
-    graph = Graph()
     
-    # Define operators
-    featureSelector = OpFeatureSelection(graph=graph)
-    reader = OpInputDataReader(graph=graph)
-
-    # Set input data
-    reader.FilePath.setValue( '5d.npy' )
-
-    # Connect input
-    featureSelector.InputImages.resize(1)
-    featureSelector.InputImages[0].connect( reader.Output )
-
-    # Configure scales        
-    scales = [0.3, 0.7, 1, 1.6, 3.5, 5.0, 10.0]
-    featureSelector.Scales.setValue(scales)
-
-    # Configure feature types
-    featureIds = [ 'GaussianSmoothing',
-                   'LaplacianOfGaussian',
-                   'StructureTensorEigenvalues',
-                   'HessianOfGaussianEigenvalues',
-                   'GaussianGradientMagnitude',
-                   'DifferenceOfGaussians' ]
-    featureSelector.FeatureIds.setValue(featureIds)
-
-    # Configure matrix
-    featureSelectionMatrix = numpy.array(numpy.zeros((len(featureIds),len(scales))), dtype=bool)
-    featureSelectionMatrix[0,0] = True
-    featureSelectionMatrix[1,1] = True
-    featureSelectionMatrix[2,2] = True
-    featureSelectionMatrix[2,3] = True
-    featureSelector.Matrix.setValue(featureSelectionMatrix)
-
-    # Compute results for the top slice only
-    topSlice = [0, slice(None), slice(None), 0, slice(None)]
-    result = featureSelector.OutputImages[0][topSlice].allocate().wait()
     
-    numFeatures = numpy.sum(featureSelectionMatrix)
-    inputChannels = reader.Output.meta.shape[-1]
-    outputChannels = result.shape[-1]
-    assert outputChannels == inputChannels*numFeatures
     
-    # Export the first slice of each channel of the results as a separate image for display purposes.
-    import vigra
-    numFeatures = result.shape[-1]
-    for featureIndex in range(0, numFeatures):
-        featureSlice = list(topSlice)
-        featureSlice[-1] = featureIndex
-        vigra.impex.writeImage(result[featureSlice], "test_feature" + str(featureIndex) + ".bmp")
-
-
-
-
+    
 
 
 
