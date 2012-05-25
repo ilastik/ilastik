a506dedfdcd101b58857b0635f2466ce3d695578
cstraehl <christoph.straehle@iwr.uni-heidelberg.de>
2012-05-25 10:18:41 +0200

    initial, ugly, somewhat working interactive segmentation applet. worst algorithm, worst parameters, just playing around.

---
+++ ilastik-shell/applets/seededWatershed/opSeededWatershed.py
@@ -0,0 +1,353 @@
+import numpy, vigra, h5py
+from lazyflow.operators import OpPixelFeaturesPresmoothed, OpBlockedArrayCache, OpArrayPiper, Op5ToMulti, OpBlockedSparseLabelArray, OpArrayCache, \
+                               OpTrainRandomForestBlocked, OpPredictRandomForest, OpSlicedBlockedArrayCache
+
+from lazyflow.graph import Operator, InputSlot, OutputSlot, MultiInputSlot, MultiOutputSlot
+from threading import Lock
+import pyximport; pyximport.install()
+from cylemon.segmentation import GCSegmentor, MSTSegmentorKruskal, MSTSegmentor, PerturbMSTSegmentor
+import json
+
+from json import decoder, scanner
+
+from json.scanner import make_scanner
+from _json import scanstring as c_scanstring
+
+_CONSTANTS = json.decoder._CONSTANTS
+
+py_make_scanner = scanner.py_make_scanner
+
+# Convert from unicode to str
+def str_scanstring(*args, **kwargs):
+    result = c_scanstring(*args, **kwargs)
+    return str(result[0]), result[1]
+
+# Little dirty trick here
+json.decoder.scanstring = str_scanstring
+
+class StrJSONDecoder(decoder.JSONDecoder):
+    def __init__(self, encoding=None, object_hook=None, parse_float=None,
+            parse_int=None, parse_constant=None, strict=True,
+            object_pairs_hook=None):
+        self.encoding = encoding
+        self.object_hook = object_hook
+        self.object_pairs_hook = object_pairs_hook
+        self.parse_float = parse_float or float
+        self.parse_int = parse_int or int
+        self.parse_constant = parse_constant or _CONSTANTS.__getitem__
+        self.strict = strict
+        self.parse_object = decoder.JSONObject
+        self.parse_array = decoder.JSONArray
+        self.parse_string = str_scanstring
+        self.scan_once = py_make_scanner(self)
+
+# And another little dirty trick there    
+_default_decoder = StrJSONDecoder(encoding=None, object_hook=None,
+                               object_pairs_hook=None)
+
+json._default_decoder = _default_decoder
+
+
+class OpSegmentor(Operator):
+
+  image     = InputSlot()
+
+  writeSeeds = InputSlot(optional=True)
+  deleteSeed = InputSlot(optional=True)
+  update = InputSlot(value = False)
+  eraser = InputSlot(value=100)
+  algorithm = InputSlot(value="PrioMST")
+  parameters = InputSlot(value="{}")
+  
+  sigma = InputSlot(value=1.6)
+  border_indicator = InputSlot(value="hessian_ev_0")
+  initial_segmentor = InputSlot(optional = True)
+
+  segmentor = OutputSlot()
+
+  raw = OutputSlot()
+  seeds = OutputSlot()
+  segmentation = OutputSlot()
+  uncertainty = OutputSlot()
+  regions = OutputSlot()
+
+  seedNumbers = OutputSlot()
+  
+  maxUncertainFG = OutputSlot()
+  maxUncertainBG = OutputSlot()
+
+
+  def __init__(self,parent):
+    Operator.__init__(self,parent)
+    self._dirty   = True
+    self._fname = None
+    self._algorithm = ""
+    self._parameters = {}
+    self.seg = None
+    self._dirty = True
+    self.lock = Lock()
+    self.initial_seg = None
+    self._dirtySeg = True
+    self.initial_segmentor.notifyConnect(self.onInitialSegmentor)
+    self.initial_segmentor.notifyMetaChanged(self.onInitialSegmentor)
+
+  def onInitialSegmentor(self, slot):
+    if slot.meta.shape is not None:
+      print "================= setting initial segmentor"
+      seg = slot.value
+      if self.initial_seg != seg:
+        self.seg = seg
+        self._dirtySeg = False
+      self.initial_seg = seg
+  
+
+  def updateSeeds(self, oldseg, newseg):
+    newseg.seeds.lut[:] = oldseg.seeds.lut[:]
+
+  def setupOutputs(self):  
+
+    self.segmentor.meta.shape = (1,)
+    self.segmentor.meta.dtype = object
+
+    self.seedNumbers.meta.shape = (1,)
+    self.seedNumbers.meta.dtype = object
+    
+    self.deleteSeed.meta.shape = (1,)
+    self.deleteSeed.meta.dtype = numpy.uint8
+
+    shape = self.raw.meta.shape = self.image.meta.shape
+    self.raw.meta.dtype = self.image.meta.dtype
+
+    self.seeds.meta.shape = shape
+    self.seeds.meta.dtype = numpy.uint8
+
+    self.segmentation.meta.shape = shape
+    self.segmentation.meta.dtype = numpy.uint8
+    
+    self.uncertainty.meta.shape = shape
+    self.uncertainty.meta.dtype = numpy.uint8
+
+    self.regions.meta.shape = shape
+    self.regions.meta.dtype = numpy.int32
+
+    self._eraser = self.eraser.value
+
+    self.writeSeeds.meta.shape = shape
+    self.writeSeeds.meta.dtype = numpy.uint8
+
+    print "####################################### segmentor setupOutputs ############################"
+    
+  def setInSlot(self, slot, key, value):
+    if self.seg is not None:
+      print "  ========================= setInSlot"
+      if slot == self.writeSeeds:
+        print "  =========================== WriteSeeds"
+        key = key[1:-1]
+        value = numpy.where(value == self._eraser, 255, value[:])
+
+        self.seg.seeds[key] = value
+        self._dirty = True
+      
+      elif slot == self.deleteSeed:
+
+        lut = self.seg.seeds.lut[:]
+        label = value
+        print "DELETING SEED", label
+        lut = numpy.where(lut == label, 0, lut)
+        lut = numpy.where(lut > label, lut - 1, lut)
+        self.seg.seeds.lut[:] = lut
+        self._dirty = True
+
+
+  def execute(self, slot, roi, result):
+    key = roi.toSlice()[1:-1]
+    if slot == self.raw:
+      if self.seg is not None:
+        res = self.seg.raw[key]
+        result[0,:,:,:,0] = res[:]
+    elif slot == self.seeds:
+      if self.seg is not None:
+        res = self.seg.seeds[key]
+        result[0,:,:,:,0] = res[:]
+    elif slot == self.regions:
+      if self.seg is not None:
+        res = self.seg.regionVol[key] % 256
+        result[0,:,:,:,0] = res[:]
+    elif slot == self.seedNumbers:
+      if self.seg is not None:
+        result[0] = numpy.unique(self.seg.seeds.lut)
+      else:
+        result[0] = 0
+      return result
+    elif slot == self.segmentor:
+      if self._dirtySeg:
+        volume = self.image[:].wait()
+        border_indicator = self.border_indicator.value
+        sigma = self.sigma.value
+
+        print "Preprocessor: Eigenvalues..."
+        fvol = volume.astype(numpy.float32)[0,:,:,:,0]
+        print fvol.shape, fvol.dtype, sigma
+        if border_indicator == "hessian_ev_0":
+          volume_feat = vigra.filters.hessianOfGaussianEigenvalues(fvol,sigma)[:,:,:,0]
+        volume_ma = numpy.max(volume_feat)
+        volume_mi = numpy.min(volume_feat)
+        volume_feat = (volume_feat - volume_mi) * 255.0 / (volume_ma-volume_mi)
+        print "Preprocessor: Watershed..."
+        labelVolume = vigra.analysis.watersheds(volume_feat)[0].astype(numpy.int32)
+        print "Preprocessor: Construct MSTSegmentor..."
+        mst = MSTSegmentor(labelVolume, volume_feat.astype(numpy.float32), edgeWeightFunctor = "minimum")
+        mst.raw = volume[0,:,:,:,0]
+        self.seg = mst
+        self._dirtySeg = False
+      result[0] = self.seg
+      return result
+
+    else:    # segmentation or uncertainty is requested
+      self.lock.acquire()
+      # get own outputslot
+      segmentor = self.segmentor.value
+
+      algorithm = self.algorithm.value
+      try:
+        parameters = self._parameters = json.loads(self.parameters.value)
+      except:
+        print "algorithmsOptions not a valid json string: ", self.parameters.value
+        parameters = self._parameters = {}
+        # self._fname = fileName
+        self._dirty = True
+        self.seg = segmentor
+      
+      labelNumbers = numpy.unique(self.seg.seeds.lut)
+
+      if self._dirty and self.update.value == True:
+        labelCount = len(labelNumbers)
+        prios = [1.0] * labelCount
+        if self._parameters.has_key("prioBG"):
+          prios[1] = self._parameters["prioBG"]
+        print "using labelCount", labelCount
+        unaries =  numpy.zeros((self.seg.numNodes,labelCount)).astype(numpy.float32)
+        print "parameters", self._parameters
+        self.seg.run(unaries, prios = prios, **self._parameters)
+        self._dirty = False
+      self.lock.release()
+
+      if slot == self.segmentation:
+        print " ========== getting segmentation"
+        res = self.seg.segmentation[key]
+        for l in labelNumbers:
+          print "Label=%r, count = %r" % ( l, numpy.sum(numpy.where(res == l, 1, 0)))
+      if slot == self.uncertainty:
+        res = self.seg.uncertainty[key]
+      print res.shape, result.shape
+      result[0,:,:,:,0] = res[:]
+    return result
+  
+  def propagateDirty(self, slot, roi):
+    if slot in [self.image, self.sigma, self.border_indicator]:
+      print "  ======================= setting segmentor to dirty"
+      self._dirty = True
+      self._dirtySeg = True
+      self.segmentor.setDirty(slice(0,1,None))
+      self.segmentation.setDirty(slice(None,None,None))
+
+
+  def maxUncertainFG(self):
+    ufg = numpy.where(self.seg.segmentation.lut > 1, self.seg.uncertainty.lut, 0)
+    index = numpy.argmax(ufg)
+    return self.seg.regionCenter[index]
+
+  def maxUncertainBG(self):
+    ufg = numpy.where(self.seg.segmentation.lut == 1, self.seg.uncertainty.lut, 0)
+    index = numpy.argmax(ufg)
+    return self.seg.regionCenter[index]
+
+
+
+
+class Preprocessor(Operator):
+  name="Preprocessor"
+
+
+  def setupOutputs(self):
+
+    print "####################################### preprocessor setupOutputs ############################"
+
+  def execute(self, slot, roi, result):
+    if self._initial_segmentor is not None:
+      self._mst = self._initial_segmentor
+    if self._dirty:
+      volume = self.image[:].wait()
+      border_indicator = self.border_indicator.value
+      sigma = self.sigma.value
+
+      print "Preprocessor: Eigenvalues..."
+      fvol = volume.astype(numpy.float32)[0,:,:,:,0]
+      print fvol.shape, fvol.dtype, sigma
+      if border_indicator == "hessian_ev_0":
+        volume_feat = vigra.filters.hessianOfGaussianEigenvalues(fvol,sigma)[:,:,:,0]
+      volume_ma = numpy.max(volume_feat)
+      volume_mi = numpy.min(volume_feat)
+      volume_feat = (volume_feat - volume_mi) * 255.0 / (volume_ma-volume_mi)
+      print "Preprocessor: Watershed..."
+      labelVolume = vigra.analysis.watersheds(volume_feat)[0].astype(numpy.int32)
+      print "Preprocessor: Construct MSTSegmentor..."
+      mst = MSTSegmentor(labelVolume, volume_feat.astype(numpy.float32), edgeWeightFunctor = "minimum")
+      mst.raw = volume[0,:,:,:,0]
+      self._mst = mst
+      self._dirty = False
+    result[0] = self._mst
+    return result
+
+  def propagateDirty(self, slot, roi):
+    if slot in [self.image, self.sigma, self.border_indicator]:
+      self._dirty = True
+      self._initial_segmentor = None
+      self.segmentor.setDirty(slice(0,1,None))
+
+
+
+class OpShapeReader(Operator):
+    """
+    This operator outputs the shape of its input image, except the number of channels is set to 1.
+    """
+    Input = InputSlot()
+    OutputShape = OutputSlot(stype='shapetuple')
+    
+    def setupOutputs(self):
+        self.OutputShape.meta.shape = (1,)
+        self.OutputShape.meta.axistags = 'shapetuple'
+        self.OutputShape.meta.dtype = tuple
+    
+    def execute(self, slot, roi, result):
+        # Our 'result' is simply the shape of our input, but with only one channel
+        channelIndex = self.Input.meta.axistags.index('c')
+        shapeList = list(self.Input.meta.shape)
+        shapeList[channelIndex] = 1
+        result[0] = tuple(shapeList)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ilastik-shell/applets/seededWatershed/seededWatershedSerializer.py
@@ -0,0 +1,138 @@
+import numpy
+from utility.dataImporter import DataImporter
+
+from opSeededWatershed import GCSegmentor, MSTSegmentorKruskal, MSTSegmentor, PerturbMSTSegmentor
+
+class SeededWatershedSerializer(object):
+    """
+    Encapsulate the serialization scheme for pixel classification workflow parameters and datasets.
+    """
+    TopGroupName = 'InteractiveSeededWatershed'
+    SerializerVersion = 0.1
+    
+    def __init__(self, topLevelOperator):
+        self.mainOperator = topLevelOperator
+    
+    def serializeToHdf5(self, hdf5File, filePath):
+        print "===================================  segmentor serialize ========================================"
+        # The group we were given is the root (file).
+        # Check the version
+        ilastikVersion = hdf5File["ilastikVersion"].value
+
+        # TODO: Fix this when the version number scheme is more thought out
+        if ilastikVersion != 0.6:
+            # This class is for 0.6 projects only.
+            # v0.5 projects are handled in a different serializer (below).
+            return
+
+        # Access our top group (create it if necessary)
+        topGroup = self.getOrCreateGroup(hdf5File, self.TopGroupName)
+        
+        # Set the version
+        if 'StorageVersion' not in topGroup.keys():
+            topGroup.create_dataset('StorageVersion', data=self.SerializerVersion)
+        else:
+            topGroup['StorageVersion'][()] = self.SerializerVersion
+        
+        # Delete all labels from the file
+        self.deleteIfPresent(topGroup, 'segmentors')
+        segDir = topGroup.create_group('segmentors')
+
+        numImages = len(self.mainOperator.image)
+        for imageIndex in range(numImages):
+            # Create a group for this image
+            segGroupName = 'segmentor{:03d}'.format(imageIndex)
+            segGroup = segDir.create_group(segGroupName)
+
+            mst = self.mainOperator.segmentor[imageIndex].value
+
+            mst.saveH5G(segGroup)
+            
+
+    def deserializeFromHdf5(self, hdf5File, filePath):
+        print "===================================  segmentor deserialize ========================================"
+        # Check the overall version.
+        # We only support v0.6 at the moment.
+        ilastikVersion = hdf5File["ilastikVersion"].value
+        if ilastikVersion != 0.6:
+            return
+
+        # Access the top group and all required datasets
+        #  If something is missing we simply return without adding any input to the operator.
+        try:
+            topGroup = hdf5File[self.TopGroupName]
+        except KeyError:
+            return
+
+        segDir = topGroup["segmentors"]
+        numImages = len(segDir)
+        self.mainOperator.initial_segmentor.resize(numImages)
+       
+        # For each image in the file
+        for index, (groupName, group) in enumerate( sorted(segDir.items()) ):
+          mst = MSTSegmentor.loadH5G(group)
+          self.mainOperator.initial_segmentor[index].setValue(mst)
+
+
+
+
+
+
+    def getOrCreateGroup(self, parentGroup, groupName):
+        try:
+            return parentGroup[groupName]
+        except KeyError:
+            return parentGroup.create_group(groupName)
+
+    def deleteIfPresent(self, parentGroup, name):
+        try:
+            del parentGroup[name]
+        except KeyError:
+            pass
+
+    def slicingToString(self, slicing):
+        """
+        Convert the given slicing into a string of the form '[0:1,2:3,4:5]'
+        """
+        strSlicing = '['
+        for s in slicing:
+            strSlicing += str(s.start)
+            strSlicing += ':'
+            strSlicing += str(s.stop)
+            strSlicing += ','
+        
+        # Drop the last comma
+        strSlicing = strSlicing[:-1]
+        strSlicing += ']'
+        return strSlicing
+        
+    def stringToSlicing(self, strSlicing):
+        """
+        Parse a string of the form '[0:1,2:3,4:5]' into a slicing (i.e. list of slices)
+        """
+        slicing = []
+        # Drop brackets
+        strSlicing = strSlicing[1:-1]
+        sliceStrings = strSlicing.split(',')
+        for s in sliceStrings:
+            ends = s.split(':')
+            start = int(ends[0])
+            stop = int(ends[1])
+            slicing.append(slice(start, stop))
+        
+        return slicing
+
+    def isDirty(self):
+        """
+        Return true if the current state of this item 
+        (in memory) does not match the state of the HDF5 group on disk.
+        """
+        return True
+
+    def unload(self):
+        """
+        Called if either
+        (1) the user closed the project or
+        (2) the project opening process needs to be aborted for some reason
+            (e.g. not all items could be deserialized properly due to a corrupted ilp)
+        This way we can avoid invalid state due to a partially loaded project. """ 
