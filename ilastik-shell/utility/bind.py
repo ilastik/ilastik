9e9bda14010d480f4e2124595be952908cffa566
Stuart Berg <bergs@janelia.hhmi.org>
2012-06-11 17:56:32 -0400

    bind: bind objects are compared for equality so they can be easily used for signal disconnections.

--- ilastik-shell/utility/bind.py
+++ ilastik-shell/utility/bind.py
@@ -5,6 +5,7 @@ class bind(object):
     """
     Inspired by boost::bind (C++).
     Behaves like functools.partial, but discards any extra parameters it gets when called.
+    Also, bind objects can be compared for equality.
     """
     def __init__(self, f, *args):
         self.f = f
@@ -15,3 +16,14 @@ class bind(object):
     def __call__(self, *args):
         self.f(*(self.bound_args + args[0:self.numUnboundArgs]))
 
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return False
+        result = True
+        result &= ( self.f == other.f )
+        result &= ( self.bound_args == other.bound_args )
+        result &= ( self.numUnboundArgs == other.numUnboundArgs )
+        return result
+    
+    def __ne__(self, other):
+        return not ( self == other )
