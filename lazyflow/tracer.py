670bb619b88924cc44f106750ef89217effaf32b
Stuart Berg <bergs@janelia.hhmi.org>
2012-06-25 15:40:56 -0400

    tracer: - Added optional message parameter. - Optimized for case when trace statements are disabled.

--- lazyflow/tracer.py
+++ lazyflow/tracer.py
@@ -15,15 +15,21 @@ class Tracer(object):
         Function f is running...
         DEBUG TRACE.mymodule:__exit__: f
     """
-    def __init__(self, logger, level=logging.DEBUG):
+    def __init__(self, logger, level=logging.DEBUG, msg=''):
         if type(logger) == str:
             self._logger = logging.getLogger(logger)
         else:
             self._logger = logger
         self._level = level
-        self._caller = inspect.stack()[1][3]
+        self._msg = msg
+
     def __enter__(self):
-        self._logger.log(self._level, self._caller)
+        if self._logger.isEnabledFor( self._level ):
+            stack = inspect.stack()
+            self._caller = stack[1][3]
+            stackDepth = len(stack) - 1
+            self._logger.log(self._level, '({})'.format(stackDepth) + self._caller + ' ' + self._msg)
 
     def __exit__(self, *args):
-        self._logger.log(self._level, self._caller)
+        if self._logger.isEnabledFor( self._level ):
+            self._logger.log(self._level, self._caller)
