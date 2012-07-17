88618ad7211c18068b0fee1d7c8955c21665e12c
Stuart Berg <bergs@janelia.hhmi.org>
2012-07-17 15:21:46 -0400

    Added decorator for trace logging.

--- lazyflow/tracer.py
+++ lazyflow/tracer.py
@@ -15,21 +15,61 @@ class Tracer(object):
         Function f is running...
         DEBUG TRACE.mymodule:__exit__: f
     """
-    def __init__(self, logger, level=logging.DEBUG, msg=''):
+    def __init__(self, logger, level=logging.DEBUG, msg='', print_caller=True):
         if type(logger) == str:
             self._logger = logging.getLogger(logger)
         else:
             self._logger = logger
         self._level = level
-        self._caller = None
+        self._caller = ''
+        self._print_caller = print_caller
         self._msg = msg
 
     def __enter__(self):
         if self._logger.isEnabledFor( self._level ):
-            stack = inspect.stack()
-            self._caller = stack[1][3]
-            self._logger.log(self._level, self._caller + ' ' + self._msg)
+            if self._print_caller:
+                stack = inspect.stack()
+                self._caller = stack[1][3] + ' '
+            self._logger.log(self._level, "(enter) " + self._caller + self._msg)
 
     def __exit__(self, *args):
-        if self._logger.isEnabledFor( self._level ) and self._caller is not None:
-            self._logger.log(self._level, self._caller)
+        if self._logger.isEnabledFor( self._level ):
+            self._logger.log(self._level, "(exit) " + self._caller)
+
+from functools import wraps
+
+def traceLogged(logger, suffix=''):
+    """Returns a decorator that logs the entry and exit of its target function."""
+    def decorator(func):
+        """A closure that logs the entry and exit of func using the logger."""
+        if hasattr(func, 'im_func'):
+            name = func.im_func.func_name
+        else:
+            name = func.func_name
+            
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            with Tracer(logger, msg=name, print_caller=False):
+                return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+if __name__=='__main__':
+    import sys
+    logger = logging.getLogger(__name__)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter( logging.Formatter("%(levelname)s %(thread)d %(name)s:%(funcName)s:%(lineno)d %(message)s") )
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    
+    def func1():
+        with Tracer(logger):
+            print "I'm func 1"
+
+    @traceLogged(logger)
+    def func2():
+        print "I'm func 2"
+
+    func1()
+    func2()
+
