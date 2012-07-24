e691207c74403fb888feb57b2534ec06e013475f
Stuart Berg <bergs@janelia.hhmi.org>
2012-07-24 11:59:54 -0400

    Add logging initialization to threshold workflow example.

--- ilastik-shell/workflows/examples/thresholdMaskingWorkflowMain.py
+++ ilastik-shell/workflows/examples/thresholdMaskingWorkflowMain.py
@@ -5,6 +5,9 @@ from ilastikshell import IlastikShell, SideSplitterSizePolicy
 from applets.dataSelection import DataSelectionApplet
 from applets.thresholdMasking import ThresholdMaskingApplet
 
+import ilastik_logging
+ilastik_logging.startUpdateInterval(10)
+
 def createShell():
     # This Graph is shared by all applets and operators
     graph = lazyflow.graph.Graph()
@@ -26,6 +29,9 @@ def createShell():
 
     return shell    
 
+def test(shell):
+    shell.openProjectFile('/tmp/test_project.ilp')
+
 if __name__ == "__main__":
     app = QApplication([])
     
@@ -42,6 +48,11 @@ if __name__ == "__main__":
 
     # Hide the splash screen
     splashScreen.finish(shell)
+
+    ## Run a test
+    #from PyQt4.QtCore import QTimer
+    #from functools import partial
+    #QTimer.singleShot(1, partial(test, shell) )
     
     app.exec_()
     
