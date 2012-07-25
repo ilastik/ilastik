15b9a1d8d43d5d989f5cee9ea5c83e94420be23c
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-07-25 12:22:38 +0200

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+packages=['ilastik',
+          'ilastik.applets',
+          'ilastik.applets.batchIo',
+          'ilastik.applets.connectedComponents',
+          'ilastik.applets.dataSelection',          
+          'ilastik.applets.featureSelection',
+          'ilastik.applets.layerViewer',
+          'ilastik.applets.pixelClassification',
+          'ilastik.applets.projectMetadata',
+          'ilastik.applets.seededWatershed',
+          'ilastik.applets.thresholdMasking',
+          'ilastik.applets.vigraWatershedViewer',
+          'ilastik.ilastikshell',
+          'ilastik.ilastik_logging',
+          'ilastik.utility']
+
+package_data={'ilastik': ['ilastik-splash.png',
+                          'ilastik-splash.xcf',
+                          'logging_config.json'],
+          'ilastik.applets.batchIo': ['*.ui'],
+          'ilastik.applets.connectedComponents': ['*.ui'],
+          'ilastik.applets.dataSelection': ['*.ui'],          
+          'ilastik.applets.featureSelection': ['*.ui'],
+          'ilastik.applets.layerViewer': ['*.ui'],
+          'ilastik.applets.pixelClassification': ['*.ui'],
+          'ilastik.applets.projectMetadata': ['*.ui'],
+          'ilastik.applets.seededWatershed': ['*.ui'],
+          'ilastik.applets.thresholdMasking': ['*.ui'],
+          'ilastik.applets.vigraWatershedViewer': ['*.ui'],
+          'ilastik.ilastikshell': ['ui/*.ui']
+              }
+
+setup(name='ilastik',
+      version='0.6a',
+      description='Interactive Image Analysis',
+      url='https://github.com/Ilastik',
+      packages=packages,
+      package_data=package_data
+     )
