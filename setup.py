361474876c0e00b1a7db1c2e53917de217fba23b
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-07-25 15:13:56 +0200

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+packages=['igms']
+
+setup(name='ilastik-widgets',
+      version='0.6a',
+      description='Ilastik Widgets',
+      url='https://github.com/Ilastik',
+      packages=packages,
+     )
+
