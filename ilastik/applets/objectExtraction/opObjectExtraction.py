529045e0b26f5c84b53b125b419171e119474d11
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-08-09 14:27:17 +0200

    merge OpRegionCenter with opObjectEctraction module

--- ilastik/applets/objectExtraction/opObjectExtraction.py
+++ ilastik/applets/objectExtraction/opObjectExtraction.py
@@ -6,7 +6,45 @@ from lazyflow.stype import Opaque
 from lazyflow.rtype import Everything
 from lazyflow.operators.ioOperators.opStreamingHdf5Reader import OpStreamingHdf5Reader
 
-from opObjectFeatures import OpRegionCenters
+class OpRegionCenters( Operator ):
+    LabelImage = InputSlot()
+    Output = OutputSlot( stype=Opaque, rtype=list )
+
+    
+    def __init__( self, parent=None, graph=None, register=True ):
+        super(OpRegionCenters, self).__init__(parent=parent,
+                                              graph=graph,
+                                              register=register)
+        self._cache = {}
+
+    def setupOutputs( self ):
+        self.Output.meta.shape = self.LabelImage.meta.shape
+        self.Output.meta.dtyp = self.LabelImage.meta.dtype
+    
+    def execute( self, slot, roi, result ):
+        if slot is self.Output:
+            def extract( a ):
+                labels = numpy.asarray(a, dtype=numpy.uint32)
+                data = numpy.asarray(a, dtype=numpy.float32)
+                feats = vigra.analysis.extractRegionFeatures(data, labels, features=['RegionCenter'], ignoreLabel=0)
+                centers = numpy.asarray(feats['RegionCenter'], dtype=numpy.uint16)
+                centers = centers[1:,:]
+                return centers
+                
+            centers = {}
+            for t in roi:
+                if t in self._cache:
+                    print "Cached!"
+                    centers_at = self._cache[t]
+                else:
+                    troi = SubRegion( self.LabelImage, start = [t,] + (len(self.LabelImage.meta.shape) - 1) * [0,], stop = [t+1,] + list(self.LabelImage.meta.shape[1:]))
+                    a = self.LabelImage.get(troi).wait()
+                    a = a[0,...,0] # assumes t,x,y,z,c
+                    centers_at = extract(a)
+                    self._cache[t] = centers_at
+                centers[t] = centers_at
+
+            return centers
 
 class OpObjectExtraction( Operator ):
     name = "Object Extraction"
@@ -24,10 +62,15 @@ class OpObjectExtraction( Operator ):
         self._mem_h5 = h5py.File('mem.h5', driver='core', backing_store=False)
         with h5py.File('/home/bkausler/src/ilastik/tracking/relabeled-stack/objects.h5', 'r') as f:
             f.copy('/objects', self._mem_h5)
+            f.copy('/seg', self._mem_h5)
 
         self._labelImageReader = OpStreamingHdf5Reader( graph = graph )
         self._labelImageReader.Hdf5File.setValue(self._mem_h5)
         self._labelImageReader.InternalPath.setValue('/objects')
+
+        self._segReader = OpStreamingHdf5Reader( graph = graph )
+        self._segReader.Hdf5File.setValue(self._mem_h5)
+        self._segReader.InternalPath.setValue('/seg')
 
         self._opRegCent = OpRegionCenters( graph = graph )
         self._opRegCent.LabelImage.connect(self._labelImageReader.OutputImage)
