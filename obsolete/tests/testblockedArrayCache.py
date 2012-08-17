381d31c13b76a5379c0e850af984bf99bf7f10e6
Stuart Berg <bergs@janelia.hhmi.org>
2012-08-17 17:14:55 -0400

    Fixed import statements in obsolete blocked array cache test.

--- obsolete/tests/testblockedArrayCache.py
+++ obsolete/tests/testblockedArrayCache.py
@@ -3,14 +3,7 @@ import threading
 from lazyflow.graph import *
 import copy
 
-from lazyflow.operators.operators import OpArrayPiper
-from lazyflow.operators.vigraOperators import *
-from lazyflow.operators.valueProviders import *
-from lazyflow.operators.classifierOperators import *
-from lazyflow.operators.generic import *
-
-from lazyflow import operators
-
+from lazyflow.operators import *
 
 def test1():
 
