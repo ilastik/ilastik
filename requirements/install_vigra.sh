d4b7d189e44748cb84c383f6a86bb054350f49a0
Stuart Berg <bergs@janelia.hhmi.org>
2012-08-19 16:17:03 -0400

    travis: Fix git clone line in install_vigra.sh

--- requirements/install_vigra.sh
+++ requirements/install_vigra.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#git clone http://github.com/ukoethe/vigra /tmp/vigra
+git clone http://github.com/ukoethe/vigra /tmp/vigra
 mkdir -p /tmp/vigra/build
 cd /tmp/vigra/build
 cmake ..
 make install
-#make check
+
 
