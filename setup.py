c8ab46eb6c81c30159b33d66941074a979d4f268
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-09-04 16:58:21 +0200

    updated setup.py to latest changes

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ packages=['ilastik',
           'ilastik.applets.connectedComponents',
           'ilastik.applets.dataSelection',          
           'ilastik.applets.featureSelection',
+          'ilastik.applets.labeling',          
           'ilastik.applets.layerViewer',
           'ilastik.applets.pixelClassification',
           'ilastik.applets.projectMetadata',
@@ -21,19 +22,20 @@ packages=['ilastik',
           'ilastik.utility.gui']
 
 package_data={'ilastik': ['ilastik-splash.png',
-                          'ilastik-splash.xcf',
-                          'logging_config.json'],
+                          'ilastik-splash.xcf'],
           'ilastik.applets.batchIo': ['*.ui'],
           'ilastik.applets.connectedComponents': ['*.ui'],
           'ilastik.applets.dataSelection': ['*.ui'],          
           'ilastik.applets.featureSelection': ['*.ui'],
+          'ilastik.applets.labeling': ['*.ui', 'icons/*.png', 'icons/*.jpg'],
           'ilastik.applets.layerViewer': ['*.ui'],
           'ilastik.applets.pixelClassification': ['*.ui'],
           'ilastik.applets.projectMetadata': ['*.ui'],
           'ilastik.applets.seededWatershed': ['*.ui'],
           'ilastik.applets.thresholdMasking': ['*.ui'],
           'ilastik.applets.vigraWatershedViewer': ['*.ui'],
-          'ilastik.shell.gui': ['ui/*.ui']
+          'ilastik.shell.gui': ['ui/*.ui'],
+          'ilastik.ilastik_logging': ['logging_config.json']              
               }
 
 setup(name='ilastik',
