9ed20a990fc6e7cb4d733574e08e54cf28d02ed3
Stuart Berg <bergs@janelia.hhmi.org>
2012-09-05 14:04:45 -0400

    OpVigraWatershed: Added an optional SeedImage input.

--- tests/testOpVigraWatershed.py
+++ tests/testOpVigraWatershed.py
@@ -14,6 +14,7 @@ class TestOpVigraWatershed(object):
         inputData = inputData.astype('float32')
         inputData = inputData.view(vigra.VigraArray)
         inputData.axistags = vigra.defaultAxistags('txyzc')
+        self.inputData = inputData
 
         self.op = OpVigraWatershed(graph=graph)
         self.op.InputImage.setValue( inputData )
@@ -27,12 +28,64 @@ class TestOpVigraWatershed(object):
         result = self.op.Output[slicing].wait()
         assert result.shape == slicing2shape(slicing)
 
-    def testDirty(self):
+    def testWithSeeds(self):
+        """
+        Bare-minimum test: Just make sure we can request the output without crashing.
+        """
+        seeds = numpy.zeros( self.inputData.shape, dtype=numpy.uint32 )
+        self.op.SeedImage.setValue( seeds )
+
+        slicing = sl[0:1, 0:5, 6:20, 30:40, 0:1]
+        result = self.op.Output[slicing].wait()
+
+        assert result.shape == slicing2shape(slicing)
+
+    def testWithFullSeeds(self):
+        """
+        If every pixel is seeded, there's nothing for the watershed to do.
+        Output should be a copy of the seeds
+        """
+        # Random seed data
+        seeds = 4 * numpy.random.random( self.inputData.shape )
+        seeds = seeds.astype(numpy.uint32)
+        seeds += 1
+        
+        self.op.SeedImage.setValue( seeds )
+
+        slicing = sl[0:1, 0:5, 6:20, 30:40, 0:1]
+        result = self.op.Output[slicing].wait()
+
+        assert result.shape == slicing2shape(slicing)
+        assert (result == seeds[slicing]).all()
+
+    def testDirtyInput(self):
+        """
+        Mark the input dirty and check to see that the output slot was notified of the right roi
+        """
         dirtyRois = []
         def handleDirty( slot, roi ):
             dirtyRois.append(roi)
         self.op.Output.notifyDirty(handleDirty)
         self.op.InputImage.setDirty( sl[0:1, 5:6, 45:55, 5:15, 0:1] )
+    
+        assert len(dirtyRois) == 1, "Didn't get dirty notification"
+        
+        # Dirty region should include the padding (10 pixels)
+        dirtySlice = dirtyRois[0].toSlice()
+        assert dirtySlice == sl[0:1, 0:10, 35:65, 0:25, 0:1]
+
+    def testDirtySeeds(self):
+        """
+        Mark the seed input dirty and check to see that the output slot was notified of the right roi
+        """
+        seeds = numpy.zeros( self.inputData.shape, dtype=numpy.uint32 )
+        self.op.SeedImage.setValue( seeds )
+
+        dirtyRois = []
+        def handleDirty( slot, roi ):
+            dirtyRois.append(roi)
+        self.op.Output.notifyDirty(handleDirty)
+        self.op.SeedImage.setDirty( sl[0:1, 5:6, 45:55, 5:15, 0:1] )
     
         assert len(dirtyRois) == 1, "Didn't get dirty notification"
         
