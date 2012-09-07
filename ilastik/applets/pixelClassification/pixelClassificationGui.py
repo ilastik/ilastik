5eecca7a20da9738146bd86d296dee2dcaa085bf
Stuart Berg <bergs@janelia.hhmi.org>
2012-09-07 14:50:10 -0400

    PixelClassification: Don't get label list data for prediction channels that aren't ready.

--- ilastik/applets/pixelClassification/pixelClassificationGui.py
+++ ilastik/applets/pixelClassification/pixelClassificationGui.py
@@ -99,8 +99,8 @@ class PixelClassificationGui(LabelingGui):
 
         # Add each of the predictions
         for channel, predictionSlot in enumerate(self.pipeline.PredictionProbabilityChannels[currentImageIndex]):
-            ref_label = labels[channel]
             if predictionSlot.ready():
+                ref_label = labels[channel]
                 predictsrc = LazyflowSource(predictionSlot)
                 predictLayer = AlphaModulatedLayer( predictsrc,
                                                     tintColor=ref_label.color,
