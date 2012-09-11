5ac048db08312920a9448de1bf1aad73d08e026d
Stuart Berg <bergs@janelia.hhmi.org>
2012-09-11 10:04:10 +0200

    LabelingGui: First seven label colors match the colors from ilastik 0.5.

--- ilastik/applets/labeling/labelingGui.py
+++ ilastik/applets/labeling/labelingGui.py
@@ -511,28 +511,36 @@ class LabelingGui(LayerViewerGui):
     @traceLogged(traceLogger)
     def _createDefault16ColorColorTable(self):
         colors = []
-        colors.append(QColor(0,0,0,0)) # Transparent for the zero label
-        colors.append(QColor(0, 0, 255))
-        colors.append(QColor(255, 255, 0))
-        colors.append(QColor(255, 0, 0))
-        colors.append(QColor(0, 255, 0))
-        colors.append(QColor(0, 255, 255))
-        colors.append(QColor(255, 0, 255))
-        colors.append(QColor(255, 105, 180)) #hot pink
-        colors.append(QColor(102, 205, 170)) #dark aquamarine
-        colors.append(QColor(165,  42,  42)) #brown        
-        colors.append(QColor(0, 0, 128))     #navy
-        colors.append(QColor(255, 165, 0))   #orange
-        colors.append(QColor(173, 255,  47)) #green-yellow
-        colors.append(QColor(128,0, 128))    #purple
-        colors.append(QColor(192, 192, 192)) #silver
-        colors.append(QColor(240, 230, 140)) #khaki
-        colors.append(QColor(69, 69, 69))    # dark grey
-        
+
+        # Transparent for the zero label
+        colors.append(QColor(0,0,0,0))
+
+        # ilastik v0.5 colors
+        colors.append( QColor( Qt.red ) )
+        colors.append( QColor( Qt.green ) )
+        colors.append( QColor( Qt.yellow ) )
+        colors.append( QColor( Qt.blue ) )
+        colors.append( QColor( Qt.magenta ) )
+        colors.append( QColor( Qt.darkYellow ) )
+        colors.append( QColor( Qt.lightGray ) )
+
+        # Additional colors
+        colors.append( QColor( Qt.cyan ) )
+        colors.append( QColor(255, 105, 180) ) #hot pink
+        colors.append( QColor(102, 205, 170) ) #dark aquamarine
+        colors.append( QColor(165,  42,  42) ) #brown        
+        colors.append( QColor(0, 0, 128) )     #navy
+        colors.append( QColor(255, 165, 0) )   #orange
+        colors.append( QColor(173, 255,  47) ) #green-yellow
+        colors.append( QColor(128,0, 128) )    #purple
+
+#        colors.append( QColor(192, 192, 192) ) #silver
+#        colors.append( QColor(240, 230, 140) ) #khaki
+#        colors.append( QColor(69, 69, 69) )    # dark grey
+
+        assert len(colors) == 16
+
         return [c.rgba() for c in colors]
-
-
-
 
 
 
