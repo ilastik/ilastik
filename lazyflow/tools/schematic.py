a3ed828a9dd86d8dba7b44c50ab9b84d396d822c
Stuart Berg <bergs@janelia.hhmi.org>
2012-09-23 10:46:14 -0400

    schematic: drawable objects return their size after drawAt()

--- lazyflow/tools/schematic.py
+++ lazyflow/tools/schematic.py
@@ -1,5 +1,6 @@
 from schematic_abc import DrawableABC, ConnectableABC, ConnectionABC
 import svg
+import numpy
 
 def createSvgSlot(slot):
     if slot.level > 0:
@@ -14,14 +15,16 @@ class SvgSlot(DrawableABC, ConnectableABC):
         self.name = slot.name 
 
     def size(self):
-        d = 2*self.Radius
-        return (d,d)
+        return self.drawAt("", (0,0))
     
     def drawAt(self, canvas, upperLeft):
         x,y = upperLeft
         cx = x + self.Radius
         cy = y + self.Radius
         canvas += svg.circle(cx=cx, cy=cy, r=self.Radius, fill='white', stroke='black')
+        
+        lowerRight = (x + 2*self.Radius, y + 2 * self.Radius)
+        return lowerRight
     
     def inputPointOffset(self):
         return (0, self.Radius)
@@ -43,45 +46,33 @@ class SvgMultiSlot(DrawableABC, ConnectableABC):
     
     def __len__(self):
         return len(self.subslots)
-
-    def resize(self, n):
-        if len(self.subslots) > n:
-            self.subslots = self.subslots[0:n]
-        while len(self.subslots) < n:
-            if self.level > 1:
-                self.subslots.append( SvgMultiSlot(self.level-1) )
-            else:
-                self.subslots.append( SvgSlot() )
-
+    
     def size(self):
-        if self.subslots:
-            width = self.subslots[0].size()[0] + 2*self.Padding
-        else:
-            width = (SvgSlot.Radius + 2*self.Padding)
-
-        if self.level > 1:
-            height = self.Padding * (len(self) + 1)
-        else:
-            height = self.Padding * 2
-        for slot in self.subslots:
-            height += slot.size()[1]
-        return (width, height)
+        return self.drawAt("", (0,0))
     
     def drawAt(self, canvas, upperLeft):
-        # Draw our outer rectangle
         x,y = upperLeft
-        width, height = self.size()
-        canvas += svg.rect(x=x, y=y, width=width, height=height, stroke='black', fill='white')
         y += self.Padding
         x += self.Padding
 
+        lowerRight = (x,y)
+
         # Draw each subslot, separated by some padding
         for slot in self.subslots:
-            slot.drawAt(canvas, (x,y))
-            y += slot.size()[1]
+            lowerRight = slot.drawAt(canvas, (x,y))
+            y = lowerRight[1]
             # No padding between level-0 subslots
             if self.level > 1:
                 y += self.Padding
+
+        lowerRight = (lowerRight[0] + self.Padding, y + self.Padding )
+
+        # Draw our outer rectangle
+        width = lowerRight[0] - upperLeft[0]
+        height = lowerRight[1] - upperLeft[1]
+        canvas += svg.rect(x=upperLeft[0], y=upperLeft[1], width=width, height=height, stroke='black', fill='transparent')
+
+        return lowerRight
         
     def inputPointOffset(self):
         return ( 0, self.size()[1]/2 )
@@ -105,15 +96,7 @@ class SvgOperator( DrawableABC ):
             self.outputs[key] = createSvgSlot( slot )
 
     def size(self):
-        width = self.getRectWidth()
-        height = 2 * self.TitleHeight
-        
-        inputSize = self.getInputSize()
-        outputSize = self.getOutputSize()
-        
-        width += inputSize[0] + outputSize[0]
-        height += max(inputSize[1], outputSize[1])
-        return (width, height)
+        return self.drawAt("", (0,0))
 
     def getInputSize(self):
         inputWidth = 0
@@ -136,22 +119,8 @@ class SvgOperator( DrawableABC ):
 
     def drawAt(self, canvas, upperLeft):
         x, y = upperLeft
-        size = self.size()
-        rect_width=self.getRectWidth()
-        height = size[1]
-        r = self.TitleHeight - 2
 
-        rect_x = x + self.getInputSize()[0]
-        rect_y = y
-
-        canvas += svg.rect(x=rect_x, y=rect_y, width=rect_width, height=height, rx=r, ry=r, stroke='black', stroke_width=2, fill='white' )
-        path_d = 'M {startx} {y} L {endx} {y} Z'.format(startx=rect_x, y=rect_y+r+2, endx=rect_x+rect_width)
-        canvas += svg.path(d=path_d, stroke='black', stroke_width=1)
-
-        with block(svg.text, x=rect_x+rect_width/2, y=y+r, text_anchor='middle'):
-            canvas += self.op.name + '\n'
-
-        x, y = upperLeft
+        # Draw inputs
         y += self.TitleHeight
         inputSize = self.getInputSize()
         for slot in self.inputs.values():
@@ -159,6 +128,21 @@ class SvgOperator( DrawableABC ):
             slot.drawAt( canvas, (x+(inputSize[0]-size[0]),y) )
             y += size[1] + self.PaddingBetweenSlots
 
+        # Draw outer rectangle
+        r = self.TitleHeight - 2
+        rect_x = upperLeft[0] + self.getInputSize()[0]
+        rect_y = upperLeft[1]
+        rect_width=self.getRectWidth()
+        height = max(self.getInputSize()[1], self.getOutputSize()[1]) + 2*r
+
+        canvas += svg.rect(x=rect_x, y=rect_y, width=rect_width, height=height, rx=r, ry=r, stroke='black', stroke_width=2, fill='transparent' )
+        path_d = 'M {startx} {y} L {endx} {y} Z'.format(startx=rect_x, y=rect_y+r+2, endx=rect_x+rect_width)
+        canvas += svg.path(d=path_d, stroke='black', stroke_width=1)
+
+        with block(svg.text, x=rect_x+rect_width/2, y=rect_y+r, text_anchor='middle'):
+            canvas += self.op.name + '\n'
+
+        # Draw outputs
         x, y = upperLeft
         x += rect_width + inputSize[0]
         y += self.TitleHeight
@@ -166,7 +150,10 @@ class SvgOperator( DrawableABC ):
             size = slot.size()
             slot.drawAt( canvas, (x,y) )
             y += size[1] + self.PaddingBetweenSlots
-
+        
+        totalSize = numpy.add( self.getInputSize(), self.getOutputSize() )
+        totalSize [0] += self.getRectWidth()
+        return tuple(totalSize)
             
     def getRectWidth(self):
         return 100
