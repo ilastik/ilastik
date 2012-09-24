b485ccab06186d0ee27395e826f4ae04f7038efa
Stuart Berg <bergs@janelia.hhmi.org>
2012-09-24 14:33:49 -0400

    Schematic: Rudimentary support for drawing internal operators (doesn't draw connections yet).

--- lazyflow/tools/schematic.py
+++ lazyflow/tools/schematic.py
@@ -1,6 +1,7 @@
 from schematic_abc import DrawableABC, ConnectableABC, ConnectionABC
 import svg
 import numpy
+from functools import partial
 
 def createSvgSlot(slot):
     if slot.level > 0:
@@ -80,9 +81,62 @@ class SvgMultiSlot(DrawableABC, ConnectableABC):
     def outputPointOffset(self):
         return ( self.size()[0], self.size()[1]/2 )
 
+def hasAncestor(x, parent):
+    if x.parent == parent:
+        return True
+    elif x.parent is None:
+        return False
+    else:
+        return hasAncestor(x.parent, parent)
+
+def isDownStream(op1, op2, parent):
+    for islot in op1.inputs.values():
+        if islot.partner is None:
+            continue
+        upStreamOp = islot.partner.getRealOperator()
+        if upStreamOp is op2:
+            return True # A is downstream from B
+        elif hasAncestor(upStreamOp, parent):
+            if isDownStream(upStreamOp, op2, parent):
+                return True
+    return False
+
+def streamPos(parent, a,b):
+    # A and B must both be owned by the parent, otherwise they aren't comparable
+    if not hasAncestor(a, parent) or not hasAncestor(b, parent):
+        return 0
+    if isDownStream(a, b, parent):
+        return 1
+    if isDownStream(b, a, parent):
+        return -1
+    return 0
+
+def sortByPos(l, parent):
+    sorted_l = []
+    for nextOp in l:
+        i = 0
+        for i, op in enumerate(sorted_l):
+            pos = streamPos(parent, nextOp, op)
+            if pos == 1:
+                break
+        sorted_l.insert(i, nextOp)
+    
+#    sorted_l2 = []
+#    for nextOp in sorted_l:
+#        i = 0
+#        for i, op in enumerate(sorted_l2):
+#            pos = streamPos(parent, nextOp, op)
+#            if pos == -1:
+#                break
+#        sorted_l2.insert(i, nextOp)
+
+    return sorted_l
+
+
 class SvgOperator( DrawableABC ):
     TitleHeight = 15
     PaddingBetweenSlots = 5
+    PaddingBetweenInternalOps = 10
         
     def __init__(self, op):
         self.op = op
@@ -100,17 +154,16 @@ class SvgOperator( DrawableABC ):
 
     def getInputSize(self):
         inputWidth = 0
-        inputHeight = self.PaddingBetweenSlots * (len(self.inputs)-1)
+        inputHeight = self.PaddingBetweenSlots * (len(self.inputs)+1)
         for svgSlot in self.inputs.values():
             slotSize = svgSlot.size()
             inputWidth = max(inputWidth, slotSize[0])
             inputHeight += slotSize[1]
         return (inputWidth, inputHeight)
 
-
     def getOutputSize(self):
         outputWidth = 0
-        outputHeight = self.PaddingBetweenSlots * (len(self.outputs)-1)
+        outputHeight = self.PaddingBetweenSlots * (len(self.outputs)+1)
         for svgSlot in self.outputs.values():
             slotSize = svgSlot.size()
             outputWidth = max(outputWidth, slotSize[0])
@@ -120,27 +173,72 @@ class SvgOperator( DrawableABC ):
     def drawAt(self, canvas, upperLeft):
         x, y = upperLeft
 
-        # Draw inputs
-        y += self.TitleHeight
         inputSize = self.getInputSize()
-        for slot in self.inputs.values():
-            size = slot.size()
-            slot.drawAt( canvas, (x+(inputSize[0]-size[0]),y) )
-            y += size[1] + self.PaddingBetweenSlots
+        outputSize = self.getOutputSize()
+
+        child_ordering = {}
+        if len(self.op.children) > 0:
+            # Draw child operators
+            sorted_children = sorted( self.op.children, cmp=partial(streamPos, self) )
+            #sorted_children = sortByPos(self.op.children, self)
+    
+            child_ordering[0] = [sorted_children[0]]
+            col_index = 0
+            for child, next_child in zip(sorted_children[0:-1], sorted_children[1:]):
+                if isDownStream(next_child, child, self):
+                    col_index += 1
+                    child_ordering[col_index] = []
+                child_ordering[col_index].append(next_child)
+    
+            print [child.name for child in sorted_children]
+            for col in sorted(child_ordering.keys()):
+                print col, ":", [child.name for child in child_ordering[col]]
+        
+        r = self.TitleHeight - 2
+        rect_x = upperLeft[0] + inputSize[0]
+        rect_y = upperLeft[1]
+
+        child_x = rect_x + r
+        child_y = rect_y + 2*r
+        max_child_y = child_y
+        max_child_x = child_x + self.PaddingBetweenInternalOps
+        for col_index, col_children in sorted( child_ordering.items() ):
+            for child in col_children:
+                svgChild = SvgOperator(child)
+                lowerRight = svgChild.drawAt(canvas, (child_x, child_y) )
+                max_child_x = max(lowerRight[0], max_child_x)
+                child_y = lowerRight[1] + self.PaddingBetweenInternalOps
+            
+            max_child_x += self.PaddingBetweenInternalOps
+            max_child_y = max(max_child_y, child_y)
+            child_x = max_child_x
+            child_y = rect_y + 2*r
+
+        rect_width = max_child_x - rect_x
+        rect_width = max( rect_width, 2*self.PaddingBetweenInternalOps )
+        rect_width = max( rect_width, 9*len(self.op.name) ) # Correct width depends on font...
+        rect_width += r
+
+        rect_height = max_child_y - rect_y
+        rect_height = max(rect_height, outputSize[1])
+        rect_height = max(rect_height, inputSize[1])
+        rect_height += 2*r
+        rect_height = max( rect_height, 50 )
 
         # Draw outer rectangle
-        r = self.TitleHeight - 2
-        rect_x = upperLeft[0] + self.getInputSize()[0]
-        rect_y = upperLeft[1]
-        rect_width=self.getRectWidth()
-        height = max(self.getInputSize()[1], self.getOutputSize()[1]) + 2*r
-
-        canvas += svg.rect(x=rect_x, y=rect_y, width=rect_width, height=height, rx=r, ry=r, stroke='black', stroke_width=2, fill='transparent' )
+        canvas += svg.rect(x=rect_x, y=rect_y, width=rect_width, height=rect_height, rx=r, ry=r, stroke='black', stroke_width=2, style='fill-opacity:0' )
         path_d = 'M {startx} {y} L {endx} {y} Z'.format(startx=rect_x, y=rect_y+r+2, endx=rect_x+rect_width)
         canvas += svg.path(d=path_d, stroke='black', stroke_width=1)
 
         with block(svg.text, x=rect_x+rect_width/2, y=rect_y+r, text_anchor='middle'):
             canvas += self.op.name + '\n'
+
+        # Draw inputs
+        y += self.TitleHeight
+        for slot in self.inputs.values():
+            size = slot.size()
+            slot.drawAt( canvas, (x+(inputSize[0]-size[0]),y) )
+            y += size[1] + self.PaddingBetweenSlots
 
         # Draw outputs
         x, y = upperLeft
@@ -151,28 +249,72 @@ class SvgOperator( DrawableABC ):
             slot.drawAt( canvas, (x,y) )
             y += size[1] + self.PaddingBetweenSlots
         
-        totalSize = numpy.add( self.getInputSize(), self.getOutputSize() )
-        totalSize [0] += self.getRectWidth()
-        return tuple(totalSize)
+        lowerRight_x = upperLeft[0] + rect_width + inputSize[0] + outputSize[0]
+        lowerRight_y = upperLeft[1] + rect_height
+        return (lowerRight_x, lowerRight_y)
             
     def getRectWidth(self):
         return 100
          
 if __name__ == "__main__":
-    from functools import partial
     canvas = svg.SvgCanvas("")
 
     from lazyflow.graph import Graph, Operator, InputSlot, OutputSlot
     
+    class OpChild(Operator):
+        name = "OpChild"
+        
+        InputX = InputSlot()
+        InputY = InputSlot()
+        Output = OutputSlot()
+    
     class OpTest(Operator):
+        name = "OpTest"
+        
         InputA = InputSlot()
         InputB = InputSlot(level=1)
         Output = OutputSlot()
+        
+        def __init__(self, *args, **kwargs):
+            super(OpTest, self).__init__(*args, **kwargs)
+            self.internal0 = OpChild(graph=self.graph, parent=self)
+            self.internal1 = OpChild(graph=self.graph, parent=self)
+            self.internal2 = OpChild(graph=self.graph, parent=self)
+            self.internal3 = OpChild(graph=self.graph, parent=self)
+            self.internal4 = OpChild(graph=self.graph, parent=self)
+
+            self.internal0.name = "Op0"
+            self.internal1.name = "Op1"
+            self.internal2.name = "Op2"
+            self.internal3.name = "Op3"
+            self.internal4.name = "Op4"
+            
+            #self.internal0.InputX.connect(self.internal1.Output)
+            self.internal2.InputY.connect(self.internal0.Output)
+            self.internal2.InputX.connect(self.internal1.Output)
+            self.internal3.InputX.connect(self.internal2.Output)
+            self.internal4.InputX.connect(self.internal2.Output)
+
+#            assert isDownStream(self.internal2, self.internal0, self)
+#            assert isDownStream(self.internal2, self.internal1, self)
+#            #assert isDownStream(self.internal1, self.internal0, self)
+#            assert isDownStream(self.internal3, self.internal1, self)
+    
     
     graph=Graph()
-    opTest = OpTest(graph=graph)
-    opTest.InputB.resize(3)
+#    opTest = OpTest(graph=graph)
+#    opTest.InputB.resize(3)
 
+    from lazyflow.graph import OperatorWrapper
+    from lazyflow.operators.ioOperators import OpInputDataReader
+    opInput = OpInputDataReader(graph=graph)
+    opInput.FilePath.setValue("/magnetic/synapse_small.npy")
+
+    opTest = OperatorWrapper( OpInputDataReader, graph=graph )
+    opTest.FilePath.resize(2)
+    opTest.FilePath[0].setValue("/magnetic/synapse_small.npy")
+    opTest.FilePath[1].setValue("/magnetic/gigacube.h5/volume/data")
+    
     svgOp = SvgOperator(opTest)
     
     block = partial(svg.tagblock, canvas)
@@ -187,7 +329,7 @@ if __name__ == "__main__":
 #                slot[i][j].resize(j+1)
 #            slot.drawAt(canvas, (0,0) )
     
-    print canvas.getvalue()
+    #print canvas.getvalue()
     f = file("/Users/bergs/Documents/svgfiles/canvas.html", 'w')
     f.write( canvas.getvalue() )
     f.close()
