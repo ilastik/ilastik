4e4978b0fca7fe1546cfa31d3bf5bec820f00a01
mschiegg <martin.schiegg@iwr.uni-heidelberg.de>
2012-10-02 18:40:04 +0200

    set up object extraction for multiple classes

---
+++ ilastik/applets/objectExtraction/opObjectExtractionMultiClass.py
@@ -0,0 +1,137 @@
+from lazyflow.graph import Operator, InputSlot, OutputSlot
+from lazyflow.stype import Opaque
+from lazyflow.rtype import List
+from lazyflow.operators.obsolete.generic import OpSubRegion
+from ilastik.applets.objectExtraction.opObjectExtraction import OpObjectExtraction
+import numpy
+import h5py
+#from ilastik.applets.objectExtraction.opObjectExtraction import OpObjectExtraction
+
+
+class OpObjectExtractionMultiClass(Operator):
+    name = "Object Extraction Multi-Class"
+    
+    # not necessarily a binary image (will be thresholded in the first step)
+    Images = InputSlot()   
+
+    LabelImage = OutputSlot()
+    ObjectCenterImage = OutputSlot()
+    RegionCenters = OutputSlot(stype=Opaque, rtype=List)
+    RegionFeatures = OutputSlot(stype=Opaque, rtype=List)
+    ClassMapping = OutputSlot(stype=Opaque, rtype=List)
+    BinaryImage = OutputSlot()
+
+    def __init__(self, parent=None, graph=None):        
+        super(OpObjectExtractionMultiClass, self).__init__(parent=parent, graph=graph)
+        print "OpObjectExtractionMultiClass::init"
+
+        self._mem_h5 = h5py.File(str(id(self)), driver='core', backing_store=False)
+        
+        self._opThresholding = OpThresholding(graph=graph)
+        self._opThresholding.Threshold.setValue(0.5)
+        self._opThresholding.Input.connect(self.Images)
+        self.BinaryImage.connect(self._opThresholding.BinaryImage)
+        
+        # TODO: generalize to m classes
+        self._opSubRegionBgImage = OpSubRegion(graph=graph)        
+        self._opSubRegionBgImage.inputs["Input"].connect(self._opThresholding.BinaryImage)
+                
+        self._opSubRegionDivImage = OpSubRegion(graph=graph)        
+        self._opSubRegionDivImage.inputs["Input"].connect(self._opThresholding.BinaryImage)        
+        
+        self._opObjectExtractionBg = OpObjectExtraction(graph=graph)
+        self._opObjectExtractionBg.BinaryImage.connect(self._opSubRegionBgImage.outputs["Output"])
+        
+        self._opObjectExtractionDiv = OpObjectExtraction(graph=graph)
+        self._opObjectExtractionDiv.BinaryImage.connect(self._opSubRegionDivImage.outputs["Output"])
+        
+        self.LabelImage.connect(self._opObjectExtractionBg.LabelImage)
+        self.ObjectCenterImage.connect(self._opObjectExtractionBg.ObjectCenterImage)
+        self.RegionCenters.connect(self._opObjectExtractionBg.RegionCenters)
+        self.RegionFeatures.connect(self._opObjectExtractionBg.RegionFeatures)
+        
+        self._opClassExtraction = OpClassExtraction(graph=graph)
+        self._opClassExtraction.inputs["LabelImageBg"].connect(self._opObjectExtractionBg.LabelImage)
+        self._opClassExtraction.inputs["LabelImageDiv"].connect(self._opObjectExtractionDiv.LabelImage)
+        self._opClassExtraction.inputs["RegionFeaturesBg"].connect(self._opObjectExtractionBg.RegionFeatures)
+        self._opClassExtraction.inputs["RegionFeaturesDiv"].connect(self._opObjectExtractionDiv.RegionFeatures)
+        
+        self.ClassMapping.connect(self._opClassExtraction.outputs["ClassMapping"])        
+        
+
+    def setupOutputs(self):
+        # TODO: set values to channel 0 (if label 0 == bg)
+        # assumes that background == label 0, assumes t,x,y,z,c
+        backgroundlabel = 0
+        start = (len(self.Images.meta.shape) - 1) * [0,] + [backgroundlabel,]         
+        stop = list(self.Images.meta.shape[0:-1]) + [backgroundlabel + 1,]        
+        self._opSubRegionBgImage.inputs["Start"].setValue(tuple(start))        
+        self._opSubRegionBgImage.inputs["Stop"].setValue(tuple(stop)) 
+        
+        # assumes that division == label 2, assumes t,x,y,z,c
+        divisionlabel = 2
+        start[-1] = divisionlabel
+        stop[-1] = divisionlabel + 1                
+        self._opSubRegionDivImage.inputs["Start"].setValue(tuple(start))        
+        self._opSubRegionDivImage.inputs["Stop"].setValue(tuple(stop))
+    
+    def execute(self, slot, subindex, roi, result):        
+        pass
+
+    def propagateDirty(self, inputSlot, roi):
+        pass
+
+
+
+class OpClassExtraction(Operator):    
+    name = "Class Extraction"
+    
+    LabelImageBg = InputSlot()
+    LabelImageDiv = InputSlot()
+    RegionFeaturesBg = InputSlot()
+    RegionFeaturesDiv = InputSlot()
+    
+    ClassMapping = OutputSlot(stype=Opaque, rtype=List)
+    
+    def setupOutputs(self):
+        pass
+    
+    def execute(self, slot, subindex, roi, result):
+        # initialize prob dict with 1.0 for all bg labels
+        # get both labelimages (roi)
+        # for all div labels
+            # lookup bg-label for the current div label
+            # lookup bg-volume for this bg-label
+            # lookup div-volume for this bg-label
+            # prob[bg-label] = ratio, 1-ratio
+        # return the dict (do not use result!)
+        pass
+    
+    def propagateDirty(self, slot, subindex, roi):
+        pass
+        
+
+class OpThresholding(Operator):
+    name = "Thresholding"
+    
+    Input = InputSlot()
+    Threshold = InputSlot()
+    BinaryImage = OutputSlot()
+    
+    maximum = None
+    def setupOutputs(self):
+        self.BinaryImage.meta.dtype = numpy.bool
+        self.BinaryImage.meta.shape = self.Input.meta.shape
+        self.BinaryImage.meta.axistags = self.Input.meta.axistags
+    
+    def execute(self, slot, subindex, roi, result):
+        img = self.Input.get(roi).wait()
+        threshold = self.Threshold.value
+        if self.maximum is None:
+            self.maximum = numpy.ceil(numpy.max(img))
+        threshold *= self.maximum            
+        return (img > threshold)        
+    
+    def propagateDirty(self, slot, subindex, roi):
+        pass
+    
---
+++ workflows/tracking/trackingWorkflowNN.py
@@ -0,0 +1,67 @@
+from lazyflow.graph import Graph, Operator, OperatorWrapper
+
+from ilastik.workflow import Workflow
+
+from ilastik.applets.dataSelection import DataSelectionApplet
+from ilastik.applets.objectExtraction import ObjectExtractionApplet
+from ilastik.applets.tracking import TrackingApplet
+
+
+from lazyflow.graph import Operator, InputSlot, OutputSlot
+from lazyflow.stype import Opaque
+from lazyflow.operators.ioOperators.opInputDataReader import OpInputDataReader
+from ilastik.applets.tracking.opTracking import *
+import ctracking
+from lazyflow.operators.obsolete.valueProviders import OpAttributeSelector
+
+class TrackingWorkflowNN( Workflow ):
+    def __init__( self ):
+        super(TrackingWorkflowNN, self).__init__()
+        self._applets = []
+        self._imageNameListSlot = None
+        self._graph = None
+
+        # Create a graph to be shared by all operators
+        graph = Graph()
+    
+        ######################
+        # Interactive workflow
+        ######################
+        
+        ## Create applets 
+        self.dataSelectionApplet = DataSelectionApplet(graph, "Input Segmentation", "Input Segmentation", batchDataGui=False)
+
+        self.objectExtractionApplet = ObjectExtractionApplet( graph )
+        self.trackingApplet = TrackingApplet( graph )
+
+        ## Access applet operators
+        opData = self.dataSelectionApplet.topLevelOperator
+        opObjExtraction = self.objectExtractionApplet.topLevelOperator
+        opTracking = self.trackingApplet.topLevelOperator
+        
+        ## Connect operators ##
+        opObjExtraction.Images.connect( opData.Image )
+
+        opTracking.LabelImage.connect( opObjExtraction.LabelImage )
+        opTracking.ObjectFeatures.connect( opObjExtraction.RegionFeatures )
+
+        self._applets.append(self.dataSelectionApplet)
+        self._applets.append(self.objectExtractionApplet)
+        self._applets.append(self.trackingApplet)
+
+        # The shell needs a slot from which he can read the list of image names to switch between.
+        # Use an OpAttributeSelector to create a slot containing just the filename from the OpDataSelection's DatasetInfo slot.
+        opSelectFilename = OperatorWrapper( OpAttributeSelector, graph=graph )
+        opSelectFilename.InputObject.connect( opData.Dataset )
+        opSelectFilename.AttributeName.setValue( 'filePath' )
+
+        self._imageNameListSlot = opSelectFilename.Result
+
+    @property
+    def applets(self):
+        return self._applets
+
+    @property
+    def imageNameListSlot(self):
+        return self._imageNameListSlot
+    
