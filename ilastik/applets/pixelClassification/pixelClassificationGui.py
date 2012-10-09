3f80116c86ecf43d640c07cf27ffe55825e732a0
Stuart Berg <bergs@janelia.hhmi.org>
2012-10-09 15:14:52 -0400

    PixelClassificationGui: Disable applets during entire "save prediction" process.

--- ilastik/applets/pixelClassification/pixelClassificationGui.py
+++ ilastik/applets/pixelClassification/pixelClassificationGui.py
@@ -50,7 +50,7 @@ class PixelClassificationGui(LabelingGui):
     ###########################################
 
     @traceLogged(traceLogger)
-    def __init__(self, pipeline, shellRequestSignal, predictionSerializer ):
+    def __init__(self, pipeline, shellRequestSignal, guiControlSignal, predictionSerializer ):
         # Tell our base class which slots to monitor
         labelSlots = LabelingGui.LabelingSlots()
         labelSlots.labelInput = pipeline.LabelInputs
@@ -68,6 +68,7 @@ class PixelClassificationGui(LabelingGui):
         
         self.pipeline = pipeline
         self.shellRequestSignal = shellRequestSignal
+        self.guiControlSignal = guiControlSignal
         self.predictionSerializer = predictionSerializer
         
         self.interactiveModeActive = False
@@ -340,23 +341,29 @@ class PixelClassificationGui(LabelingGui):
             originalButtonText = "Save Predictions Now"
             self.labelingDrawerUi.savePredictionsButton.setText("Cancel Save")
 
+            @traceLogged(traceLogger)
             def saveThreadFunc():
-                with Tracer(traceLogger):
-                    # First, do a regular save.
-                    # During a regular save, predictions are not saved to the project file.
-                    # (It takes too much time if the user only needs the classifier.)
-                    self.shellRequestSignal.emit( ShellRequest.RequestSave )
-                    
-                    # Enable prediction storage and ask the shell to save the project again.
-                    # (This way the second save will occupy the whole progress bar.)
-                    self.predictionSerializer.predictionStorageEnabled = True
-                    self.shellRequestSignal.emit( ShellRequest.RequestSave )
-                    self.predictionSerializer.predictionStorageEnabled = False
-    
-                    # Restore original states (must use events for UI calls)
-                    self.thunkEventHandler.post(self.labelingDrawerUi.savePredictionsButton.setText, originalButtonText)
-                    self.pipeline.FreezePredictions.setValue(predictionsFrozen)
-                    self._currentlySavingPredictions = False
+                # Disable all applets                    
+                self.guiControlSignal.emit( ControlCommand.DisableAll )
+
+                # First, do a regular save.
+                # During a regular save, predictions are not saved to the project file.
+                # (It takes too much time if the user only needs the classifier.)
+                self.shellRequestSignal.emit( ShellRequest.RequestSave )
+                
+                # Enable prediction storage and ask the shell to save the project again.
+                # (This way the second save will occupy the whole progress bar.)
+                self.predictionSerializer.predictionStorageEnabled = True
+                self.shellRequestSignal.emit( ShellRequest.RequestSave )
+                self.predictionSerializer.predictionStorageEnabled = False
+
+                # Restore original states (must use events for UI calls)
+                self.thunkEventHandler.post(self.labelingDrawerUi.savePredictionsButton.setText, originalButtonText)
+                self.pipeline.FreezePredictions.setValue(predictionsFrozen)
+                self._currentlySavingPredictions = False
+
+                # Re-enable all applets
+                self.guiControlSignal.emit( ControlCommand.Pop )
 
             saveThread = threading.Thread(target=saveThreadFunc)
             saveThread.start()
