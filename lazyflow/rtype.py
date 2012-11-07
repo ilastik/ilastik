f40cb8b6541f89a742de753fe45910c1db4a11b7
Stuart Berg <bergs@janelia.hhmi.org>
2012-11-07 10:32:41 -0500

    Reverted changes to rtype.py made in commit d7f19ca71387473a48b453e225f5e84e18500beb

--- lazyflow/rtype.py
+++ lazyflow/rtype.py
@@ -2,19 +2,66 @@ from roi import sliceToRoi, roiToSlice
 import vigra,numpy,copy
 from lazyflow.roi import TinyVector
 from lazyflow import slicingtools
+import cPickle as pickle
+
+class RoiMeta(type):
+    """
+    Roi metaclass.  This mechanism automatically registers all roi 
+    subclasses for string serialization via Roi._registerSubclass.
+    """
+    def __new__(cls, name, bases, classDict):
+        cls = type.__new__(cls, name, bases, classDict)
+        # Don't register the Roi baseclass itself.
+        if bases[0] != object > 1:
+            Roi._registerSubclass(cls)
+        return cls
 
 class Roi(object):
+    __metaclass__ = RoiMeta
+
     def __init__(self, slot):
         self.slot = slot
         pass
     pass
 
+    all_subclasses = set()
 
+    @classmethod
+    def _registerSubclass(cls, roiType):
+        Roi.all_subclasses.add(roiType)
+
+    @staticmethod
+    def _toString(roi):
+        """
+        Convert roi into a string that can be converted back into a roi via _fromString().
+        The default implementation uses pickle.
+        Subclasses may override this and _fromString for more human-friendly string representations.
+        """
+        return pickle.dumps(roi)
+    
+    @staticmethod
+    def _fromString(s):
+        """
+        Convert string 's' into a roi.
+        The default implementation uses pickle.
+        Subclasses may override this and _toString for more human-friendly string representations.
+        """
+        return pickle.loads(s)
+
+    @staticmethod
+    def dumps(roi):
+        return roi.__class__.__name__ + ":" + roi.__class__._toString(roi)
+
+    @staticmethod
+    def loads(s):
+        for cls in Roi.all_subclasses:
+            if s.startswith(cls.__name__):
+                return cls._fromString( s[len(cls.__name__)+1:] )
+        assert False, "Class name does not refer to any Roi subclasses."
 
 class Everything(Roi):
     '''Fallback Roi for Slots that can't operate on subsets of their input data.'''
     pass
-
 
 class List(Roi):
     def __init__(self, slot, iterable=()):
@@ -26,7 +73,6 @@ class List(Roi):
         return len(self._l)
     def __str__( self ):
         return str(self._l)
-
 
     
 class SubRegion(Roi):
@@ -53,6 +99,16 @@ class SubRegion(Roi):
     def __str__( self ):
         return "".join(("Subregion: start '", str(self.start), "' stop '", str(self.stop), "'"))
 
+    @staticmethod
+    def _toString(roi):
+        assert isinstance(roi, SubRegion)
+        assert roi.slot is None, "Can't stringify SubRegions with no slot"
+        return "SubRegion(None, {}, {})".format(roi.start, roi.stop)
+
+    @staticmethod
+    def _fromString(s):
+        return eval(s)
+
     def setInputShape(self,inputShape):
         assert type(inputShape) == tuple
         self.inputShape = inputShape
@@ -65,9 +121,8 @@ class SubRegion(Roi):
         remove the i'th dimension from the SubRegion
         works inplace !
         """
-        if dim is not None:
-            self.start.pop(dim)
-            self.stop.pop(dim)
+        self.start.pop(dim)
+        self.stop.pop(dim)
         return self
 
     def setDim(self, dim , start, stop):
