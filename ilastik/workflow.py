261640be0cfaea3691fe37381888353e0cbf1b87
Stuart Berg <bergs@janelia.hhmi.org>
2012-11-29 17:08:04 -0500

    workflow: Don't require that all applets have top-level operators (e.g. the project metadata applet)

--- ilastik/workflow.py
+++ ilastik/workflow.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta, abstractproperty, abstractmethod
+from abc import abstractproperty, abstractmethod
 
 from lazyflow.graph import Operator
 
@@ -24,13 +24,15 @@ class Workflow( Operator ):
         
     def _createNewImageLane(self, multislot, index, *args):
         for a in self.applets:
-            a.addLane(index)
+            if a.topLevelOperator is not None:
+                a.topLevelOperator.addLane(index)
         
         self.connectLane(index)
     
     def _removeImageLane(self, multislot, index, finalLength):
         for a in self.applets:
-            a.removeLane(index, finalLength)
+            if a.topLevelOperator is not None:
+                a.topLevelOperator.removeLane(index, finalLength)
 
     @abstractmethod
     def connectLane(self):
