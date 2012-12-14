20d6c7e0c1043a26a856d3f93695c1ae94780e3b
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-12-14 10:02:57 +0100

    Tracking and ObjectExtraction applet: adopted latest API changes

--- ilastik/applets/objectExtraction/objectExtractionApplet.py
+++ ilastik/applets/objectExtraction/objectExtractionApplet.py
@@ -5,13 +5,17 @@ from objectExtractionGui import ObjectExtractionGui
 from objectExtractionSerializer import ObjectExtractionSerializer
 
 class ObjectExtractionApplet( StandardApplet ):
-    def __init__( self, name="Object Extraction", workflow=None ):
+    def __init__( self, name="Object Extraction", workflow=None, projectFileGroupName="ObjectExtraction" ):
         super(ObjectExtractionApplet, self).__init__( name=name, workflow=workflow )
         self._serializableItems = [ ObjectExtractionSerializer(self.topLevelOperator, projectFileGroupName) ]
 
     @property
     def singleLaneOperatorClass( self ):
         return OpObjectExtraction
+
+    @property
+    def broadcastingSlots( self ):
+        return []
 
     @property
     def singleLaneGuiClass( self ):
