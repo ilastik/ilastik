920e33f8eb7b2989cd0d9ecdc5caf7274c92999f
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2012-12-17 19:48:18 +0100

    chaingraph wf: givin BackgroundLabel slot a value; fixes volumina crashes since the graph is no fully initialized

--- workflows/tracking/chaingraph/chaingraphTrackingWorkflow.py
+++ workflows/tracking/chaingraph/chaingraphTrackingWorkflow.py
@@ -42,7 +42,8 @@ class ChaingraphTrackingWorkflow( Workflow ):
         ## Connect operators ##
         opObjExtraction.RawImage.connect( opRawData.Image )
         opObjExtraction.BinaryImage.connect( opData.Image )
-        
+        opObjExtraction.BackgroundLabel.setValue( 0 )
+
         opTracking.RawImage.connect( opRawData.Image )
         opTracking.LabelImage.connect( opObjExtraction.LabelImage )
         opTracking.ObjectFeatures.connect( opObjExtraction.RegionFeatures )
