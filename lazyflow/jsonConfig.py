1503430a0bc75dc74e8d20fb574b78381f14d193
Stuart Berg <bergs@janelia.hhmi.org>
2012-12-21 11:26:25 -0500

    wip: opRESTfulVolumeReader.py

---
+++ lazyflow/jsonConfig.py
@@ -0,0 +1,134 @@
+import json
+import re
+
+import numpy # We import numpy here so that it can be used in AutoEval fields.
+
+class Namespace(object):
+    pass
+
+class AutoEval(object):
+    """
+    Callable that serves as a pseudo-type.
+    Converts a value to a specific type, unless the value is a string, in which case it is evaluated first.
+    """
+    def __init__(self, t=None):
+        """
+        If a type t is provided, the value from the config will be converted using t as the constructor.
+        If t is not provided, the (possibly eval'd) value will be returned 'as-is' with no conversion.
+        """
+        self._t = t
+        if t is None:
+            # If no conversion type was provided, we'll assume that the result of eval() is good enough. 
+            self._t = lambda x:x
+        
+    def __call__(self, x):
+        if type(x) is self._t:
+            return x
+        if type(x) is str or type(x) is unicode and self._t is not str:
+            return self._t(eval(x))
+        return self._t(x)
+
+class FormattedField(object):
+    """
+    Callable that serves as a pseudo-type for config values that will be used by ilastik as format strings.
+    Doesn't actually transform the given value, but does check it for the required format fields.
+    """
+    def __init__(self, requiredFields, optionalFields=[]):
+        assert isinstance(requiredFields, list)
+        assert isinstance(optionalFields, list)
+        
+        self._requiredFields = requiredFields
+        self._optionalFields = optionalFields
+    
+    def __call__(self, x):
+        """
+        Convert x to str (no unicode), and check it for the required fields.
+        """
+        x = str(x)
+        for f in self._requiredFields:
+            fieldRegex = re.compile('{[^}]*' + f +  '}')
+            if fieldRegex.search(x) is None:
+                raise JsonConfigSchema.ParsingError( "Format string is missing required field: {{{f}}}".format(f=f) )
+
+        # TODO: Also validate that all format fields the user provided are known required/optional fields.
+        return x
+
+#class AutoDirField(object):
+#    def __init__(self, replaceString):
+#        self._replaceString = replaceString
+#    def __call__(self, x):
+#        x = str(x)
+#        if self._replaceString not in x:
+#            return x
+#        
+#        # Must be /some/dir/<AUTO>, not /some/dir/<AUTO>/plus/otherstuff
+#        replaceIndex = x.index(self._replaceString)
+#        assert replaceIndex + len(self._replaceString) == len(x), "Auto-replaced dir name must appear at the end of the config value."
+#        
+#        baseDir, fileBase = os.path.split( x[0:replaceIndex] )
+#        next_unused_index = 1
+#        for filename in os.listdir(baseDir):
+#            m = re.match("("+ fileBase + ")(\d+)", filename)
+#            if m:
+#                used_index = int(m.groups()[1])
+#                next_unused_index = max( next_unused_index, used_index+1 )
+#
+#        return os.path.join( baseDir, fileBase + "{}".format(next_unused_index)  )
+
+
+class JsonConfigSchema( object ):
+    """
+    Simple config schema for json config files.
+    Currently, only a very small set of json is supported.
+    The schema fields must be a single non-nested dictionary of name : type (or pseudo-type) pairs.
+    """
+    class ParsingError(Exception):
+        pass
+    
+    def __init__(self, fields):
+        self._fields = fields
+    
+    def parseConfigFile(self, configFilePath):
+        with open(configFilePath) as configFile:
+            try:
+                configDict = { str(k) : v for k,v in json.load( configFile ).items() }
+            except:
+                import sys
+                sys.stderr.write( "File '{}' is not valid json.  See stdout for exception details.".format(configFilePath) )
+                raise
+
+            try:
+                return self._getNamespace(configDict)
+            except JsonConfigSchema.ParsingError, e:
+                raise JsonConfigSchema.ParsingError( "Error parsing config file '{f}':\n{msg}".format( f=configFilePath, msg=e.args[0] ) )
+
+    def _getNamespace(self, configDict):
+        namespace = Namespace()
+        # All config fields are None by default
+        for key in self._fields.keys():
+            setattr(namespace, key, None)
+        
+        # Keys that the user gave us are 
+        for key, value in configDict.items():
+            if key in self._fields.keys():
+                fieldType = self._fields[key]
+                try:
+                    finalValue = self._transformValue( fieldType, value )
+                except JsonConfigSchema.ParsingError, e:
+                    raise JsonConfigSchema.ParsingError( "Error parsing config field '{f}':\n{msg}".format( f=key, msg=e.args[0] ) )
+                else:
+                    setattr( namespace, key, finalValue )
+        return namespace
+    
+    def _transformValue(self, fieldType, val):
+        # config file is allowed to contain null values, in which case the value is set to None
+        if val is None:
+            return None
+
+        # Check special error cases
+        if fieldType is bool and not isinstance(val, bool):
+            raise JsonConfigSchema.ParsingError( "Expected bool, got {}".format( type(val) ) )
+        
+        # Other special types will error check when they construct.
+        return fieldType( val )
+
---
+++ lazyflow/operators/ioOperators/opRESTfulVolumeReader.py
@@ -0,0 +1,189 @@
+import tempfile
+import urllib2
+from lazyflow.graph import Operator, InputSlot, OutputSlot
+import vigra
+from lazyflow.jsonConfig import JsonConfigSchema
+
+RESTfulVolumeDescriptionFields = \
+{
+    "name" : str,
+    "format" : str,
+    "axes" : str,
+    "shape" : list,
+    "dtype" : AutoEval(),
+    "origin_offset" : list,
+    "url_format" : FormattedField( requiredFields=["x_start", "x_stop", "y_start", "y_stop", "z_start", "z_stop"], 
+                                   optionalFields=["t_start", "t_stop", "c_start", "c_stop"] ),
+    "hdf5_dataset" : str
+}
+
+class OpRESTfulVolumeReader(Operator):
+    """
+    An operator to retrieve hdf5 volumes from a remote server that provides a RESTful interface.
+    The operator requires a LOCAL json config file that describes the remote dataset and interface.
+    """
+    name = "OpRESTfulVolumeReader"
+
+    DescriptionFilePath = InputSlot(stype='filestring')
+    Output = OutputSlot()
+
+    def __init__(self, *args, **kwargs):
+        super(OpRESTfulVolumeReader, self).__init__(*args, **kwargs)
+        self._configSchema = JsonConfigSchema( RESTfulVolumeDescriptionFields )
+        self._origin_offset = None
+        self._urlFormat = None
+        self._axes = None
+        self._hdf5_dataset = None
+
+    def setupOutputs(self):
+        # Read the dataset description file
+        descriptionFields = self._configSchema( self.DescriptionFilePath.value )
+
+        assert descriptionFields.format == "hdf5", "Only hdf5 RESTful volumes are supported so far."
+        
+        outputShape = tuple( descriptionFields.shape )
+        axes = descriptionFields.axes
+        assert False not in map(lambda a: a in 'txyzc', axes), "Unknown axis type.  Known axes: txyzc  Your axes:".format(axes)
+
+        self._datasetName = descriptionFields.hdf5_dataset
+        assert self._datasetName is not None, "RESTful volume description file must specify the hdf5_dataset name"
+
+#        # If the dataset has no channel axis, add one.
+#        if 'c' not in axes:
+#            outputShape += (1,)
+#            axes += 'c'
+
+        self._axes = axes
+        axistags = vigra.defaultAxistags(self._axes)
+        
+        self._origin_offset = numpy.array(descriptionFields.origin_offset)
+
+        self.Output.meta.shape = outputShape
+        self.Output.meta.dtype = descriptionFields.dtype
+        self.Output.meta.axistags = axistags
+
+    def execute(self, slot, subindex, roi, result):
+
+        
+        accessStart = numpy.array(roi.start)
+        accessStart += self._origin_offset
+        accessStop = numpy.array(roi.stop)
+        accessStop += self._origin_offset
+
+        axistags = self.Output.meta.axistags
+        RESTArgs = {}
+        for axisindex, tag in enumerate(axistags):
+            startKey = '{}_start'.format(tag.key)
+            stopKey = '{}_stop'.format(tag.key)
+            RESTArgs[startKey] = accessStart[ axisindex ]
+            RESTArgs[stopKey] = accessStop[ axisindex ]
+
+        # Open the url
+        url = self._urlFormat.format( **RESTArgs )
+        hdf5FileBytes = urllib2.urlopen( url, timeout=10 )
+
+        # Write the data from the url out to disk (in a temporary file)
+        hdf5RawHandle, hdf5FilePath = tempfile.mkstemp()
+        rawFileToWrite = os.fdopen( hdf5RawHandle )
+        rawFileToWrite.write( hdf5FileBytes )
+        rawFileToWrite.close()
+
+        # Open the file we just created using h5py
+        with h5py.File( hdf5FilePath, 'r' ) as hdf5File:
+            result[...] = hdf5File[self._hdf5_dataset]        
+        return result
+
+    def propagateDirty(self, slot, subindex, roi):
+        assert slot == self.DescriptionFilePath, "Unknown input slot."
+        self.Output.setDirty( slice(None) )
+
+if __name__ == "__main__":
+    testConfig = """
+{
+    "name" : "Bock11-level0",
+    "format" : "hdf5",
+    "axes" : "zxy",
+    "shape" : [1239, 135424, 119808],
+    "dtype" : "numpy.uint8",
+    "offset_address" : [2917, 0, 0],
+    "url_format" : "http://openconnecto.me/emca/bock11/hdf5/0/{x_start},{x_stop}/{y_start},{y_stop}/{z_start},{z_stop}/",
+    "hdf5_dataset" : "cube"
+}
+"""
+
+    import os
+    from lazyflow.graph import Graph
+    
+    descriptionFilePath = os.path.join(tempfile.mkdtemp(), 'desc.json')
+    with open(descriptionFilePath, 'w') as descFile:
+        descFile.write( testConfig )
+    
+    graph = Graph()
+    op = OpRESTfulVolumeReader(graph=graph)
+    op.DescriptionFilePath.setValue( descriptionFilePath )
+    
+    data = op.Output[0:100, 50000:50200, 50000:50200].wait()
+    assert data.shape == ( 100, 200, 200 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
