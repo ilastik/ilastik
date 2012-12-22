21432f503f0dc9b6bd62fbbf6b19c3a1ff8e7b87
Stuart Berg <bergs@janelia.hhmi.org>
2012-12-22 17:56:00 -0500

    wip: blockwise filesets

--- lazyflow/operators/ioOperators/opBlockwiseFilesetReader.py
+++ lazyflow/operators/ioOperators/opBlockwiseFilesetReader.py
@@ -5,21 +5,21 @@ import numpy
 import h5py
 import vigra
 from lazyflow.graph import Operator, InputSlot, OutputSlot
+from lazyflow.roi import roiToSlice
 from lazyflow.jsonConfig import JsonConfigSchema, AutoEval, FormattedField
 
 import logging
 logger = logging.getLogger(__name__)
 
-OpBlockwiseFilesetDescriptionFields = \
+BlockwiseFilesetDescriptionFields = \
 {
     "name" : str,
     "format" : str,
     "axes" : str,
     "shape" : list,
     "dtype" : AutoEval(),
-    "block_side" : { 't' : 1, 'x' : 500, 'y' : 500, 'z' : 500, 'c' : 100 },
-    "block_file_name_format" : "cube{roi}.h5/volume/data",
-    "hdf5_dataset" : str
+    "block_dims" : dict,
+    "block_file_name_format" : FormattedField( requiredFields=["roiString"] )
 }
 
 class OpBlockwiseFilesetReader(Operator):
@@ -34,6 +34,7 @@ class OpBlockwiseFilesetReader(Operator):
         super(OpBlockwiseFilesetReader, self).__init__(*args, **kwargs)
         self._configSchema = JsonConfigSchema( OpBlockwiseFilesetReader )
         self._axes = None
+        self._blockshape = None
 
     def setupOutputs(self):
         # Read the dataset description file
@@ -49,58 +50,136 @@ class OpBlockwiseFilesetReader(Operator):
         self._urlFormat = descriptionFields.url_format
         self._origin_offset = numpy.array(descriptionFields.origin_offset)
         self._block_file_name_format = descriptionFields.block_file_name_format
+        self._blockshape = map( lambda key: descriptionFields.block_dims[key], descriptionFields.axes )
 
         outputShape = tuple( descriptionFields.shape )
 
-        # If the dataset has no channel axis, add one.
-        if 'c' not in axes:
-            outputShape += (1,)
-            self._axes += 'c'
-            self._origin_offset = numpy.array( list(self._origin_offset) + [0] )
+#        # If the dataset has no channel axis, add one to the output as a convenience.
+#        if 'c' not in axes:
+#            outputShape += (1,)
+#            self._axes += 'c'
+#            self._origin_offset = numpy.array( list(self._origin_offset) + [0] )
 
         self.Output.meta.shape = outputShape
         self.Output.meta.dtype = descriptionFields.dtype
         self.Output.meta.axistags = vigra.defaultAxistags(self._axes)
 
     def execute(self, slot, subindex, roi, result):
-        accessStart = numpy.array(roi.start)
-        accessStart += self._origin_offset
-        accessStop = numpy.array(roi.stop)
-        accessStop += self._origin_offset
+        # Get the list of blocks we need to access
+        blockstarts = self.getIntersectingBlocks( (roi.start, roi.stop) )
 
-        axistags = self.Output.meta.axistags
-        RESTArgs = {}
-        for axisindex, tag in enumerate(axistags):
-            startKey = '{}_start'.format(tag.key)
-            stopKey = '{}_stop'.format(tag.key)
-            RESTArgs[startKey] = accessStart[ axisindex ]
-            RESTArgs[stopKey] = accessStop[ axisindex ]
+        # TODO: Parallelize this loop
+        for blockstart in blockstarts:
+            blockroi = self.getBlockIntersection( blockstart, (roi.start, roi.stop) )
+            resultSlice = roiToSlice(blockstart + blockroi[0], blockstart + blockroi[1])
+            
+            hdf5Dataset = self.getDatasetForBlock( blockstart )
+            result[ resultSlice ] = hdf5Dataset[ roiToSlice(blockroi[0], blockroi[1]) ]
 
-        # Open the url
-        url = self._urlFormat.format( **RESTArgs )
-        logger.debug( "Downloading region {}..{}: {}".format(roi.start, roi.stop, url) )
-        hdf5RawFileObject = urllib2.urlopen( url, timeout=10 )
-
-        # Write the data from the url out to disk (in a temporary file)
-        hdf5FilePath = os.path.join(tempfile.mkdtemp(), 'cube.h5')
-        with open(hdf5FilePath, 'w') as rawFileToWrite:
-            rawFileToWrite.write( hdf5RawFileObject.read() )
-
-        # Open the file we just created using h5py
-        with h5py.File( hdf5FilePath, 'r' ) as hdf5File:
-            dataset = hdf5File[self._hdf5_dataset]
-            if len(result.shape) > len(dataset.shape):
-                result[...,0] = dataset[...]
-            else:
-                result[...] = dataset[...]
-        return result
+    
+        expectedFilePath = self._block_file_name_format.format(  )
 
     def propagateDirty(self, slot, subindex, roi):
         assert slot == self.DescriptionFilePath, "Unknown input slot."
         self.Output.setDirty( slice(None) )
+        
+    def getBlockIntersection(self, blockstart, roi):
+        return getBlockIntersection(self.Output.meta.shape, self._blockshape, blockstart, roi)
+    
+    def getIntersectingBlocks(self, roi):
+        return getIntersectingBlocks(self._blockshape, roi)
+    
+    def getDatasetForBlock(self, blockstart):
+        baseDir = os.path.split( self.DescriptionFilePath.value )[0]
+        datasetDir = getDatasetDirectory(baseDir)
+        datasetPath = os.path.join( self._block_file_name_format.format( roiString=str(blockstart) ) )
+        
+
+def getDatasetDirectory(baseDir, axes, blockstart ):
+    datasetDir = baseDir
+    for axis, start in zip(axes, blockstart):
+        blockFilePath = os.path.join( datasetDir, "{}_{:08d}".format( axis, start ) )
+    return blockFilePath
+
+def getBlockIntersection(outershape, blockshape, blockstart, roi):
+    """
+    Clip the given roi until it fits within a particular block region.
+    The block is specified via blockshape and blockstart.
+    Also clip the result to outershape, which is a 
+    """
+    # Get all needed shapes, etc. as numpy arrays for easy manipulation 
+    outershape = numpy.array( outershape )
+    blockshape = numpy.array( blockshape )
+    blockstart = numpy.array( blockstart )
+    blockstop = blockstart + blockshape
+    roistart = numpy.array( roi[0] )
+    roistop = numpy.array( roi[1] )    
+    outerstart = numpy.zeros( (len(outershape)), dtype=int )
+
+    # Clip to outer bounds
+    roistart = numpy.maximum( roistart, outerstart )
+    roistop = numpy.minimum( roistop, outershape )
+
+    # Clip to inner bounds
+    roistart = numpy.maximum( roistart, blockstart )    
+    roistop = numpy.minimum( roistop, blockstop )
+
+    assert numpy.prod( roistop - roistart ) > 0, "Roi {} does not intersect this block.  Why are you asking for the intersection?".format(roi)    
+    return (roistart, roistop)
+
+def getIntersectingBlocks( blockshape, roi ):
+    """
+    Returns the start coordinate of each block that the given roi intersects.
+    For example:
+
+    >>> getIntersectingBlocks( (10, 20), [(15, 25),(23, 40)] )
+    array([[10, 20],
+           [20, 20]])
+
+    >>> getIntersectingBlocks( (10, 20), [(15, 25),(23, 41)] )
+    array([[10, 20],
+           [10, 40],
+           [20, 20],
+           [20, 40]]) 
+
+    """
+    roistart = numpy.array( roi[0] )
+    roistop = numpy.array( roi[1] )
+    blockshape = numpy.array( blockshape )
+    
+    block_index_map_start = roistart / blockshape
+    block_index_map_stop = ( roistop + (blockshape - 1) ) / blockshape # Add (blockshape-1) first as a faster alternative to ceil() 
+    block_index_map_shape = block_index_map_stop - block_index_map_start
+    
+    num_axes = len(blockshape)
+    block_indices = numpy.indices( block_index_map_shape )
+    block_indices = numpy.rollaxis( block_indices, 0, num_axes+1 )
+    block_indices += block_index_map_start
+
+    indices_shape = block_indices.shape
+    indices_list = numpy.reshape( block_indices, (numpy.prod(indices_shape[0:-1]), indices_shape[-1]) )
+
+    # Multiply by blockshape to get the list of start coordinates
+    return (indices_list * blockshape)
 
 
+if __name__ == "__main__":
+    testConfig = \
+"""
+{
+    "name" : "gigacube",
+    "format" : "hdf5",
+    "axes" : "txyzc",
+    "shape" : [1, 1020, 1020, 1020, 1],
+    "dtype" : "numpy.uint8"
+    "block_dims" : { 't' : 1, 'x' : 500, 'y' : 500, 'z' : 500, 'c' : 1 }
+    "block_file_name_format" : "cube{roiString}.h5/volume/data",
+}
+"""
 
+inter = getIntersectingBlocks( (10,10,10), [(25, 15, 0), (45, 55, 10)] )
+print inter.shape
+print inter
 
 
 
