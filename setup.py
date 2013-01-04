331618c1280c3ff1c0eeaa3d56299617d009a8e5
Stuart Berg <bergs@janelia.hhmi.org>
2013-01-04 16:14:52 -0500

    autocontext: setup.py: Added FIXME notes for how to build on Mac.

--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@
 import os
 import vigra
 
+# FIXME: If using a special python prefix, CMake will probably find the 
+#        WRONG boost_python/vigra/python libs and headers.
+#        You may have to launch cmake-gui to select the correct paths by hand.
+
+# FIXME: On Mac, the build process produces a file called cppcontext.dylib.
+#        Python apparently expects it to be named cppcontext.so.
+#        Simply renaming the file seems to fix the problem.
+
 build='build'
 context='context'
 
