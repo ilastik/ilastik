e6a1c43ca9516a7a1bd5e1c4cd97d06778f27aa2
Stuart Berg <bergs@janelia.hhmi.org>
2013-01-07 18:04:40 -0500

    Attempting to fix travis: Try to fix vigra build by adding virtual env to path when building vigra.

--- requirements/install_vigra.sh
+++ requirements/install_vigra.sh
@@ -3,7 +3,9 @@
 git clone http://github.com/ukoethe/vigra /tmp/vigra
 mkdir -p /tmp/vigra/build
 cd /tmp/vigra/build
-cmake ..
-make install
+
+ENV_STRING='PATH=$VIRTUAL_ENV/bin:$PATH LD_LIBRARY_PATH=$VIRTUAL_ENV/lib:$LD_LIBRARY_PATH'
+$ENV_STRING cmake ..
+$ENV_STRING make install
 
 
