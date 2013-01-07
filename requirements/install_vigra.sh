d2dff3162f7dfec7eccfd64ace39ace93746c236
Stuart Berg <bergs@janelia.hhmi.org>
2013-01-07 18:32:36 -0500

    Still trying to fix travis...

--- requirements/install_vigra.sh
+++ requirements/install_vigra.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/bash -v
 
 git clone http://github.com/ukoethe/vigra /tmp/vigra
 mkdir -p /tmp/vigra/build
 cd /tmp/vigra/build
+
+echo "VIRTUAL_ENV is $VIRTUAL_ENV"
+
+export VIRTUAL_ENV=$1
 
 export PATH=$VIRTUAL_ENV/bin:$PATH
 export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib:$LD_LIBRARY_PATH
