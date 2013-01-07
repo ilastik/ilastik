ef3ebb64189114fbfcbcf8f821aa72aa2e0bba66
Stuart Berg <bergs@janelia.hhmi.org>
2013-01-07 17:29:06 -0500

    Attempting to fix travis: testeach script did not work as intended.

--- tests/testeach.sh
+++ tests/testeach.sh
@@ -6,5 +6,7 @@ for f in `find $NOSE_ARG -iname "*test*.py"`
 do
   echo "Running $f"; python ./nose_single.py --nologcapture $f
   RETVAL=$?
-  [ $RETVAL -ne 0 ] && exit $RETVAL
+  if [[ $RETVAL -ne 0 ]]; then
+    exit $RETVAL
+  fi
 done
