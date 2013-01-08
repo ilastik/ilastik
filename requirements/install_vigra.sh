d674aa26e6e52da3fb435715834d68ef537b6fda
Stuart Berg <bergs@janelia.hhmi.org>
2013-01-08 15:19:43 -0500

    Travis: vigra: Manually specify paths to python and boost python libraries.

--- requirements/install_vigra.sh
+++ requirements/install_vigra.sh
@@ -10,7 +10,7 @@ echo "VIRTUAL_ENV is $VIRTUAL_ENV"
 export PATH=$VIRTUAL_ENV/bin:$PATH
 export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib:$LD_LIBRARY_PATH
 
-cmake -DDEPENDENCY_SEARCH_PREFIX=$VIRTUAL_ENV ..
+cmake -DDEPENDENCY_SEARCH_PREFIX=$VIRTUAL_ENV -DVIGRANUMPY_LIBRARIES="/usr/lib/libpython2.7.so;/usr/lib/libboost_python.so" ..
 make install
 
 
