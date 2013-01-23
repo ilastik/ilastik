0de25674103e9be31f7a69079444fd7e3b74d98d
Stuart Berg <bergs@janelia.hhmi.org>
2013-01-22 19:44:27 -0500

    roiRequestBatch: Rewritten to benefit from the request_rewrite callback timing guarantee (smooth progress).

--- lazyflow/utility/roiRequestBatch.py
+++ lazyflow/utility/roiRequestBatch.py
@@ -7,6 +7,8 @@ from lazyflow.utility import OrderedSignal
 import itertools
 import lazyflow.stype
 
+from lazyflow.request import RequestLock
+
 class RoiRequestBatch( object ):
     """
     A simple utility for requesting a list of rois from an output slot.
@@ -16,6 +18,7 @@ class RoiRequestBatch( object ):
     def __init__( self, outputSlot, roiIterator, totalVolume=None, batchSize=10 ):
         """
         Constructor.
+
         :param outputSlot: The slot to request data from.
         :param roiIterator: An iterator providing new rois.
         :param totalVolume: The total volume to be processed.  Used to provide the progress reporting signal.  If not provided, then no intermediate progress will be signaled.
@@ -30,7 +33,9 @@ class RoiRequestBatch( object ):
         self._outputSlot = outputSlot
         self._roiIter = roiIterator
         self._batchSize = batchSize
+
         self._activeRequests = collections.deque()
+        self._lock = RequestLock()
 
         # Progress bookkeeping
         self._totalVolume = totalVolume
@@ -45,28 +50,9 @@ class RoiRequestBatch( object ):
             self._activateNewRequest()
 
         # Wait for each request in FIFO order.  
-        # When each is finished, pull it off the "active requests" queue and replace it with a new request until there are none left.
-        #
-        # NOTE: This is extremely non-optimal.
-        #       We are simply waiting for the oldest active request, but requests may finish in an arbitrary (non-FIFO) order.
-        #       What we would *like* is to put the queue operations inside a request notify_finished callback handler,
-        #       but that is complicated by the fact that request.wait() is allowed to return BEFORE the callbacks are finished.
-        #       A future version of the request system will fix this, so we'll wait for that instead of hand-optimizing this function today.
         roi, next_request = self._popOldestActiveRequest()
         while next_request is not None:
             next_request.wait()
-
-            # Replace with new work
-            self._activateNewRequest()
-            
-            # Signal the user with the result
-            self.resultSignal(roi, next_request.result)
-
-            # Report progress (if possible)
-            if self._totalVolume is not None:
-                self._processedVolume += numpy.prod( roi[1] - roi[0] )
-                progress =  100 * self._processedVolume / self._totalVolume
-                self.progressSignal( progress )
 
             # Get next request to wait for
             roi, next_request = self._popOldestActiveRequest()
@@ -79,22 +65,39 @@ class RoiRequestBatch( object ):
         If the active request queue is not empty, return a roi and its request (in FIFO order).
         Otherwise, return (None,None)
         """
-        if len(self._activeRequests) > 0:
-            return self._activeRequests.popleft()
-        else:
-            return (None, None)
+        with self._lock:
+            if len(self._activeRequests) > 0:
+                return self._activeRequests.popleft()
+            else:
+                return (None, None)
+
+    def _handleCompletedRequest(self, roi, completed_request, result):
+        # Add a new request to the batch to replace this finished one.
+        self._activateNewRequest()
+
+        with self._lock:
+            # Signal the user with the result
+            self.resultSignal(roi, completed_request.result)
+
+            # Report progress (if possible)
+            if self._totalVolume is not None:
+                self._processedVolume += numpy.prod( roi[1] - roi[0] )
+                progress =  100 * self._processedVolume / self._totalVolume
+                self.progressSignal( progress )
 
     def _activateNewRequest(self):
         """
         Creates and activates a new request if there are more rois to process.  Otherwise, does nothing.
         """
-        try:
-            roi = self._roiIter.next()
-        except StopIteration:
-            pass
-        else:
-            req = self._outputSlot( roi[0], roi[1] )
-            self._activeRequests.append( (roi, req) )
-            req.submit()
+        with self._lock:
+            try:
+                roi = self._roiIter.next()
+            except StopIteration:
+                pass
+            else:
+                req = self._outputSlot( roi[0], roi[1] )
+                self._activeRequests.append( (roi, req) )
+                req.notify_finished( partial( self._handleCompletedRequest, roi, req ) )
+                req.submit()
 
 
