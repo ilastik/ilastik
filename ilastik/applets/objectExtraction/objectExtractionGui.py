df39af4b80e3f6cd9c94febebbbb8605b9f050ee
Kemal Eren <kemal.eren@iwr.uni-heidelberg.de>
2013-02-13 12:00:52 +0100

    completed renaming of buttons in object extraction gui

--- ilastik/applets/objectExtraction/objectExtractionGui.py
+++ ilastik/applets/objectExtraction/objectExtractionGui.py
@@ -21,13 +21,13 @@ traceLogger = logging.getLogger('TRACE.' + __name__)
 class ObjectExtractionGui( QWidget ):
     """
     """
-   
+
     ###########################################
     ### AppletGuiInterface Concrete Methods ###
     ###########################################
 
     def centralWidget( self ):
-        """ Return the widget that will be displayed in the main viewer area. """ 
+        """ Return the widget that will be displayed in the main viewer area. """
         return self.volumeEditorWidget
 
     def appletDrawer( self ):
@@ -38,13 +38,13 @@ class ObjectExtractionGui( QWidget ):
 
     def viewerControlWidget( self ):
         return self._viewerControlWidget
- 
+
     def stopAndCleanUp( self ):
         pass
-  
+
     ###########################################
     ###########################################
-    
+
     def __init__(self, topLevelOperatorView):
         """
         """
@@ -87,7 +87,7 @@ class ObjectExtractionGui( QWidget ):
         mainOperator = self.mainOperator
 
         ct = [QColor(0,0,0,0).rgba(), QColor(0,0,255,255).rgba()] # blue foreground on transparent background
-        self.binaryimagesrc = LazyflowSource( mainOperator.BinaryImage )        
+        self.binaryimagesrc = LazyflowSource( mainOperator.BinaryImage )
         self.binaryimagesrc.setObjectName("Binary LazyflowSrc")
         layer = ColortableLayer( self.binaryimagesrc, ct )
         layer.name = "Binary Image"
@@ -107,9 +107,9 @@ class ObjectExtractionGui( QWidget ):
         layer.name = "Object Centers"
         layer.visible = False
         self.layerstack.append( layer )
-        
+
         ## raw data layer
-        self.rawsrc = None        
+        self.rawsrc = None
         self.rawsrc = LazyflowSource( self.mainOperator.RawImage )
         self.rawsrc.setObjectName("Raw Lazyflow Src")
         layerraw = GrayscaleLayer( self.rawsrc )
@@ -117,20 +117,20 @@ class ObjectExtractionGui( QWidget ):
         self.layerstack.insert( len(self.layerstack), layerraw )
 
         mainOperator.RawImage.notifyReady( self._onReady )
-        mainOperator.RawImage.notifyMetaChanged( self._onMetaChanged )           
+        mainOperator.RawImage.notifyMetaChanged( self._onMetaChanged )
 
         if mainOperator.BinaryImage.meta.shape:
             self.editor.dataShape = mainOperator.BinaryImage.meta.shape
         mainOperator.BinaryImage.notifyMetaChanged( self._onMetaChanged )
 
- 
+
     def _initEditor(self):
         """
         Initialize the Volume Editor GUI.
         """
 
         self.editor = VolumeEditor(self.layerstack)
-        
+
         self.volumeEditorWidget = VolumeEditorWidget()
         self.volumeEditorWidget.init(self.editor)
 
@@ -138,7 +138,7 @@ class ObjectExtractionGui( QWidget ):
         model = self.editor.layerStack
         model.canMoveSelectedUp.connect(self._viewerControlWidget.UpButton.setEnabled)
         model.canMoveSelectedDown.connect(self._viewerControlWidget.DownButton.setEnabled)
-        model.canDeleteSelected.connect(self._viewerControlWidget.DeleteButton.setEnabled)     
+        model.canDeleteSelected.connect(self._viewerControlWidget.DeleteButton.setEnabled)
 
         # Connect our layer movement buttons to the appropriate layerstack actions
         self._viewerControlWidget.layerWidget.init(model)
@@ -147,17 +147,17 @@ class ObjectExtractionGui( QWidget ):
         self._viewerControlWidget.DeleteButton.clicked.connect(model.deleteSelected)
 
         self.editor._lastImageViewFocus = 0
-        
 
-            
+
+
     def _initAppletDrawerUi(self):
         # Load the ui file (find it in our own directory)
         localDir = os.path.split(__file__)[0]
         self._drawer = uic.loadUi(localDir+"/drawer.ui")
 
         self._drawer.labelImageButton.pressed.connect(self._onLabelImageButtonPressed)
-        self._drawer.extractObjectsButton.pressed.connect(self._onExtractObjectsButtonPressed)        
-        
+        self._drawer.calculateFeaturesButton.pressed.connect(self._calculateFeaturesButtonPressed)
+
 #        self._drawer.doAllButton.pressed.connect(self._onDoAllButtonPressed)
 
 
@@ -177,9 +177,9 @@ class ObjectExtractionGui( QWidget ):
 
         self.mainOperator._opLabelImage._fixed = False
         reqs = []
-        for t in range(maxt):            
+        for t in range(maxt):
             reqs.append(self.mainOperator._opLabelImage.LabelImageComputation([t]))
-            reqs[-1].submit()            
+            reqs[-1].submit()
 
         for i, req in enumerate(reqs):
             progress.setValue(i)
@@ -187,18 +187,18 @@ class ObjectExtractionGui( QWidget ):
                 req.cancel()
             else:
                 req.wait()
-                
-        progress.setValue(maxt)        
-        
+
+        progress.setValue(maxt)
+
         roi = SubRegion(self.mainOperator.LabelImage, start=5*(0,), stop=m.shape)
         self.mainOperator.LabelImage.setDirty(roi)
-        
+
         print 'Label Segmentation: done.'
 
 
-    def _onExtractObjectsButtonPressed( self ):
-        maxt = self.mainOperator.LabelImage.meta.shape[0] 
-        progress = QProgressDialog("Extracting objects...", "Stop", 0, maxt)
+    def _calculateFeaturesButtonPressed( self ):
+        maxt = self.mainOperator.LabelImage.meta.shape[0]
+        progress = QProgressDialog("Calculating featuers...", "Stop", 0, maxt)
         progress.setWindowModality(Qt.ApplicationModal)
         progress.setMinimumDuration(0)
         progress.setCancelButtonText(QString())
@@ -208,24 +208,22 @@ class ObjectExtractionGui( QWidget ):
         for t in range(maxt):
             reqs.append(self.mainOperator.RegionFeatures([t]))
             reqs[-1].submit()
-            
+
         for i, req in enumerate(reqs):
             progress.setValue(i)
             if progress.wasCanceled():
                 req.cancel()
             else:
                 req.wait()
-                
-        self.mainOperator._opRegFeats.fixed = True         
+
+        self.mainOperator._opRegFeats.fixed = True
         progress.setValue(maxt)
-        
-        self.mainOperator.ObjectCenterImage.setDirty( SubRegion(self.mainOperator.ObjectCenterImage))        
-                
+
+        self.mainOperator.ObjectCenterImage.setDirty( SubRegion(self.mainOperator.ObjectCenterImage))
+
         print 'Object Extraction: done.'
 
-#        
-#    def _onDoAllButtonPressed(self):    
+#
+#    def _onDoAllButtonPressed(self):
 #        self._onLabelImageButtonPressed()
-#        self._onExtractObjectsButtonPressed()
-        
-        
+#        self._calculateFeaturesButtonPressed()
