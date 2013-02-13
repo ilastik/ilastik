a5cbbf6d39b024ac7a867381cee4f89c7449acfe
Kemal Eren <kemal.eren@iwr.uni-heidelberg.de>
2013-02-13 13:47:51 +0100

    object extraction serializer supports multiple images.
    WARNING: breaks backwards compatibility with existing project files.

--- ilastik/applets/objectExtraction/objectExtractionSerializer.py
+++ ilastik/applets/objectExtraction/objectExtractionSerializer.py
@@ -3,52 +3,63 @@ from ilastik.applets.base.appletSerializer import AppletSerializer,\
 
 class ObjectExtractionSerializer(AppletSerializer):
     def __init__(self, mainOperator, projectFileGroupName):
-        super( ObjectExtractionSerializer, self ).__init__( projectFileGroupName )
+        super(ObjectExtractionSerializer, self).__init__(projectFileGroupName)
         self.mainOperator = mainOperator
 
     def _serializeToHdf5(self, topGroup, hdf5File, projectFilePath):
-        op = self.mainOperator.innerOperators[0]
         print "object extraction: serializeToHdf5", topGroup, hdf5File, projectFilePath
         print "object extraction: saving label image"
-        deleteIfPresent( topGroup, "LabelImage")
-        op._opLabelImage._mem_h5.copy('/LabelImage', topGroup)
+        deleteIfPresent(topGroup, "LabelImage")
+        group = getOrCreateGroup(topGroup, "LabelImage")
+        for i, op in enumerate(self.mainOperator.innerOperators):
+            src = op._opLabelImage._mem_h5
+            group.copy(src['/LabelImage'], group, name=str(i))
 
         print "object extraction: saving region features"
-        deleteIfPresent( topGroup, "samples")
-        samples_gr = getOrCreateGroup( topGroup, "samples" )
-        for t in op._opRegFeats._cache.keys():
-            t_gr = samples_gr.create_group(str(t))
-            for ch in range(len(op._opRegFeats._cache[t])):
-                ch_gr = t_gr.create_group(str(ch))
-                feats = op._opRegFeats._cache[t][ch]
-                for key, val in feats.iteritems():
-                    ch_gr.create_dataset(name=key, data=val)
+        deleteIfPresent(topGroup, "samples")
+        samples_gr = getOrCreateGroup(topGroup, "samples")
+        for i, op in enumerate(self.mainOperator.innerOperators):
+            opgroup = getOrCreateGroup(samples_gr, str(i))
+            for t in op._opRegFeats._cache.keys():
+                t_gr = opgroup.create_group(str(t))
+                for ch in range(len(op._opRegFeats._cache[t])):
+                    ch_gr = t_gr.create_group(str(ch))
+                    feats = op._opRegFeats._cache[t][ch]
+                    for key, val in feats.iteritems():
+                        ch_gr.create_dataset(name=key, data=val)
 
 
     def _deserializeFromHdf5(self, topGroup, groupVersion, hdf5File, projectFilePath):
         print "objectExtraction: deserializeFromHdf5", topGroup, groupVersion, hdf5File, projectFilePath
 
         print "objectExtraction: loading label image"
-        op = self.mainOperator.innerOperators[0]
 
         if 'LabelImage' in topGroup.keys():
-            dest = op._opLabelImage._mem_h5
-            del dest['LabelImage']
-            topGroup.copy('LabelImage', dest)
-            op._opLabelImage._fixed = False
-            op._opLabelImage._processedTimeSteps = range(topGroup['LabelImage'].shape[0])
+            opgroup = topGroup['LabelImage']
+            self.mainOperator.LabelImage.resize(len(opgroup))
+            for inner in opgroup.keys():
+                op = self.mainOperator.innerOperators[int(inner)]
+                dest = op._opLabelImage._mem_h5
+                del dest['LabelImage']
+                dest.copy(opgroup[inner], dest, name='LabelImage')
+                op._opLabelImage._fixed = False
+                op._opLabelImage._processedTimeSteps = range(opgroup[inner].shape[0])
 
         print "objectExtraction: loading region features"
         if "samples" in topGroup.keys():
-            cache = {}
-            for t in topGroup["samples"].keys():
-                cache[int(t)] = []
-                for ch in sorted(topGroup["samples"][t].keys()):
-                    feat = dict()
-                    for key in topGroup["samples"][t][ch].keys():
-                        feat[key] = topGroup["samples"][t][ch][key].value
-                    cache[int(t)].append(feat)
-            self.mainOperator.innerOperators[0]._opRegFeats._cache = cache
+            opgroup = topGroup['samples']
+            for inner in opgroup.keys():
+                gr = opgroup[inner]
+                op = self.mainOperator.innerOperators[int(inner)]
+                cache = {}
+                for t in gr.keys():
+                    cache[int(t)] = []
+                    for ch in sorted(gr[t].keys()):
+                        feat = dict()
+                        for key in gr[t][ch].keys():
+                            feat[key] = gr[t][ch][key].value
+                        cache[int(t)].append(feat)
+                op._opRegFeats._cache = cache
 
     def isDirty(self):
         return True
