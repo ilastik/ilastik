bf3cdd69bc0789201d3b9b0f8269d176308a6d9d
Kemal Eren <kemal.eren@iwr.uni-heidelberg.de>
2013-02-13 14:58:16 +0100

    command-line options for object workflow

--- workflows/objectClassification/objectClassificationWorkflowMainGui.py
+++ workflows/objectClassification/objectClassificationWorkflowMainGui.py
@@ -1,17 +1,32 @@
 from ilastik.shell.gui.startShellGui import startShellGui
-from objectClassificationWorkflow import ObjectClassificationWorkflow
-
-
-def debug_with_existing(shell, workflow):
-    """
-    (Function for debug and testing.)
-    """
-    # set this variable
-    projFilePath = ""
-    # Open a project
-    shell.openProjectFile(projFilePath)
+from objectClassificationWorkflow import ObjectClassificationWorkflow as pixel_workflow
+from objectClassificationWorkflowBinary import ObjectClassificationWorkflow as binary_workflow
 
 
 if __name__=="__main__":
-    #startShellGui( ObjectClassificationWorkflow, debug_with_existing )
-    startShellGui(ObjectClassificationWorkflow)
+
+    from optparse import OptionParser
+    usage = "%prog [options] filename"
+    parser = OptionParser(usage)
+    parser.add_option("-b", "--binary",
+                      action="store_true",
+                      dest="binary",
+                      default=False,
+                      help="use binary workflow")
+
+    (options, args) = parser.parse_args()
+
+    if options.binary:
+        workflow = binary_workflow
+    else:
+        workflow = pixel_workflow
+
+    # Start the GUI
+    if len(args) == 1:
+        def loadProject(shell):
+            shell.openProjectFile(args[0])
+        startShellGui(workflow, loadProject)
+    elif len(args) == 0:
+        startShellGui(workflow)
+    else:
+        parser.error("incorrect number of arguments")
