d60e1ab95ebb2e929710798f7fadfc79983c3b28
Stuart Berg <bergs@janelia.hhmi.org>
2013-02-28 14:42:47 -0500

    OpInterpMissingData: Fixed formatting and imports.

--- lazyflow/operators/opInterpMissingData.py
+++ lazyflow/operators/opInterpMissingData.py
@@ -1,92 +1,92 @@
 from lazyflow.graph import Operator, InputSlot, OutputSlot
 import numpy as np
 
-   
+    
 class OpInterpMissingData(Operator):
 
-   name = "OpInterpMissingData"
+    name = "OpInterpMissingData"
 
-   InputVolume = InputSlot()
-   Output = OutputSlot()
+    InputVolume = InputSlot()
+    Output = OutputSlot()
 
-   def __init__(self, *args, **kwargs):
-      super(OpInterpMissingData, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(OpInterpMissingData, self).__init__(*args, **kwargs)
 
-   def propagateDirty(self, slot, subindex, roi):
-      self.Output.setDirty(roi)
+    def propagateDirty(self, slot, subindex, roi):
+        self.Output.setDirty(roi)
  
-   def setupOutputs(self):
+    def setupOutputs(self):
 
-      self.Output.meta.assignFrom( self.InputVolume.meta )
-      self.Output.meta.dtype=np.uint8
-      shape = list(self.InputVolume.meta.shape)
-      self.Output.meta.shape = tuple(shape)
-      pass
+        self.Output.meta.assignFrom( self.InputVolume.meta )
+        self.Output.meta.dtype=np.uint8
+        shape = list(self.InputVolume.meta.shape)
+        self.Output.meta.shape = tuple(shape)
+        pass
 
-   def execute(self, slot, subindex, roi, result):
-      data = self.InputVolume.get(roi).wait()
-      data = self.InterpMissingLayer(data)
-      result=data
-      return result
+    def execute(self, slot, subindex, roi, result):
+        data = self.InputVolume.get(roi).wait()
+        data = self.InterpMissingLayer(data)
+        result=data
+        return result
 
-   def InterpMissingLayer(self, data):
-      fl=sum(sum(data))==0 #Full Layer Array
+    def InterpMissingLayer(self, data):
+        fl=sum(sum(data))==0 #Full Layer Array
 
 
 
-      #Interpolate First Block
-      pos=-1
-      if fl[0]==1:
-         for i in range(fl.shape[0]):
-            if fl[i]==0:
-               pos=i
-               break
-      data[:,:,0:pos+1]=self.FirstBlock(data[:,:,0:pos+1])
+        #Interpolate First Block
+        pos=-1
+        if fl[0]==1:
+            for i in range(fl.shape[0]):
+                if fl[i]==0:
+                    pos=i
+                    break
+        data[:,:,0:pos+1]=self.FirstBlock(data[:,:,0:pos+1])
  
 
 
-      #Interpolate Center Leyers
-      pos0=0
-      pos1=0
-      for i in range(fl.shape[0]-1):
-         if fl[i]==0 and fl[i-1]==1:
-            pos1=i
-            if pos0!=0 and pos1!=0:
-               data[:,:,pos0:pos1+1]=self.CenterBlock(data[:,:,pos0:pos1+1])
-         if fl[i]==0 and fl[i+1]==1:
-            pos0=i
+        #Interpolate Center Leyers
+        pos0=0
+        pos1=0
+        for i in range(fl.shape[0]-1):
+            if fl[i]==0 and fl[i-1]==1:
+                pos1=i
+                if pos0!=0 and pos1!=0:
+                    data[:,:,pos0:pos1+1]=self.CenterBlock(data[:,:,pos0:pos1+1])
+            if fl[i]==0 and fl[i+1]==1:
+                pos0=i
 
 
 
-      #Interpolate Last Block 
-      if fl[fl.shape[0]-1]==1:
-         for i in range(fl.shape[0]):
-            i_rev=fl.shape[0]-i-1
-            if fl[i_rev]==0:
-               pos=i_rev
-               break
-      data[:,:,pos:data.shape[2]]=self.LastBlock(data[:,:,pos:data.shape[2]])
+        #Interpolate Last Block 
+        if fl[fl.shape[0]-1]==1:
+            for i in range(fl.shape[0]):
+                i_rev=fl.shape[0]-i-1
+                if fl[i_rev]==0:
+                    pos=i_rev
+                    break
+        data[:,:,pos:data.shape[2]]=self.LastBlock(data[:,:,pos:data.shape[2]])
 
 
 
-      return data
-   
-   def FirstBlock(self,data):
-      for i in range(data.shape[2]-1):
-         data[:,:,i]=data[:,:,data.shape[2]-1]
-      return data
+        return data
     
-   def CenterBlock(self,sub_data):
-      sub_data= np.transpose(sub_data)
-      Total=sub_data.shape[0]-1
-      L_0=np.array(sub_data[0,:,:], dtype=np.float32)
-      L_1=np.array(sub_data[Total,:,:], dtype=np.float32) 
-      for t in range(Total+1):
-         Layer=(L_1*t+L_0*(Total-t))/Total  
-         sub_data[t,:,:]=Layer
-      return np.transpose(sub_data)
+    def FirstBlock(self,data):
+        for i in range(data.shape[2]-1):
+            data[:,:,i]=data[:,:,data.shape[2]-1]
+        return data
+     
+    def CenterBlock(self,sub_data):
+        sub_data= np.transpose(sub_data)
+        Total=sub_data.shape[0]-1
+        L_0=np.array(sub_data[0,:,:], dtype=np.float32)
+        L_1=np.array(sub_data[Total,:,:], dtype=np.float32) 
+        for t in range(Total+1):
+            Layer=(L_1*t+L_0*(Total-t))/Total  
+            sub_data[t,:,:]=Layer
+        return np.transpose(sub_data)
 
-   def LastBlock(self,data):
-      for i in range(data.shape[2]):
-         data[:,:,i]=data[:,:,0]
-      return data
+    def LastBlock(self,data):
+        for i in range(data.shape[2]):
+            data[:,:,i]=data[:,:,0]
+        return data
--- tests/testOpInterpMissingData.py
+++ tests/testOpInterpMissingData.py
@@ -1,58 +1,60 @@
-from lazyflow.graph import Operator, InputSlot, OutputSlot
 from lazyflow.graph import Graph
 
 import numpy as np
-import OpInterpMissingData as Oper
+from lazyflow.operators.opInterpMissingData import OpInterpMissingData
 
 class TestInterpMissingData(object):
-   def setUp(self):
-      
-      #Large Block + 1 Layer is missing
-      self.d1=np.ones((10,10,100))
-      for i in range(100): self.d1[:,:,i]*=(i+1)
-      self.d1[:,:,30:50]=0
-      self.d1[:,:,70]=0
-      
-
-      #Fist Block is missing
-      self.d2=np.ones((10,10,100))
-      for i in range(100): self.d2[:,:,i]*=(i+1)
-      self.d2[:,:,0:10]=0
-
-      #Last Layer is missing
-      self.d3=np.ones((10,10,100))
-      for i in range(100): self.d3[:,:,i]*=(i+1)
-      self.d3[:,:,99]=0
-
-
-
-   def testStuff(self):
-
-      Ones=np.ones((10,10))
-      g=Graph()
-      op = Oper.OpInterpMissingData(graph = g)
-
-
-      op.InputVolume.setValue( self.d1 )
-      
-      #Layer in center of large black Block
-      assert np.any(op.Output[:].wait()[:,:,40]==Ones*41)==True
-
-      #Single Layer
-      assert np.any(op.Output[:].wait()[:,:,70]==Ones*71)==True
-   
-
-      #First Layers 
-      op.InputVolume.setValue( self.d2 )
-      assert np.any(op.Output[:].wait()[:,:,4]==Ones*11)==True
-
-      #Last Layer
-      op.InputVolume.setValue( self.d3 )
-      assert np.any(op.Output[:].wait()[:,:,99]==Ones*99)==True
-     
-     
-     
+    def setUp(self):
         
+        #Large Block + 1 Layer is missing
+        self.d1=np.ones((10,10,100))
+        for i in range(100): self.d1[:,:,i]*=(i+1)
+        self.d1[:,:,30:50]=0
+        self.d1[:,:,70]=0
+        
+
+        #Fist Block is missing
+        self.d2=np.ones((10,10,100))
+        for i in range(100): self.d2[:,:,i]*=(i+1)
+        self.d2[:,:,0:10]=0
+
+        #Last Layer is missing
+        self.d3=np.ones((10,10,100))
+        for i in range(100): self.d3[:,:,i]*=(i+1)
+        self.d3[:,:,99]=0
+
+
+
+    def testStuff(self):
+
+        Ones=np.ones((10,10))
+        g=Graph()
+        op = OpInterpMissingData(graph = g)
+
+
+        op.InputVolume.setValue( self.d1 )
+        
+        #Layer in center of large black Block
+        assert np.any(op.Output[:].wait()[:,:,40]==Ones*41)==True
+
+        #Single Layer
+        assert np.any(op.Output[:].wait()[:,:,70]==Ones*71)==True
+    
+
+        #First Layers 
+        op.InputVolume.setValue( self.d2 )
+        assert np.any(op.Output[:].wait()[:,:,4]==Ones*11)==True
+
+        #Last Layer
+        op.InputVolume.setValue( self.d3 )
+        assert np.any(op.Output[:].wait()[:,:,99]==Ones*99)==True
+      
+      
+      
+          
 if __name__ == "__main__":
+    import sys
     import nose
-    nose.run(defaultTest=__file__, env={'NOSE_NOCAPTURE' : 1})
+    sys.argv.append("--nocapture")    # Don't steal stdout.  Show it on the console as usual.
+    sys.argv.append("--nologcapture") # Don't set the logging level to DEBUG.  Leave it alone.
+    nose.run(defaultTest=__file__)
