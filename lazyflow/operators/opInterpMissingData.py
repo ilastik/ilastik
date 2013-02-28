bb35343489a276958bb084f532a091c420b0503b
Stuart Berg <bergs@janelia.hhmi.org>
2013-02-28 16:00:26 -0500

    OpInterpMissingData: Works for 5D data and doesn't assume xyz axis order.

--- lazyflow/operators/opInterpMissingData.py
+++ lazyflow/operators/opInterpMissingData.py
@@ -1,38 +1,45 @@
 from lazyflow.graph import Operator, InputSlot, OutputSlot
 import numpy as np
-
+import vigra
     
 class OpInterpMissingData(Operator):
-
     name = "OpInterpMissingData"
 
     InputVolume = InputSlot()
     Output = OutputSlot()
 
-    def __init__(self, *args, **kwargs):
-        super(OpInterpMissingData, self).__init__(*args, **kwargs)
-
-    def propagateDirty(self, slot, subindex, roi):
-        self.Output.setDirty(roi)
- 
     def setupOutputs(self):
-
+        # Output has the same shape/axes/dtype/drange as input
         self.Output.meta.assignFrom( self.InputVolume.meta )
-        self.Output.meta.dtype=np.uint8
-        shape = list(self.InputVolume.meta.shape)
-        self.Output.meta.shape = tuple(shape)
-        pass
+        
+        # Check for errors
+        taggedShape = self.InputVolume.meta.getTaggedShape()
+        if 't' in taggedShape:
+            assert taggedShape['t'] == 1, "Non-spatial dimensions must be of length 1"
+        if 'c' in taggedShape:
+            assert taggedShape['c'] == 1, "Non-spatial dimensions must be of length 1"
 
     def execute(self, slot, subindex, roi, result):
         data = self.InputVolume.get(roi).wait()
-        data = self.InterpMissingLayer(data)
-        result=data
+        data = data.view( vigra.VigraArray )
+        data.axistags = self.InputVolume.meta.axistags
+        
+        self._interpMissingLayer(data.withAxes(*'xyz'))
+        result[:] = data
         return result
 
-    def InterpMissingLayer(self, data):
-        fl=sum(sum(data))==0 #Full Layer Array
-
-
+    def propagateDirty(self, slot, subindex, roi):
+        # TODO: This implementation of propagateDirty() isn't correct.
+        #       That's okay for now, since this operator will never be used with input data that becomes dirty.
+        self.Output.setDirty(roi)
+ 
+    def _interpMissingLayer(self, data):
+        """
+        Description: TODO
+        
+        :param data: Must be 3d, in xyz order.
+        """
+        fl=data.sum(0).sum(0)==0 #Full Layer Array
 
         #Interpolate First Block
         pos=-1
@@ -41,9 +48,7 @@ class OpInterpMissingData(Operator):
                 if fl[i]==0:
                     pos=i
                     break
-        data[:,:,0:pos+1]=self.FirstBlock(data[:,:,0:pos+1])
- 
-
+        data[:,:,0:pos+1]=self._firstBlock(data[:,:,0:pos+1])
 
         #Interpolate Center Leyers
         pos0=0
@@ -52,11 +57,9 @@ class OpInterpMissingData(Operator):
             if fl[i]==0 and fl[i-1]==1:
                 pos1=i
                 if pos0!=0 and pos1!=0:
-                    data[:,:,pos0:pos1+1]=self.CenterBlock(data[:,:,pos0:pos1+1])
+                    data[:,:,pos0:pos1+1]=self._centerBlock(data[:,:,pos0:pos1+1])
             if fl[i]==0 and fl[i+1]==1:
                 pos0=i
-
-
 
         #Interpolate Last Block 
         if fl[fl.shape[0]-1]==1:
@@ -65,28 +68,39 @@ class OpInterpMissingData(Operator):
                 if fl[i_rev]==0:
                     pos=i_rev
                     break
-        data[:,:,pos:data.shape[2]]=self.LastBlock(data[:,:,pos:data.shape[2]])
-
-
-
-        return data
+        data[:,:,pos:data.shape[2]]=self._lastBlock(data[:,:,pos:data.shape[2]])
     
-    def FirstBlock(self,data):
+    def _firstBlock(self,data):
+        """
+        Description: TODO
+        
+        :param data: Must be 3d, in xyz order.
+        """
         for i in range(data.shape[2]-1):
             data[:,:,i]=data[:,:,data.shape[2]-1]
         return data
      
-    def CenterBlock(self,sub_data):
-        sub_data= np.transpose(sub_data)
+    def _centerBlock(self,sub_data):
+        """
+        Description: TODO
+        
+        :param sub_data: Must be 3d, in xyz order.
+        """
+        sub_data = sub_data.transpose()
         Total=sub_data.shape[0]-1
         L_0=np.array(sub_data[0,:,:], dtype=np.float32)
         L_1=np.array(sub_data[Total,:,:], dtype=np.float32) 
         for t in range(Total+1):
             Layer=(L_1*t+L_0*(Total-t))/Total  
             sub_data[t,:,:]=Layer
-        return np.transpose(sub_data)
+        return sub_data.transpose()
 
-    def LastBlock(self,data):
+    def _lastBlock(self,data):
+        """
+        Description: TODO
+        
+        :param data: Must be 3d, in xyz order.
+        """
         for i in range(data.shape[2]):
             data[:,:,i]=data[:,:,0]
         return data
--- tests/testOpInterpMissingData.py
+++ tests/testOpInterpMissingData.py
@@ -1,38 +1,42 @@
 from lazyflow.graph import Graph
 
 import numpy as np
+import vigra
 from lazyflow.operators.opInterpMissingData import OpInterpMissingData
 
 class TestInterpMissingData(object):
+
     def setUp(self):
-        
         #Large Block + 1 Layer is missing
-        self.d1=np.ones((10,10,100))
-        for i in range(100): self.d1[:,:,i]*=(i+1)
-        self.d1[:,:,30:50]=0
-        self.d1[:,:,70]=0
+        d1 = vigra.VigraArray( np.ones((10,10,100)), axistags=vigra.defaultAxistags('xyz') )
+        for i in range(100): d1[:,:,i]*=(i+1)
+        d1[:,:,30:50]=0
+        d1[:,:,70]=0
         
 
         #Fist Block is missing
-        self.d2=np.ones((10,10,100))
-        for i in range(100): self.d2[:,:,i]*=(i+1)
-        self.d2[:,:,0:10]=0
+        d2=np.ones((10,10,100))
+        for i in range(100): d2[:,:,i]*=(i+1)
+        d2[:,:,0:10]=0
 
         #Last Layer is missing
-        self.d3=np.ones((10,10,100))
-        for i in range(100): self.d3[:,:,i]*=(i+1)
-        self.d3[:,:,99]=0
+        d3=np.ones((10,10,100))
+        for i in range(100): d3[:,:,i]*=(i+1)
+        d3[:,:,99]=0
 
+        self.d1 = d1
+        self.d2 = d2
+        self.d3 = d3
 
-
-    def testStuff(self):
+    def testBasic(self):
+        d1 = self.d1
+        d2 = self.d2
+        d3 = self.d3
 
         Ones=np.ones((10,10))
         g=Graph()
         op = OpInterpMissingData(graph = g)
-
-
-        op.InputVolume.setValue( self.d1 )
+        op.InputVolume.setValue( d1 )
         
         #Layer in center of large black Block
         assert np.any(op.Output[:].wait()[:,:,40]==Ones*41)==True
@@ -40,15 +44,39 @@ class TestInterpMissingData(object):
         #Single Layer
         assert np.any(op.Output[:].wait()[:,:,70]==Ones*71)==True
     
-
         #First Layers 
-        op.InputVolume.setValue( self.d2 )
+        op.InputVolume.setValue( d2 )
         assert np.any(op.Output[:].wait()[:,:,4]==Ones*11)==True
 
         #Last Layer
-        op.InputVolume.setValue( self.d3 )
+        op.InputVolume.setValue( d3 )
         assert np.any(op.Output[:].wait()[:,:,99]==Ones*99)==True
-      
+
+    def testAxesReversed(self):
+        d1 = self.d1.transpose()
+        d2 = self.d2.transpose()
+        d3 = self.d3.transpose()        
+
+        Ones=np.ones((10,10))
+        g=Graph()
+        op = OpInterpMissingData(graph = g)
+        op.InputVolume.setValue( d1 )
+        
+        #Layer in center of large black Block
+        assert np.any(op.Output[:].wait()[40,:,:]==Ones*41)==True
+
+        #Single Layer
+        assert np.any(op.Output[:].wait()[70,:,:]==Ones*71)==True
+    
+
+        #First Layers 
+        op.InputVolume.setValue( d2 )
+        assert np.any(op.Output[:].wait()[4,:,:]==Ones*11)==True
+
+        #Last Layer
+        op.InputVolume.setValue( d3 )
+        assert np.any(op.Output[:].wait()[99,:,:]==Ones*99)==True
+
       
       
           
