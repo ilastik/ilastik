106eb40640846f30d023c6635467cd4f8d5adc63
Stuart Berg <bergs@janelia.hhmi.org>
2013-03-05 21:59:39 -0500

    OpFilterLabels: Added an optional input slot to filter based on Max size, too.

--- lazyflow/operators/opFilterLabels.py
+++ lazyflow/operators/opFilterLabels.py
@@ -15,6 +15,7 @@ class OpFilterLabels(Operator):
 
     Input = InputSlot() 
     MinLabelSize = InputSlot()
+    MaxLabelSize = InputSlot(optional=True)
         
     Output = OutputSlot()
     
@@ -23,8 +24,11 @@ class OpFilterLabels(Operator):
         
     def execute(self, slot, subindex, roi, result):
         minSize = self.MinLabelSize.value
+        maxSize = None
+        if self.MaxLabelSize.ready():
+            maxSize = self.MaxLabelSize.value
         self.Input.get(roi, result).wait()
-        self.remove_small_connected_components(result, min_size=minSize, in_place=True)
+        self.remove_outlier_connected_components(result, min_size=minSize, max_size=maxSize, in_place=True)
         return result
         
     def propagateDirty(self, inputSlot, subindex, roi):
@@ -32,7 +36,7 @@ class OpFilterLabels(Operator):
         assert inputSlot == self.Input or inputSlot == self.MinLabelSize
         self.Output.setDirty( slice(None) )
 
-    def remove_small_connected_components(self, a, min_size, in_place):
+    def remove_outlier_connected_components(self, a, min_size, max_size, in_place):
         """
         Adapted from http://github.com/jni/ray/blob/develop/ray/morpho.py
         (MIT License)
@@ -43,8 +47,10 @@ class OpFilterLabels(Operator):
         if min_size == 0: # shortcut for efficiency
             return a
         component_sizes = numpy.bincount( a.ravel() )
-        too_small = component_sizes < min_size
-        too_small_locations = too_small[a]
-        a[too_small_locations] = 0
+        bad_sizes = component_sizes < min_size
+        if max_size is not None:
+            bad_sizes = numpy.logical_or( bad_sizes, component_sizes > max_size )
+        bad_locations = bad_sizes[a]
+        a[bad_locations] = 0
         return a.astype(original_dtype)
 
