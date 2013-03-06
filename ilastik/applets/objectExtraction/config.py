0b08a2d00339e8278a901d1d380884b309e11f0f
Anna Kreshuk <anna.kreshuk@iwr.uni-heidelberg.de>
2013-03-06 16:59:19 +0100

    change the config file for the correct features

--- ilastik/applets/objectExtraction/config.py
+++ ilastik/applets/objectExtraction/config.py
@@ -1,11 +1,9 @@
 # FIXME: use ilastik config file
 
-compress_labels = True
+compress_labels = False
 
-# all these features are precalculated in opExtractObjects
-vigra_features = ['Count']
-
-# only these features are used. eventually these will be chosen
-# interactively. They many include features not in 'vigra_features',
-# in the case that some other features are also used.
-selected_features = ['Count']
+# which features to use for classification
+vigra_features = ['Count', 'Mean', 'Variance', 'Skewness', 'Kurtosis']
+selected_features = ['Count', 'Mean_obj', 'Mean_excl', 'Variance_obj', \
+                 'Variance_excl', 'Skewness_obj', 'Skewness_excl', \
+                 'Kurtosis_obj', 'Kurtosis_excl']
