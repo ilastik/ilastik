bf58e72507195b50eccdc991eebfd4afd86dfac2
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2013-03-15 15:32:28 +0100

    carving: fix loading

--- ilastik/workflows/carving/opCarving.py
+++ ilastik/workflows/carving/opCarving.py
@@ -410,7 +410,6 @@ class OpCarving(Operator):
         self.HasSegmentation.setValue(False)
 
         objects = self._mst.object_names.keys()
-        print "save: len = ", len(objects)
         self.AllObjectNames.meta.shape = len(objects)
 
         #now that 'name' is no longer part of the set of finished objects, rebuild the done overlay
@@ -555,18 +554,13 @@ class OpCarving(Operator):
 
         elif slot == self.CarvingGraphFile:
             if self._mst is not None:
+                objects = self._mst.object_names.keys()
+                self.AllObjectNames.meta.shape = len(objects)
                 #if the carving graph file is not valid, all outputs must be invalid
                 for output in self.outputs.values():
+                    if output is self.AllObjectNames:
+                        continue
                     output.setDirty(slice(0,None))
-
-            #FIXME: currently, the carving graph file is loaded in the constructor
-            #       refactor such that it is only loaded here
-            '''
-            fname = self.CarvingGraphFile.value
-
-            self._mst = MSTSegmentor.loadH5(fname,  "graph")
-            print "[Carving id=%d] loading graph file %s (mst=%d)" % (id(self), fname, id(self._mst))
-            '''
 
             self.Segmentation.setDirty(slice(None))
 
