3cdbfcb77cf4796047dc37828d9a7c4bbd19c2ba
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2013-04-02 20:39:40 +0200

    added h5py to install requirements since it's required...

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires = ['greenlet', 'psutil', 'blist'],
+    install_requires = ['greenlet', 'psutil', 'blist', 'h5py'],
 
     package_data = {
         'lazyflow': ['*.txt', '*.py'],
