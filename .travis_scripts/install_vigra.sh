9e9c2b7585f99bd44e25dd2c87631a5ce2d74a07
Bernhard X. Kausler <bernhard.kausler@iwr.uni-heidelberg.de>
2013-04-03 13:25:05 +0200

    travis

--- .travis_scripts/install_vigra.sh
+++ .travis_scripts/install_vigra.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash -v
 
 git clone http://github.com/ukoethe/vigra /tmp/vigra
 mkdir -p /tmp/vigra/build
 cd /tmp/vigra/build
-cmake ..
+
+export VIRTUAL_ENV=$1
+echo "VIRTUAL_ENV is $VIRTUAL_ENV"
+
+export PATH=$VIRTUAL_ENV/bin:$PATH
+export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib:$LD_LIBRARY_PATH
+
+cmake -DDEPENDENCY_SEARCH_PREFIX=$VIRTUAL_ENV -DVIGRANUMPY_LIBRARIES="/usr/lib/libpython2.7.so;/usr/lib/libboost_python.so" ..
 make install
 
 
