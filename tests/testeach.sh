538230d96ee7619b9f1513ef4d8bf1665f76c8a6
Stuart Berg <bergs@janelia.hhmi.org>
2013-04-07 23:58:54 +0200

    tests: Don't skip tests that are now passing again

--- tests/testeach.sh
+++ tests/testeach.sh
@@ -4,33 +4,8 @@ NOSE_ARG=${1-"."}
 
 for f in `find $NOSE_ARG -iname "*test*.py"`
 do
-  if [[ $f == "./test_applets/thresholdTwoLevels/testThresholdTwoLevels.py" ]]; then
-      echo "Skipping $f which is known to fail"
-      continue
-  fi
-
   if [[ $f == "./test_applets/pixelClassification/testPixelClassificationBenchmarking.py" ]]; then
       echo "Skipping $f because is takes too long"
-      continue
-  fi
-
-  if [[ $f == "./test_applets/base/testSerializer.py" ]]; then
-      echo "Skipping $f which is known to fail"
-      continue
-  fi
-
-  if [[ $f == "./test_applets/featureSelection/testFeatureSelectionSerializer.py" ]]; then
-      echo "Skipping $f which is known to fail"
-      continue
-  fi
-
-  if [[ $f == "./test_applets/objectExtraction/testOperators.py" ]]; then
-      echo "Skipping $f which is known to fail"
-      continue
-  fi
-
-  if [[ $f == "./test_applets/objectClassification/testOperators.py" ]]; then
-      echo "Skipping $f which is known to fail"
       continue
   fi
 
