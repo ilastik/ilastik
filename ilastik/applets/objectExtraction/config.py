818e8e0d11190343843a969c68d128ea00ae3643
Buote Xu <Buote.Xu@gmail.com>
2013-04-08 15:37:46 +0200

    made lbp resp. skimage optional

--- ilastik/applets/objectExtraction/config.py
+++ ilastik/applets/objectExtraction/config.py
@@ -15,6 +15,16 @@ other_features = ['lbp', 'lbp_incl', 'lbp_excl', 'bad_slices']
 selected_features = ['Count', 'Mean', 'Mean_excl', 'Variance', \
                      'Variance_excl', 'Skewness', 'Skewness_excl', \
                       'Kurtosis', 'Kurtosis_excl', 'lbp', 'lbp_excl']
+has_skimage = True
+try:
+    import skimage.feature
+except:
+    has_skimage = False
+    print "scikit-image is not installed, some features disabled"
+if not has_skimage:
+    selected_features = [feature for feature in selected_features if "lbp" not
+                         in feature]
+
 
 #selected_features = ['lbp_excl', 'lbp_obj']
 
