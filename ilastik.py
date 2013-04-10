4c0d694869fc05e010c57387c5b9f939f8890b2d
bheuer <ben-heuer@web.de>
2013-04-10 10:37:01 +0200

    work on path input of ilastik.py

--- ilastik.py
+++ ilastik.py
@@ -16,7 +16,15 @@ if len(args)==0:
     startShellGui(workflowClass=options.workflow)
 
 elif len(args)==1:
+    
+    from ilastik.utility.pathHelpers import PathComponents
+    
+    #convert path to convenient format
+    path = PathComponents(args[0]).totalPath()
+    
     def loadProject(shell):
-        shell.openProjectFile(args[0])
+        shell.openProjectFile(path)
     workflowClass = getWorkflowFromName(options.workflow)
     startShellGui(workflowClass,loadProject)
+else:
+    raise TypeError("please pass either no argument for showing the start screen or one argument which is a ilastik project file for opening a project directly")
