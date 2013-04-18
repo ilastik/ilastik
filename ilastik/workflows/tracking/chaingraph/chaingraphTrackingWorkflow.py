f3666d559cc6d9b886e66c9bce4632b45a63153a
mschiegg <martin.schiegg@iwr.uni-heidelberg.de>
2013-04-18 17:39:43 +0200

    tracking workflow includes thresholding applet now to start directly from predictions rather than segmentation

--- ilastik/workflows/tracking/chaingraph/chaingraphTrackingWorkflow.py
+++ ilastik/workflows/tracking/chaingraph/chaingraphTrackingWorkflow.py
@@ -3,6 +3,8 @@ from ilastik.workflow import Workflow
 from ilastik.applets.dataSelection import DataSelectionApplet
 from ilastik.applets.objectExtraction import ObjectExtractionApplet
 from ilastik.applets.tracking.chaingraph.chaingraphTrackingApplet import ChaingraphTrackingApplet
+from ilastik.applets.thresholdTwoLevels.thresholdTwoLevelsApplet import ThresholdTwoLevelsApplet
+from lazyflow.operators.adaptors import Op5ifyer
 
 
 class ChaingraphTrackingWorkflow( Workflow ):
@@ -18,14 +20,18 @@ class ChaingraphTrackingWorkflow( Workflow ):
                                                        "Input: Raw",
                                                        "Input Raw",
                                                        batchDataGui=False,
-                                                       force5d=True)
+                                                       force5d=False)
         
         self.dataSelectionApplet = DataSelectionApplet(self,
-                                                       "Input: Segmentation",
-                                                       "Input Segmentation",
+                                                       "Input: Prediction",
+                                                       "Input Prediction",
                                                        batchDataGui=False,
-                                                       force5d=True)
+                                                       force5d=False)
                 
+        self.thresholdTwoLevelsApplet = ThresholdTwoLevelsApplet( self, 
+                                                                  "Threshold & Size Filter", 
+                                                                  "ThresholdTwoLevels" )
+        
         self.objectExtractionApplet = ObjectExtractionApplet( workflow=self, interactive=False )
         
         self.trackingApplet = ChaingraphTrackingApplet( workflow=self )
@@ -33,6 +39,7 @@ class ChaingraphTrackingWorkflow( Workflow ):
         self._applets = []                
         self._applets.append(self.rawDataSelectionApplet)
         self._applets.append(self.dataSelectionApplet)
+        self._applets.append(self.thresholdTwoLevelsApplet)
         self._applets.append(self.objectExtractionApplet)
         self._applets.append(self.trackingApplet)
 
@@ -47,15 +54,29 @@ class ChaingraphTrackingWorkflow( Workflow ):
     def connectLane( self, laneIndex ):
         opData = self.dataSelectionApplet.topLevelOperator.getLane(laneIndex)
         opRawData = self.rawDataSelectionApplet.topLevelOperator.getLane(laneIndex)
+        opTwoLevelThreshold = self.thresholdTwoLevelsApplet.topLevelOperator.getLane(laneIndex)
         opObjExtraction = self.objectExtractionApplet.topLevelOperator.getLane(laneIndex)
         opTracking = self.trackingApplet.topLevelOperator.getLane(laneIndex)
-        
+                
         ## Connect operators ##
-        opObjExtraction.RawImage.connect( opRawData.Image )
-        opObjExtraction.BinaryImage.connect( opData.Image )
-#        opObjExtraction.BackgroundLabels.setValue( [0,] )
-
-        opTracking.RawImage.connect( opRawData.Image )
+        op5Raw = Op5ifyer(parent=self)
+        op5Raw.input.connect(opRawData.Image)
+        
+        op5Predictions = Op5ifyer( parent=self )
+        op5Predictions.input.connect( opData.Image )
+               
+        opTwoLevelThreshold.InputImage.connect( opData.Image )
+        opTwoLevelThreshold.RawInput.connect( opRawData.Image ) # Used for display only
+        
+        # Use Op5ifyers for both input datasets such that they are guaranteed to 
+        # have the same axis order after thresholding
+        op5Binary = Op5ifyer( parent=self )                
+        op5Binary.input.connect( opTwoLevelThreshold.CachedOutput )
+        
+        opObjExtraction.RawImage.connect( op5Raw.output )
+        opObjExtraction.BinaryImage.connect( op5Binary.output )
+        
+        opTracking.RawImage.connect( op5Raw.output )
         opTracking.LabelImage.connect( opObjExtraction.LabelImage )
         opTracking.ObjectFeatures.connect( opObjExtraction.RegionFeatures )        
         
