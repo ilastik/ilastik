b24d70943862d55b8ca710e7f5a3f36a4175de22
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2013-04-21 15:00:13 +0200

    delete old OpStackWriter

--- lazyflow/operators/ioOperators/ioOperators.py
+++ lazyflow/operators/ioOperators/ioOperators.py
@@ -107,65 +107,6 @@ class OpStackLoader(Operator):
             result[...,i,:] = vigra.impex.readImage(fileName)[key[0],key[1],key[3]]
             i = i+1
 
-
-class OpStackWriter(Operator):
-    name = "Stack File Writer"
-    category = "Output"
-
-    inputSlots = [InputSlot("filepath", stype = "string"),
-                  InputSlot("dummy", stype = "list"),
-                  InputSlot("input")]
-    outputSlots = [OutputSlot("WritePNGStack")]
-
-    def setupOutputs(self):
-        assert self.inputs['input'].meta.getAxisKeys() == ['t', 'x', 'y', 'z', 'c']
-        assert self.inputs['input'].meta.shape is not None
-        self.outputs["WritePNGStack"].meta.shape = self.inputs['input'].meta.shape
-        self.outputs["WritePNGStack"].meta.dtype = object
-
-    def execute(self, slot, subindex, roi, result):
-        image = self.inputs["input"][roi.toSlice()].wait()
-
-        filepath = self.inputs["filepath"].value
-        filepath = filepath.split(".")
-        filetype = filepath[-1]
-        filepath = filepath[0:-1]
-        filepath = "/".join(filepath)
-        dummy = self.inputs["dummy"].value
-
-        if "xy" in dummy:
-            pass
-        if "xz" in dummy:
-            pass
-        if "xt" in dummy:
-            for i in range(image.shape[2]):
-                for j in range(image.shape[3]):
-                    for k in range(image.shape[4]):
-                        vigra.impex.writeImage(image[:,:,i,j,k],
-                                               filepath+"-xt-y_%04d_z_%04d_c_%04d." % (i,j,k)+filetype)
-        if "yz" in dummy:
-            for i in range(image.shape[0]):
-                for j in range(image.shape[1]):
-                    for k in range(image.shape[4]):
-                        vigra.impex.writeImage(image[i,j,:,:,k],
-                                               filepath+"-yz-t_%04d_x_%04d_c_%04d." % (i,j,k)+filetype)
-        if "yt" in dummy:
-            for i in range(image.shape[1]):
-                for j in range(image.shape[3]):
-                    for k in range(image.shape[4]):
-                        vigra.impex.writeImage(image[:,i,:,j,k],
-                                               filepath+"-yt-x_%04d_z_%04d_c_%04d." % (i,j,k)+filetype)
-        if "zt" in dummy:
-            for i in range(image.shape[1]):
-                for j in range(image.shape[2]):
-                    for k in range(image.shape[4]):
-                        vigra.impex.writeImage(image[:,i,j,:,k],
-                                               filepath+"-zt-x_%04d_y_%04d_c_%04d." % (i,j,k)+filetype)
-
-    def propagateDirty(self, slot, subindex, roi):
-        self.WritePNGStack.setDirty(slice(None))
-
-
 class OpStackWriter(Operator):
     name = "Stack File Writer"
     category = "Output"
