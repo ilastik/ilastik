472110053eaf9f5573d6c576a2bb201d0dce965c
Stuart Berg <bergs@janelia.hhmi.org>
2013-05-17 17:19:29 -0400

    Removed obsolete *MainGui.py launch files.  Use ilastik.py for all workflow guis.

--- ilastik/workflows/predictionViewer/predictionViewerWorkflowMainGui.py
+++
@@ -1,5 +0,0 @@
-from ilastik.shell.gui.startShellGui import startShellGui
-from predictionViewerWorkflow import PredictionViewerWorkflow
-
-if __name__ == "__main__":
-    startShellGui( PredictionViewerWorkflow )
