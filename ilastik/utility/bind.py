30fcace0743787a3408470acdf11d45239daeacf
Kemal Eren <kemal.eren@iwr.uni-heidelberg.de>
2013-05-27 13:36:12 +0200

    bind now subclasses tuple, so it is hashable and immutable

--- ilastik/utility/bind.py
+++ ilastik/utility/bind.py
@@ -6,36 +6,39 @@ def getRootArgSpec(f):
     else:
         return inspect.getargspec(f)
 
-class bind(object):
-    """
+class bind(tuple):
+    """Behaves like functools.partial, but discards any extra
+    parameters it gets when called. Also, bind objects can be compared
+    for equality and hashed.
+
+    bind objects are immutable.
+
     Inspired by boost::bind (C++).
-    Behaves like functools.partial, but discards any extra parameters it gets when called.
-    Also, bind objects can be compared for equality.
+
     """
-    def __init__(self, f, *args):
-        """
-        Create a callable for the partial invocation of f using the specified args.
-        """
-        self.f = f
-        self.bound_args = args
+    def __new__(cls, f, *args):
+        bound_args = args
         expected_args = getRootArgSpec(f).args
-        self.numUnboundArgs = len(expected_args) - len(self.bound_args)
+        numUnboundArgs = len(expected_args) - len(bound_args)
         if len(expected_args) > 0 and expected_args[0] == 'self':
-            self.numUnboundArgs -= 1
+            numUnboundArgs -= 1
+        return tuple.__new__(cls, (f, bound_args, numUnboundArgs))
+
+    @property
+    def f(self):
+        return self[0]
+
+    @property
+    def bound_args(self):
+        return self[1]
+
+    @property
+    def numUnboundArgs(self):
+        return self[2]
+
     def __call__(self, *args):
-        """
-        When execute the callback.  If more args are provided than the callback accepts, silently discard the extra args.
+        """Execute the callback. If more args are provided than
+        the callback accepts, silently discard the extra args.
+
         """
         self.f(*(self.bound_args + args[0:self.numUnboundArgs]))
-
-    def __eq__(self, other):
-        if type(self) != type(other):
-            return False
-        result = True
-        result &= ( self.f == other.f )
-        result &= ( self.bound_args == other.bound_args )
-        result &= ( self.numUnboundArgs == other.numUnboundArgs )
-        return result
-    
-    def __ne__(self, other):
-        return not ( self == other )
