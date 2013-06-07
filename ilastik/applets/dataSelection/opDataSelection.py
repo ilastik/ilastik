58a93ddd8e30a43d43c91a5ef0220ca55ee85770
bheuer <ben-heuer@web.de>
2013-06-07 21:10:04 +0200

    make normalization optional to fix #408, #405

--- ilastik/applets/dataSelection/opDataSelection.py
+++ ilastik/applets/dataSelection/opDataSelection.py
@@ -22,6 +22,7 @@ class DatasetInfo(object):
         self._datasetId = ""                # The name of the data within the project file (if it is stored locally)
         self.allowLabels = True             # Whether or not this dataset should be used for training a classifier.
         self.drange = None
+        self.normalizeDisplay = False
         self.fromstack = False
         self.nickname = ""
         self.axistags = None
@@ -109,11 +110,12 @@ class OpDataSelection(Operator):
             opReader.FilePath.setValue(datasetInfo.filePath)
             providerSlot = opReader.Output
             self._opReaders.append(opReader)
-
+        
         # Inject metadata if the dataset info specified any.
-        if datasetInfo.drange or datasetInfo.axistags is not None:
+        if datasetInfo.normalizeDisplay is not None or datasetInfo.drange or datasetInfo.axistags is not None:
             metadata = {}
             metadata['drange'] = datasetInfo.drange
+            metadata['normalizeDisplay'] = datasetInfo.normalizeDisplay
             if datasetInfo.axistags is not None:
                 metadata['axistags'] = datasetInfo.axistags
             opMetadataInjector = OpMetadataInjector( parent=self )
@@ -138,7 +140,7 @@ class OpDataSelection(Operator):
             op5.input.connect( providerSlot )
             providerSlot = op5.output
             self._opReaders.append( op5 )
-
+        
         # Connect our external outputs to the internal operators we chose
         self.Image.connect(providerSlot)
         
