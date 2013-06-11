41bbfa679001e92a21e78d23240518c699418120
Stuart Berg <bergs@janelia.hhmi.org>
2013-06-11 13:06:04 -0400

    SplitBodyCarving: Override _update_rendering() from Carving base class so that only (masked) fragments and the current segmentation are shown.

--- ilastik/applets/splitBodyCarving/splitBodyCarvingGui.py
+++ ilastik/applets/splitBodyCarving/splitBodyCarvingGui.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from functools import partial
 import numpy
@@ -10,12 +11,15 @@ from volumina.layer import ColortableLayer
 from volumina.pixelpipeline.datasources import LazyflowSource
 
 from ilastik.workflows.carving.carvingGui import CarvingGui
+from lazyflow.request import Request
 
 from opSplitBodyCarving import OpSplitBodyCarving
 
 from bodySplitInfoWidget import BodySplitInfoWidget
 
 from ilastik.applets.labeling.labelingGui import Tool
+
+from ilastik.utility.gui import threadRouted
 
 class SplitBodyCarvingGui(CarvingGui):
     
@@ -36,6 +40,22 @@ class SplitBodyCarvingGui(CarvingGui):
         self._labelControlUi.saveControlLabel.hide()
         self._labelControlUi.save.hide()
         self._labelControlUi.saveAs.hide()
+
+        fragmentColors = [ QColor(0,0,0,0), # transparent (background)
+                           QColor(0, 255, 255),   # cyan
+                           QColor(255, 0, 255),   # magenta
+                           QColor(255, 165, 0),   # orange
+                           QColor(173, 255,  47), # green-yellow
+                           QColor(255, 105, 180), # hot pink
+                           QColor(165,  42,  42), # brown        
+                           QColor(0, 0, 128),     # navy
+                           QColor(102, 205, 170), # dark aquamarine
+                           QColor(128,0, 128),    # purple
+                           QColor(192, 192, 192), # silver
+                           QColor(240, 230, 140), # khaki
+                           QColor(69, 69, 69) ]   # dark grey
+
+        self._fragmentColors = fragmentColors
 
         # In this workflow, you aren't allowed to make brushstrokes unless there is a "current fragment"
         def handleEditingFragmentChange(slot, *args):
@@ -79,6 +99,76 @@ class SplitBodyCarvingGui(CarvingGui):
     # - Repeat, but now show raveler object excluding pieces that have been cut off, and show next split point (possibly in that same raveler object)
     # - Deleting an object means that it's pixels will automatically go back to the raveler object's pixels
     
+    def _update_rendering(self):
+        """
+        Override from the base class.
+        """
+        # This update has to be performed in a different thread to avoid a deadlock
+        # (Because this function is running in the context of a dirty notification!)
+        req = Request( self.__update_rendering )        
+        def handle_rendering_failure( exc, exc_info ):
+            import traceback
+            traceback.print_exception(*exc_info)
+            sys.stderr.write("Exception raised during volume rendering update.  See traceack above.\n")
+        req.notify_failed( handle_rendering_failure )
+        req.submit()
+    
+    def __update_rendering(self):
+        if not self.render:
+            return
+
+        fragmentColors = self._fragmentColors
+        op = self.topLevelOperatorView
+        if not self._renderMgr.ready:
+            self._renderMgr.setup(op.InputData.meta.shape[1:4])
+
+        self._renderMgr.clear()
+        # Create a 5D view of the render mgr's memory
+        renderVol5d = self._renderMgr.volume[numpy.newaxis, ..., numpy.newaxis]
+
+        ravelerLabel = op.CurrentRavelerLabel.value
+        if ravelerLabel != 0:
+            op.CurrentFragmentSegmentation[:].writeInto(renderVol5d).wait()
+
+            fragmentNames = op.getSavedObjectNamesForRavelerLabel(ravelerLabel)
+            numFragments = len(fragmentNames)
+
+            print "BLABLA 0"
+            renderLabels = []
+            for i, name in enumerate(fragmentNames):
+                if name != op.CurrentEditingFragment.value:
+                    print "BLABLA 0.{}".format(i)
+                    assert i < len(self._fragmentColors), "Too many fragments: colortable is too small"
+                    color = ( fragmentColors[i+1].red() / 255.0,
+                              fragmentColors[i+1].green() / 255.0,
+                              fragmentColors[i+1].blue() / 255.0 )
+                    renderLabel = self._renderMgr.addObject( color=color )
+                    renderLabels.append( renderLabel )
+
+            if op.CurrentEditingFragment.value != "":
+                print "BLABLA 1"
+                maskedSegmentation = op.MaskedSegmentation[:].wait()
+                segLabel = numFragments
+                renderVol5d[:] = numpy.where(maskedSegmentation != 0, segLabel, renderVol5d)
+
+                segmentationColor = (0.0, 1.0, 0.0)
+                renderLabel = self._renderMgr.addObject( color=segmentationColor )
+                renderLabels.append( renderLabel )
+
+            print "BLABLA 2"
+            # Relabel with the labels we were given by the renderer.
+            # (We can skip this step if the renderer is guaranteed to give labels 1,2,3...)
+            if renderLabels != list(range(len(renderLabels))):
+                renderVol5d[:] = numpy.array([0] + renderLabels)[renderVol5d]
+
+        print "BLABLA 3"
+        self._refreshRenderMgr()
+
+    @threadRouted    
+    def _refreshRenderMgr(self):
+        self._renderMgr.update()    
+
+    
     def setupLayers(self):
         def findLayer(f, layerlist):
             for l in layerlist:
@@ -111,12 +201,7 @@ class SplitBodyCarvingGui(CarvingGui):
 
         fragmentSegSlot = self.topLevelOperatorView.CurrentFragmentSegmentation
         if fragmentSegSlot.ready():
-            colortable = [QColor(0, 0, 0, 0).rgba(),
-                          QColor(255, 255, 0).rgba(),
-                          QColor(0, 255, 255).rgba(),
-                          QColor(255, 0, 255).rgba(),
-                          QColor(255, 255, 255).rgba(),
-                          ]
+            colortable = map(QColor.rgba, self._fragmentColors)
             fragSegLayer = ColortableLayer(LazyflowSource(fragmentSegSlot), colortable, direct=True)
             fragSegLayer.name = "Saved Fragments"
             fragSegLayer.visible = True
