e1c97cce50c80716df38fbf7c8ff399de57635f6
Stuart Berg <bergs@janelia.hhmi.org>
2013-06-21 14:22:20 -0400

    Workflow: Changed signature of onProjectLoaded()

--- ilastik/workflow.py
+++ ilastik/workflow.py
@@ -59,7 +59,7 @@ class Workflow( Operator ):
         """
         raise NotImplementedError
     
-    def onProjectLoaded(self):
+    def onProjectLoaded(self, projectManager):
         """
         Called by the project manager after the project is loaded (deserialized).
         Extra workflow initialization be done here.
