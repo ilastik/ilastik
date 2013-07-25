76b1ad8d9156905b86aa1c930abcb8449e726647
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2013-07-25 17:07:44 +0200

    potential install fix for cylemon

--- .travis_scripts/install_cylemon.sh
+++ .travis_scripts/install_cylemon.sh
@@ -3,6 +3,12 @@
 rm -rf /tmp/cylemon
 git clone http://github.com/cstraehl/cylemon.git /tmp/cylemon
 
+export VIRTUAL_ENV=$1
+echo "VIRTUAL_ENV is $VIRTUAL_ENV"
+
+export PATH=$VIRTUAL_ENV/bin:$PATH
+export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib:$LD_LIBRARY_PATH
+
 cd /tmp/cylemon
 python setup.py config
 python setup.py build
