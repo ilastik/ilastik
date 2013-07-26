d20389d61c5bdf191d8ad8b6e46b3c68a3eaf5d7
vagrant <vagrant@precise64.(none)>
2013-07-26 07:47:32 +0000

    potential cylemon build fix

--- .travis_scripts/install_cylemon.sh
+++ .travis_scripts/install_cylemon.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -v
 
-rm -rf /tmp/cylemon
-git clone http://github.com/cstraehl/cylemon.git /tmp/cylemon
+mkdir -p $HOME/tmp
+rm -rf $HOME/tmp/cylemon
+git clone http://github.com/cstraehl/cylemon.git $HOME/tmp/cylemon
 
 export VIRTUAL_ENV=$1
 echo "VIRTUAL_ENV is $VIRTUAL_ENV"
@@ -9,9 +10,12 @@ echo "VIRTUAL_ENV is $VIRTUAL_ENV"
 export PATH=$VIRTUAL_ENV/bin:$PATH
 export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib:$LD_LIBRARY_PATH
 
-cd /tmp/cylemon
+cd $HOME/tmp/cylemon
+echo "*** setup.py config ***"
 python setup.py config
+echo "*** setup.py build ***"
 python setup.py build
-sudo python setup.py install
+echo "*** setup.py install ***"
+python setup.py install
 
 
