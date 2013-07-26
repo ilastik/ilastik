5877f32c73b2c12924acdbe36f6434f0278517a8
mschiegg <martin.schiegg@iwr.uni-heidelberg.de>
2013-07-26 18:19:20 +0200

    chaingraph tracking: running tracking computations and export in separate threads to prevent gui freezing

--- ilastik/applets/tracking/base/trackingUtilities.py
+++ ilastik/applets/tracking/base/trackingUtilities.py
@@ -67,48 +67,51 @@ def write_events(events, directory, t, labelImage, mergers=None):
     
         
         
-        # write only if file exists
-        with LineageH5(fn, 'a') as f_curr:
-            # delete old label image
-            if "segmentation" in f_curr.keys():
-                del f_curr["segmentation"]
-            
-            seg = f_curr.create_group("segmentation")            
-            # write label image
-            seg.create_dataset("labels", data = labelImage, dtype=np.uint32, compression=1)
-            
-            # delete old tracking
-            if "tracking" in f_curr.keys():
-                del f_curr["tracking"]
-
-            tg = f_curr.create_group("tracking")            
-            
-            # write associations
-            if len(app):
-                ds = tg.create_dataset("Appearances", data=app[:, :-1], dtype=np.uint32, compression=1)
-                ds.attrs["Format"] = "cell label appeared in current file"    
-                ds = tg.create_dataset("Appearances-Energy", data=app[:, -1], dtype=np.double, compression=1)
-                ds.attrs["Format"] = "lower energy -> higher confidence"    
-            if len(dis):
-                ds = tg.create_dataset("Disappearances", data=dis[:, :-1], dtype=np.uint32, compression=1)
-                ds.attrs["Format"] = "cell label disappeared in current file"
-                ds = tg.create_dataset("Disappearances-Energy", data=dis[:, -1], dtype=np.double, compression=1)
-                ds.attrs["Format"] = "lower energy -> higher confidence"    
-            if len(mov):
-                ds = tg.create_dataset("Moves", data=mov[:, :-1], dtype=np.uint32, compression=1)
-                ds.attrs["Format"] = "from (previous file), to (current file)"    
-                ds = tg.create_dataset("Moves-Energy", data=mov[:, -1], dtype=np.double, compression=1)
-                ds.attrs["Format"] = "lower energy -> higher confidence"                
-            if len(div):
-                ds = tg.create_dataset("Splits", data=div[:, :-1], dtype=np.uint32, compression=1)
-                ds.attrs["Format"] = "ancestor (previous file), descendant (current file), descendant (current file)"    
-                ds = tg.create_dataset("Splits-Energy", data=div[:, -1], dtype=np.double, compression=1)
-                ds.attrs["Format"] = "lower energy -> higher confidence"
-            if len(merger):
-                ds = tg.create_dataset("Mergers", data=merger[:, :-1], dtype=np.uint32, compression=1)
-                ds.attrs["Format"] = "descendant (current file), number of objects"    
-                ds = tg.create_dataset("Mergers-Energy", data=merger[:, -1], dtype=np.double, compression=1)
-                ds.attrs["Format"] = "lower energy -> higher confidence"
+        try:
+            with LineageH5(fn, 'w-') as f_curr:
+                # delete old label image
+                if "segmentation" in f_curr.keys():
+                    del f_curr["segmentation"]
+                
+                seg = f_curr.create_group("segmentation")            
+                # write label image
+                seg.create_dataset("labels", data = labelImage, dtype=np.uint32, compression=1)
+                
+                # delete old tracking
+                if "tracking" in f_curr.keys():
+                    del f_curr["tracking"]
+    
+                tg = f_curr.create_group("tracking")            
+                
+                # write associations
+                if len(app):
+                    ds = tg.create_dataset("Appearances", data=app[:, :-1], dtype=np.uint32, compression=1)
+                    ds.attrs["Format"] = "cell label appeared in current file"    
+                    ds = tg.create_dataset("Appearances-Energy", data=app[:, -1], dtype=np.double, compression=1)
+                    ds.attrs["Format"] = "lower energy -> higher confidence"    
+                if len(dis):
+                    ds = tg.create_dataset("Disappearances", data=dis[:, :-1], dtype=np.uint32, compression=1)
+                    ds.attrs["Format"] = "cell label disappeared in current file"
+                    ds = tg.create_dataset("Disappearances-Energy", data=dis[:, -1], dtype=np.double, compression=1)
+                    ds.attrs["Format"] = "lower energy -> higher confidence"    
+                if len(mov):
+                    ds = tg.create_dataset("Moves", data=mov[:, :-1], dtype=np.uint32, compression=1)
+                    ds.attrs["Format"] = "from (previous file), to (current file)"    
+                    ds = tg.create_dataset("Moves-Energy", data=mov[:, -1], dtype=np.double, compression=1)
+                    ds.attrs["Format"] = "lower energy -> higher confidence"                
+                if len(div):
+                    ds = tg.create_dataset("Splits", data=div[:, :-1], dtype=np.uint32, compression=1)
+                    ds.attrs["Format"] = "ancestor (previous file), descendant (current file), descendant (current file)"    
+                    ds = tg.create_dataset("Splits-Energy", data=div[:, -1], dtype=np.double, compression=1)
+                    ds.attrs["Format"] = "lower energy -> higher confidence"
+                if len(merger):
+                    ds = tg.create_dataset("Mergers", data=merger[:, :-1], dtype=np.uint32, compression=1)
+                    ds.attrs["Format"] = "descendant (current file), number of objects"    
+                    ds = tg.create_dataset("Mergers-Energy", data=merger[:, -1], dtype=np.double, compression=1)
+                    ds.attrs["Format"] = "lower energy -> higher confidence"
+        except IOError:                    
+            raise IOError("File " + str(fn) + " exists already. Please choose a different folder or delete the file(s).")
+                
     
         print "-> results successfully written"
 
--- ilastik/applets/tracking/chaingraph/chaingraphTrackingGui.py
+++ ilastik/applets/tracking/chaingraph/chaingraphTrackingGui.py
@@ -3,9 +3,11 @@ import os
 
 import logging
 from ilastik.applets.tracking.base.trackingBaseGui import TrackingBaseGui
+from lazyflow.request import Request
 import sys
 import traceback
 import re
+from ilastik.utility.gui.threadRouter import threadRouted
 
 logger = logging.getLogger(__name__)
 traceLogger = logging.getLogger('TRACE.' + __name__)
@@ -45,66 +47,89 @@ class ChaingraphTrackingGui( TrackingBaseGui ):
         self._allowedTimeoutInputRegEx = re.compile('^[0-9]*$')    
         self._drawer.timeoutBox.textChanged.connect(self._onTimeoutBoxChanged)
         
-                
-    def _onTimeoutBoxChanged(self):        
+    @threadRouted            
+    def _onTimeoutBoxChanged(self, *args):        
         inString = str(self._drawer.timeoutBox.text())
         if self._allowedTimeoutInputRegEx.match(inString) is None:
             self._drawer.timeoutBox.setText(inString.decode("utf8").encode("ascii", "replace")[:-1])
     
+    
     def _onTrackButtonPressed( self ):    
         if not self.mainOperator.ObjectFeatures.ready():
-            QtGui.QMessageBox.critical(self, "Error", "You have to select object features first.", QtGui.QMessageBox.Ok)
+            self._criticalMessage("You have to compute object features first.")            
             return
         
-        app = self._drawer.appSpinBox.value()
-        dis = self._drawer.disSpinBox.value()
-        opp = self._drawer.oppSpinBox.value()
-        noiserate = self._drawer.noiseRateSpinBox.value()
-        noiseweight = self._drawer.noiseWeightSpinBox.value()
-        epGap = self._drawer.epGapSpinBox.value()
-        n_neighbors = self._drawer.nNeighborsSpinBox.value()
-        with_div = self._drawer.withDivisionsBox.isChecked()
-        cplex_timeout = None
-        if len(str(self._drawer.timeoutBox.text())):
-            cplex_timeout = int(self._drawer.timeoutBox.text())
+        
+        def _track():
+            app = self._drawer.appSpinBox.value()
+            dis = self._drawer.disSpinBox.value()
+            opp = self._drawer.oppSpinBox.value()
+            noiserate = self._drawer.noiseRateSpinBox.value()
+            noiseweight = self._drawer.noiseWeightSpinBox.value()
+            epGap = self._drawer.epGapSpinBox.value()
+            n_neighbors = self._drawer.nNeighborsSpinBox.value()
+            with_div = self._drawer.withDivisionsBox.isChecked()
+            cplex_timeout = None
+            if len(str(self._drawer.timeoutBox.text())):
+                cplex_timeout = int(self._drawer.timeoutBox.text())
+                
+            from_t = self._drawer.from_time.value()
+            to_t = self._drawer.to_time.value()
+            from_x = self._drawer.from_x.value()
+            to_x = self._drawer.to_x.value()
+            from_y = self._drawer.from_y.value()
+            to_y = self._drawer.to_y.value()        
+            from_z = self._drawer.from_z.value()
+            to_z = self._drawer.to_z.value()        
+            from_size = self._drawer.from_size.value()
+            to_size = self._drawer.to_size.value()        
             
-        from_t = self._drawer.from_time.value()
-        to_t = self._drawer.to_time.value()
-        from_x = self._drawer.from_x.value()
-        to_x = self._drawer.to_x.value()
-        from_y = self._drawer.from_y.value()
-        to_y = self._drawer.to_y.value()        
-        from_z = self._drawer.from_z.value()
-        to_z = self._drawer.to_z.value()        
-        from_size = self._drawer.from_size.value()
-        to_size = self._drawer.to_size.value()        
-
-        try:
-            self.mainOperator.track(
-                        time_range = range(from_t, to_t + 1),
-                        x_range = (from_x, to_x + 1),
-                        y_range = (from_y, to_y + 1),
-                        z_range = (from_z, to_z + 1),
-                        size_range = (from_size, to_size + 1),
-                        x_scale = self._drawer.x_scale.value(),
-                        y_scale = self._drawer.y_scale.value(),
-                        z_scale = self._drawer.z_scale.value(),
-                        app=app,
-                        dis=dis,
-                        noiserate = noiserate,
-                        noiseweight = noiseweight,
-                        opp=opp,                        
-                        ep_gap=epGap,
-                        n_neighbors=n_neighbors,
-                        with_div=with_div,
-                        cplex_timeout=cplex_timeout)
-        except Exception:
-            ex_type, ex, tb = sys.exc_info()
-            traceback.print_tb(tb)            
-            QtGui.QMessageBox.critical(self, "Error", "Exception(" + str(ex_type) + "): " + str(ex), QtGui.QMessageBox.Ok)
-            return
+            try:
+                self.mainOperator.track(
+                            time_range = range(from_t, to_t + 1),
+                            x_range = (from_x, to_x + 1),
+                            y_range = (from_y, to_y + 1),
+                            z_range = (from_z, to_z + 1),
+                            size_range = (from_size, to_size + 1),
+                            x_scale = self._drawer.x_scale.value(),
+                            y_scale = self._drawer.y_scale.value(),
+                            z_scale = self._drawer.z_scale.value(),
+                            app=app,
+                            dis=dis,
+                            noiserate = noiserate,
+                            noiseweight = noiseweight,
+                            opp=opp,                        
+                            ep_gap=epGap,
+                            n_neighbors=n_neighbors,
+                            with_div=with_div,
+                            cplex_timeout=cplex_timeout)
+            except Exception:
+                ex_type, ex, tb = sys.exc_info()
+                traceback.print_tb(tb)    
+                self._criticalMessage("Exception(" + str(ex_type) + "): " + str(ex))                        
+                return
     
-        self._drawer.exportButton.setEnabled(True)
-        self._drawer.exportTifButton.setEnabled(True)
-        self._setLayerVisible("Objects", False)    
+        
+        def _handle_finished(*args):
+            self.applet.progressSignal.emit(100)
+            self._drawer.TrackButton.setEnabled(True)
+            self._drawer.exportButton.setEnabled(True)
+            self._drawer.exportTifButton.setEnabled(True)
+            self._setLayerVisible("Objects", False) 
+            
+        def _handle_failure( exc, exc_info ):
+            self.applet.progressSignal.emit(100)
+            traceback.print_exception(*exc_info)
+            sys.stderr.write("Exception raised during tracking.  See traceback above.\n")
+            self._drawer.TrackButton.setEnabled(True)
+              
+        self._drawer.TrackButton.setEnabled(False)        
+        self.applet.progressSignal.emit(0)
+        self.applet.progressSignal.emit(-1)
+        req = Request( _track )
+        req.notify_failed( _handle_failure )
+        req.notify_finished( _handle_finished )
+        req.submit()
+
+        
             
