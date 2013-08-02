ilastik/ilastik

commit b307dc27025f7a2a354f05f9289380d58ff497ec

synchronize label names accross multiple images

ilastik/applets/pixelClassification/pixelClassificationGui.py
@@ -71,6 +71,9 @@ class PixelClassificationGui(LabelingGui):
         labelSlots.labelDelete = topLevelOperatorView.opLabelPipeline.opLabelArray.deleteLabel
         labelSlots.maxLabelValue = topLevelOperatorView.MaxLabelValue
         labelSlots.labelsAllowed = topLevelOperatorView.LabelsAllowedFlags
+        labelSlots.LabelNames = topLevelOperatorView.LabelNames
+        labelSlots.LabelColors = topLevelOperatorView.LabelColors
+        labelSlots.PmapColors = topLevelOperatorView.PmapColors
 
         # We provide our own UI file (which adds an extra control for interactive mode)
         labelingDrawerUiPath = os.path.split(__file__)[0] + '/labelingDrawer.ui'

ilastik/applets/labeling/labelingGui.py
@@ -113,6 +113,10 @@ class LabelingGui(LayerViewerGui):
 
             # Slot to specify which images the user is allowed to label.
             self.labelsAllowed = None # labelsAllowed.value == True
+            
+            self.LabelNames = None
+            self.LabelColors = None
+            self.PmapColors = None            
 
     @traceLogged(traceLogger)
     def __init__(self, labelingSlots, topLevelOperatorView, drawerUiPath=None, rawInputSlot=None, crosshair=True):
@@ -138,7 +142,10 @@ class LabelingGui(LayerViewerGui):
         self._rawInputSlot = rawInputSlot
 
         self._labelingSlots.maxLabelValue.notifyDirty( bind(self._updateLabelList) )
+        if self._labelingSlots.LabelNames is not None:
+            self._labelingSlots.LabelNames.notifyDirty( bind(self._updateLabelList) )
 
+        
         self._colorTable16 = self._createDefault16ColorColorTable()
         self._programmaticallyRemovingLabels = False
 
@@ -461,8 +468,12 @@ class LabelingGui(LayerViewerGui):
         # Get the number of labels in the label data
         # (Or the number of the labels the user has added.)
         numLabels = None
+        names = []
+        if self._labelingSlots.LabelNames is not None:
+            names = self._labelingSlots.LabelNames.value
+            
         if self._labelingSlots.maxLabelValue.ready():
-            numLabels = max(self._labelingSlots.maxLabelValue.value, self._labelControlUi.labelListModel.rowCount())
+            numLabels = max(self._labelingSlots.maxLabelValue.value, self._labelControlUi.labelListModel.rowCount(), len(names))
         if numLabels is None:
             numLabels = 0
 
@@ -470,6 +481,10 @@ class LabelingGui(LayerViewerGui):
         while self._labelControlUi.labelListModel.rowCount() < numLabels:
             self._addNewLabel()
 
+        # synchronize labelNames
+        for i,n in enumerate(names):
+            self._labelControlUi.labelListModel[i].name = n
+                
         if hasattr(self._labelControlUi, "AddLabelButton"):
             self._labelControlUi.AddLabelButton.setEnabled(numLabels < self.maxLabelNumber)
 

commit c3f635e22c4151d3d78c88562f2077c0948236e2

PixelClassification: Gui: Fixed an issue that caused label deletion not to work properly due to label names slot being out-of-sync with label list model. (Fixes an issue introduced by commit b307dc27025f7a2a354f05f9289380d58ff497ec)

ilastik/applets/pixelClassification/pixelClassificationGui.py
@@ -475,12 +475,17 @@ class PixelClassificationGui(LabelingGui):
         slot.setValue(_listReplace(old, new))
 
     def _onLabelRemoved(self, parent, start, end):
-        super(PixelClassificationGui, self)._onLabelRemoved(parent, start, end)
+        # Update the label names/colors BEFORE calling the base class,
+        #  which will update the operator and expects the 
+        #  label names list to be correct.
         op = self.topLevelOperatorView
         for slot in (op.LabelNames, op.LabelColors, op.PmapColors):
             value = slot.value
             value.pop(start)
             slot.setValue(value)
+        
+        # Call the base class to update the operator.
+        super(PixelClassificationGui, self)._onLabelRemoved(parent, start, end)
 
     def getNextLabelName(self):
         return self._getNext(self.topLevelOperatorView.LabelNames,