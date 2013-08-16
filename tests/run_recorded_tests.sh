e4fd4b4333840ef56264a8dafb7986f374e1c49b
Stuart Berg <bergs@janelia.hhmi.org>
2013-08-16 18:16:40 +0000

    tests: In the ilastik-test-vm, clean environment before playing back a recorded test case.

--- tests/run_recorded_tests.sh
+++ tests/run_recorded_tests.sh
@@ -6,6 +6,12 @@ echo "BEGIN RECORDED TESTS"
 
 for f in `find $NOSE_ARG -iname "*.py" | grep recorded_test_cases`
 do
+  if [[ `whoami` = "vagrant" ]]
+  then
+    # On the ilastik-test-vm, we clean the environment to ensure reproducibility of test runs.
+    echo "Cleaning environment..."
+    rm -f ~/*.ilp && rm -f ~/.ilastik_preferences
+  fi
   echo "Running recorded test: $f"; python ilastik.py --playback_script=$f --exit_on_failure --exit_on_success
   RETVAL=$?
   if [[ $RETVAL -ne 0 ]]; then
