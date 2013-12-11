666c5659fd9adaa548e6f7946d740b43e7e94c32
Stuart Berg <bergs@janelia.hhmi.org>
2013-12-11 14:45:31 -0500

    Fix import order for submodule imports.

--- lazyflow/__init__.py
+++ lazyflow/__init__.py
@@ -1,12 +1,3 @@
-import utility
-import roi
-import rtype
-import stype
-import operators
-import request
-import graph
-import slot
-
 import os
 this_file = os.path.abspath(__file__)
 lazyflow_package_dir = os.path.dirname(this_file)
@@ -16,3 +7,12 @@ submodule_dir = os.path.join( lazyflow_repo_dir, 'submodules' )
 # Add all submodules to the PYTHONPATH
 import expose_submodules
 expose_submodules.expose_submodules(submodule_dir)
+
+import utility
+import roi
+import rtype
+import stype
+import operators
+import request
+import graph
+import slot
--- lazyflow/operators/ioOperators/__init__.py
+++ lazyflow/operators/ioOperators/__init__.py
@@ -1,16 +1,11 @@
 from ioOperators import *
+
+# All "Read" operators must come before OpInputDataReader, which uses them.
 from opNpyFileReader import *
 from opStreamingHdf5Reader import *
 from opRESTfulVolumeReader import *
 from opBlockwiseFilesetReader import *
 from opRESTfulBlockwiseFilesetReader import *
-from opInputDataReader import *
-from opNpyWriter import OpNpyWriter
-from opExport2DImage import OpExport2DImage
-from opExportMultipageTiff import OpExportMultipageTiff
-from opExportMultipageTiffSequence import OpExportMultipageTiffSequence
-from opExportSlot import OpExportSlot
-from opFormattedDataExport import OpFormattedDataExport
 
 # Try to import the dvid-related operator.
 # If it fails, that's okay.
@@ -20,3 +15,13 @@ except ImportError as ex:
     # If the exception was not related to dvidclient, then re-raise it.
     if 'dvidclient' not in ex.args[0]:
         raise
+
+from opInputDataReader import *
+
+from opNpyWriter import OpNpyWriter
+from opExport2DImage import OpExport2DImage
+from opExportMultipageTiff import OpExportMultipageTiff
+from opExportMultipageTiffSequence import OpExportMultipageTiffSequence
+from opExportSlot import OpExportSlot
+from opFormattedDataExport import OpFormattedDataExport
+
