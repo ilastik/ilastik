3c9f68bfb2d8fc980ffa5722679842fc75a6da9c
Stuart Berg <bergs@janelia.hhmi.org>
2014-01-16 15:08:48 -0500

    submodules: Automatically discover submodules and add them to sys.path

--- ilastik/__init__.py
+++ ilastik/__init__.py
@@ -1,5 +1,18 @@
 import sys
 
+################################
+## Add Submodules to sys.path ##
+################################
+import os
+this_file = os.path.abspath(__file__)
+lazyflow_package_dir = os.path.dirname(this_file)
+lazyflow_repo_dir = os.path.dirname(lazyflow_package_dir)
+submodule_dir = os.path.join( lazyflow_repo_dir, 'submodules' )
+
+# Add all submodules to the PYTHONPATH
+import expose_submodules
+expose_submodules.expose_submodules(submodule_dir)
+
 ##################
 ## Version info ##
 ##################
