ba3b50b618fc681e680dedacc06a005e4b6a910d
Stuart Berg <bergs@janelia.hhmi.org>
2014-01-22 23:11:27 -0500

    Ensure that submodule discovery works even if lazyflow is accessed via symbolic link.

--- lazyflow/__init__.py
+++ lazyflow/__init__.py
@@ -1,5 +1,6 @@
 import os
 this_file = os.path.abspath(__file__)
+this_file = os.path.realpath( this_file )
 lazyflow_package_dir = os.path.dirname(this_file)
 lazyflow_repo_dir = os.path.dirname(lazyflow_package_dir)
 submodule_dir = os.path.join( lazyflow_repo_dir, 'submodules' )
