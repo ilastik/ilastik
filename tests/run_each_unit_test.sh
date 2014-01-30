112d5feea6fa528b2546ff1bc38bdd4c8537a2e9
Stuart Berg <bergs@janelia.hhmi.org>
2014-01-30 17:09:45 -0500

    tests: Optionally skip gui tests with an env variable SKIP_GUI_TESTS=1

--- tests/run_each_unit_test.sh
+++ tests/run_each_unit_test.sh
@@ -3,6 +3,8 @@
 TESTS_DIR=`dirname $0`
 NOSE_ARG=${1-$TESTS_DIR}
 
+SKIP_GUI_TESTS=${SKIP_GUI_TESTS-0}
+
 for f in `find $NOSE_ARG -iname "*test*.py"`
 do
   if echo $f | grep -q "testPixelClassificationBenchmarking.py"; then
@@ -13,6 +15,12 @@ do
   if echo $f | grep -q "testAutocontextGui.py"; then
       echo "Skipping $f which is known to fail"
       continue
+  fi
+
+  if [ $SKIP_GUI_TESTS -ne 0 ]; then
+      if echo $f | grep -iq "gui"; then
+	  continue
+      fi
   fi
 
   echo "Running $f"; python $TESTS_DIR/nose_single.py --nologcapture $f
