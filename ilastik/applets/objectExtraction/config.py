cbaa3336c07a659af4428f1efd169fec9a75bb7a
mschiegg <martin.schiegg@iwr.uni-heidelberg.de>
2014-02-10 19:15:38 +0100

    revert config-file modifications

--- ilastik/applets/objectExtraction/config.py
+++ ilastik/applets/objectExtraction/config.py
@@ -2,35 +2,31 @@
 compress_labels = False
 '''
 # all these features are precalculated in opExtractObjects
-'''
-
-vigra_features = ['Count', 'RegionCenter', 'Mean', 'Variance', 'Coord<ValueList>', 'RegionRadii']
-features_vigra_name = 'Standard Object Features'
-
+#vigra_features = ['Count', 'Mean', 'Variance', 'Skewness', 'Kurtosis', 'RegionCenter', 'RegionAxes']
+vigra_features = ['Count', 'Mean']
 other_features = []
+
+#vigra_features = ['Count', 'Mean']
+#other_features = []
 
 # only these features are used. eventually these will be chosen
 # interactively. They many include features not in 'vigra_features',
 # in the case that some other features are also used.
-features_division_detection_name = 'Cell Division Features'
-selected_features_division_detection = {}
-selected_features_division_detection[features_division_detection_name] = ['SquaredDistance01', 'SquaredDistance02', \
-                     'SquaredDistance00', 'AngleDaughters', 'ChildrenSizeRatio', 'SquaredDistanceRatio',  
-                     'ParentChildrenSizeRatio', 'ChildrenMeanRatio', 'ParentChildrenMeanRatio',\
-                     'SquaredDistance01_corr', 'SquaredDistance02_corr', \
-                     'SquaredDistance00_corr', 'AngleDaughters_corr', 'ChildrenSizeRatio_corr', \
-                     'SquaredDistanceRatio_corr', 'ParentChildrenSizeRatio_corr', 'ChildrenMeanRatio_corr', \
-                     'ParentChildrenMeanRatio_corr']
-selected_features_division_detection[features_vigra_name] = [ 'Count', 'Mean', 'Variance' ]
+#selected_features = ['Count', 'Mean', 'Mean_excl', 'Variance', \
+#                     'Variance_excl', 'Skewness', 'Skewness_excl', \
+#                      'Kurtosis', 'Kurtosis_excl']
+selected_features = ['Count', 'Mean']
+has_skimage = True
+try:
+    import skimage.feature
+except:
+    has_skimage = False
+    print "scikit-image is not installed, some features disabled"
+if not has_skimage:
+    selected_features = [feature for feature in selected_features if "lbp" not
+                         in feature]
 
+#selected_features = ['lbp_excl', 'lbp_obj']
 
-features_cell_classification_name = 'Cell Classification Features'
-selected_features_cell_classification = {}
-selected_features_cell_classification[features_vigra_name] = ['Count', 'Mean', 'Variance', 'RegionRadii' ]
-#selected_features_cell_classification[features_cell_classification_name] = ['GMM_BIC']
-selected_features_cell_classification[features_cell_classification_name] = [ ]
-
-
-num_max_objects = 2
-
-image_scale = [1.0, 1.0, 1.0]
+#selected_features = ['Histogram_excl', 'Histogram_obj', 'lbp_excl', 'lbp_obj']
+'''
