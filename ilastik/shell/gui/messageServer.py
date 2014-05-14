a508f2af09cd3f0ef245342c38bf46e14804fa7c
christophdecker <mail@cdecker.info>
2014-05-14 14:59:14 +0200

    Easy hack to change position in volumina for each applet in current workflow

--- ilastik/shell/gui/messageServer.py
+++ ilastik/shell/gui/messageServer.py
@@ -2,6 +2,7 @@ import SocketServer
 import threading
 import socket
 import logging
+import string
 
 logger = logging.getLogger(__name__)
 
@@ -70,6 +71,36 @@ class MessageServer(object):
 
 class TCPRequestHandler(SocketServer.StreamRequestHandler):                 
     def handle(self):
-        self.data = self.rfile.readline().strip()
-        logger.info("%s wrote: %s" % (self.client_address[0], self.data))
+        data = self.rfile.readline().strip()
+        logger.info("%s wrote: %s" % (self.client_address[0], data))
         
+        split = string.split(data[1:-1],':')
+        command = split[0]
+        param = split[1]
+        
+        
+        if command == 'jumpToPos':
+            pos5d = {'t': 0, 'x': 0, 'y': 0, 'z': 0, 'c': 0}
+            coords = string.split(param, ',')
+            for coord in coords:
+                split = string.split(coord, '=')
+                axis = split[0]
+                pos = int(split[1])
+                if axis in pos5d and isinstance(pos, int):
+                    pos5d[axis] = pos
+            print pos5d        
+        
+        print self.server.shell
+        if not self.server.shell.workflow is None:
+            for app in self.server.shell.workflow.applets:
+                try:
+                    app._gui.currentGui().editor.posModel.slicingPos = [pos5d['x'],pos5d['y'],pos5d['z']]
+                    app._gui.currentGui().editor.posModel.time = [pos5d['t']]
+                    app._gui.currentGui().editor.posModel.channel = [pos5d['c']]
+                except:
+                    pass
+                
+        """
+        editor.posModel.slicingPos = midpos3d
+        self.editor.navCtrl.panSlicingViews( midpos3d, [0,1,2] )
+        """
