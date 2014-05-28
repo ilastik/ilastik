ilastik/ilastik

commit 0d2c2a24663cb6991dc260eac6cbcccf2abf1ed6

Cleanup requests submitted to RequestPool as they are completed.
Since the RequestPool keeps a reference to all submitted requests, these
are only cleaned up when the pool is cleaned itself, either by the user
explicitly with the .clean() method or by the garbage collector. This
means that the memory allocated for the results returned from requests
is kept around until all requests are done.

This patch adds a callback function to remove the reference to the
request once it is finished running.

lazyflow/request/request.py
@@ -872,6 +872,15 @@ class RequestPool(object):
             # For now, we forbid this because it would allow some corner cases that we aren't unit-testing yet.
             # If this exception blocks a desirable use case, then change this behavior and provide a unit test.
             raise RequestPool.RequestPoolError("Attempted to add a request to a pool that was already started!")
+        def remove_request(result):
+            try:
+                self._requests.remove(req)
+            except KeyError:
+                # request may have been removed already by the while
+                # loop in the wait() method
+                pass
+
+        req.notify_finished(remove_request)
         self._requests.add(req)
 
     def submit(self):
@@ -880,8 +889,12 @@ class RequestPool(object):
         """
         if self._started:
             raise RequestPool.RequestPoolError("Can't re-start a RequestPool that was already started.")
-        for req in self._requests:
-            req.submit()
+        # shallow copy prevents python complaining when finished requests
+        # remove themselves from self._requests
+        requests = self._requests.copy()
+        # while loop with pop() allows the gc to clean up completed requests
+        while requests:
+            requests.pop().submit()
 
     def wait(self):
         """
@@ -889,7 +902,15 @@ class RequestPool(object):
         """
         if not self._started:
             self.submit()
-        for req in self._requests:
+        # do not use a for loop since _requests will be modified as
+        # finished requests remove themselves from the set
+        while self._requests:
+            try:
+                req = self._requests.pop()
+            except KeyError:
+                # the _requests set was modified in the mean time
+                # we can quit the loop since there are no more requests
+                break
             req.block()
 
     def cancel(self):

tests/testRequestPool.py
@@ -1,0 +1,110 @@
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# Copyright 2011-2014, the ilastik developers
+
+import sys
+import time
+import psutil
+import numpy
+import gc
+
+from lazyflow.request.request import Request, RequestPool
+from lazyflow.utility.tracer import traceLogged
+
+import logging
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(levelname)s %(name)s %(message)s')
+handler.setFormatter(formatter)
+
+# Test
+logger = logging.getLogger("tests.testRequestRewrite")
+# Test Trace
+traceLogger = logging.getLogger("TRACE." + logger.name)
+
+def test_basic():
+    """
+    Check if a request pool executes all added requests.
+    """
+    # threadsafe way to count how many requests ran
+    import itertools
+    result_counter = itertools.count()
+
+    def increase_counter():
+        time.sleep(0.1)
+        result_counter.next()
+
+    pool = RequestPool()
+    for i in xrange(500):
+        pool.add(Request(increase_counter))
+    pool.wait()
+
+    assert result_counter.next() == 500, "RequestPool has not run all submitted requests {} out of 500".format(result_counter.next() - 1)
+
+def test_cleanup():
+    """
+    Check if requests added to a RequestPool are cleaned when they are
+    completed without waiting for the RequestPool itself to be cleaned.
+    """
+    cur_process = psutil.Process()
+    def getMemoryUsage():
+        # Collect garbage first
+        gc.collect()
+        return cur_process.memory_info().vms
+        #return mem_usage_mb
+
+    starting_usage = getMemoryUsage()
+    def getMemoryIncrease():
+        return getMemoryUsage() - starting_usage
+
+
+    num_workers = len(Request.global_thread_pool.workers)
+    # maximum memory this tests should use
+    # tests should not cause the machine to swap unnecessarily
+    max_mem = 1<<29 # 512 Mb
+    mem_per_req = max_mem / num_workers
+
+    # some leeway
+    max_allowed_mem = (max_mem + 2*mem_per_req)
+
+    def memoryhog():
+        increase = getMemoryIncrease()
+        assert increase < max_allowed_mem, "memory use should not go beyond {}, current use: {}".format(max_mem, increase)
+        return numpy.zeros(mem_per_req, dtype=numpy.uint8)
+
+    pool = RequestPool()
+    for i in xrange(num_workers**2):
+        pool.add(Request(memoryhog))
+
+    pool.wait()
+
+    assert len(pool._requests) == 0, "Not all requests were executed by the RequestPool"
+
+
+if __name__ == "__main__":
+
+    # Logging is OFF by default when running from command-line nose, i.e.:
+    # nosetests thisFile.py)
+    # but ON by default if running this test directly, i.e.:
+    # python thisFile.py
+    logging.getLogger().addHandler( handler )
+    logger.setLevel(logging.DEBUG)
+    traceLogger.setLevel(logging.DEBUG)
+
+    import sys
+    import nose
+    sys.argv.append("--nocapture")    # Don't steal stdout.  Show it on the console as usual.
+    sys.argv.append("--nologcapture") # Don't set the logging level to DEBUG.  Leave it alone.
+    ret = nose.run(defaultTest=__file__)
+    if not ret: sys.exit(1)

tests/testRequestRewrite.py
@@ -627,16 +627,15 @@ class TestRequest(object):
         so we MUST make sure that cycles between requests (e.g. parent/child and blocking/pending) 
         are broken.  Preferably, requests should be deleted early as possible.
         """
-        def getMemoryUsageMb():
+        cur_process = psutil.Process()
+        def getMemoryUsage():
             # Collect garbage first
             gc.collect()
-            vmem = psutil.virtual_memory()
-            mem_usage_mb = (vmem.total - vmem.available) / (1000*1000)
-            return mem_usage_mb
-        
-        starting_usage_mb = getMemoryUsageMb()
-        def getMemoryIncreaseMb():
-            return getMemoryUsageMb() - starting_usage_mb
+            return cur_process.memory_info().vms
+
+        starting_usage = getMemoryUsage()
+        def getMemoryIncrease():
+            return getMemoryUsage() - starting_usage
 
         resultShape = (500,1000,1000)
         resultSize = numpy.prod(resultShape)
@@ -644,10 +643,10 @@ class TestRequest(object):
             """
             Simulate the memory footprint of a series of computation steps.
             """
-            logger.debug( "Usage delta before depth {}: {} MB".format(recursionDepth, getMemoryIncreaseMb() ) )
+            logger.debug( "Usage delta before depth {}: {}".format(recursionDepth, getMemoryIncrease() ) )
 
             if recursionDepth == 0:
-                # A 500GB result
+                # A 500MB result
                 result = numpy.zeros(shape=resultShape, dtype=numpy.uint8)
             else:
                 req = Request( partial(getBigArray, directExecute=directExecute, recursionDepth=recursionDepth-1) )
@@ -659,9 +658,9 @@ class TestRequest(object):
             
             # Note that we expect there to be 2X memory usage here:
             #  1x for our result and 1x for the child, which hasn't been cleaned up yet.
-            memory_increase_mb = getMemoryIncreaseMb()
-            logger.debug( "Usage delta after depth {}: {} MB".format(recursionDepth, memory_increase_mb ) )
-            assert memory_increase_mb < 2.5*resultSize, "Memory from finished requests didn't get freed!"
+            memory_increase = getMemoryIncrease()
+            logger.debug( "Usage delta after depth {}: {}".format(recursionDepth, memory_increase ) )
+            assert memory_increase < 2.5*resultSize, "Memory from finished requests didn't get freed!"
             
             return result
 
@@ -674,9 +673,9 @@ class TestRequest(object):
         test_impl(True)
         test_impl(False)
 
-        memory_increase_mb = getMemoryIncreaseMb()
-        logger.debug( "Finished test with memory usage delta at: {} MB".format( memory_increase_mb ) )
-        assert memory_increase_mb < resultSize, "All requests are finished an inaccessible, but not all memory was released!"
+        memory_increase = getMemoryIncrease()
+        logger.debug( "Finished test with memory usage delta at: {}".format( memory_increase ) )
+        assert memory_increase < resultSize, "All requests are finished an inaccessible, but not all memory was released!"
 
     def testThreadPoolReset(self):
         Request.reset_thread_pool(num_workers=1)

commit 3c78e17e3cc39e35b0b42d7e1ef50947058f0762

RequestPool: Fix wait() so that it doesn't violate the 'Callback Timing Guarantee'. (Fixes an issue introduced in 0d2c2a24663cb6991dc260eac6cbcccf2abf1ed6)

lazyflow/request/request.py
@@ -864,6 +864,7 @@ class RequestPool(object):
 
     def __init__(self):
         self._requests = set()
+        self._finishing_requests = set()
         self._started = False
 
     def __len__(self):
@@ -878,6 +879,9 @@ class RequestPool(object):
             # If this exception blocks a desirable use case, then change this behavior and provide a unit test.
             raise RequestPool.RequestPoolError("Attempted to add a request to a pool that was already started!")
         def remove_request(result):
+            # This request is done executing, but not quite finished with its callbacks.
+            # See docstring in wait() for details.
+            self._finishing_requests.add(req)
             try:
                 self._requests.remove(req)
             except KeyError:
@@ -903,20 +907,52 @@ class RequestPool(object):
 
     def wait(self):
         """
-        Wait for all requests in the pool to complete.
+        If the pool hasn't been submitted yet, submit it. Then wait for all requests in the pool to complete.
+        
+        To be efficient with memory, we attempt to discard requests quickly after they complete.
+        To achieve this, we keep requests in two sets:
+        
+        _requests: All requests that are still executing or 'finishing'
+        _finishing_requests: Requests whose main work has completed, but may still be executing callbacks
+                             (e.g. handlers for notify_finished)
+
+        Requests are transferred from the first set to the second as they complete.
+        
+        We try to block() for 'finishing' requests first, so they can be discarded quickly.
+        (If we didn't block for 'finishing' requests at all, we'd be violating the Request 'Callback Timing Guarantee', 
+        which must hold for both Requests and RequestPools.  See Request docs for details.)        
         """
+        def _clear_finishing_requests():
+            while self._finishing_requests:
+                try:
+                    req = self._finishing_requests.pop()
+                except KeyError:
+                    break
+                else:
+                    req.block()            
+        
         if not self._started:
             self.submit()
-        # do not use a for loop since _requests will be modified as
-        # finished requests remove themselves from the set
+        
         while self._requests:
+            # First, clear the queue of 'finishing' requests.
+            # We want to discard them as soon as possible.
+            _clear_finishing_requests()
+            
+            # Next, wait for the next non-'finishing' request.
             try:
                 req = self._requests.pop()
             except KeyError:
                 # the _requests set was modified in the mean time
                 # we can quit the loop since there are no more requests
                 break
-            req.block()
+            else:
+                req.block()
+
+        # Finally, now all the requests are either 'finishing' or totally complete.
+        # DON'T EXIT until all requests are totally complete. 
+        #  (Once their callbacks have completed, they are no longer 'finishing')
+        _clear_finishing_requests()
 
     def cancel(self):
         """