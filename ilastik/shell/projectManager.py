ilastik/ilastik

commit 1ebc672e81b997baa2b680c957786eed7c1ea528

carving: implement preprocessing
fixes #88, fixes #163

ilastik/shell/projectManager.py
@@ -239,10 +239,9 @@ class ProjectManager(object):
         2) touch Old.ilp
         3) copycontents New.ilp -> Old.ilp
         4) Save current applet state to current project (New.ilp)
-        
         Postconditions: - Original project state is saved to a new file with the original name.
-                        - Current project file is still open, but has a new name.
-                        - Current project file has been saved (it is in sync with the applet states)
+        - Current project file is still open, but has a new name.
+        - Current project file has been saved (it is in sync with the applet states)
         """
         # If our project is read-only, we can't be efficient.
         # We have to take a snapshot, then close our current project and open the snapshot
@@ -252,7 +251,11 @@ class ProjectManager(object):
 
         oldPath = self.currentProjectPath
         try:
+            self.currentProjectFile.close()
+            if os.path.isfile(newPath):
+                os.remove(newPath)
             os.rename( oldPath, newPath )
+            self.currentProjectFile = h5py.File(newPath)
         except OSError, err:
             msg = 'Could not rename your project file to:\n'
             msg += newPath + '\n'
@@ -264,12 +267,12 @@ class ProjectManager(object):
 
         # The file has been renamed
         self.currentProjectPath = newPath
-
-        # Copy the contents of the current project file to a newly-created file (with the old name)        
-        with h5py.File(oldPath, 'w') as oldFile:
+        
+        # Copy the contents of the current project file to a newly-created file (with the old name)
+        with h5py.File(oldPath, 'a') as oldFile:
             for key in self.currentProjectFile.keys():
                 oldFile.copy(self.currentProjectFile[key], key)
-
+        
         # Save the current project state
         self.saveProject()
         

.gitignore
@@ -2,6 +2,7 @@
 build
 tests/*.h5
 *.pyc
+*.goutputstream
 *.pyo
 *.kdev4
 .DS_Store

ilastik/applets/base/applet.py
@@ -30,7 +30,7 @@ class Applet( object ):
         #: .. note:: To update the progress bar correctly, the shell expects that progress updates always 
         #:           begin with at least one zero update and end with at least one 100 update.
         #:           That is: 
-        #:           ``self.progressSignal(0)`` ... more updates ... ``self.progressSignal(100)``
+        #:           ``self.progressSignal.emit(0)`` ... more updates ... ``self.progressSignal.emit(100)``
         self.progressSignal = SimpleSignal()
         
         #: GUI control signal

ilastik/workflows/carving/carving.py
@@ -3,6 +3,7 @@
 import os 
 import functools
 from carvingWorkflow import CarvingWorkflow
+import h5py
 
 #//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -13,11 +14,7 @@ if __name__ == "__main__":
     import socket
     import logging
     logging.basicConfig(level=logging.INFO)
-
-#    import sys
-#    sys.argv.append( "/magnetic/denk.h5" )
-#    sys.argv.append( "/magnetic/carving_test.ilp" )
-
+    
     graph = lazyflow.graph.Graph()
     
     from optparse import OptionParser
@@ -32,9 +29,28 @@ if __name__ == "__main__":
                   help="specify a file which adds a pmap overlay")
 
     options, args = parser.parse_args()
-    print options,args
     
-    if len(args) == 2:
+    if len(args)==0:
+        workflowKwargs={'hintoverlayFile' : options.hintoverlayFile,
+                        'pmapoverlayFile' : options.pmapoverlayFile }
+        
+        def loadProject(shell):
+        #   shell.openProjectFile("C:/Users/Ben/Desktop/carvingData/test3.ilp")
+			pass
+        
+        startShellGui(functools.partial(CarvingWorkflow, **workflowKwargs),loadProject)
+        
+    elif len(args)==1:
+        projectFilename = args[0]
+        
+        def loadProject(shell):
+            shell.openProjectFile(projectFilename)
+        
+        workflowKwargs={'hintoverlayFile' : options.hintoverlayFile,
+                        'pmapoverlayFile' : options.pmapoverlayFile }
+        startShellGui( functools.partial(CarvingWorkflow, **workflowKwargs), loadProject)
+        
+    elif len(args) == 2:
         carvingGraphFilename = args[0]
         projectFilename = args[1]
         def loadProject(shell):
@@ -44,19 +60,20 @@ if __name__ == "__main__":
                 shell.openProjectFile(projectFilename)
             
             workflow = shell.projectManager.workflow
-            workflow.setCarvingGraphFile(carvingGraphFilename)
             # Add a file
             from ilastik.applets.dataSelection.opDataSelection import DatasetInfo
             info = DatasetInfo()
             info.filePath = carvingGraphFilename + "/graph/raw"
             opDataSelection = workflow.dataSelectionApplet.topLevelOperator
+            preprocessingApplet = workflow.preprocessingApplet
+            
+            preprocessingApplet.ThorbenWantsToAddAlreadyPreprocessedFilesDirectly(carvingGraphFilename)
             opDataSelection.Dataset.resize(1)
             opDataSelection.Dataset[0].setValue(info)
-            shell.setSelectedAppletDrawer(2)
 
         workflowKwargs={'carvingGraphFile': carvingGraphFilename,
                         'hintoverlayFile' : options.hintoverlayFile,
                         'pmapoverlayFile' : options.pmapoverlayFile }
         startShellGui( functools.partial(CarvingWorkflow, **workflowKwargs), loadProject)
     else:
-        parser.error("incorrect number of arguments %d, expected 2" % len(args))
+        parser.error("incorrect number of arguments %d, expected at most 2" % len(args))

ilastik/workflows/carving/carvingApplet.py
@@ -27,7 +27,7 @@ class CarvingApplet(LabelingApplet):
         Override from base class.
         """
         return self._topLevelOperator
-
+    
     def createSingleLaneGui(self, laneIndex):
         """
         Override from base class.

ilastik/workflows/carving/carvingGui.py
@@ -4,6 +4,7 @@ import numpy
 import random
 
 #PyQt
+from PyQt4.QtCore import QTimer
 from PyQt4.QtGui import QShortcut, QKeySequence
 from PyQt4.QtGui import QColor, QMenu
 from PyQt4.QtGui import QInputDialog, QMessageBox
@@ -203,7 +204,6 @@ class CarvingGui(LabelingGui):
         addLayerToggleShortcut("hints","h")
 
         '''
-        #For benchmarking, this shows the time it took each tile to arrive.
         def updateLayerTimings():
             s = "Layer timings:\n"
             for l in self.layerstack:
@@ -271,18 +271,18 @@ class CarvingGui(LabelingGui):
 
         op = self.topLevelOperatorView.opCarving
         if not self._renderMgr.ready:
-            self._renderMgr.setup(op._mst.raw.shape)
+            self._renderMgr.setup(op.MST.value.raw.shape)
 
         # remove nonexistent objects
         self._shownObjects3D = dict((k, v) for k, v in self._shownObjects3D.iteritems()
-                                    if k in op._mst.object_lut.keys())
+                                    if k in op.MST.value.object_lut.keys())
 
-        lut = numpy.zeros(len(op._mst.objects.lut), dtype=numpy.int32)
+        lut = numpy.zeros(len(op.MST.value.objects.lut), dtype=numpy.int32)
         for name, label in self._shownObjects3D.iteritems():
-            objectSupervoxels = op._mst.object_lut[name]
+            objectSupervoxels = op.MST.value.object_lut[name]
             lut[objectSupervoxels] = label
 
-        self._renderMgr.volume = lut[op._mst.regionVol]
+        self._renderMgr.volume = lut[op.MST.value.regionVol]
         self._update_colors()
         self._renderMgr.update()
 
@@ -291,7 +291,7 @@ class CarvingGui(LabelingGui):
         ctable = self._doneSegmentationLayer.colorTable
 
         for name, label in self._shownObjects3D.iteritems():
-            color = QColor(ctable[op._mst.object_names[name]])
+            color = QColor(ctable[op.MST.value.object_names[name]])
             color = (color.red() / 255.0, color.green() / 255.0, color.blue() / 255.0)
             self._renderMgr.setColor(label, color)
 
@@ -349,8 +349,8 @@ class CarvingGui(LabelingGui):
         #segmentation 
         seg = self.topLevelOperatorView.opCarving.Segmentation
         
-        #seg = self.topLevelOperatorView.opCarving._mst.segmentation
-        #temp = self._done_lut[self._mst.regionVol[sl[1:4]]]
+        #seg = self.topLevelOperatorView.opCarving.MST.value.segmentation
+        #temp = self._done_lut[self.MST.value.regionVol[sl[1:4]]]
         if seg.ready():
             #source = RelabelingArraySource(seg)
             #source.setRelabeling(numpy.arange(256, dtype=numpy.uint8))
@@ -433,7 +433,9 @@ class CarvingGui(LabelingGui):
         #
         # here we load the actual raw data from an ArraySource rather than from a LazyflowSource for speed reasons
         #
-        raw = self.topLevelOperatorView.opCarving._mst.raw
+        
+        #
+        raw = numpy.add.reduce(self.topLevelOperatorView.RawData.value,3)
         raw5D = numpy.zeros((1,)+raw.shape+(1,), dtype=raw.dtype)
         raw5D[0,:,:,:,0] = raw[:,:,:]
         layer = GrayscaleLayer(ArraySource(raw5D), direct=True)

ilastik/workflows/carving/carvingSerializer.py
@@ -8,7 +8,6 @@ class CarvingSerializer( AppletSerializer ):
         
     def _serializeToHdf5(self, topGroup, hdf5File, projectFilePath):
         obj = getOrCreateGroup(topGroup, "objects")
-        
         for imageIndex, opCarving in enumerate( self._o.opCarving.innerOperators ):
             mst = opCarving._mst 
             for name in opCarving._dirtyObjects:

ilastik/workflows/carving/carvingWorkflow.py
@@ -7,6 +7,7 @@ from ilastik.applets.projectMetadata import ProjectMetadataApplet
 from ilastik.applets.dataSelection import DataSelectionApplet
 
 from carvingApplet import CarvingApplet
+from preprocessingApplet import PreprocessingApplet
 
 class CarvingWorkflow(Workflow):
 
@@ -34,27 +35,43 @@ class CarvingWorkflow(Workflow):
         ## Create applets 
         self.projectMetadataApplet = ProjectMetadataApplet()
         self.dataSelectionApplet = DataSelectionApplet(self, "Input Data", "Input Data", supportIlastik05Import=True, batchDataGui=False)
-
+        
         self.carvingApplet = CarvingApplet(workflow=self,
                                            projectFileGroupName="carving",
                                            carvingGraphFile = carvingGraphFile,
                                            hintOverlayFile=hintoverlayFile,
                                            pmapOverlayFile=pmapoverlayFile)
+        
+        self.preprocessingApplet = PreprocessingApplet(workflow=self,
+                                           title = "Preprocessing",
+                                           projectFileGroupName="carving")
+        
+        #self.carvingApplet.topLevelOperator.MST.connect(self.preprocessingApplet.topLevelOperator.PreprocessedData)
+        
         # Expose to shell
         self._applets = []
         self._applets.append(self.projectMetadataApplet)
         self._applets.append(self.dataSelectionApplet)
+        self._applets.append(self.preprocessingApplet)
         self._applets.append(self.carvingApplet)
+        
+        
 
     def connectLane(self, laneIndex):
+        print "connect"
+        print laneIndex
         ## Access applet operators
         opData = self.dataSelectionApplet.topLevelOperator.getLane(laneIndex)
+        opPreprocessing = self.preprocessingApplet.topLevelOperator.getLane(laneIndex)
         opCarvingTopLevel = self.carvingApplet.topLevelOperator.getLane(laneIndex)
         
-        ## Connect operators ##
-        opCarvingTopLevel.RawData.connect( opData.Image )
+        ## Connect operators
+        opPreprocessing.RawData.connect(opData.Image)
+        opCarvingTopLevel.RawData.connect(opData.Image)
+        #opCarvingTopLevel.opCarving.RawData.connect( opData.Image )
+        opCarvingTopLevel.opCarving.MST.connect(opPreprocessing.PreprocessedData)
         opCarvingTopLevel.opCarving.opLabeling.LabelsAllowedFlag.connect( opData.AllowLabels )
         opCarvingTopLevel.opCarving.UncertaintyType.setValue("none")
-
+        
     def setCarvingGraphFile(self, fname):
         self.carvingApplet.topLevelOperator.opCarving.CarvingGraphFile.setValue(fname)

commit c71efecd856b72aad27532d36d0b73610e33180b

ProjectManager: Fixed "Save As" behavior: The project file was closed without being reloaded, causing errors if the user's dataset was stored inside the project file. This bug was introduced in 1ebc672e81b997baa2b680c957786eed7c1ea528, in an attempt to deal with #163. This commit fixes #163 by taking the "slow" approach for Windows: "Save As" is performed by taking a snapshot and loading it.

ilastik/shell/projectManager.py
@@ -21,6 +21,7 @@
 import os
 import gc
 import copy
+import platform
 import h5py
 import logging
 import time
@@ -314,17 +315,14 @@ class ProjectManager(object):
         """
         # If our project is read-only, we can't be efficient.
         # We have to take a snapshot, then close our current project and open the snapshot
-        if self.currentProjectIsReadOnly:
+        # Furthermore, windows does not permit renaming an open file, so we must take this approach.
+        if self.currentProjectIsReadOnly or platform.system() == 'Windows':
             self._takeSnapshotAndLoadIt(newPath)
             return
 
         oldPath = self.currentProjectPath
         try:
-            self.currentProjectFile.close()
-            if os.path.isfile(newPath):
-                os.remove(newPath)
             os.rename( oldPath, newPath )
-            self.currentProjectFile = h5py.File(newPath)
         except OSError, err:
             msg = 'Could not rename your project file to:\n'
             msg += newPath + '\n'