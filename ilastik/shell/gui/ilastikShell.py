8b7dd859118d76e03be5a82c475b4aa83842f016
mschiegg <martin.schiegg@iwr.uni-heidelberg.de>
2014-07-03 19:49:47 +0200

    changing back to original values

--- ilastik/shell/gui/ilastikShell.py
+++ ilastik/shell/gui/ilastikShell.py
@@ -728,7 +728,7 @@ class IlastikShell( QMainWindow ):
         """
         Update the title bar and allowable shell actions based on the state of the currently loaded project.
         """
-        windowTitle = "ilastik for Conservation Tracking - "
+        windowTitle = "ilastik - "
         if self.projectManager is None or self.projectManager.closed:
             windowTitle += "No Project Loaded"
         else:
