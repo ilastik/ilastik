898c57c66f67ed04455963ca509b137378386f40
Stuart Berg <bergs@janelia.hhmi.org>
2014-07-11 17:57:03 +0200

    DataExport: Check for None before touching output_filename_format

--- ilastik/applets/dataExport/dataExportApplet.py
+++ ilastik/applets/dataExport/dataExportApplet.py
@@ -92,7 +92,8 @@ class DataExportApplet( Applet ):
         parsed_args, unused_args = arg_parser.parse_known_args(cmdline_args)
 
         # Replace '~' with home dir
-        parsed_args.output_filename_format = os.path.expanduser( parsed_args.output_filename_format )
+        if parsed_args.output_filename_format is not None:
+            parsed_args.output_filename_format = os.path.expanduser( parsed_args.output_filename_format )
 
         ### Convert from strings, check for obvious errors
 
