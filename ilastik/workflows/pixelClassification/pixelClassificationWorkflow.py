25e1c178cd9b47302fbddb799904fe85785e3728
Stuart Berg <bergs@janelia.hhmi.org>
2014-08-06 16:10:53 -0400

    PixelClassificaiton: Headless: Warn the user if his project file contains no classifier and therefore needs retraining.

--- ilastik/workflows/pixelClassification/pixelClassificationWorkflow.py
+++ ilastik/workflows/pixelClassification/pixelClassificationWorkflow.py
@@ -363,6 +363,12 @@ class PixelClassificationWorkflow(Workflow):
         if self._batch_export_args:
             self.batchResultsApplet.configure_operator_with_parsed_args( self._batch_export_args )
 
+        if self.pcApplet.topLevelOperator.classifier_cache._dirty:
+            logger.warn("Your project file has no classifier.  A new classifier will be trained for this run.")
+
+        # Let's see the messages from the training operator.
+        logging.getLogger("lazyflow.operators.classifierOperators").setLevel(logging.DEBUG)
+        
         if self.retrain:
             # Cause the classifier to be dirty so it is forced to retrain.
             # (useful if the stored labels were changed outside ilastik)
