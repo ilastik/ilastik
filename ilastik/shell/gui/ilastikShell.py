a5f5bcd2c8caf69c155d0a4b93d096cca82d77f4
Stuart Berg <bergs@janelia.hhmi.org>
2014-09-29 09:53:07 -0400

    shell: Save and restore frame geometry across restarts (not just window size) Fixes #1006

--- ilastik/shell/gui/ilastikShell.py
+++ ilastik/shell/gui/ilastikShell.py
@@ -321,9 +321,21 @@ class IlastikShell( QMainWindow ):
 
         self.errorMessageFilter = ErrorMessageFilter(self)
 
-        windowSize = PreferencesManager().get("shell","startscreenSize")
-        if windowSize is not None:
-            self.resize(*windowSize)
+        frame_geometry = PreferencesManager().get("shell","startscreenGeometry")
+        if frame_geometry is not None:
+            x,y,w,h = frame_geometry
+            self.move( x, y )
+
+            # The frameGeometry() function doesn't actually include the
+            #  window frame padding until the window has been shown at least once.
+            # Hence, show it now before doing our calculations.
+            self.show()
+            
+            # Qt offers no function for setting the size of the entire frame, 
+            # so instead we have to calculate the target size of the internal geometry.            
+            frame_padding_width = self.frameGeometry().width() - self.geometry().size().width()
+            frame_padding_height = self.frameGeometry().height() - self.geometry().size().height()
+            self.resize( w - frame_padding_width, h - frame_padding_height )
 
         self._initShortcuts()
 
@@ -1434,7 +1446,9 @@ class IlastikShell( QMainWindow ):
         return True
 
     def closeAndQuit(self, quitApp=True):
-        PreferencesManager().set( 'shell', 'startscreenSize', (self.size().width(),self.size().height()))
+        geom = self.frameGeometry()
+        x, y, width, height = geom.x(), geom.y(), geom.width(), geom.height()
+        PreferencesManager().set( 'shell', 'startscreenGeometry', ( x, y, width, height ))
 
         if self.projectManager is not None:
             self.closeCurrentProject()
