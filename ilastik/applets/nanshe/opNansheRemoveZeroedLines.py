0c5ecbbcba3f0f17bdbb1d52784bc8c39782293f
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-10-14 16:31:56 -0400

    ilastik/applets/nanshe/opNansheRemoveZeroedLines.py: First attempt at an operator to wrap nanshe.advanced_image_processing.remove_zeroed_lines.

---
+++ ilastik/applets/nanshe/opNansheRemoveZeroedLines.py
@@ -0,0 +1,67 @@
+###############################################################################
+#   ilastik: interactive learning and segmentation toolkit
+#
+#       Copyright (C) 2011-2014, the ilastik developers
+#                                <team@ilastik.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# In addition, as a special exception, the copyright holders of
+# ilastik give you permission to combine ilastik with applets,
+# workflows and plugins which are not covered under the GNU
+# General Public License.
+#
+# See the LICENSE file for details. License information is also available
+# on the ilastik web site at:
+#		   http://ilastik.org/license.html
+###############################################################################
+from lazyflow.graph import Operator, InputSlot, OutputSlot
+
+import numpy
+
+import nanshe
+import nanshe.advanced_image_processing
+
+
+class OpNansheRemoveZeroedLines(Operator):
+    """
+    Given an input image and max/min bounds,
+    masks out (i.e. sets to zero) all pixels that fall outside the bounds.
+    """
+    name = "OpNansheRemoveZeroedLines"
+    category = "Pointwise"
+
+    InputImage = InputSlot()
+
+    ErosionShape = InputSlot()
+    DilationShape = InputSlot()
+    
+    Output = OutputSlot()
+    
+    def setupOutputs(self):
+        # Copy the input metadata to both outputs
+        self.Output.meta.assignFrom( self.InputImage.meta )
+    
+    def execute(self, slot, subindex, roi, result):
+        key = roi.toSlice()
+        raw = self.InputImage[key].wait()
+        erosion_shape = self.ErosionShape.value
+        dilation_shape = self.DilationShape.value
+
+        processed = nanshe.advanced_image_processing.remove_zeroed_lines(raw,
+                                                                         erosion_shape=erosion_shape,
+                                                                         dilation_shape=dilation_shape)
+        
+        if slot.name == 'Output':
+            result[...] = processed
+
+    def propagateDirty(self, slot, subindex, roi):
+        if slot.name == "InputImage":
+            self.Output.setDirty(roi)
+        elif slot.name == "ErosionShape" or slot.name == "DilationShape":
+            self.Output.setDirty( slice(None) )
+        else:
+            assert False, "Unknown dirty input slot"
