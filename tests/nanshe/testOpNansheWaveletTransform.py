ff830b01be2b8edae8990f57510ba205485ba12c
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-10-15 16:04:16 -0400

    tests/nanshe/testOpNansheWaveletTransform.py: Added a test for OpNansheWaveletTransform.

---
+++ tests/nanshe/testOpNansheWaveletTransform.py
@@ -0,0 +1,58 @@
+###############################################################################
+#   lazyflow: data flow based lazy parallel computation framework
+#
+#       Copyright (C) 2011-2014, the ilastik developers
+#                                <team@ilastik.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the Lesser GNU General Public License
+# as published by the Free Software Foundation; either version 2.1
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# See the files LICENSE.lgpl2 and LICENSE.lgpl3 for full text of the
+# GNU Lesser General Public License version 2.1 and 3 respectively.
+# This information is also available on the ilastik web site at:
+#		   http://ilastik.org/license/
+###############################################################################
+import numpy
+from lazyflow.graph import Graph
+
+
+import ilastik
+import ilastik.applets
+import ilastik.applets.nanshe
+import ilastik.applets.nanshe.opNansheWaveletTransform
+from ilastik.applets.nanshe.opNansheWaveletTransform import OpNansheWaveletTransform
+
+class TestOpNansheExtractF0(object):
+    def testBasic(self):
+        a = numpy.eye(3, dtype = numpy.float32)
+        expected_b = numpy.array([[[ 0.59375, -0.375  , -0.34375],
+                                   [-0.375  ,  0.625  , -0.375  ],
+                                   [-0.34375, -0.375  ,  0.59375]]], dtype=numpy.float32)
+
+        graph = Graph()
+        op = OpNansheWaveletTransform(graph=graph)
+
+        op.InputImage.setValue(a)
+
+        op.Scale.setValue(1)
+        op.IncludeLowerScales.setValue(True)
+
+        b = op.Output[...].wait()
+
+        assert((b == expected_b).all())
+
+
+if __name__ == "__main__":
+    import sys
+    import nose
+    sys.argv.append("--nocapture")    # Don't steal stdout.  Show it on the console as usual.
+    sys.argv.append("--nologcapture") # Don't set the logging level to DEBUG.  Leave it alone.
+    ret = nose.run(defaultTest=__file__)
+    if not ret: sys.exit(1)
