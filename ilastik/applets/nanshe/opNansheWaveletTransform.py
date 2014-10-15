24db771eb5942d6a3e43aebe37f1f0165f372c82
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-10-15 15:47:08 -0400

    ilastik/applets/nanshe/opNansheWaveletTransform.py: Dropped config quote.

--- ilastik/applets/nanshe/opNansheWaveletTransform.py
+++ ilastik/applets/nanshe/opNansheWaveletTransform.py
@@ -34,68 +34,7 @@ class OpNansheWaveletTransform(Operator):
     name = "OpNansheWaveletTransform"
     category = "Pointwise"
 
-    """ "preprocess_data" : {
 
-                "__comment__remove_zeroed_lines" : "Optional. Interpolates over missing lines that could not be registered. This is done by finding an outline around all missing points to use for calculating the interpolation. This step only works on 2D data.",
-
-                "remove_zeroed_lines" : {
-                    "__comment__dilation_shape" : "Kernel shape for performing dilation. Axis order is [y, x].",
-                    "__comment__erosion_shape" : "Kernel shape for performing erosion. Axis order is [y, x].",
-
-                    "erosion_shape" : [21, 1],
-                    "dilation_shape" : [1, 3]
-                },
-
-
-                "__comment__extract_f0" : "Optional. Estimates and removes f0 from the data using a percentile (rank order) filter.",
-
-                "extract_f0" : {
-
-                    "__comment__bias" : "To avoid division by zero, this constant is added to the data. If unspecified, a bias will be found so that the smallest value is 1.",
-
-                    "bias" : 100,
-
-
-                    "__comment__temporal_smoothing_gaussian_filter_stdev" : "What standard deviation to use for the smoothing gaussian applied along time.",
-
-                    "temporal_smoothing_gaussian_filter_stdev" : 5.0,
-
-
-                    "__comment__half_window_size" : "How many frames to include in half of the window. All windows are odd. So, the total window size will be 2 * half_window_size + 1.",
-
-                    "half_window_size" : 400,
-
-
-                    "__comment__which_quantile" : "The quantile to be used for filtering. Must be a single float from [0.0, 1.0]. If set to 0.5, this is a median filter.",
-
-                    "which_quantile" : 0.15,
-
-
-                    "__comment__spatial_smoothing_gaussian_filter_stdev" : "What standard deviation to use for the smoothing gaussian applied along each spatial dimension, independently.",
-
-                    "spatial_smoothing_gaussian_filter_stdev" : 5.0
-                },
-
-
-                "__comment__wavelet_transform" : "Optional. Runs a wavelet transform on the data.",
-
-                "wavelet_transform" : {
-
-                    "__comment__scale" : "This can be a single value, which is then applied on all axes or it can be an array. For the array, the axis order is [t, y, x] for 2D and [t, z, y, x] for 3D.",
-
-                    "scale" : [4, 4, 4]
-                },
-
-
-                "__comment__normalize_data" : "How to normalize data. L_2 norm recommended.",
-
-                "normalize_data" : {
-                    "simple_image_processing.renormalized_images": {
-                        "ord" : 2
-                    }
-                }
-            },"""
-    
     InputImage = InputSlot()
 
     Scale = InputSlot()
