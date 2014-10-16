75885e8696d6a8255a976b5d66f183c81cfd059a
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-10-16 16:04:03 -0400

    ilastik/workflows/nanshe/nansheWorkflow.py: Created workflow for nanshe.

---
+++ ilastik/workflows/nanshe/nansheWorkflow.py
@@ -0,0 +1,57 @@
+###############################################################################
+#   ilastik: interactive learning and segmentation toolkit
+#
+#       Copyright (C) 2011-2014, the ilastik developers
+#                                <team@ilastik.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# In addition, as a special exception, the copyright holders of
+# ilastik give you permission to combine ilastik with applets,
+# workflows and plugins which are not covered under the GNU
+# General Public License.
+#
+# See the LICENSE file for details. License information is also available
+# on the ilastik web site at:
+#		   http://ilastik.org/license.html
+###############################################################################
+from ilastik.workflow import Workflow
+
+from lazyflow.graph import Graph
+
+from ilastik.applets.dataSelection import DataSelectionApplet
+from ilastik.applets.nanshe.nanshePreprocessingApplet import NanshePreprocessingApplet
+
+class NansheWorkflow(Workflow):
+    def __init__(self, shell, headless, workflow_cmdline_args, project_creation_args):
+        # Create a graph to be shared by all operators
+        graph = Graph()
+        super(NansheWorkflow, self).__init__(shell, headless, workflow_cmdline_args, project_creation_args, graph=graph)
+        self._applets = []
+
+        # Create applets 
+        self.dataSelectionApplet = DataSelectionApplet(self, "Input Data", "Input Data", supportIlastik05Import=True, batchDataGui=False)
+        self.nanshePreprocessingApplet = NanshePreprocessingApplet(self, "Preprocessing", "NanshePreprocessing")
+        opDataSelection = self.dataSelectionApplet.topLevelOperator
+        opDataSelection.DatasetRoles.setValue( ['Raw Data'] )
+
+        self._applets.append( self.dataSelectionApplet )
+        self._applets.append( self.nanshePreprocessingApplet )
+
+    def connectLane(self, laneIndex):
+        opDataSelection = self.dataSelectionApplet.topLevelOperator.getLane(laneIndex)        
+        opThresholdMasking = self.nanshePreprocessingApplet.topLevelOperator.getLane(laneIndex)
+
+        # Connect top-level operators
+        opThresholdMasking.InputImage.connect( opDataSelection.Image )
+
+    @property
+    def applets(self):
+        return self._applets
+
+    @property
+    def imageNameListSlot(self):
+        return self.dataSelectionApplet.topLevelOperator.ImageName
