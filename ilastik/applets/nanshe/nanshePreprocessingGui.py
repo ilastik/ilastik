f375734ff39e41b7e07191cf7642eb34d437a135
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-10-16 15:28:24 -0400

    ilastik/applets/nanshe/nanshePreprocessingGui.py: Added a start for the GUI.

---
+++ ilastik/applets/nanshe/nanshePreprocessingGui.py
@@ -0,0 +1,80 @@
+###############################################################################
+#   ilastik: interactive learning and segmentation toolkit
+#
+#       Copyright (C) 2011-2014, the ilastik developers
+#                                <team@ilastik.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# In addition, as a special exception, the copyright holders of
+# ilastik give you permission to combine ilastik with applets,
+# workflows and plugins which are not covered under the GNU
+# General Public License.
+#
+# See the LICENSE file for details. License information is also available
+# on the ilastik web site at:
+#		   http://ilastik.org/license.html
+###############################################################################
+import os
+
+from PyQt4 import uic
+
+from ilastik.applets.layerViewer.layerViewerGui import LayerViewerGui
+
+class NanshePreprocessingGui(LayerViewerGui):
+    """
+    Simple example of an applet tha  
+    """
+    
+    ###########################################
+    ### AppletGuiInterface Concrete Methods ###
+    ###########################################
+
+    def appletDrawer(self):
+        return self._drawer
+
+    # (Other methods already provided by our base class)
+
+    ###########################################
+    ###########################################
+    
+    def __init__(self, parentApplet, topLevelOperatorView):
+        """
+        """
+        self.topLevelOperatorView = topLevelOperatorView
+        super(NanshePreprocessingGui, self).__init__(parentApplet, self.topLevelOperatorView)
+            
+    def initAppletDrawerUi(self):
+        # Load the ui file (find it in our own directory)
+        localDir = os.path.split(__file__)[0]
+        self._drawer = uic.loadUi(localDir+"/drawer.ui")
+
+        # Initialize the gui with the operator's current values
+        self.apply_operator_settings_to_gui()
+
+    def apply_operator_settings_to_gui(self):
+        pass
+
+    def apply_gui_settings_to_operator(self):
+        pass
+    
+    def setupLayers(self):
+        """
+        Overridden from LayerViewerGui.
+        Create a list of all layer objects that should be displayed.
+        """
+        layers = []
+        
+        # Show the raw input data
+        inputImageSlot = self.topLevelOperatorView.InputImage
+        if inputImageSlot.ready():
+            inputLayer = self.createStandardLayerFromSlot( inputImageSlot )
+            inputLayer.name = "Raw Input"
+            inputLayer.visible = True
+            inputLayer.opacity = 1.0
+            layers.append(inputLayer)
+
+        return layers
