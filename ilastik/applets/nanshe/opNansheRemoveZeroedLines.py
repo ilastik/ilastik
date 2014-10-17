1eb43c55276d506cff9cfa7e774b3cfa3beec117
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-10-17 13:13:10 -0400

    ilastik/applets/nanshe/opNansheRemoveZeroedLines.py: Add author and date.

--- ilastik/applets/nanshe/opNansheRemoveZeroedLines.py
+++ ilastik/applets/nanshe/opNansheRemoveZeroedLines.py
@@ -18,6 +18,12 @@
 # on the ilastik web site at:
 #		   http://ilastik.org/license.html
 ###############################################################################
+
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Oct 14, 2014 16:31:56 EDT$"
+
+
+
 from lazyflow.graph import Operator, InputSlot, OutputSlot
 
 import numpy
