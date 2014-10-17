cd5358c4e319563c6487559a0952c4e442a67a85
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-10-17 11:42:21 -0400

    ilastik/applets/nanshe/nanshePreprocessingGui.py: Created the ability for setting to be applied to the top level operator and vice versa.

--- ilastik/applets/nanshe/nanshePreprocessingGui.py
+++ ilastik/applets/nanshe/nanshePreprocessingGui.py
@@ -20,7 +20,7 @@
 ###############################################################################
 import os
 
-from PyQt4 import uic
+from PyQt4 import uic, QtCore
 
 from ilastik.applets.layerViewer.layerViewerGui import LayerViewerGui
 
@@ -45,6 +45,7 @@ class NanshePreprocessingGui(LayerViewerGui):
         """
         """
         self.topLevelOperatorView = topLevelOperatorView
+        self.ndim = 0
         super(NanshePreprocessingGui, self).__init__(parentApplet, self.topLevelOperatorView)
             
     def initAppletDrawerUi(self):
@@ -54,12 +55,186 @@ class NanshePreprocessingGui(LayerViewerGui):
 
         # Initialize the gui with the operator's current values
         self.apply_operator_settings_to_gui()
+        self._drawer.Apply.clicked.connect(self.apply_gui_settings_to_operator)
 
     def apply_operator_settings_to_gui(self):
-        pass
+        self.ndim = len(self.topLevelOperatorView.InputImage.meta.shape)
+
+        if not isinstance(self.topLevelOperatorView.Scale.value, (list, tuple)):
+            self.topLevelOperatorView.Scale.setValue(self.ndim*[self.topLevelOperatorView.Scale.value])
+
+        assert(4 <= self.ndim <= 5)
+
+        if self.ndim == 4:
+            self._drawer.ErosionShapeValue_Z.hide()
+            self._drawer.DilationShapeValue_Z.hide()
+            self._drawer.ScaleValue_Z.hide()
+
+            self._drawer.ErosionShapeLabel.setText(self._drawer.ErosionShapeLabel.text().replace("Z, ", ""))
+            self._drawer.DilationShapeLabel.setText(self._drawer.DilationShapeLabel.text().replace("Z, ", ""))
+            self._drawer.ScaleLabel.setText(self._drawer.ScaleLabel.text().replace("Z, ", ""))
+
+
+            self._drawer.RemoveZeroedLinesEnabled.setChecked(self.topLevelOperatorView.ToRemoveZeroedLines.value)
+
+            self._drawer.ErosionShapeValue_Y.setValue(self.topLevelOperatorView.ErosionShape.value[0])
+            self._drawer.ErosionShapeValue_X.setValue(self.topLevelOperatorView.ErosionShape.value[1])
+
+            self._drawer.DilationShapeValue_Y.setValue(self.topLevelOperatorView.DilationShape.value[0])
+            self._drawer.DilationShapeValue_X.setValue(self.topLevelOperatorView.DilationShape.value[1])
+
+
+            self._drawer.ExtractF0Enabled.setChecked(self.topLevelOperatorView.ToExtractF0.value)
+
+            self._drawer.HalfWindowSizeValue.setValue(self.topLevelOperatorView.HalfWindowSize.value)
+            self._drawer.QuantileValue.setValue(self.topLevelOperatorView.WhichQuantile.value)
+            self._drawer.TemporalSmoothingValue.setValue(self.topLevelOperatorView.TemporalSmoothingGaussianFilterStdev.value)
+            self._drawer.SpatialSmoothingValue.setValue(self.topLevelOperatorView.SpatialSmoothingGaussianFilterStdev.value)
+
+            if self.topLevelOperatorView.Bias.ready():
+                self._drawer.BiasEnabled.setChecked(True)
+                self._drawer.BiasValue.setEnabled(True)
+                self._drawer.BiasValue.setValue(self.topLevelOperatorView.Bias.value)
+            else:
+                self._drawer.BiasEnabled.setChecked(False)
+                self._drawer.BiasValue.setEnabled(False)
+                self._drawer.BiasValue.setValue(0)
+
+
+            self._drawer.WaveletTransformEnabled.setChecked(self.topLevelOperatorView.ToWaveletTransform.value)
+            self._drawer.ScaleValue_T.setValue(self.topLevelOperatorView.Scale.value[0])
+            self._drawer.ScaleValue_Y.setValue(self.topLevelOperatorView.Scale.value[1])
+            self._drawer.ScaleValue_X.setValue(self.topLevelOperatorView.Scale.value[2])
+
+
+            self._drawer.NormValue.setValue(self.topLevelOperatorView.Ord.value)
+
+        elif self.ndim == 5:
+            self._drawer.ErosionShapeValue_Z.show()
+            self._drawer.DilationShapeValue_Z.show()
+            self._drawer.ScaleValue_Z.show()
+
+            if "Z, " not in self._drawer.ErosionShapeLabel.text():
+                self._drawer.ErosionShapeLabel.setText(self._drawer.ErosionShapeLabel.text().replace("Y, ", "Z, Y, "))
+            if "Z, " not in self._drawer.DilationShapeLabel.text():
+                self._drawer.DilationShapeLabel.setText(self._drawer.DilationShapeLabel.text().replace("Y, ", "Z, Y, "))
+            if "Z, " not in self._drawer.ScaleLabel.text():
+                self._drawer.ScaleLabel.setText(self._drawer.ScaleLabel.text().replace("Y, ", "Z, Y, "))
+
+
+            self._drawer.RemoveZeroedLinesEnabled.setChecked(self.topLevelOperatorView.ToRemoveZeroedLines.value)
+
+            self._drawer.ErosionShapeValue_Z.setValue(self.topLevelOperatorView.ErosionShape.value[0])
+            self._drawer.ErosionShapeValue_Y.setValue(self.topLevelOperatorView.ErosionShape.value[1])
+            self._drawer.ErosionShapeValue_X.setValue(self.topLevelOperatorView.ErosionShape.value[2])
+
+            self._drawer.DilationShapeValue_Z.setValue(self.topLevelOperatorView.DilationShape.value[0])
+            self._drawer.DilationShapeValue_Y.setValue(self.topLevelOperatorView.DilationShape.value[1])
+            self._drawer.DilationShapeValue_X.setValue(self.topLevelOperatorView.DilationShape.value[2])
+
+
+            self._drawer.ExtractF0Enabled.setChecked(self.topLevelOperatorView.ToExtractF0.value)
+
+            self._drawer.HalfWindowSizeValue.setValue(self.topLevelOperatorView.HalfWindowSize.value)
+            self._drawer.QuantileValue.setValue(self.topLevelOperatorView.WhichQuantile.value)
+            self._drawer.TemporalSmoothingValue.setValue(self.topLevelOperatorView.TemporalSmoothingGaussianFilterStdev.value)
+            self._drawer.SpatialSmoothingValue.setValue(self.topLevelOperatorView.SpatialSmoothingGaussianFilterStdev.value)
+
+            if self.topLevelOperatorView.Bias.ready():
+                self._drawer.BiasEnabled.setChecked(True)
+                self._drawer.BiasValue.setEnabled(True)
+                self._drawer.BiasValue.setValue(self.topLevelOperatorView.Bias.value)
+            else:
+                self._drawer.BiasEnabled.setChecked(False)
+                self._drawer.BiasValue.setEnabled(False)
+
+
+            self._drawer.WaveletTransformEnabled.setChecked(self.topLevelOperatorView.ToWaveletTransform.value)
+            self._drawer.ScaleValue_T.setValue(self.topLevelOperatorView.Scale.value[0])
+            self._drawer.ScaleValue_Z.setValue(self.topLevelOperatorView.Scale.value[1])
+            self._drawer.ScaleValue_Y.setValue(self.topLevelOperatorView.Scale.value[2])
+            self._drawer.ScaleValue_X.setValue(self.topLevelOperatorView.Scale.value[3])
+
+
+            self._drawer.NormValue.setValue(self.topLevelOperatorView.Ord.value)
 
     def apply_gui_settings_to_operator(self):
-        pass
+        if self.ndim == 4:
+            self._drawer.ErosionShapeValue_Z.hide()
+            self._drawer.DilationShapeValue_Z.hide()
+            self._drawer.ScaleValue_Z.hide()
+
+            self._drawer.ErosionShapeLabel.setText(self._drawer.ErosionShapeLabel.text().replace("Z, ", ""))
+            self._drawer.DilationShapeLabel.setText(self._drawer.DilationShapeLabel.text().replace("Z, ", ""))
+            self._drawer.ScaleLabel.setText(self._drawer.ScaleLabel.text().replace("Z, ", ""))
+
+
+            self.topLevelOperatorView.ToRemoveZeroedLines.setValue(self._drawer.RemoveZeroedLinesEnabled.isChecked())
+
+            self.topLevelOperatorView.ErosionShape.setValue([self._drawer.ErosionShapeValue_Y.value(), self._drawer.ErosionShapeValue_X.value()])
+            self.topLevelOperatorView.DilationShape.setValue([self._drawer.DilationShapeValue_Y.value(), self._drawer.DilationShapeValue_X.value()])
+
+
+            self.topLevelOperatorView.ToExtractF0.setValue(self._drawer.ExtractF0Enabled.isChecked())
+
+            self.topLevelOperatorView.HalfWindowSize.setValue(self._drawer.HalfWindowSizeValue.value())
+            self.topLevelOperatorView.WhichQuantile.setValue(self._drawer.QuantileValue.value())
+            self.topLevelOperatorView.TemporalSmoothingGaussianFilterStdev.setValue(self._drawer.TemporalSmoothingValue.value())
+            self.topLevelOperatorView.SpatialSmoothingGaussianFilterStdev.setValue(self._drawer.SpatialSmoothingValue.value())
+
+            if self._drawer.BiasEnabled.isChecked():
+                self._drawer.BiasValue.setEnabled(True)
+                self.topLevelOperatorView.Bias.setValue(self._drawer.BiasValue.value())
+            else:
+                self._drawer.BiasValue.setEnabled(False)
+                self.topLevelOperatorView.Bias.setValue(None)
+
+
+            self.topLevelOperatorView.ToWaveletTransform.setValue(self._drawer.WaveletTransformEnabled.isChecked())
+            self.topLevelOperatorView.Scale.setValue([self._drawer.ScaleValue_T.value(), self._drawer.ScaleValue_Y.value(), self._drawer.ScaleValue_X.value()])
+
+
+            self.topLevelOperatorView.Ord.setValue(self._drawer.NormValue.value())
+
+        elif self.ndim == 5:
+            self._drawer.ErosionShapeValue_Z.show()
+            self._drawer.DilationShapeValue_Z.show()
+            self._drawer.ScaleValue_Z.show()
+
+            if "Z, " not in self._drawer.ErosionShapeLabel.text():
+                self._drawer.ErosionShapeLabel.setText(self._drawer.ErosionShapeLabel.text().replace("Y, ", "Z, Y, "))
+            if "Z, " not in self._drawer.DilationShapeLabel.text():
+                self._drawer.DilationShapeLabel.setText(self._drawer.DilationShapeLabel.text().replace("Y, ", "Z, Y, "))
+            if "Z, " not in self._drawer.ScaleLabel.text():
+                self._drawer.ScaleLabel.setText(self._drawer.ScaleLabel.text().replace("Y, ", "Z, Y, "))
+
+
+            self.topLevelOperatorView.ToRemoveZeroedLines.setValue(self._drawer.RemoveZeroedLinesEnabled.isChecked())
+
+            self.topLevelOperatorView.ErosionShape.setValue([self._drawer.ErosionShapeValue_Z.value(), self._drawer.ErosionShapeValue_Y.value(), self._drawer.ErosionShapeValue_X.value()])
+            self.topLevelOperatorView.DilationShape.setValue([self._drawer.DilationShapeValue_Z.value(), self._drawer.ErosionShapeValue_Y.value(), self._drawer.DilationShapeValue_X.value()])
+
+
+            self.topLevelOperatorView.ToExtractF0.setValue(self._drawer.ExtractF0Enabled.isChecked())
+
+            self.topLevelOperatorView.HalfWindowSize.setValue(self._drawer.HalfWindowSizeValue.value())
+            self.topLevelOperatorView.WhichQuantile.setValue(self._drawer.QuantileValue.value())
+            self.topLevelOperatorView.TemporalSmoothingGaussianFilterStdev.setValue(self._drawer.TemporalSmoothingValue.value())
+            self.topLevelOperatorView.SpatialSmoothingGaussianFilterStdev.setValue(self._drawer.SpatialSmoothingValue.value())
+
+            if self._drawer.BiasEnabled.isChecked():
+                self._drawer.BiasValue.setEnabled(True)
+                self.topLevelOperatorView.Bias.setValue(self._drawer.BiasValue.value())
+            else:
+                self._drawer.BiasValue.setEnabled(False)
+                self.topLevelOperatorView.Bias.setValue(None)
+
+
+            self.topLevelOperatorView.ToWaveletTransform.setValue(self._drawer.WaveletTransformEnabled.isChecked())
+            self.topLevelOperatorView.Scale.setValue([self._drawer.ScaleValue_T.value(), self._drawer.ScaleValue_Z.value(), self._drawer.ScaleValue_Y.value(), self._drawer.ScaleValue_X.value()])
+
+
+            self.topLevelOperatorView.Ord.setValue(self._drawer.NormValue.value())
     
     def setupLayers(self):
         """
@@ -73,6 +248,15 @@ class NanshePreprocessingGui(LayerViewerGui):
         if inputImageSlot.ready():
             inputLayer = self.createStandardLayerFromSlot( inputImageSlot )
             inputLayer.name = "Raw Input"
+            inputLayer.visible = True
+            inputLayer.opacity = 1.0
+            layers.append(inputLayer)
+
+        # Show the raw input data
+        outputImageSlot = self.topLevelOperatorView.Output
+        if outputImageSlot.ready():
+            inputLayer = self.createStandardLayerFromSlot( outputImageSlot )
+            inputLayer.name = "Output"
             inputLayer.visible = True
             inputLayer.opacity = 1.0
             layers.append(inputLayer)
