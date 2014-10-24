36f1cc673729ea7a0623a48f57815f166229db7d
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-10-24 12:00:08 -0400

    ilastik/applets/nanshe/postprocessing/opNanshePostprocessing.py: Added the overarching operator for all of postprocessing (with caching).

---
+++ ilastik/applets/nanshe/postprocessing/opNanshePostprocessing.py
@@ -0,0 +1,100 @@
+###############################################################################
+#   ilastik: interactive learning and segmentation toolkit
+#
+#       Copyright (C) 2011-2014, the ilastik developers
+#                                <team@ilastik.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# In addition, as a special exception, the copyright holders of
+# ilastik give you permission to combine ilastik with applets,
+# workflows and plugins which are not covered under the GNU
+# General Public License.
+#
+# See the LICENSE file for details. License information is also available
+# on the ilastik web site at:
+#		   http://ilastik.org/license.html
+###############################################################################
+
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Oct 23, 2014 09:45:39 EDT$"
+
+
+
+from lazyflow.graph import Operator, InputSlot, OutputSlot
+
+from ilastik.applets.nanshe.postprocessing.opNanshePostprocessData import OpNanshePostprocessDataCached
+
+import vigra
+
+import numpy
+
+import nanshe
+import nanshe.expanded_numpy
+import nanshe.advanced_image_processing
+
+
+class OpNanshePostprocessing(Operator):
+    """
+    Given an input image and max/min bounds,
+    masks out (i.e. sets to zero) all pixels that fall outside the bounds.
+    """
+    name = "OpNanshePostprocessing"
+    category = "Pointwise"
+
+
+    InputImage = InputSlot()
+
+    SignificanceThreshold = InputSlot(value=3.0, stype="float")
+    WaveletTransformScale = InputSlot(value=4, stype="int")
+    NoiseThreshold = InputSlot(value=4.0, stype="float")
+    AcceptedRegionShapeConstraints_MajorAxisLength_Min = InputSlot(value=0.0, stype="float", optional=True)
+    AcceptedRegionShapeConstraints_MajorAxisLength_Max = InputSlot(value=25.0, stype="float", optional=True)
+
+    PercentagePixelsBelowMax = InputSlot(value=0.8, stype="float")
+    MinLocalMaxDistance = InputSlot(value=20.0, stype="float")
+
+    AcceptedNeuronShapeConstraints_Area_Min = InputSlot(value=45, stype="int", optional=True)
+    AcceptedNeuronShapeConstraints_Area_Max = InputSlot(value=60, stype="int", optional=True)
+
+    AcceptedNeuronShapeConstraints_Eccentricity_Min = InputSlot(value=0.0, stype="float", optional=True)
+    AcceptedNeuronShapeConstraints_Eccentricity_Max = InputSlot(value=0.9, stype="float", optional=True)
+
+    AlignmentMinThreshold = InputSlot(value=0.6, stype="float")
+    OverlapMinThreshold = InputSlot(value=0.6, stype="float")
+
+    Fuse_FractionMeanNeuronMaxThreshold = InputSlot(value=0.01, stype="float")
+
+    Output = OutputSlot()
+
+    def __init__(self, *args, **kwargs):
+        super( OpNanshePostprocessing, self ).__init__( *args, **kwargs )
+
+        self.opPostprocess = OpNanshePostprocessDataCached(parent=self)
+
+        self.opPostprocess.SignificanceThreshold.connect(self.SignificanceThreshold)
+        self.opPostprocess.WaveletTransformScale.connect(self.WaveletTransformScale)
+        self.opPostprocess.NoiseThreshold.connect(self.NoiseThreshold)
+        self.opPostprocess.AcceptedRegionShapeConstraints_MajorAxisLength_Min.connect(self.AcceptedRegionShapeConstraints_MajorAxisLength_Min)
+        self.opPostprocess.AcceptedRegionShapeConstraints_MajorAxisLength_Max.connect(self.AcceptedRegionShapeConstraints_MajorAxisLength_Max)
+        self.opPostprocess.PercentagePixelsBelowMax.connect(self.PercentagePixelsBelowMax)
+        self.opPostprocess.MinLocalMaxDistance.connect(self.MinLocalMaxDistance)
+        self.opPostprocess.AcceptedNeuronShapeConstraints_Area_Min.connect(self.AcceptedNeuronShapeConstraints_Area_Min)
+        self.opPostprocess.AcceptedNeuronShapeConstraints_Area_Max.connect(self.AcceptedNeuronShapeConstraints_Area_Max)
+        self.opPostprocess.AcceptedNeuronShapeConstraints_Eccentricity_Min.connect(self.AcceptedNeuronShapeConstraints_Eccentricity_Min)
+        self.opPostprocess.AcceptedNeuronShapeConstraints_Eccentricity_Max.connect(self.AcceptedNeuronShapeConstraints_Eccentricity_Max)
+        self.opPostprocess.AlignmentMinThreshold.connect(self.AlignmentMinThreshold)
+        self.opPostprocess.OverlapMinThreshold.connect(self.OverlapMinThreshold)
+        self.opPostprocess.Fuse_FractionMeanNeuronMaxThreshold.connect(self.Fuse_FractionMeanNeuronMaxThreshold)
+
+        self.opPostprocess.InputImage.connect( self.InputImage )
+        self.Output.connect( self.opPostprocess.Output )
+
+    def setupOutputs(self):
+        self.Output.meta.assignFrom( self.opPostprocess.Output.meta )
+
+    def propagateDirty(self, slot, subindex, roi):
+        pass
