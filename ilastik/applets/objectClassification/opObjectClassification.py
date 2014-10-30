d44d19a7f21d3fad4cd15bfdc21615f1c0a93f8d
Janez Ales <janez.ales@iwr.uni-heidelberg.de>
2014-10-30 09:24:28 +0100

    Passing labels to the constructor only.

--- ilastik/applets/objectClassification/opObjectClassification.py
+++ ilastik/applets/objectClassification/opObjectClassification.py
@@ -782,7 +782,7 @@ class OpObjectTrain(Operator):
             return
         allLabels=map(long, range(1,numLabels+1))
         classifier_factory = ParallelVigraRfLazyflowClassifierFactory( self._tree_count, self.ForestCount.value, labels=allLabels )
-        classifier = classifier_factory.create_and_train( featMatrix.astype(numpy.float32), numpy.asarray(labelsMatrix, dtype=numpy.uint32), labels=allLabels )
+        classifier = classifier_factory.create_and_train( featMatrix.astype(numpy.float32), numpy.asarray(labelsMatrix, dtype=numpy.uint32) )
         avg_oob = numpy.mean(classifier.oobs)
         logger.info("training finished, average out-of-bag error: {}".format(avg_oob))
         result[0] = classifier
