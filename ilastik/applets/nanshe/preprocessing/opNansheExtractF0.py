92bb95caccced6bfcafe57574658b52fd72df619
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-11-05 12:02:20 -0500

    ilastik/applets/nanshe/preprocessing/opNansheExtractF0.py: Changed blocked array cache's shapes to use a smaller number of frames.

--- ilastik/applets/nanshe/preprocessing/opNansheExtractF0.py
+++ ilastik/applets/nanshe/preprocessing/opNansheExtractF0.py
@@ -203,7 +203,16 @@ class OpNansheExtractF0Cached(Operator):
     def setupOutputs(self):
         axes_shape_iter = itertools.izip(self.opExtractF0.Output.meta.axistags, self.opExtractF0.Output.meta.shape)
 
-        block_shape = [_v if not _k.isSpatial() else 256 for _k, _v in axes_shape_iter]
+        block_shape = []
+
+        for each_axistag, each_len in axes_shape_iter:
+            if each_axistag.isSpatial():
+                each_len = 256
+            elif each_axistag.isTemporal():
+                each_len = 10
+
+            block_shape.append(each_len)
+
         block_shape = tuple(block_shape)
 
         self.opCache.innerBlockShape.setValue(block_shape)
