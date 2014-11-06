845d9d8460038ff2e1cfa59210954372cdd39184
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-11-06 16:54:03 -0500

    ilastik/applets/nanshe/preprocessing/opNansheWaveletTransform.py: Take floor when determining center.

--- ilastik/applets/nanshe/preprocessing/opNansheWaveletTransform.py
+++ ilastik/applets/nanshe/preprocessing/opNansheWaveletTransform.py
@@ -25,6 +25,7 @@ __date__ = "$Oct 14, 2014 16:35:36 EDT$"
 
 
 import itertools
+import math
 
 from lazyflow.graph import Operator, InputSlot, OutputSlot
 
@@ -184,7 +185,8 @@ class OpNansheWaveletTransformCached(Operator):
 
             if each_axistag.isTemporal() or each_axistag.isSpatial():
                 each_halo_center /= 2.0
-                each_halo_center = int(round(each_halo_center))
+                # Must take floor consider the singleton dimension case
+                each_halo_center = int(math.floor(each_halo_center))
                 each_halo_center_slicing = slice(each_halo_center, each_halo_center + 1, 1)
 
             halo_center_slicing.append(each_halo_center_slicing)
