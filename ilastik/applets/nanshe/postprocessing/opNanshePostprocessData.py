91b87f4aee1b86e926234e33ebcc388cf95c25c3
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-11-10 14:49:55 -0500

    ilastik/applets/nanshe/postprocessing/opNanshePostprocessData.py: Correct area max.

--- ilastik/applets/nanshe/postprocessing/opNanshePostprocessData.py
+++ ilastik/applets/nanshe/postprocessing/opNanshePostprocessData.py
@@ -62,7 +62,7 @@ class OpNanshePostprocessData(Operator):
 
     AcceptedNeuronShapeConstraints_Area_Min = InputSlot(value=45, stype="int")
     AcceptedNeuronShapeConstraints_Area_Min_Enabled = InputSlot(value=True, stype="bool")
-    AcceptedNeuronShapeConstraints_Area_Max = InputSlot(value=60, stype="int")
+    AcceptedNeuronShapeConstraints_Area_Max = InputSlot(value=600, stype="int")
     AcceptedNeuronShapeConstraints_Area_Max_Enabled = InputSlot(value=True, stype="bool")
 
     AcceptedNeuronShapeConstraints_Eccentricity_Min = InputSlot(value=0.0, stype="float")
@@ -278,7 +278,7 @@ class OpNanshePostprocessDataCached(Operator):
 
     AcceptedNeuronShapeConstraints_Area_Min = InputSlot(value=45, stype="int")
     AcceptedNeuronShapeConstraints_Area_Min_Enabled = InputSlot(value=True, stype="bool")
-    AcceptedNeuronShapeConstraints_Area_Max = InputSlot(value=60, stype="int")
+    AcceptedNeuronShapeConstraints_Area_Max = InputSlot(value=600, stype="int")
     AcceptedNeuronShapeConstraints_Area_Max_Enabled = InputSlot(value=True, stype="bool")
 
     AcceptedNeuronShapeConstraints_Eccentricity_Min = InputSlot(value=0.0, stype="float")
