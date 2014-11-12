9b924886a86b7c4f1d4c077df8069d25fce65f5b
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-11-12 15:25:32 -0500

    ilastik/applets/nanshe/opMeanProjection.py: Added OpMeanProjection for computing the mean projection of an image stack.

--- ilastik/applets/nanshe/opMeanProjection.py
+++ ilastik/applets/nanshe/opMeanProjection.py
@@ -23,3 +23,89 @@ __date__ = "$Nov 12, 2014 15:20:09 EST$"
 
 
 
+import itertools
+import math
+
+from lazyflow.graph import Operator, InputSlot, OutputSlot
+
+import vigra
+
+import nanshe
+import nanshe.wavelet_transform
+import nanshe.additional_generators
+
+
+class OpMeanProjection(Operator):
+    """
+    Given an input image and max/min bounds,
+    masks out (i.e. sets to zero) all pixels that fall outside the bounds.
+    """
+    name = "OpMeanProjection"
+    category = "Pointwise"
+
+
+    InputImage = InputSlot()
+
+    Axis = InputSlot(value=0, stype="int")
+
+    Output = OutputSlot()
+
+    def __init__(self, *args, **kwargs):
+        super( OpMeanProjection, self ).__init__( *args, **kwargs )
+
+        self._generation = {self.name : 0}
+
+    def setupOutputs(self):
+        # Copy the input metadata to both outputs
+        self.Output.meta.assignFrom( self.InputImage.meta )
+
+        self.Output.meta.shape = self.Output.meta.shape[:self.Axis.value] +\
+                                 (1,) +\
+                                 self.Output.meta.shape[self.Axis.value+1:]
+
+        self.Output.meta.generation = self._generation
+
+    @staticmethod
+    def compute_halo(slicing, image_shape, axis):
+        slicing = nanshe.additional_generators.reformat_slices(slicing, image_shape)
+
+        halo_slicing = slicing
+        halo_slicing = list(halo_slicing)
+        halo_slicing[axis] = nanshe.additional_generators.reformat_slice(slice(None), image_shape[axis])
+        halo_slicing = tuple(halo_slicing)
+
+        within_halo_slicing = halo_slicing
+        within_halo_slicing = list(within_halo_slicing)
+        within_halo_slicing[axis] = slice(0, 1, 1)
+        within_halo_slicing = tuple(within_halo_slicing)
+
+        return(halo_slicing, within_halo_slicing)
+
+    def execute(self, slot, subindex, roi, result):
+        image_shape = self.InputImage.meta.shape
+
+        key = roi.toSlice()
+        halo_key, within_halo_key = OpMeanProjection.compute_halo(key, image_shape, self.Axis.value)
+
+        raw = self.InputImage[halo_key].wait()
+
+        processed = raw.mean(axis=self.Axis.value)
+        processed = nanshe.expanded_numpy.add_singleton_axis_pos(processed, self.Axis.value)
+
+        if slot.name == 'Output':
+            result[...] = processed
+
+    def setInSlot(self, slot, subindex, roi, value):
+        pass
+
+    def propagateDirty(self, slot, subindex, roi):
+        if slot.name == "InputImage":
+            self._generation[self.name] += 1
+            self.Output.setDirty(OpMeanProjection.compute_halo(roi.toSlice(),
+                                                              self.InputImage.meta.shape,
+                                                              self.Axis.value)[0])
+        elif slot.name == "Axis":
+            self._generation[self.name] += 1
+            self.Output.setDirty( slice(None) )
+        else:
+            assert False, "Unknown dirty input slot"
