773f6bc6f82385c06817ea18cd68588055e9dbe4
Stuart Berg <bergs@janelia.hhmi.org>
2014-11-12 10:17:32 -0500

    logging: If logfile is '/dev/null', force console-only mode.

--- ilastik/ilastik_logging/default_config.py
+++ ilastik/ilastik_logging/default_config.py
@@ -217,6 +217,10 @@ def get_default_config( prefix="",
     return default_log_config
 
 def init(format_prefix="", output_mode=OutputMode.LOGFILE_WITH_CONSOLE_ERRORS, logfile_path=DEFAULT_LOGFILE_PATH):
+    if output_mode == "/dev/null":
+        assert output_mode != OutputMode.LOGFILE, "Must enable a logging mode."
+        output_mode = OutputMode.CONSOLE
+
     # Start with the default
     logging.config.dictConfig( get_default_config( format_prefix, output_mode, logfile_path ) )
     
