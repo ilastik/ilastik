599b2ce127d1b73d69b38d070e8fa8e67ece2910
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-11-12 16:31:45 -0500

    ilastik/applets/nanshe/dataExport/nansheDataExportApplet.py: Provided NansheDataExportApplet, which overrides DataExportApplet to use OpNansheDataExport instead of OpDataExport.

--- ilastik/applets/nanshe/dataExport/nansheDataExportApplet.py
+++ ilastik/applets/nanshe/dataExport/nansheDataExportApplet.py
@@ -24,9 +24,32 @@ __date__ = "$Nov 10, 2014 17:28:16 EST$"
 
 
 from ilastik.applets.dataExport.dataExportApplet import DataExportApplet
+from ilastik.applets.dataExport.dataExportSerializer import DataExportSerializer
+from ilastik.utility import OpMultiLaneWrapper
+
+from opNansheDataExport import OpNansheDataExport
 
 
 class NansheDataExportApplet(DataExportApplet):
+    def __init__( self, workflow, title, isBatch=False ):
+        # Our operator is a subclass of the generic data export operator
+        self._topLevelOperator = OpMultiLaneWrapper( OpNansheDataExport, parent=workflow,
+                                         promotedSlotNames=set(['RawData', 'Inputs', 'RawDatasetInfo']) )
+        self._gui = None
+        self._title = title
+        self._serializers = [ DataExportSerializer(self._topLevelOperator, title) ]
+
+        # Base class init
+        super(NansheDataExportApplet, self).__init__(workflow, title, isBatch)
+
+    @property
+    def dataSerializers(self):
+        return self._serializers
+
+    @property
+    def topLevelOperator(self):
+        return self._topLevelOperator
+
     def getMultiLaneGui(self):
         if self._gui is None:
             from nansheDataExportGui import NansheDataExportGui
