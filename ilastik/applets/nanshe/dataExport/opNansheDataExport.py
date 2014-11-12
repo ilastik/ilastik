bc9e503f17958b5598c1b0a024053a355f19cd7f
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-11-12 16:29:41 -0500

    ilastik/applets/nanshe/dataExport/opNansheDataExport.py: Provides OpNansheDataExport, which overrides OpDataExport, to handle the display of the max and mean projection of the original data.

--- ilastik/applets/nanshe/dataExport/opNansheDataExport.py
+++ ilastik/applets/nanshe/dataExport/opNansheDataExport.py
@@ -23,3 +23,76 @@ __date__ = "$Nov 12, 2014 15:12:47 EST$"
 
 
 
+from lazyflow.graph import OutputSlot
+
+from ilastik.applets.dataExport.opDataExport import OpDataExport,OpRawSubRegionHelper, OpFormattedDataExport
+
+from ilastik.applets.nanshe.opMaxProjection import OpMaxProjection
+from ilastik.applets.nanshe.opMeanProjection import OpMeanProjection
+
+
+class OpNansheDataExport( OpDataExport ):
+    # Add these additional input slots, to be used by the GUI.
+    FormattedMaxProjection = OutputSlot()
+    FormattedMeanProjection = OutputSlot()
+
+    def __init__(self,*args,**kwargs):
+        super(OpNansheDataExport, self).__init__(*args, **kwargs)
+
+
+        # We don't export the max projection, but we connect it to it's own op
+        # so it can be displayed alongside the data to export in the same viewer.
+        # This keeps axis order, shape, etc. in sync with the displayed export data.
+        # Note that we must not modify the channels of the raw data, so it gets passed through a helper.
+        self._opMax = OpMaxProjection( parent=self )
+        self._opMax.InputImage.connect( self.RawData )
+        self._opMax.Axis.setValue( 0 )
+
+        self._opMaxHelper = OpRawSubRegionHelper( parent=self )
+        self._opMaxHelper.RawImage.connect( self._opMax.Output )
+        self._opMaxHelper.ExportStart.connect( self.RegionStart )
+        self._opMaxHelper.ExportStop.connect( self.RegionStop )
+
+        self._opFormatMax = OpFormattedDataExport( parent=self )
+        self._opFormatMax.TransactionSlot.connect( self.TransactionSlot )
+        self._opFormatMax.Input.connect( self._opMax.Output )
+        self._opFormatMax.RegionStart.connect( self._opMaxHelper.RawStart )
+        self._opFormatMax.RegionStop.connect( self._opMaxHelper.RawStop )
+        # Don't normalize the raw data.
+        #self._opFormatMax.InputMin.connect( self.InputMin )
+        #self._opFormatMax.InputMax.connect( self.InputMax )
+        #self._opFormatMax.ExportMin.connect( self.ExportMin )
+        #self._opFormatMax.ExportMax.connect( self.ExportMax )
+        #self._opFormatMax.ExportDtype.connect( self.ExportDtype )
+        self._opFormatMax.OutputAxisOrder.connect( self.OutputAxisOrder )
+        self._opFormatMax.OutputFormat.connect( self.OutputFormat )
+        self.FormattedMaxProjection.connect( self._opFormatMax.ImageToExport )
+
+
+        # We don't export the mean projection, but we connect it to it's own op
+        # so it can be displayed alongside the data to export in the same viewer.
+        # This keeps axis order, shape, etc. in sync with the displayed export data.
+        # Note that we must not modify the channels of the raw data, so it gets passed through a helper.
+        self._opMean = OpMeanProjection( parent=self )
+        self._opMean.InputImage.connect( self.RawData )
+        self._opMean.Axis.setValue( 0 )
+
+        self._opMeanHelper = OpRawSubRegionHelper( parent=self )
+        self._opMeanHelper.RawImage.connect( self._opMean.Output )
+        self._opMeanHelper.ExportStart.connect( self.RegionStart )
+        self._opMeanHelper.ExportStop.connect( self.RegionStop )
+
+        self._opFormatMean = OpFormattedDataExport( parent=self )
+        self._opFormatMean.TransactionSlot.connect( self.TransactionSlot )
+        self._opFormatMean.Input.connect( self._opMean.Output )
+        self._opFormatMean.RegionStart.connect( self._opMeanHelper.RawStart )
+        self._opFormatMean.RegionStop.connect( self._opMeanHelper.RawStop )
+        # Don't normalize the raw data.
+        #self._opFormatMean.InputMin.connect( self.InputMin )
+        #self._opFormatMean.InputMax.connect( self.InputMax )
+        #self._opFormatMean.ExportMin.connect( self.ExportMin )
+        #self._opFormatMean.ExportMax.connect( self.ExportMax )
+        #self._opFormatMean.ExportDtype.connect( self.ExportDtype )
+        self._opFormatMean.OutputAxisOrder.connect( self.OutputAxisOrder )
+        self._opFormatMean.OutputFormat.connect( self.OutputFormat )
+        self.FormattedMeanProjection.connect( self._opFormatMean.ImageToExport )
