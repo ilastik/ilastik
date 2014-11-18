da29eb419bece51ee151012222b57f23f3052b7d
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-11-18 15:40:07 -0500

    ilastik/applets/nanshe/opMeanProjection.py: Remove singleton dimension from mean projection.

--- ilastik/applets/nanshe/opMeanProjection.py
+++ ilastik/applets/nanshe/opMeanProjection.py
@@ -23,9 +23,6 @@ __date__ = "$Nov 12, 2014 15:20:09 EST$"
 
 
 
-import itertools
-import math
-
 from lazyflow.graph import Operator, InputSlot, OutputSlot
 from lazyflow.operators import OpBlockedArrayCache
 
@@ -60,38 +57,27 @@ class OpMeanProjection(Operator):
         # Copy the input metadata to both outputs
         self.Output.meta.assignFrom( self.InputImage.meta )
 
+        self.Output.meta.axistags = vigra.AxisTags(
+            *list(nanshe.additional_generators.iter_with_skip_indices( self.Output.meta.axistags, self.Axis.value))
+        )
+
         self.Output.meta.shape = self.Output.meta.shape[:self.Axis.value] +\
-                                 (1,) +\
                                  self.Output.meta.shape[self.Axis.value+1:]
 
         self.Output.meta.generation = self._generation
 
-    @staticmethod
-    def compute_halo(slicing, image_shape, axis):
-        slicing = nanshe.additional_generators.reformat_slices(slicing, image_shape)
-
-        halo_slicing = slicing
-        halo_slicing = list(halo_slicing)
-        halo_slicing[axis] = nanshe.additional_generators.reformat_slice(slice(None), image_shape[axis])
-        halo_slicing = tuple(halo_slicing)
-
-        within_halo_slicing = halo_slicing
-        within_halo_slicing = list(within_halo_slicing)
-        within_halo_slicing[axis] = slice(0, 1, 1)
-        within_halo_slicing = tuple(within_halo_slicing)
-
-        return(halo_slicing, within_halo_slicing)
-
     def execute(self, slot, subindex, roi, result):
-        image_shape = self.InputImage.meta.shape
+        axis = self.Axis.value
 
         key = roi.toSlice()
-        halo_key, within_halo_key = OpMeanProjection.compute_halo(key, image_shape, self.Axis.value)
+        key = list(key)
+        key = key[:axis] + [slice(None)] + key[axis:]
+        key[axis] = nanshe.additional_generators.reformat_slice(key[axis], self.InputImage.meta.shape[axis])
+        key = tuple(key)
 
-        raw = self.InputImage[halo_key].wait()
+        raw = self.InputImage[key].wait()
 
         processed = raw.mean(axis=self.Axis.value)
-        processed = nanshe.expanded_numpy.add_singleton_axis_pos(processed, self.Axis.value)
 
         if slot.name == 'Output':
             result[...] = processed
@@ -102,9 +88,15 @@ class OpMeanProjection(Operator):
     def propagateDirty(self, slot, subindex, roi):
         if slot.name == "InputImage":
             self._generation[self.name] += 1
-            self.Output.setDirty(OpMeanProjection.compute_halo(roi.toSlice(),
-                                                              self.InputImage.meta.shape,
-                                                              self.Axis.value)[0])
+
+            axis = self.Axis.value
+
+            slicing = roi.toSlice()
+            slicing = list(slicing)
+            slicing = slicing[:axis] + slicing[axis+1:]
+            slicing = tuple(slicing)
+
+            self.Output.setDirty(slicing)
         elif slot.name == "Axis":
             self._generation[self.name] += 1
             self.Output.setDirty( slice(None) )
@@ -143,43 +135,16 @@ class OpMeanProjectionCached(Operator):
         self.Output.connect( self.opCache.Output )
 
     def setupOutputs(self):
-        axes_shape_iter = itertools.izip(self.opMeanProjection.Output.meta.axistags,
-                                         self.opMeanProjection.Output.meta.shape)
-
-        halo_center_slicing = []
-
-        for each_axistag, each_len in axes_shape_iter:
-            each_halo_center = each_len
-            each_halo_center_slicing = slice(0, each_len, 1)
-
-            if each_axistag.isTemporal() or each_axistag.isSpatial():
-                each_halo_center /= 2.0
-                # Must take floor consider the singleton dimension case
-                each_halo_center = int(math.floor(each_halo_center))
-                each_halo_center_slicing = slice(each_halo_center, each_halo_center + 1, 1)
-
-            halo_center_slicing.append(each_halo_center_slicing)
-
-        halo_center_slicing = tuple(halo_center_slicing)
-
-        halo_slicing = self.opMeanProjection.compute_halo(halo_center_slicing,
-                                                         self.InputImage.meta.shape,
-                                                         self.Axis.value)[0]
-
-
-        block_shape = nanshe.additional_generators.len_slices(halo_slicing)
-
+        block_shape = self.opMeanProjection.Output.meta.shape
 
         block_shape = list(block_shape)
-
         for i, each_axistag in enumerate(self.opMeanProjection.Output.meta.axistags):
             if each_axistag.isSpatial():
-                block_shape[i] = max(block_shape[i], 256)
+                block_shape[i] = 256
 
             block_shape[i] = min(block_shape[i], self.opMeanProjection.Output.meta.shape[i])
 
         block_shape = tuple(block_shape)
-
 
         self.opCache.innerBlockShape.setValue(block_shape)
         self.opCache.outerBlockShape.setValue(block_shape)
