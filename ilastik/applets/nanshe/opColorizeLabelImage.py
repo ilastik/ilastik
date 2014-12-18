298e7747fc986d34b3371492c8ab5912c15cee10
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-12-18 18:52:39 -0500

    ilastik/applets/nanshe/opColorizeLabelImage.py: An operator to lazily colorize label images. Requires matplotlib.

--- ilastik/applets/nanshe/opColorizeLabelImage.py
+++ ilastik/applets/nanshe/opColorizeLabelImage.py
@@ -23,4 +23,95 @@ __date__ = "$Oct 24, 2014 08:05:35 EDT$"
 
 
 
+from lazyflow.graph import Operator, InputSlot, OutputSlot
 
+import numpy
+import matplotlib
+import matplotlib.colors
+import matplotlib.cm
+
+import vigra
+
+import nanshe
+import nanshe.additional_generators
+import nanshe.advanced_image_processing
+import nanshe.expanded_numpy
+
+
+class OpColorizeLabelImage(Operator):
+    """
+        Lazy computation of colors
+    """
+    name = "OpColorizeLabelImage"
+    category = "Pointwise"
+
+
+    InputImage = InputSlot()
+
+    Output = OutputSlot()
+
+    @staticmethod
+    def colorTableList(num_colors = 256):
+        colors = []
+
+        # Transparent for the zero label
+        colors.append((0,0,0))
+
+        rgb_color_values = list(nanshe.additional_generators.splitting_xrange(num_colors))
+        converter = matplotlib.colors.ColorConverter()
+
+        for _ in rgb_color_values:
+            a_rgb_color = tuple()
+            for __ in converter.to_rgb(matplotlib.cm.gist_rainbow(_)):
+                 a_rgb_color += ( int(round(255*__)), )
+
+            colors.append(a_rgb_color)
+
+        colors = numpy.asarray(colors, dtype=numpy.uint8)
+
+        return(colors)
+
+    def __init__(self, *args, **kwargs):
+        super( OpColorizeLabelImage, self ).__init__( *args, **kwargs )
+
+        self.colors = OpColorizeLabelImage.colorTableList()
+
+    def setupOutputs(self):
+        # Copy the input metadata to both outputs
+        self.Output.meta.assignFrom( self.InputImage.meta )
+        self.Output.meta.shape = self.Output.meta.shape[:-1] + (3,)
+        self.Output.meta.dtype = numpy.uint8
+
+        dims = [_ for _ in self.Output.meta.axistags if not _.isChannel()]
+
+        self.Output.meta.axistags = vigra.AxisTags(*(dims + [vigra.AxisInfo.c]))
+
+    def execute(self, slot, subindex, roi, result):
+        key = roi.toSlice()
+
+        input_key = list(key)
+        input_key = input_key[:-1] + [slice(None)]
+        input_key = tuple(input_key)
+
+        raw = self.InputImage[input_key].wait()
+
+        processed = numpy.empty(nanshe.additional_generators.len_slices(key), dtype=numpy.uint8)
+        for each_label in numpy.unique(raw):
+            mask = (raw == each_label)
+            mask = mask[..., 0]
+            processed[mask, :] = self.colors[each_label, key[-1]]
+
+        if slot.name == 'Output':
+            result[...] = processed
+
+    def propagateDirty(self, slot, subindex, roi):
+        if (slot.name == "InputImage"):
+            key = roi.toSlice()
+
+            key = list(key)
+            key = key[:-1] + [slice(None)]
+            key = tuple(key)
+
+            self.Output.setDirty(key)
+        else:
+            assert False, "Unknown dirty input slot"
