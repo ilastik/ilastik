25f182df09eb7a549fbdd4327e9ddb9906d681a1
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-12-19 16:48:30 -0500

    tests/nanshe/testOpColorizeLabelImage.py: Added a test for OpColorizeLabelImage.

--- tests/nanshe/testOpColorizeLabelImage.py
+++ tests/nanshe/testOpColorizeLabelImage.py
@@ -22,3 +22,48 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Dec 19, 2014 16:00:58 EST$"
 
 
+
+import numpy
+
+import vigra
+
+from lazyflow.graph import Graph
+from lazyflow.operators import OpArrayPiper
+
+from nanshe.expanded_numpy import expand_view, array_to_matrix
+
+import ilastik
+import ilastik.applets
+import ilastik.applets.nanshe
+import ilastik.applets.nanshe.opColorizeLabelImage
+from ilastik.applets.nanshe.opColorizeLabelImage import OpColorizeLabelImage
+
+class TestOpColorizeLabelImage(object):
+    def testBasic(self):
+        a = numpy.arange(256)
+        a = expand_view(a, 256)
+        a = a[..., None]
+        a = vigra.taggedView(a, "xyc")
+
+
+        graph = Graph()
+        op = OpColorizeLabelImage(graph=graph)
+
+        opPrep = OpArrayPiper(graph=graph)
+        opPrep.Input.setValue(a)
+
+        op.InputImage.connect(opPrep.Output)
+
+        b = op.Output[...].wait()
+
+        b_colors = set([tuple(_) for _ in list(array_to_matrix(b.T).T)])
+        assert(len(b_colors) == 256)
+
+
+if __name__ == "__main__":
+    import sys
+    import nose
+    sys.argv.append("--nocapture")    # Don't steal stdout.  Show it on the console as usual.
+    sys.argv.append("--nologcapture") # Don't set the logging level to DEBUG.  Leave it alone.
+    ret = nose.run(defaultTest=__file__)
+    if not ret: sys.exit(1)
