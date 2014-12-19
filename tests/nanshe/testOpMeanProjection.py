0ea1315c3d4f34c84a3ff83c0e77fbe9862190cf
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-12-19 15:47:27 -0500

    tests/nanshe/testOpMeanProjection.py: Added a stub for testing operators from opMeanProjection.

---
+++ tests/nanshe/testOpMeanProjection.py
@@ -0,0 +1,25 @@
+###############################################################################
+#   lazyflow: data flow based lazy parallel computation framework
+#
+#       Copyright (C) 2011-2014, the ilastik developers
+#                                <team@ilastik.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the Lesser GNU General Public License
+# as published by the Free Software Foundation; either version 2.1
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# See the files LICENSE.lgpl2 and LICENSE.lgpl3 for full text of the
+# GNU Lesser General Public License version 2.1 and 3 respectively.
+# This information is also available on the ilastik web site at:
+#		   http://ilastik.org/license/
+###############################################################################
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Dec 19, 2014 15:46:32 EST$"
+
+
