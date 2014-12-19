7727fca11940d4ff2ba564e9bf76e3735e70b411
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-12-19 14:11:43 -0500

    tests/nanshe/dictionaryLearning/testOpNansheDictionaryLearning.py: Used taggedView instead of reconstructing the array.

--- tests/nanshe/dictionaryLearning/testOpNansheDictionaryLearning.py
+++ tests/nanshe/dictionaryLearning/testOpNansheDictionaryLearning.py
@@ -50,10 +50,7 @@ class TestOpNansheDictionaryLearning(object):
         g = synthetic_data.synthetic_data.generate_hypersphere_masks(space, p, radii)
         gv = g[..., None]
         gv = gv.astype(float)
-        gv = vigra.VigraArray(gv, axistags=vigra.AxisTags(vigra.AxisInfo.t,
-                                                          vigra.AxisInfo.y,
-                                                          vigra.AxisInfo.x,
-                                                          vigra.AxisInfo.c))
+        gv = vigra.taggedView(gv, "tyxc")
 
         graph = Graph()
         op = OpNansheDictionaryLearning(graph=graph)
