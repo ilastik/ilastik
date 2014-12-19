938bed24ccb0fe8085fc1f178a8c862f2ef854f8
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-12-19 14:44:04 -0500

    tests/nanshe/preprocessing/testOpNanshePreprocessData.py: Added a test for OpNanshePreprocessData.

--- tests/nanshe/preprocessing/testOpNanshePreprocessData.py
+++ tests/nanshe/preprocessing/testOpNanshePreprocessData.py
@@ -23,3 +23,72 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Dec 19, 2014 14:41:37 EST$"
 
 
+
+import numpy
+
+from lazyflow.graph import Graph
+from lazyflow.operators import OpArrayPiper
+
+import vigra
+
+import synthetic_data.synthetic_data
+
+
+import ilastik
+import ilastik.applets
+import ilastik.applets.nanshe
+import ilastik.applets.nanshe.preprocessing
+import ilastik.applets.nanshe.preprocessing.opNanshePreprocessData
+from ilastik.applets.nanshe.preprocessing.opNanshePreprocessData import OpNanshePreprocessData
+
+class TestOpNanshePreprocessData(object):
+    def testBasic(self):
+        # Does NOT test accuracy.
+
+        space = numpy.array([100, 100, 100])
+        radii = numpy.array([5, 6])
+        magnitudes = numpy.array([15, 16])
+        points = numpy.array([[20, 30, 24],
+                              [70, 59, 65]])
+
+        masks = synthetic_data.synthetic_data.generate_hypersphere_masks(space, points, radii)
+        images = synthetic_data.synthetic_data.generate_gaussian_images(space, points, radii/3.0, magnitudes) * masks
+        image_stack = images.max(axis = 0)
+        image_stack = image_stack[..., None]
+        image_stack = vigra.taggedView(image_stack, "tyxc")
+
+        graph = Graph()
+
+        opPrep = OpArrayPiper(graph=graph)
+        opPrep.Input.setValue(image_stack)
+
+        op = OpNanshePreprocessData(graph=graph)
+        op.InputImage.connect(opPrep.Output)
+
+
+        op.ToRemoveZeroedLines.setValue(True)
+        op.ErosionShape.setValue([21, 1])
+        op.DilationShape.setValue([1, 3])
+
+        op.ToExtractF0.setValue(True)
+        op.HalfWindowSize.setValue(20)
+        op.WhichQuantile.setValue(0.5)
+        op.TemporalSmoothingGaussianFilterStdev.setValue(5.0)
+        op.SpatialSmoothingGaussianFilterStdev.setValue(5.0)
+        op.Bias.setValue(100)
+        op.BiasEnabled.setValue(True)
+
+        op.ToWaveletTransform.setValue(True)
+        op.Scale.setValue([3, 4, 4])
+
+        b = op.Output[...].wait()
+        b = vigra.taggedView(b, "tyxc")
+
+
+if __name__ == "__main__":
+    import sys
+    import nose
+    sys.argv.append("--nocapture")    # Don't steal stdout.  Show it on the console as usual.
+    sys.argv.append("--nologcapture") # Don't set the logging level to DEBUG.  Leave it alone.
+    ret = nose.run(defaultTest=__file__)
+    if not ret: sys.exit(1)
