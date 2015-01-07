860f8e3235c72923a979b051eb1cc527654e4211
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-01-07 18:54:03 -0500

    ilastik/applets/nanshe/preprocessing/opNansheExtractF0.py: Use OpNansheEstimateF0 to calculate F_0 instead of doing it directly in OpNansheExtractF0.

--- ilastik/applets/nanshe/preprocessing/opNansheExtractF0.py
+++ ilastik/applets/nanshe/preprocessing/opNansheExtractF0.py
@@ -32,6 +32,8 @@ from lazyflow.operators import OpBlockedArrayCache
 
 from ilastik.applets.base.applet import DatasetConstraintError
 
+from ilastik.applets.nanshe.preprocessing.opNansheEstimateF0 import OpNansheEstimateF0
+
 import itertools
 
 import numpy
@@ -67,6 +69,20 @@ class OpNansheExtractF0(Operator):
         super( OpNansheExtractF0, self ).__init__( *args, **kwargs )
 
         self._generation = {self.name : 0}
+
+        self.opEstimateF0 = OpNansheEstimateF0(parent=self)
+
+        self.opEstimateF0.HalfWindowSize.connect(self.HalfWindowSize)
+        self.opEstimateF0.WhichQuantile.connect(self.WhichQuantile)
+        self.opEstimateF0.TemporalSmoothingGaussianFilterStdev.connect(self.TemporalSmoothingGaussianFilterStdev)
+        self.opEstimateF0.TemporalSmoothingGaussianFilterWindowSize.connect(self.TemporalSmoothingGaussianFilterWindowSize)
+        self.opEstimateF0.SpatialSmoothingGaussianFilterStdev.connect(self.SpatialSmoothingGaussianFilterStdev)
+        self.opEstimateF0.SpatialSmoothingGaussianFilterWindowSize.connect(self.SpatialSmoothingGaussianFilterWindowSize)
+
+        self.opEstimateF0.InputImage.connect( self.InputImage )
+        self.F0.connect( self.opEstimateF0.Output )
+
+        self.dF_F.notifyDirty(self._f0BecameDirty)
 
         self.InputImage.notifyReady( self._checkConstraints )
 
@@ -122,133 +138,50 @@ class OpNansheExtractF0(Operator):
                 raise RuntimeError("%d-th axis %r is not spatial" % (i, ax[i]))
 
     def setupOutputs(self):
-        # Copy the input metadata to both outputs
-        self.F0.meta.assignFrom( self.InputImage.meta )
-        self.F0.meta.dtype = numpy.float32
-
-        self.F0.meta.generation = self._generation
-
-
+        # Copy the input metadata to dF_F. F0 will be set by OpNansheEstimateF0.
         self.dF_F.meta.assignFrom( self.InputImage.meta )
         self.dF_F.meta.dtype = numpy.float32
 
         self.dF_F.meta.generation = self._generation
 
-    @staticmethod
-    def compute_halo(slicing,
-                     image_shape,
-                     half_window_size,
-                     temporal_smoothing_gaussian_filter_stdev,
-                     temporal_smoothing_gaussian_filter_window_size,
-                     spatial_smoothing_gaussian_filter_stdev,
-                     spatial_smoothing_gaussian_filter_window_size):
-        slicing = nanshe.nanshe.additional_generators.reformat_slices(slicing, image_shape)
-
-        halo = list(itertools.repeat(0, len(slicing) - 1))
-        halo[0] = max(int(math.ceil(temporal_smoothing_gaussian_filter_window_size*temporal_smoothing_gaussian_filter_stdev)), half_window_size)
-        for i in xrange(1, len(halo)):
-            halo[i] = int(math.ceil(spatial_smoothing_gaussian_filter_window_size*spatial_smoothing_gaussian_filter_stdev))
-
-        halo_slicing = list(slicing)
-        within_halo_slicing = list(slicing)
-
-        for i in xrange(len(halo)):
-            halo_slicing_i_start = (halo_slicing[i].start - halo[i])
-            within_halo_slicing_i_start = halo[i]
-            if (halo_slicing_i_start < 0):
-                within_halo_slicing_i_start += halo_slicing_i_start
-                halo_slicing_i_start = 0
-
-
-            halo_slicing_i_stop = (halo_slicing[i].stop + halo[i])
-            within_halo_slicing_i_stop = (slicing[i].stop - slicing[i].start) + within_halo_slicing_i_start
-            if (halo_slicing_i_stop > image_shape[i]):
-                halo_slicing_i_stop = image_shape[i]
-
-            halo_slicing[i] = slice(halo_slicing_i_start, halo_slicing_i_stop, halo_slicing[i].step)
-            within_halo_slicing[i] = slice(within_halo_slicing_i_start,
-                                           within_halo_slicing_i_stop,
-                                           within_halo_slicing[i].step)
-
-        halo_slicing = tuple(halo_slicing)
-        within_halo_slicing = tuple(within_halo_slicing)
-
-        return(halo_slicing, within_halo_slicing)
-
     def execute(self, slot, subindex, roi, result):
-        half_window_size = self.HalfWindowSize.value
-        which_quantile = self.WhichQuantile.value
+        key = roi.toSlice()
+
+        raw = self.InputImage[key].wait()
+        f0 = self.F0[key].wait()
 
         bias = None
         if self.BiasEnabled.value:
             bias = self.Bias.value
+        else:
+            bias = 1 - raw.min()
 
-        temporal_smoothing_gaussian_filter_stdev = self.TemporalSmoothingGaussianFilterStdev.value
-        temporal_smoothing_gaussian_filter_window_size = self.TemporalSmoothingGaussianFilterWindowSize.value
+        df_f = (raw - f0) / (f0 + bias)
 
-        spatial_smoothing_gaussian_filter_stdev = self.SpatialSmoothingGaussianFilterStdev.value
-        spatial_smoothing_gaussian_filter_window_size = self.SpatialSmoothingGaussianFilterWindowSize.value
-
-
-        image_shape = self.InputImage.meta.shape
-
-        key = roi.toSlice()
-        halo_key, within_halo_key = OpNansheExtractF0.compute_halo(key,
-                                                                   image_shape,
-                                                                   half_window_size,
-                                                                   temporal_smoothing_gaussian_filter_stdev,
-                                                                   temporal_smoothing_gaussian_filter_window_size,
-                                                                   spatial_smoothing_gaussian_filter_stdev,
-                                                                   spatial_smoothing_gaussian_filter_window_size)
-
-        raw = self.InputImage[halo_key].wait()
-        raw = raw[..., 0]
-
-        f0, df_f = nanshe.nanshe.advanced_image_processing.extract_f0(
-            raw,
-            half_window_size=half_window_size,
-            which_quantile=which_quantile,
-            temporal_smoothing_gaussian_filter_stdev=temporal_smoothing_gaussian_filter_stdev,
-            temporal_smoothing_gaussian_filter_window_size=temporal_smoothing_gaussian_filter_window_size,
-            spatial_smoothing_gaussian_filter_stdev=spatial_smoothing_gaussian_filter_stdev,
-            spatial_smoothing_gaussian_filter_window_size=spatial_smoothing_gaussian_filter_window_size,
-            bias=bias,
-            return_f0=True
-        )
-
-        f0 = f0[..., None]
-        df_f = df_f[..., None]
-
-        if slot.name == 'F0':
-            result[...] = f0[within_halo_key]
-        elif slot.name == 'dF_F':
-            result[...] = df_f[within_halo_key]
+        if slot.name == 'dF_F':
+            result[...] = df_f
 
     def setInSlot(self, slot, subindex, roi, value):
         pass
 
+    def _f0BecameDirty(self, roi):
+        self._generation[self.name] += 1
+        self.dF_F.setDirty(roi.toSlice())
+
     def propagateDirty(self, slot, subindex, roi):
-        if slot.name == "InputImage":
+        if slot.name == "InputImage" or \
+           slot.name == "HalfWindowSize" or slot.name == "WhichQuantile" or \
+           slot.name == "TemporalSmoothingGaussianFilterStdev" or \
+           slot.name == "TemporalSmoothingGaussianFilterWindowSize" or \
+           slot.name == "SpatialSmoothingGaussianFilterStdev" or \
+           slot.name == "SpatialSmoothingGaussianFilterWindowSize":
+            # Dirtiness is propagated through OpNansheEstimateF0 to F0.
+            #
+            # In order to propagate this dirty region to dF_F, we simply receive dirty notifications for F0
+            # and apply the same dirty regions to dF_F as they are the same.
+            pass
+        elif slot.name == "Bias" or slot.name == "BiasEnabled":
             self._generation[self.name] += 1
-
-            roi_halo = OpNansheExtractF0.compute_halo(roi.toSlice(),
-                                                      self.InputImage.meta.shape,
-                                                      self.HalfWindowSize.value,
-                                                      self.TemporalSmoothingGaussianFilterStdev.value,
-                                                      self.TemporalSmoothingGaussianFilterWindowSize.value,
-                                                      self.SpatialSmoothingGaussianFilterStdev.value,
-                                                      self.SpatialSmoothingGaussianFilterWindowSize.value)[0]
-
-            self.F0.setDirty(roi_halo)
-            self.dF_F.setDirty(roi_halo)
-        elif slot.name == "HalfWindowSize" or slot.name == "WhichQuantile" or \
-             slot.name == "TemporalSmoothingGaussianFilterStdev" or \
-             slot.name == "TemporalSmoothingGaussianFilterWindowSize" or \
-             slot.name == "SpatialSmoothingGaussianFilterStdev" or \
-             slot.name == "SpatialSmoothingGaussianFilterWindowSize" or \
-             slot.name == "Bias" or slot.name == "BiasEnabled":
-            self._generation[self.name] += 1
-            self.F0.setDirty( slice(None) )
             self.dF_F.setDirty( slice(None) )
         else:
             assert False, "Unknown dirty input slot"
