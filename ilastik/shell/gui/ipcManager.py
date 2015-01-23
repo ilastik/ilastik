dd59a6a4558efa1eea86ae8d6b5de7563d87c9ec
niels <buwen@stud.uni-heidelberg.de>
2015-01-23 17:59:47 +0100

    separated IPC modules into senders and receivers TCPServer -> TCPServer (receiver), TCPClient (sender)

--- ilastik/shell/gui/ipcManager.py
+++ ilastik/shell/gui/ipcManager.py
@@ -1,13 +1,11 @@
-from SocketServer import BaseRequestHandler, TCPServer
-from abc import ABCMeta, abstractmethod, abstractproperty
-from operator import itemgetter
-import socket
+from SocketServer import BaseRequestHandler, TCPServer as BaseTCPServer
 import logging
 import threading
 import atexit
 import json
-from Queue import Queue
+from itertools import chain
 from collections import OrderedDict
+from operator import itemgetter
 
 from ilastik.widgets.ipcserver.tcpServerInfoWidget import TCPServerInfoWidget
 from ilastik.widgets.ipcserver.zmqPublisherInfoWidget import ZMQPublisherInfoWidget
@@ -20,11 +18,11 @@ from sys import platform as platform_
 from PyQt4.QtGui import QInputDialog
 
 from ilastik.widgets.ipcserver.ipcServerInfoWindow import IPCServerInfoWindow
-from ilastik.utility.commandProcessor import CommandProcessor
 from ilastik.utility.decorators import lazy
 from ilastik.utility.singleton import Singleton
 from ilastik.utility.ipcProtocol import Protocol
 from ilastik.utility.numpyJsonEncoder import NumpyJsonEncoder
+from ilastik.utility.contextSocket import socket, socket_error
 
 logger = logging.getLogger(__name__)
 
@@ -35,30 +33,43 @@ class IPCFacade(object):
     def __init__(self):
         self.info = IPCServerInfoWindow()
         self.senders = {}
+        self.receivers = {}
 
-    def add_sender(self, name, server):
+    def add_sender(self, name, sender, protocol=None):
         if name in self.senders:
-            raise RuntimeError("Server {} already exists".format(name))
-        self.senders[name] = server
-        self.info.add_server(name, server.widget)
+            raise RuntimeError("Sender {} already exists".format(name))
+        self.senders[name] = sender
+        if protocol:
+            self.senders[protocol] = sender
+        self.info.add_server(name, sender.widget)
+
+    def add_receiver(self, name, receiver):
+        if name in self.receivers:
+            raise RuntimeError("Receiver {} already exists".format(name))
+        self.receivers[name] = receiver
+        self.info.add_server(name, receiver.widget)
+
+    def sender(self, key):
+        return self.senders[key]
 
     @property
     def sending(self):
-        return (server.sending for server in self.senders.itervalues())
+        return (sender.running for sender in self.senders.itervalues())
 
-    def start(self, name=None):
-        if name is None:
-            for sender in self.senders.itervalues():
-                sender.start()
-        else:
-            self.senders[name].start()
+    @property
+    def receiving(self):
+        return (rec.running for rec in self.receivers.itervalues())
 
-    def stop(self, name=None):
-        if name is None:
-            for sender in self.senders.itervalues():
-                sender.stop()
-        else:
-            self.senders[name].stop()
+    def start(self):
+        for module in self._all_modules():
+                module.start()
+
+    def stop(self):
+        for module in self._all_modules():
+                module.stop()
+
+    def _all_modules(self):
+        return chain(self.senders.itervalues(), self.receivers.itervalues())
 
     @lazy
     def broadcast(self, command):
@@ -71,6 +82,7 @@ class IPCFacade(object):
 
     def show_info(self):
         self.info.show()
+        self.info.activateWindow()  # bring to top
 
 
 class AddressAlreadyTaken(Exception):
@@ -82,40 +94,37 @@ class AddressAlreadyTaken(Exception):
         return self._what
 
 
-class Sender(object):
-    __metaclass__ = ABCMeta
+class IPCModul(object):
 
-    def __init__(self, shell):
-        self.shell = shell
-        self.cmd_processor = CommandProcessor(shell)
+    @property
+    def running(self):
+        raise NotImplementedError
+
+    @property
+    def widget(self):
+        raise NotImplementedError
 
     @staticmethod
     def available(mode=None):
-        return True
+        raise NotImplementedError
 
-    @abstractproperty
-    def running(self):
-        """
-        :return: True if the server is currently running False otherwise
-        """
-        pass
 
-    def address_prompt(self, message=""):
+class Sending(IPCModul):
+    def broadcast(self, message, log):
         """
-        Get a new address from the user ( via dialog )
-        :param message: why it is neccessary to ask for a new address
-        :type message: str
-        :return: tuple ( type: new address, bool: ok_clicked )
+        Broadcasts the message
+        :param message: the message to broadcast ( will be json encoded )
+        :type message: dict
         """
-        return None
+        if self.running:
+            self._broadcast(message)
+            self.widget.add_sent_command(log, 0)
 
-    @abstractproperty
-    def widget(self):
-        """
-        :return: the widget that displays information about this Server
-        """
-        pass
+    def _broadcast(self, message):
+        raise NotImplementedError
 
+
+class Binding(IPCModul):
     def start(self):
         """
         Start the server if it is not running
@@ -148,20 +157,6 @@ class Sender(object):
         else:
             self.start()
 
-    def broadcast(self, message, log):
-        """
-        Broadcasts the message
-        :param message: the message to broadcast ( will be json encoded )
-        :type message: dict
-        """
-        if self.running:
-            self._broadcast(message)
-            self.widget.add_sent_command(log, 0)
-
-    @abstractmethod
-    def _broadcast(self, message):
-        pass
-
     def change_address(self, address):
         """
         Changes the address. This can only be done if the server is not running
@@ -171,80 +166,92 @@ class Sender(object):
         if not self.running:
             self._change_address(address)
 
-    @abstractmethod
-    def _change_address(self, address):
-        pass
-
-    @abstractmethod
     def _start(self):
-        pass
+        raise NotImplementedError
 
-    @abstractmethod
-    def _stop(self):
-        pass
+    def _stop(self, kill=False):
+        raise NotImplementedError
+
+    def _change_address(self, address):
+        raise NotImplementedError
+
+    def address_prompt(self, message):
+        raise NotImplementedError
 
 
-class TCPHiliteServer(Sender):
+class Handler(BaseRequestHandler):
     """
-    This ServerManager updates the IPCServerInfoWidget,
-    starts/stops the server, adds connections and forwards
-    commands to the command processor
+    Basic Handler for the SocketServer
     """
-
-    DEFAULT_PORT = 9999
-
-    def __init__(self, shell):
+    def handle(self):
         """
-        :param shell: the shell for the CommandProcessor
-        :type shell: IlastikShell
+        Receive one Message and enqueue it if valid
+        Then close the connection as knime does it
         """
-        super(TCPHiliteServer, self).__init__(shell)
+        try:
+            data = self.request.recv(4096).strip()
+            host, port = self.request.getpeername()
+            if data == "":
+                return
+            command = json.loads(data)
+        except (socket.error, ValueError) as e:
+            logger.exception(e)
+            return
+        if command["command"] == "handshake":
+            command.update({"protocol": "tcp"})
+        command.update({"host": host})
+
+        self.server.queue.put(command)
+
+
+class TCPServer(Binding):
+    def __init__(self, queue):
+        self.port = 9999
+        self.queue = queue
+        self.thread = None
         self.server = None
-        self.queue = None
-        self.queue_thread = None
-        self.port = self.DEFAULT_PORT
 
         self.info = TCPServerInfoWidget()
         self.info.statusToggled.connect(self.toggle)
-        self.info.connectionChanged.connect(self.change_connection)
-        self.info.broadcast.connect(self.broadcast)
         self.info.changePort.connect(self.on_address_change)
         self.info.notify_server_status_update("port", self.port)
-
-        self.connections = OrderedDict()
-
-        atexit.register(self.stop, kill=True)
 
     def on_address_change(self):
         port, ok = self.address_prompt(level="Info")
         if ok:
             self.change_address(port)
 
-    def address_prompt(self, message="", level="Warning"):
-        message = "{}. Change port".format(message)
-        # noinspection PyTypeChecker
-        return QInputDialog.getInt(None, level, message, self.port, 1024, 65535)
+    @staticmethod
+    def available(mode=None):
+        return True
+
+    @property
+    def widget(self):
+        return self.info
+
+    @property
+    def running(self):
+        return self.server is not None
 
     def _start(self):
         try:
-            self.server = TCPServer(("0.0.0.0", self.port), Handler)
+            server = BaseTCPServer(("0.0.0.0", self.port), Handler)
         except socket.error as e:
             self.server = None
             self.info.notify_server_status_update("running", False)
             if e.errno in [13, 98]:  # Permission denied or Address already in use
                 raise AddressAlreadyTaken(str(e))
             raise
-        self.queue = Queue()
-        self.server.queue = self.queue
-        self.queue_thread = threading.Thread(target=self._handle_queue)
-        self.queue_thread.daemon = True
-        self.queue_thread.start()
+        server.queue = self.queue
+        self.server = server
 
-        server_thread = threading.Thread(target=self.server.serve_forever)
-        server_thread.daemon = True
-        server_thread.start()
+        thread = threading.Thread(target=self.server.serve_forever)
+        thread.daemon = True
+        thread.start()
         logger.info("IPC Server started on port %d" % self.server.socket.getsockname()[1])
         self.info.notify_server_status_update("running", True)
+
+        self.thread = thread
 
     def _stop(self, kill=False):
         self.server.shutdown()
@@ -253,120 +260,70 @@ class TCPHiliteServer(Sender):
         self.info.notify_server_status_update("running", False)
         logger.info("IPC Server stopped")
         self.server = None
-        self.queue.put(None)
-        self.queue_thread = None
-        self.connections = OrderedDict()
-        self.info.update_connections(self.connections)
 
-    @property
-    def running(self):
-        return self.server is not None
+    def address_prompt(self, message="", level="Warning"):
+        message = "{}. Change port".format(message)
+        return QInputDialog.getInt(QInputDialog(), level, message, self.port, 1024, 65535)
+
+    def _change_address(self, address):
+        self.port = address
+
+
+class TCPClient(Sending):
+    def __init__(self, server):
+        self.peers = OrderedDict()
+        self.server = server
+
+        self.server.widget.connectionChanged.connect(self.change_peer)
+
+    def add_peer(self, name, host, port):
+        key = (host, name)
+        if key not in self.peers:
+            self.peers[key] = {
+                "enabled": True,
+                "address": [host, port]
+            }
+
+        elif self.peers[key]["address"][1] != port:
+            self.peers[key]["address"][1] = port
+        else:
+            return
+        self.server.widget.update_connections(self.peers)
+
+    def change_peer(self, index, enabled):
+        self.peers.values()[index]["enabled"] = enabled
+
+    def _broadcast(self, message):
+        count = 0
+        for peer in filter(itemgetter("enabled"), self.peers.itervalues()):
+            try:
+                with socket() as s:
+                    # noinspection PyTypeChecker
+                    host, port = peer["address"]
+                    s.connect((host, port))
+                    s.sendall(message)
+            except socket_error as e:
+                logger.exception(e)
+            else:
+                count += 1
+        return count
 
     @property
     def widget(self):
-        return self.info
+        return self.server.widget
 
-    def add_connection(self, name, host, remote_server_port):
-        """
-        Adds an entry for the new client and notifies the InfoWidget
-        :param name: name of the remote program [knime]
-        :type name: str
-        :param host: the ip address of the remote program
-        :type host: str
-        :param remote_server_port: the port of the remote program
-        :type remote_server_port: int
-        """
-        if (name, host) not in self.connections:
-            self.connections[(name, host)] = {
-                "enabled": True,
-                "client": (host, remote_server_port)
-            }
-            self.info.update_connections(self.connections)
-        elif self.connections[(name, host)]["client"][1] != remote_server_port:
-            client = self.connections[(name, host)]["client"]
-            self.connections[(name, host)]["client"] = (client[0], remote_server_port)
+    @property
+    def running(self):
+        return True
 
-    def change_connection(self, index, enabled):
-        """
-        This is connected to the connectionChanged signal from the InfoWidget
-        :param index: the index of the connection in the OrderedDict
-        :type index: int
-        :param enabled: the status the connection will change to
-        :type enabled: bool
-        """
-        self.connections.values()[index]["enabled"] = enabled
-
-    def filter_command(self, cmd):
-        """
-        Executes the command if the remote client is allowed to
-        :param cmd: the command to filter
-        :type cmd: dict
-        """
-        #todo: filter
-        del cmd["host"]
-        success = True
-        # noinspection PyBroadException
-        try:
-            self.cmd_processor.execute(cmd["command"], cmd)
-        except Exception as e:
-            print "Exception", e
-        except:
-            print "Unknown Error"
-            success = False
-        self.info.add_command(cmd, success)
-
-    def _handle_queue(self):
-        """
-        handles each command in the queue until closed
-        """
-        while True:
-            command = self.queue.get()
-            if command is None:
-                return
-
-            cmd_name = command["command"]
-            if cmd_name == "handshake":
-                self.add_connection(command["name"], command["host"], command["port"])
-            else:
-                self.filter_command(command)
-
-    def _broadcast(self, message, to_all=False):
-        """
-        creates a socket for each connection and sends the command as json
-        :param message: the command
-        :type message: str
-        :param to_all: if True the cmd is sent to all ( even the disabled ones ) connections
-        :type to_all: bool
-        """
-        count = 0
-        for connection in filter(None if to_all else itemgetter("enabled"), self.connections.itervalues()):
-            # noinspection PyTypeChecker
-            client = connection["client"]
-            s = socket.socket()
-            try:
-                s.connect(client)
-                s.sendall(message)
-            except socket.error as e:
-                logger.exception("broadcast to %s:%s failed (%s)" % (client[0], client[1], e))
-            else:
-                count += 1
-            finally:
-                s.close()
-
-    def _change_address(self, address):
-        """
-        Changes the server port to the new port
-        The server must be shutdown to change the port
-        :param address: the new port
-        :type address: int
-        """
-        self.port = address
-        self.info.notify_server_status_update("port", address)
+    @staticmethod
+    def available(mode=None):
+        return True
 
 
-class ZMQHilitePublisher(Sender):
-    def __init__(self, shell, address):
-        super(ZMQHilitePublisher, self).__init__(shell)
+class ZMQHilitePublisher(Binding, Sending):
+    def __init__(self, address):
+        super(ZMQHilitePublisher, self).__init__()
         self.address = address
 
         self.context = zmq.Context()
@@ -422,47 +379,3 @@ class ZMQHilitePublisher(Sender):
         if kill:
             return
         self.info.notify_server_status_update("running", False)
-
-
-class Handler(BaseRequestHandler):
-    """
-    Basic Handler for the SocketServer
-    """
-    valid_commands = [
-        "handshake",
-        "setviewerposition"
-    ]
-
-    def handle(self):
-        """
-        Receive one Message and enqueue it if valid
-        Then close the connection as knime does it
-        """
-        try:
-            data = self.request.recv(4096).strip()
-        except socket.error as e:
-            logger.exception(e)
-            return
-        try:
-            command = json.loads(data)
-        except ValueError as e:
-            logger.exception(e)
-            return
-        try:
-            command_name = command["command"]
-        except KeyError as e:
-            logger.exception(e)
-            return
-        if command_name in self.valid_commands:
-            cmd = command
-            cmd.update({"host": self.client_address[0]})
-            self.server.queue.put(cmd)
-        else:
-            logger.debug("Unknown command: %s" % command_name)
-            return
-
-AvailableIPCSenders = [
-    ("TCP Server", TCPHiliteServer, None),
-    ("ZMQ TCP Publisher", ZMQHilitePublisher, "tcp://*:9998"),
-    ("ZMQ IPC Publisher", ZMQHilitePublisher, "ipc:///tmp/ilastik"),
-]
