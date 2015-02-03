5ad03036bb03ac72ea333638503545df38dee242
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-02-03 16:54:25 -0500

    tests/nanshe/preprocessing/testOpNanshePreprocessing.py: Correct imports for nanshe.

--- tests/nanshe/preprocessing/testOpNanshePreprocessing.py
+++ tests/nanshe/preprocessing/testOpNanshePreprocessing.py
@@ -26,7 +26,9 @@ from lazyflow.operators import OpArrayPiper
 
 import vigra
 
-import synthetic_data.synthetic_data
+import nanshe
+import nanshe.synthetic_data
+import nanshe.synthetic_data.synthetic_data
 
 
 import ilastik
@@ -46,8 +48,9 @@ class TestOpNanshePreprocessing(object):
         points = numpy.array([[20, 30, 24],
                               [70, 59, 65]])
 
-        masks = synthetic_data.synthetic_data.generate_hypersphere_masks(space, points, radii)
-        images = synthetic_data.synthetic_data.generate_gaussian_images(space, points, radii/3.0, magnitudes) * masks
+        masks = nanshe.synthetic_data.synthetic_data.generate_hypersphere_masks(space, points, radii)
+        images = nanshe.synthetic_data.synthetic_data.generate_gaussian_images(space, points, radii/3.0, magnitudes)
+        images *= masks
         image_stack = images.max(axis = 0)
         image_stack = image_stack[..., None]
         image_stack = vigra.taggedView(image_stack, "tyxc")
