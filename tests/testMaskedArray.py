15ae21be3390ecbcbdb9eb0cc73c4ca4bbe68891
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-02-02 23:24:29 -0500

    tests/testMaskedArray.py: Added support for running the suite on the command line.

--- tests/testMaskedArray.py
+++ tests/testMaskedArray.py
@@ -117,3 +117,12 @@ class TestOpMaskArrayIdentity(object):
         self.operator_identity.Input.disconnect()
         self.operator_identity.Output.disconnect()
         self.operator_identity.cleanUp()
+
+
+if __name__ == "__main__":
+    import sys
+    import nose
+    sys.argv.append("--nocapture")    # Don't steal stdout.  Show it on the console as usual.
+    sys.argv.append("--nologcapture") # Don't set the logging level to DEBUG.  Leave it alone.
+    ret = nose.run(defaultTest=__file__)
+    if not ret: sys.exit(1)
