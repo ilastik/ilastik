dbc1be01658e28981351338beecceb08bf291b62
Stuart Berg <bergs@janelia.hhmi.org>
2015-02-06 09:53:16 -0500

    OpIIBoostPixelClassification: Use 100 stumps.

--- ilastik/applets/iiboostPixelClassification/opIIBoostPixelClassification.py
+++ ilastik/applets/iiboostPixelClassification/opIIBoostPixelClassification.py
@@ -7,7 +7,7 @@ class OpIIBoostPixelClassification(OpPixelClassification):
         super( OpIIBoostPixelClassification, self ).__init__( *args, **kwargs )
         
         # Manually override the default classifier type
-        self.ClassifierFactory._defaultValue = IIBoostLazyflowClassifierFactory(numStumps=2, debugOutput=True)
+        self.ClassifierFactory._defaultValue = IIBoostLazyflowClassifierFactory(numStumps=100, debugOutput=True)
         
         # We only permit two label classes.
         # In IIBoost, non-synapse is hard-coded to label 1, synapse is label 2
