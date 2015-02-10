8f9d5677817136f3981c359e0148a9c41360d78b
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-02-10 15:49:02 -0500

    lazyflow/operators/opCompressedCache.py: Drop trailing space.

--- lazyflow/operators/opCompressedCache.py
+++ lazyflow/operators/opCompressedCache.py
@@ -312,7 +312,7 @@ class OpCompressedCache(OpCache):
                                         shape=datashape,
                                         dtype=self.Output.meta.dtype,
                                         chunks=chunkshape,
-                                        compression='lzf' ) # lzf should be faster than gzip, 
+                                        compression='lzf' ) # lzf should be faster than gzip,
                                                             # with a slightly worse compression ratio
 
                 self._blockLocks[block_start] = RequestLock()
