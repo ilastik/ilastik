9cd9edb628a50676baf2045aa7f788ebd70ccb00
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-02-12 00:00:14 -0500

    tests/testOpBlockedArrayCache.py: Add tests using masked arrays.

--- tests/testOpBlockedArrayCache.py
+++ tests/testOpBlockedArrayCache.py
@@ -322,6 +322,285 @@ class TestOpBlockedArrayCache(object):
             ArrayCacheMemoryMgr.instance.unpause()
 
 
+class TestOpBlockedArrayCache_masked(object):
+
+    def setUp(self):
+        self.dataShape = (1,100,100,10,1)
+        self.data = (numpy.random.random(self.dataShape) * 100).astype(int)
+        self.data = numpy.ma.masked_array(
+            self.data,
+            mask=numpy.ma.getmaskarray(self.data),
+            fill_value=numpy.iinfo(int).max,
+            shrink=False
+        )
+        self.data[:, 0] = numpy.ma.masked
+
+        graph = Graph()
+        opProvider = OpArrayPiperWithAccessCount(graph=graph)
+        opProvider.Input.meta.axistags = vigra.defaultAxistags('txyzc')
+        opProvider.Input.meta.has_mask = True
+        opProvider.Input.setValue(self.data)
+        self.opProvider = opProvider
+
+        opCache = OpBlockedArrayCache(graph=graph)
+        opCache.Input.connect(opProvider.Output)
+        opCache.innerBlockShape.setValue( (10,10,10,10,10) )
+        opCache.outerBlockShape.setValue( (20,20,20,20,20) )
+        opCache.fixAtCurrent.setValue(False)
+        self.opCache = opCache
+
+    def testCacheAccess(self):
+        opCache = self.opCache
+        opProvider = self.opProvider
+
+        expectedAccessCount = 0
+        assert opProvider.accessCount == expectedAccessCount, "Access count={}, expected={}".format(opProvider.accessCount, expectedAccessCount)
+
+        # Block-aligned request
+        slicing = make_key[0:1, 0:10, 10:20, 0:10, 0:1]
+        data = opCache.Output( slicing ).wait()
+        data.axistags = opCache.Output.meta.axistags
+        expectedAccessCount += 1
+        assert (data == self.data[slicing]).all()
+        assert (data.mask == self.data.mask[slicing]).all()
+        assert (data.fill_value == self.data.fill_value).all()
+        assert opProvider.accessCount == expectedAccessCount, "Access count={}, expected={}".format(opProvider.accessCount, expectedAccessCount)
+
+        # Same request should come from cache, so access count is unchanged
+        data = opCache.Output( slicing ).wait()
+        assert opProvider.accessCount == expectedAccessCount, "Access count={}, expected={}".format(opProvider.accessCount, expectedAccessCount)
+
+        # Not block-aligned request
+        slicing = make_key[0:1, 5:15, 10:20, 0:10, 0:1]
+        data = opCache.Output( slicing ).wait()
+        data.axistags = opCache.Output.meta.axistags
+        expectedAccessCount += 1
+        assert (data == self.data[slicing]).all()
+        assert (data.mask == self.data.mask[slicing]).all()
+        assert (data.fill_value == self.data.fill_value).all()
+        assert opProvider.accessCount == expectedAccessCount, "Access count={}, expected={}".format(opProvider.accessCount, expectedAccessCount)
+
+        # Same request should come from cache, so access count is unchanged
+        data = opCache.Output( slicing ).wait()
+        assert opProvider.accessCount == expectedAccessCount, "Access count={}, expected={}".format(opProvider.accessCount, expectedAccessCount)
+
+    def testDirtySource(self):
+        opCache = self.opCache
+        opProvider = self.opProvider
+
+        oldAccessCount = 0
+        assert opProvider.accessCount == oldAccessCount, "Access count={}, expected={}".format(opProvider.accessCount, oldAccessCount)
+
+        # Request
+        slicing = make_key[:, 0:50, 15:45, 0:10, :]
+        data = opCache.Output( slicing ).wait()
+        data.axistags = opCache.Output.meta.axistags
+        assert (data == self.data[slicing]).all()
+        assert (data.mask == self.data.mask[slicing]).all()
+        assert (data.fill_value == self.data.fill_value).all()
+
+        # Our slice intersects 3*3=9 outerBlocks, and a total of 20 innerBlocks
+        # Inner caches are allowed to split up the accesses, so there could be as many as 20
+        minAccess = oldAccessCount + 9
+        maxAccess = oldAccessCount + 20
+        assert opProvider.accessCount >= minAccess
+        assert opProvider.accessCount <= maxAccess
+        oldAccessCount = opProvider.accessCount
+
+        # Track dirty notifications
+        gotDirtyKeys = []
+        def handleDirty(slot, roi):
+            gotDirtyKeys.append( list(roiToSlice(roi.start, roi.stop)) )
+        opCache.Output.notifyDirty(handleDirty)
+
+        # Change some of the input data and mark it dirty
+        dirtykey = make_key[0:1, 10:20, 20:30, 0:3, 0:1]
+        self.data[dirtykey] = 0.12345
+        opProvider.Input.setDirty(dirtykey)
+        assert len(gotDirtyKeys) > 0
+
+        # Same request, but should need to access the data again due to dirtiness
+        data = opCache.Output( slicing ).wait()
+        data.axistags = opCache.Output.meta.axistags
+
+        assert (data == self.data[slicing]).all()
+        assert (data.mask == self.data.mask[slicing]).all()
+        assert (data.fill_value == self.data.fill_value).all()
+
+        # The dirty data intersected 1 outerBlocks and a total of 1 innerBlock.
+        minAccess = oldAccessCount + 1
+        maxAccess = oldAccessCount + 1
+        assert opProvider.accessCount >= minAccess
+        assert opProvider.accessCount <= maxAccess
+        oldAccessCount = opProvider.accessCount
+
+    def testFixAtCurrent(self):
+        opCache = self.opCache
+        opProvider = self.opProvider
+
+        # Track dirty notifications
+        gotDirtyKeys = []
+        def handleDirty(slot, roi):
+            gotDirtyKeys.append( list(roiToSlice(roi.start, roi.stop)) )
+        opCache.Output.notifyDirty(handleDirty)
+
+        opCache.fixAtCurrent.setValue(True)
+
+        oldAccessCount = 0
+        assert opProvider.accessCount == oldAccessCount, "Access count={}, expected={}".format(opProvider.accessCount, oldAccessCount)
+
+        # Request (no access to provider because fixAtCurrent)
+        slicing = make_key[:, 0:50, 15:45, 0:1, :]
+        data = opCache.Output( slicing ).wait()
+        assert opProvider.accessCount == oldAccessCount, "Access count={}, expected={}".format(opProvider.accessCount, oldAccessCount)
+
+        # We haven't accessed this data yet,
+        # but fixAtCurrent is True so the cache gives us zeros
+        assert (data == 0).all()
+        assert (data.mask == False).all()
+        assert (data.fill_value == numpy.iinfo(numpy.int_).max).all()
+
+        opCache.fixAtCurrent.setValue(False)
+
+        # Since we got zeros while the cache was fixed, the requested
+        #  tiles are signaled as dirty when the cache becomes unfixed.
+        assert len(gotDirtyKeys) == 1
+        assert gotDirtyKeys[0] == make_key[0:1, 0:60, 0:60, 0:10, 0:1]
+
+        # Request again.  Data should match this time.
+        oldAccessCount = opProvider.accessCount
+        data = opCache.Output( slicing ).wait()
+        data.axistags = opCache.Output.meta.axistags
+        assert (data == self.data[slicing]).all()
+        assert (data.mask == self.data.mask[slicing]).all()
+        assert (data.fill_value == self.data.fill_value).all()
+
+        # Our slice intersects 3*3=9 outerBlocks, and a total of 20 innerBlocks
+        # Inner caches are allowed to split up the accesses, so there could be as many as 20
+        minAccess = oldAccessCount + 9
+        maxAccess = oldAccessCount + 20
+        assert opProvider.accessCount >= minAccess
+        assert opProvider.accessCount <= maxAccess
+        oldAccessCount = opProvider.accessCount
+
+        # Request again.  Data should match WITHOUT requesting from the source.
+        data = opCache.Output( slicing ).wait()
+        data.axistags = opCache.Output.meta.axistags
+        assert (data == self.data[slicing]).all()
+        assert (data.mask == self.data.mask[slicing]).all()
+        assert (data.fill_value == self.data.fill_value).all()
+        assert opProvider.accessCount == oldAccessCount, "Access count={}, expected={}".format(opProvider.accessCount, oldAccessCount)
+
+        # Freeze it again
+        opCache.fixAtCurrent.setValue(True)
+
+        # Clear previous
+        gotDirtyKeys = []
+
+        # Change some of the input data that ISN'T cached yet and mark it dirty
+        dirtykey = make_key[0:1, 90:100, 90:100, 0:1, 0:1]
+        self.data[dirtykey] = 0.12345
+        opProvider.Input.setDirty(dirtykey)
+
+        # Dirtiness not propagated due to fixAtCurrent
+        assert len(gotDirtyKeys) == 0
+
+        # Same request.  Data should still match the previous data (not yet refreshed)
+        data2 = opCache.Output( slicing ).wait()
+        data2.axistags = opCache.Output.meta.axistags
+        assert opProvider.accessCount == oldAccessCount, "Access count={}, expected={}".format(opProvider.accessCount, oldAccessCount)
+        assert (data2 == data).all()
+        assert (data2.mask == data.mask).all()
+        assert (data2.fill_value == data.fill_value).all()
+
+        # Unfreeze.
+        opCache.fixAtCurrent.setValue(False)
+
+        # Dirty blocks are propagated after the cache is unfixed.
+        assert len(gotDirtyKeys) > 0
+
+        # Same request.  Data should be updated now that we're unfrozen.
+        data = opCache.Output( slicing ).wait()
+        data.axistags = opCache.Output.meta.axistags
+        assert (data == self.data[slicing]).all()
+        assert (data.mask == self.data.mask[slicing]).all()
+        assert (data.fill_value == self.data.fill_value).all()
+
+        # Dirty data did not intersect with this request.
+        # Data should still be cached (no extra accesses)
+        assert opProvider.accessCount == oldAccessCount, "Access count={}, expected={}".format(opProvider.accessCount, oldAccessCount)
+
+        ###########################3
+        # Freeze it again
+        opCache.fixAtCurrent.setValue(True)
+
+        # Reset tracked notifications
+        gotDirtyKeys = []
+
+        # Change some of the input data that IS cached and mark it dirty
+        dirtykey = make_key[:, 0:25, 20:40, 0:1, :]
+        self.data[dirtykey] = 0.54321
+        opProvider.Input.setDirty(dirtykey)
+
+        # Dirtiness not propagated due to fixAtCurrent
+        assert len(gotDirtyKeys) == 0
+
+        # Same request.  Data should still match the previous data (not yet refreshed)
+        data2 = opCache.Output( slicing ).wait()
+        data2.axistags = opCache.Output.meta.axistags
+        assert opProvider.accessCount == oldAccessCount, "Access count={}, expected={}".format(opProvider.accessCount, oldAccessCount)
+        assert (data2 == data).all()
+        assert (data2.mask == data.mask).all()
+        assert (data2.fill_value == data.fill_value).all()
+
+        # Unfreeze. Previous dirty notifications should now be seen.
+        opCache.fixAtCurrent.setValue(False)
+        assert len(gotDirtyKeys) > 0
+
+        # Same request.  Data should be updated now that we're unfrozen.
+        data = opCache.Output( slicing ).wait()
+        data.axistags = opCache.Output.meta.axistags
+        assert (data == self.data[slicing]).all()
+        assert (data.mask == self.data.mask[slicing]).all()
+        assert (data.fill_value == self.data.fill_value).all()
+
+        # The dirty data intersected 2 outerBlocks, and a total of 6 innerblocks
+        # Inner caches are allowed to split up the accesses, so there could be as many as 6
+        minAccess = oldAccessCount + 2
+        maxAccess = oldAccessCount + 6
+        assert opProvider.accessCount >= minAccess
+        assert opProvider.accessCount <= maxAccess
+        oldAccessCount = opProvider.accessCount
+
+        #####################
+
+        #### Repeat plain dirty test to ensure fixAtCurrent didn't mess up the block states.
+
+        gotDirtyKeys = []
+
+        # Change some of the input data and mark it dirty
+        dirtykey = make_key[0:1, 10:11, 20:21, 0:3, 0:1]
+        self.data[dirtykey] = 0.54321
+        opProvider.Input.setDirty(dirtykey)
+
+        assert len(gotDirtyKeys) > 0
+
+        # Should need access again.
+        slicing = make_key[:, 0:50, 15:45, 0:10, :]
+        data = opCache.Output( slicing ).wait()
+        data.axistags = opCache.Output.meta.axistags
+        assert (data == self.data[slicing]).all()
+        assert (data.mask == self.data.mask[slicing]).all()
+        assert (data.fill_value == self.data.fill_value).all()
+
+        # The dirty data intersected 1 outerBlocks and a total of 1 innerblock
+        minAccess = oldAccessCount + 1
+        maxAccess = oldAccessCount + 1
+        assert opProvider.accessCount >= minAccess
+        assert opProvider.accessCount <= maxAccess
+        oldAccessCount = opProvider.accessCount
+
+
 if __name__ == "__main__":
     import sys
     import nose
