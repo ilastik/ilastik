3eaf7e8d7a082ffcccec2b2e5ebfbf430064aa1e
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-02-26 11:57:35 -0500

    lazyflow/operators/opFillMaskArray.py: Added OpFillMaskArray, an operator that takes a masked array and applies the given fill value to all masked values.

--- lazyflow/operators/opFillMaskArray.py
+++ lazyflow/operators/opFillMaskArray.py
@@ -25,3 +25,49 @@ __date__ = "$Feb 26, 2015 11:47:00 EST$"
 
 
 
+import numpy
+
+from lazyflow.operator import Operator
+from lazyflow.slot import InputSlot, OutputSlot
+
+
+class OpFillMaskArray(Operator):
+    name = "OpFillMaskArray"
+    category = "Pointwise"
+
+
+    InputArray = InputSlot()
+    InputFillValue = InputSlot()
+
+    Output = OutputSlot()
+
+    def __init__(self, *args, **kwargs):
+        super( OpFillMaskArray, self ).__init__( *args, **kwargs )
+
+    def setupOutputs(self):
+        # Copy the input metadata to both outputs
+        self.Output.meta.assignFrom( self.InputArray.meta )
+        self.Output.meta.has_mask = False
+
+    def execute(self, slot, subindex, roi, result):
+        key = roi.toSlice()
+
+        # Get data
+        data = self.InputArray[key].wait()
+        value = self.InputFillValue.value
+
+        # Make a masked array
+        data = data.view(numpy.ma.masked_array)
+
+        # Copy results
+        if slot.name == 'Output':
+            result[...] = data.filled(value)
+
+    def propagateDirty(self, slot, subindex, roi):
+        if (slot.name == "InputArray"):
+            slicing = roi.toSlice()
+            self.Output.setDirty(slicing)
+        elif (slot.name == "InputFillValue"):
+            self.Output.setDirty(slice(None))
+        else:
+            assert False, "Unknown dirty input slot"
