7f86c3112f635602eb06760b07c62eb71db12775
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-02-27 10:14:05 -0500

    lazyflow/operators/opSplitMaskArray.py: Added the operator, `OpSplitMaskArray`, which splits a masked array into its relevant components.

--- lazyflow/operators/opSplitMaskArray.py
+++ lazyflow/operators/opSplitMaskArray.py
@@ -25,3 +25,55 @@ __date__ = "$Feb 27, 2015 09:51:01 EST$"
 
 
 
+import numpy
+
+from lazyflow.operator import Operator
+from lazyflow.slot import InputSlot, OutputSlot
+
+
+class OpSplitMaskArray(Operator):
+    name = "OpSplitMaskArray"
+    category = "Pointwise"
+
+    Input = InputSlot()
+
+    OutputArray = OutputSlot()
+    OutputMask = OutputSlot()
+    OutputFillValue = OutputSlot()
+
+    def __init__(self, *args, **kwargs):
+        super( OpSplitMaskArray, self ).__init__( *args, **kwargs )
+
+    def setupOutputs(self):
+        # Copy the input metadata to both outputs
+        self.OutputArray.meta.assignFrom( self.Input.meta )
+        self.OutputArray.meta.has_mask = False
+
+        self.OutputMask.meta.assignFrom( self.Input.meta )
+        self.OutputMask.meta.has_mask = False
+        self.OutputMask.meta.dtype = numpy.bool8
+
+        self.OutputFillValue.meta.assignFrom( self.Input.meta )
+        self.OutputFillValue.meta.has_mask = False
+        self.OutputFillValue.meta.shape = tuple()
+
+    def execute(self, slot, subindex, roi, result):
+        key = roi.toSlice()
+
+        input_subview = self.Input[key].wait()
+
+        if slot.name == 'OutputArray':
+            result[...] = input_subview.data
+        elif slot.name == 'OutputMask':
+            result[...] = input_subview.mask
+        elif slot.name == 'OutputFillValue':
+            result[...] = input_subview.fill_value
+
+    def propagateDirty(self, slot, subindex, roi):
+        if (slot.name == "Input"):
+            slicing = roi.toSlice()
+            self.OutputArray.setDirty(slicing)
+            self.OutputMask.setDirty(slicing)
+            self.OutputFillValue.setDirty(Ellipsis)
+        else:
+            assert False, "Unknown dirty input slot"
