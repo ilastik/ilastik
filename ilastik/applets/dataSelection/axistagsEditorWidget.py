24d036e40b1cfac1650719ed19bb8e1f7c3b76c9
Stuart Berg <bergs@janelia.hhmi.org>
2015-03-09 14:22:00 -0400

    DataSelection: AxistagsEditorWidget: Added signal axistagsUpdated

--- ilastik/applets/dataSelection/axistagsEditorWidget.py
+++ ilastik/applets/dataSelection/axistagsEditorWidget.py
@@ -1,13 +1,16 @@
 import copy
 import collections
 
-from PyQt4.QtGui import QTableWidget, QDoubleSpinBox, QLineEdit
+from PyQt4.QtCore import pyqtSignal, Qt, QEvent
+from PyQt4.QtGui import QTableWidget, QDoubleSpinBox, QLineEdit 
 
 import numpy
 import vigra
 
 RowWidgets = collections.namedtuple('RowWidgets', "resolution_box description_edit")
-class AxistagsEditorWidget(QTableWidget):    
+class AxistagsEditorWidget(QTableWidget):
+    axistagsUpdated = pyqtSignal()
+
     def __init__(self, parent=None):
         QTableWidget.__init__(self, parent)
         self.axistags = None
@@ -22,16 +25,19 @@ class AxistagsEditorWidget(QTableWidget):
     def init(self, axistags):
         self.init_ui() # See note.
         self.axistags = copy.copy(axistags)
-        axiskeys = [tag.key for tag in self.axistags]
-        self.change_axis_order( axiskeys )
+        axiskeys = [tag.key for tag in axistags]
+        self._change_axistags_order(axiskeys)
+        self._refresh_widgets_from_axistags()
 
     def change_axis_order(self, new_axiskeys):
         """
         Update our stored axistags with the given order, but preserve  
         axis infos that are carried over from the previous tags.
         """
-        self._change_axistags_order(new_axiskeys)
-        self._refresh_widgets_from_axistags()
+        old_axiskeys = [tag.key for tag in self.axistags]
+        if list(new_axiskeys) != old_axiskeys:
+            self._change_axistags_order(new_axiskeys)
+            self._refresh_widgets_from_axistags()
 
     def _change_axistags_order(self, new_axiskeys):
         # Update tags
@@ -54,11 +60,20 @@ class AxistagsEditorWidget(QTableWidget):
             resolution_box.setRange(0.0, numpy.finfo(numpy.float32).max)
             resolution_box.setValue( tag_info.resolution )
             resolution_box.valueChanged.connect( self._update_axistags_from_widgets )
+            resolution_box.installEventFilter(self)
             
             description_edit = QLineEdit(tag_info.description, parent=self)
             description_edit.textChanged.connect( self._update_axistags_from_widgets )
+            description_edit.installEventFilter(self)            
 
             row_widgets[key] = RowWidgets( resolution_box, description_edit )
+
+        # Clean up old widgets (if any)
+        for row in range(self.rowCount()):
+            for col in range(self.columnCount()):
+                w = self.cellWidget( row, col )
+                if w:
+                    w.removeEventFilter(self)
 
         # Fill table with widgets
         self.setRowCount( len(row_widgets) )
@@ -66,13 +81,21 @@ class AxistagsEditorWidget(QTableWidget):
         for row, widgets in enumerate(row_widgets.values()):
             self.setCellWidget( row, 0, widgets.resolution_box )
             self.setCellWidget( row, 1, widgets.description_edit )
-        #self.resizeColumnsToContents()
 
     def _update_axistags_from_widgets(self):
         axiskeys = [tag.key for tag in self.axistags]
         for row, key in enumerate(axiskeys):
             self.axistags[key].resolution = self.cellWidget(row, 0).value()
             self.axistags[key].description = str(self.cellWidget(row, 1).text())
+
+    def eventFilter(self, widget, event):
+        key_pressed = event.type() == QEvent.KeyPress
+        enter_pressed = key_pressed and ( event.key() == Qt.Key_Enter or event.key() == Qt.Key_Return)
+        focus_changed = event.type() == QEvent.FocusOut        
+
+        if enter_pressed or focus_changed:
+            self.axistagsUpdated.emit()
+        return False
 
 if __name__ == "__main__":
     from functools import partial
@@ -92,11 +115,16 @@ if __name__ == "__main__":
     axistags_editor.adjustSize()
     axistags_editor.raise_()
 
+    def handle_update():
+        print "Axistags were updated: {}".format( axistags_editor.axistags )
+    axistags_editor.axistagsUpdated.connect( handle_update )
+
     # Change the order after 2 seconds
     QTimer.singleShot( 2000, partial(axistags_editor.change_axis_order, "tyxc" ) )
     
     app.exec_()
 
-    # Print the final edited values    
+    # Print the final edited values
+    print "FINAL AXISTAGS:"
     print axistags_editor.axistags.toJSON()
     
