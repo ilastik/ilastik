bf0c4e6f8729dd224516ef2c776d09000d585d22
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-03-20 11:59:20 -0400

    tests: Eliminate absolute imports in favor of relative imports.

--- tests/helpers/shellGuiTestCaseBase.py
+++ tests/helpers/shellGuiTestCaseBase.py
@@ -32,7 +32,7 @@ from PyQt4.QtGui import QMouseEvent, QApplication, QPixmap, qApp
 import ilastik.config
 from ilastik.shell.gui.startShellGui import launchShell
 from ilastik.utility.gui.threadRouter import ThreadRouter
-from tests.helpers.mainThreadHelpers import wait_for_main_func, run_in_main_thread
+from helpers.mainThreadHelpers import wait_for_main_func, run_in_main_thread
 
 from ilastik.ilastik_logging import default_config
 default_config.init(output_mode=default_config.OutputMode.CONSOLE)
