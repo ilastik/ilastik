d2585c0a6bacfa92b8d7778dd1deb4bb9e9af52e
Stuart Berg <bergs@janelia.hhmi.org>
2015-04-01 10:44:48 -0400

    workflows: Only warn in debug mode if nanshe can't be imported.

--- ilastik/workflows/__init__.py
+++ ilastik/workflows/__init__.py
@@ -21,6 +21,8 @@
 import logging
 logger = logging.getLogger(__name__)
 
+import ilastik.config
+
 import pixelClassification
 
 try:
@@ -47,20 +49,17 @@ try:
     import tracking.conservation
 except ImportError as e:
     logger.warn( "Failed to import automatic tracking workflow (conservation tracking). For this workflow, see the installation"\
-             "instructions on our website ilastik.org; check dependencies: " + str(e) )
+                 "instructions on our website ilastik.org; check dependencies: " + str(e) )
 
 try:
     import nanshe.nansheWorkflow
 except ImportError as e:
-    logger.warn( "Failed to import nanshe workflow. For this workflow, see the installation"\
-             "instructions on our website ilastik.org; check dependencies: " + str(e) )
-
-
-# Examples
-import ilastik.config
+    if ilastik.config.cfg.getboolean('ilastik', 'debug'):
+        logger.warn( "Failed to import nanshe workflow. Check dependencies: " + str(e) )
 
 import examples.dataConversion
 
+# Examples
 if ilastik.config.cfg.getboolean('ilastik', 'debug'):
     import vigraWatershed
     import examples.layerViewer
