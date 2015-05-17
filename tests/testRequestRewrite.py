3f9cf47f3daa2e1d587cdb0b5e005caee82c5e19
Stuart Berg <bergs@janelia.hhmi.org>
2015-05-17 18:50:43 -0400

    tests: Fix timing issue in a request test.

--- tests/testRequestRewrite.py
+++ tests/testRequestRewrite.py
@@ -820,6 +820,10 @@ class TestRequestExceptions(object):
         req.submit()
         req.wait()
         del req
+        
+        # The ThreadPool._Worker loop has a local reference (next_task), 
+        # so wait just a tic for the ThreadPool worker to cycle back to the top of its loop (and discard the reference) 
+        time.sleep(0.1)
         assert w[0]() is None
     
      
