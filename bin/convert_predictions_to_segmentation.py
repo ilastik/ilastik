9bfe2bb91d7c95fec8b0ea9d00191ece138d3349
Stuart Berg <bergs@janelia.hhmi.org>
2015-06-04 11:12:47 -0400

    bin/convert_predictions_to_segmentation.py: Support multiple input files.

--- bin/convert_predictions_to_segmentation.py
+++ bin/convert_predictions_to_segmentation.py
@@ -8,7 +8,7 @@ from lazyflow.operators.ioOperators import OpInputDataReader, OpFormattedDataExp
 from ilastik.applets.pixelClassification.opPixelClassification import OpArgmaxChannel
 from ilastik.applets.dataExport.dataExportApplet import DataExportApplet
 
-def convert_predictions_to_segmentation( input_path, parsed_export_args ):
+def convert_predictions_to_segmentation( input_paths, parsed_export_args ):
     """
     Read exported pixel predictions and calculate/export the segmentation.
     
@@ -18,7 +18,6 @@ def convert_predictions_to_segmentation( input_path, parsed_export_args ):
     graph = Graph()
     opReader = OpInputDataReader(graph=graph)
     opReader.WorkingDirectory.setValue( os.getcwd() )
-    opReader.FilePath.setValue(input_path)
 
     opArgmaxChannel = OpArgmaxChannel( graph=graph )
     opArgmaxChannel.Input.connect( opReader.Output )
@@ -34,14 +33,15 @@ def convert_predictions_to_segmentation( input_path, parsed_export_args ):
         if progress_percent != last_progress[0]:
             last_progress[0] = progress_percent
             sys.stdout.write( " {}".format(progress_percent) )
-    
-    print "Exporting results to : {}".format( opExport.ExportPath.value )    
-    sys.stdout.write("Progress:")
     opExport.progressSignal.subscribe(print_progress)
 
-    # Begin export
-    opExport.run_export()
-    sys.stdout.write("\n")
+    for input_path in input_paths: 
+        opReader.FilePath.setValue(input_path)
+        print "Exporting results to : {}".format( opExport.ExportPath.value )    
+        sys.stdout.write("Progress:")
+        # Begin export
+        opExport.run_export()
+        sys.stdout.write("\n")
     print "DONE."
     
 def all_dataset_internal_paths(f):
@@ -57,31 +57,34 @@ def all_dataset_internal_paths(f):
 if __name__ == "__main__":
     import sys
     import argparse
-    #sys.argv += "/tmp/example_slice.h5/data --export_drange=(0,255) --output_format=png --pipeline_result_drange=(1,2)".split()
+    sys.argv += "/tmp/example_slice.h5/data /tmp/example_slice.h5/data --export_drange=(0,255) --output_format=png --pipeline_result_drange=(1,2)".split()
     
-    # Construct a parser with all the 'normal' export options, and add arg for input_path.
+    # Construct a parser with all the 'normal' export options, and add arg for prediction_image_paths.
     parser = DataExportApplet.make_cmdline_parser( argparse.ArgumentParser() )
-    parser.add_argument("input_path", help="Path to your exported predictions.")
+    parser.add_argument("prediction_image_paths", nargs='+', help="Path(s) to your exported predictions.")
     parsed_args = parser.parse_args()
     parsed_args, unused_args = DataExportApplet.parse_known_cmdline_args( sys.argv[1:], parsed_args )
     
     # As a convenience, auto-determine the internal dataset path if possible.
-    path_comp = PathComponents(parsed_args.input_path, os.getcwd())
-    if path_comp.extension in PathComponents.HDF5_EXTS and path_comp.internalDatasetName == "":
-        
-        with h5py.File(path_comp.externalPath, 'r') as f:
-            all_internal_paths = all_dataset_internal_paths(f)
-
-        if len(all_internal_paths) == 1:
-            path_comp.internalPath = all_internal_paths[0]
-            parsed_args.input_path = path_comp.totalPath()
-        elif len(all_internal_paths) == 0:
-            sys.stderr.write("Could not find any datasets in your input file.")
-            sys.exit(1)
-        else:
-            sys.stderr.write("Found more than one dataset in your input file.\n"
-                             "Please specify the dataset name, e.g. /path/to/myfile.h5/internal/dataset_name")
-            sys.exit(1)
+    for index, input_path in enumerate(parsed_args.prediction_image_paths):
+        path_comp = PathComponents(input_path, os.getcwd())
+        if path_comp.extension in PathComponents.HDF5_EXTS and path_comp.internalDatasetName == "":
+            
+            with h5py.File(path_comp.externalPath, 'r') as f:
+                all_internal_paths = all_dataset_internal_paths(f)
+    
+            if len(all_internal_paths) == 1:
+                path_comp.internalPath = all_internal_paths[0]
+                parsed_args.prediction_image_paths[index] = path_comp.totalPath()
+            elif len(all_internal_paths) == 0:
+                sys.stderr.write("Could not find any datasets in your input file:\n"
+                                 "{}\n".format(input_path))
+                sys.exit(1)
+            else:
+                sys.stderr.write("Found more than one dataset in your input file:\n"
+                                 "{}\n".format(input_path) +
+                                 "Please specify the dataset name, e.g. /path/to/myfile.h5/internal/dataset_name\n")
+                sys.exit(1)
 
     # As a convenience, if the user didn't explicitly specify an output file name, provide one for him.
     if not parsed_args.output_filename_format:
@@ -91,5 +94,5 @@ if __name__ == "__main__":
     if path_comp.extension in PathComponents.HDF5_EXTS and not parsed_args.output_internal_path:
         parsed_args.output_internal_path = "segmentation"        
 
-    sys.exit( convert_predictions_to_segmentation( parsed_args.input_path, 
+    sys.exit( convert_predictions_to_segmentation( parsed_args.prediction_image_paths, 
                                                    parsed_args ) )
