ef24dbe2e72e2771da938271e4f1ca19ba3f707b
Stuart Berg <bergs@janelia.hhmi.org>
2015-06-23 11:08:12 -0400

    classifier: If feature_names weren't provided, don't attempt to serialize them.

--- lazyflow/classifiers/parallelVigraRfLazyflowClassifier.py
+++ lazyflow/classifiers/parallelVigraRfLazyflowClassifier.py
@@ -167,7 +167,8 @@ class ParallelVigraRfLazyflowClassifier(LazyflowVectorwiseClassifierABC):
 
         h5py_group = parent_group[name]
         h5py_group['known_labels'] = self._known_labels
-        h5py_group['feature_names'] = self._feature_names
+        if self._feature_names:
+            h5py_group['feature_names'] = self._feature_names
         
         # This field is required for all classifiers
         h5py_group['pickled_type'] = pickle.dumps( type(self) )
