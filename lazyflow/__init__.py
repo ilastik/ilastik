e9fcab6c932062372956d079c48cb71a00232d4a
Stuart Berg <bergs@janelia.hhmi.org>
2015-09-15 22:20:47 -0400

    Removed submodules

--- lazyflow/__init__.py
+++ lazyflow/__init__.py
@@ -25,18 +25,6 @@ try:
 except ImportError:
     pass
 
-import os
-this_file = os.path.abspath(__file__)
-this_file = os.path.realpath( this_file )
-lazyflow_package_dir = os.path.dirname(this_file)
-lazyflow_repo_dir = os.path.dirname(lazyflow_package_dir)
-submodule_dir = os.path.join( lazyflow_repo_dir, 'submodules' )
-
-# Add all submodules to the PYTHONPATH
-import expose_submodules
-expose_submodules.expose_submodules(submodule_dir)
-
-
 import utility
 import roi
 import rtype
