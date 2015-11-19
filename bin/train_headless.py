648f58a6f60a32b31d48e040fc70aea99b711ce6
Stuart Berg <bergs@janelia.hhmi.org>
2015-11-19 11:17:41 -0500

    bin/train_headless.py: Cleaned up some obsolete lines and improved the docstrings.

--- bin/train_headless.py
+++ bin/train_headless.py
@@ -5,9 +5,11 @@ At this time, this script does not provide a command-line API for selecting the 
 Edit the feature matrix in the code below.
 
 Also, this script's cmdline API supports only a single grayscale and label volume as input, 
-but it can work with multiple input volumes if you simply edit RAW_DATA_FILEPATHS and LABEL_DATA_FILEPATHS in the code below.
+so just write your own API that calls generate_trained_project_file() if you need more than that.
 
-Lastly, this script assumes you have only two label classes.  Edit NUM_LABEL_CLASSES to change.
+Note that this example makes no attempt to be careful with RAM usage. In particular, it loads each
+label image in its entirety before pushing its data into the training pipeline.  This could be fixed,
+but would obfuscate this example somewhat.
 
 Example usage:
     # With 2D PNG input.
@@ -37,8 +39,6 @@ def main():
     parser.add_argument("raw_data")
     parser.add_argument("label_data")    
     parsed_args = parser.parse_args()
-    
-    assert len(RAW_DATA_FILEPATHS) == len(LABEL_DATA_FILEPATHS)
 
     # FIXME: This function returns hard-coded features for now.
     feature_selections = prepare_feature_selections()
@@ -106,7 +106,18 @@ def generate_trained_project_file( new_project_path,
     
     Finally, request the classifier object from the pipeline (which forces training),
     and save the project.
+    
+    new_project_path: Where to save the new project file
+    raw_data_paths: A list of paths to the raw data images to train with
+    label_data_paths: A list of paths to the label image data to train with
+    feature_selections: A matrix of bool, representing the selected features
+    classifier_factory: Override the classifier type.  Must be a subclass of either:
+                        - lazyflow.classifiers.LazyflowVectorwiseClassifierFactoryABC
+                        - lazyflow.classifiers.LazyflowPixelwiseClassifierFactoryABC
     """
+    assert len(raw_data_paths) == len(label_data_paths), \
+        "Number of label images must match number of raw images."
+    
     import ilastik_main
     from ilastik.workflows.pixelClassification import PixelClassificationWorkflow
     from lazyflow.graph import Graph
