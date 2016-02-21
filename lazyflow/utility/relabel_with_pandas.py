e2ca2fe4528b2f06431b1f53a894b7e91bb2f8c5
Stuart Berg <bergs@janelia.hhmi.org>
2016-02-20 23:43:33 -0500

    relabel_with_pandas: Optimizations (It turns out Series.replace() is not very fast.)

--- lazyflow/utility/relabel_with_pandas.py
+++ lazyflow/utility/relabel_with_pandas.py
@@ -17,10 +17,6 @@ def relabel_with_pandas(labels, mapping, out=None):
          `labels`, but not of a different type.
          For example, you can map from uint64 to uint32, but not from uint64 to float32.
     """
-    # Late import
-    # For now, pandas is not an official dependency of lazyflow
-    import pandas as pd
-    
     if out is not None:
         assert out.shape == labels.shape, \
             "'out' parameter does not match input shape: {} != {}"\
@@ -37,18 +33,81 @@ def relabel_with_pandas(labels, mapping, out=None):
     if out.flags.contiguous and out.dtype.itemsize >= labels.dtype.itemsize:
         if labels is not out:
             out[:] = labels # copy
-        flat_view = out.reshape(-1, order='A')
-        assert flat_view.flags.owndata is False
-        pd.Series(flat_view).replace(mapping, inplace=True)
+        _relabel_inplace(out, mapping)
     else:
-        # Slower path: Must work with an intermediate table, and copy it into the final result
-        copy_dtype = labels.dtype
+        # Slower path: Must work with an intermediate series, and copy it into the final result
         if labels.dtype.itemsize < out.dtype.itemsize:
             copy_dtype = out.dtype
+        else:
+            copy_dtype = labels.dtype
         copied_labels = np.array( labels, dtype=copy_dtype, copy=True, order='A' ) # allocate + copy
-        flat_view = copied_labels.reshape(-1, order='A')
-        assert flat_view.flags.owndata is False
-        series = pd.Series(flat_view)
-        relabeled_series = series.replace(mapping, inplace=True)
-        out[:] = copied_labels # copy
+        _relabel_inplace(copied_labels, mapping)
+        out[:] = copied_labels.reshape( out.shape ) # copy
     return out
+
+def _relabel_inplace(labels, mapping):
+    """
+    Relabel the given *contiguous* array in-place according to the given mapping dict.
+
+    Caveats
+    -------
+    - 'labels' must be contiguous
+    - This function uses low-level pandas functions, which may or may not
+      experience API-breaking changes in subsequent versions of pandas.
+    
+    Performance notes
+    -----------------
+    For comparison, I wrote a custom C++ function that performs relabeling on C-order uint32 data.
+    For tiny maps (e.g. len(mapping) == 10), this function is 3x slower.
+    For medium-sized maps (e.g. len(mapping) == 1e4), this function is on-par.
+    For very large maps (e.g. len(mapping) == 1e6), this function is 20% faster.
+    """
+    # Late import
+    # For now, pandas is not an official dependency of lazyflow
+    import pandas as pd
+        
+    assert labels.flags.contiguous
+    flat_view = labels.reshape(-1, order='A')
+    assert flat_view.flags.owndata is False
+    mapping_series = pd.Series(mapping, index=mapping.keys())
+    indexer = mapping_series.index.get_indexer(flat_view)
+    pd.core.common.take_1d(mapping_series.values, indexer, out=flat_view)
+
+if __name__ == "__main__":
+    import sys
+    import logging
+    logger = logging.getLogger(__name__)
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.setLevel(logging.DEBUG)
+
+    from ilastik.applets.edgeTraining.util import label_vol_mapping
+    
+    import h5py
+    watershed_path = '/magnetic/data/flyem/chris-two-stage-ilps/volumes/subvol/256/watershed-256.h5'
+    #watershed_path = '/magnetic/data/flyem/chris-two-stage-ilps/volumes/subvol/512/watershed-512.h5'
+
+    logger.info("Loading watershed...")
+    with h5py.File(watershed_path, 'r') as f:
+        watershed = f['watershed'][:]
+        if watershed.shape[-1] == 1:
+            watershed = watershed[...,0]
+    
+    groundtruth_path = '/magnetic/data/flyem/chris-two-stage-ilps/volumes/subvol/256/segmentation-256.h5'
+
+    logger.info("Loading groundtruth...")
+    with h5py.File(groundtruth_path, 'r') as f:
+        groundtruth = f['segmentation'][:]
+        if groundtruth.shape[-1] == 1:
+            groundtruth = groundtruth[...,0]
+
+    index_mapping = label_vol_mapping(watershed, groundtruth)
+    mapping = dict( enumerate( index_mapping ) )
+
+    #relabeled = relabel_via_vectorize(watershed, mapping)
+    relabeled = relabel_with_pandas(watershed, mapping)
+    
+    print groundtruth[0]
+    print relabeled[0]
+
+    assert (relabeled == groundtruth).all()
+    print "Done."
