2226aa03b7966094df1e64926b5b6a2eedced677
Stuart Berg <bergs@janelia.hhmi.org>
2016-02-25 13:18:18 -0500

    edgeTraining.util: Added relabel_volume_from_edge_decisions()

--- ilastik/applets/edgeTraining/util.py
+++ ilastik/applets/edgeTraining/util.py
@@ -2,6 +2,7 @@ import logging
 from itertools import imap, izip
 
 import numpy as np
+import networkx as nx
 import vigra
 
 from lazyflow.utility.edge_features import edge_id_mask, unique_edge_labels, label_vol_mapping
@@ -31,6 +32,51 @@ def edge_decisions( overseg_vol, groundtruth_vol, asdict=True ):
         return dict( izip(imap(tuple, unique_sp_edges), decisions) )
     return unique_sp_edges, decisions
 
+def relabel_volume_from_edge_decisions( supervoxels, edge_ids, edge_decisions, out=None ):
+    """
+    Given a supervoxel volume, and a set of edge_ids and corresponding ON/OFF labels
+    for the edges, compute a new label volume in which all supervoxels with at least
+    one inactive edge between them are merged together.
+    
+    Parameters
+    ----------
+    supervoxels: label array, labels do not need to be consecutive,
+                 but excessively high label values may lead to poor performance
+                 (see xrange call below)
+
+    edge_ids: array of shape (N,2).  Must include all INactive edges.
+              Active edges can be omitted (they'll be removed anyway).
+
+    edge_decisions: 1D bool array of shape (N,).
+                    1 means "active", i.e. the two superpixels are separated across that edge, at least
+                    0 means "inactive", i.e. the two superpixels will be joined in the final result.
+
+    out: Optional. Must be same shape as supervoxels, but have different dtype.
+    """
+    assert hasattr(supervoxels, 'axistags'), \
+        "Must provide accurate axistags, otherwise performance suffers by 10x"
+    assert out is None or hasattr(out, 'axistags'), \
+        "Must provide accurate axistags, otherwise performance suffers by 10x"
+
+    inactive_edge_ids = edge_ids[np.nonzero( np.logical_not(edge_decisions) )]
+
+    logger.debug("Finding connected components in node graph...")
+    g = nx.Graph( list(inactive_edge_ids) ) 
+    
+    # If any supervoxels are completely independent (not merged with any neighbors),
+    # they haven't been added to the graph yet.
+    # Add them now.
+    g.add_nodes_from(xrange(np.max(supervoxels)+1), dtype=edge_ids.dtype)
+    
+    sp_mapping = {}
+    for i, sp_ids in enumerate(nx.connected_components(g), start=1):
+        for sp_id in sp_ids:
+            sp_mapping[int(sp_id)] = i
+    del g
+
+    logger.debug("Relabeling supervoxels with connected components...")
+    segmentation_img = vigra.analysis.applyMapping( supervoxels, sp_mapping, out=out )
+    return segmentation_img
 
 if __name__ == "__main__":
     # 1 2
