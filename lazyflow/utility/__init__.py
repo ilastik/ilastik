528c1125efed5d9125fddcf7bba5df48e6e98de3
Stuart Berg <bergs@janelia.hhmi.org>
2016-02-25 13:04:33 -0500

    Deleted relabel_with_pandas

--- lazyflow/utility/__init__.py
+++ lazyflow/utility/__init__.py
@@ -40,4 +40,3 @@ from export_to_tiles import export_to_tiles
 from blockwise_view import blockwise_view
 from priorityQueue import PriorityQueue
 from log_exception import log_exception
-from relabel_with_pandas import relabel_with_pandas
