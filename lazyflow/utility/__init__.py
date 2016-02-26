a663c0fc514cbd0dd31b7818cdc50aeb7aa8a74c
Stuart Berg <bergs@janelia.hhmi.org>
2016-02-26 11:39:53 -0500

    Added utility.relabel_consecutive()

--- lazyflow/utility/__init__.py
+++ lazyflow/utility/__init__.py
@@ -40,3 +40,4 @@ from export_to_tiles import export_to_tiles
 from blockwise_view import blockwise_view
 from priorityQueue import PriorityQueue
 from log_exception import log_exception
+from relabel_consecutive import relabel_consecutive
