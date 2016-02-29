f5704d8a47668540cb51a9c027a20a4c545c0771
Stuart Berg <bergs@janelia.hhmi.org>
2016-02-28 22:57:35 -0500

    tests: testOpRelabelConsecutive: Fixed axistags issue.

--- tests/testOpRelabelConsecutive.py
+++ tests/testOpRelabelConsecutive.py
@@ -1,4 +1,5 @@
 import numpy as np
+import vigra
 from lazyflow.graph import Graph
 from lazyflow.operators import OpRelabelConsecutive
 
@@ -16,6 +17,7 @@ class TestOpRelabelConsecutive(object):
         op = OpRelabelConsecutive(graph=Graph())
         
         labels = 2*np.arange(0,100, dtype=np.uint8).reshape((10,10))
+        labels = vigra.taggedView(labels, 'yx')
         op.Input.setValue(labels)
         relabeled = op.Output[:].wait()
         assert (relabeled == labels/2).all()
@@ -25,6 +27,7 @@ class TestOpRelabelConsecutive(object):
         op.StartLabel.setValue(10)
         
         labels = 2*np.arange(0,100, dtype=np.uint8).reshape((10,10))
+        labels = vigra.taggedView(labels, 'yx')
         op.Input.setValue(labels)
         relabeled = op.Output[:].wait()
         assert (relabeled == 10+labels/2).all()
