ilastik/ilastik

commit 0a413b15559b47acb21c7702526e46c204e4d0fe

Labeling: Added right-click option for "Clear"

ilastik/applets/labeling/labelingGui.py
@@ -196,6 +196,18 @@ class LabelingGui(LayerViewerGui):
         _labelControlUi.labelListModel.rowsRemoved.connect(self._onLabelRemoved)
         _labelControlUi.labelListModel.elementSelected.connect(self._onLabelSelected)
 
+        def handleClearRequested( row, name ):
+            selection = QMessageBox.warning(self, "Clear labels?",
+                          "All '{}' brush strokes will be erased.  Are you sure?"
+                          .format(name),
+                          QMessageBox.Ok | QMessageBox.Cancel)
+            if selection != QMessageBox.Ok:
+                return
+
+            # This only works if the top-level operator has a 'mergeLabels' function.
+            self.topLevelOperatorView.clearLabel( row+1 )
+        _labelControlUi.labelListView.clearRequested.connect( handleClearRequested )
+
         def handleLabelMergeRequested(from_row, from_name, into_row, into_name):
             from_label = from_row+1
             into_label = into_row+1
@@ -207,7 +219,7 @@ class LabelingGui(LayerViewerGui):
                 return
 
             # This only works if the top-level operator has a 'mergeLabels' function.
-            self.topLevelOperatorView.mergeLabels( from_label, into_label )
+            self.topLevelOperatorView.mergeLabels( from_label+1, into_label+1 )
 
             names = list(self._labelingSlots.labelNames.value)
             names.pop(from_label-1)

ilastik/applets/pixelClassification/opPixelClassification.py
@@ -341,6 +341,10 @@ class OpPixelClassification( Operator ):
         for laneIndex in range(len(self.InputImages)):
             self.getLane( laneIndex ).opLabelPipeline.opLabelArray.mergeLabels(from_label, into_label)
 
+    def clearLabel(self, label_value):
+        for laneIndex in range(len(self.InputImages)):
+            self.getLane( laneIndex ).opLabelPipeline.opLabelArray.clearLabel(label_value)
+
 class OpLabelPipeline( Operator ):
     RawImage = InputSlot()
     LabelInput = InputSlot()

ilastik/widgets/labelListView.py
@@ -63,6 +63,7 @@ class ColorDialog(QDialog):
 
 
 class LabelListView(ListView):
+    clearRequested = pyqtSignal(int, str) # row, name
     mergeRequested = pyqtSignal( int, str, int, str ) # from_row, from_name, to_row, to_name
 
     def __init__(self, parent = None):
@@ -88,23 +89,23 @@ class LabelListView(ListView):
         
 
     def contextMenuEvent(self, event):
-        if not self.support_merges or not self.allowDelete:
-            return
-
         from_index = self._table.indexAt(event.pos())
         if not (0 <= from_index.row() < self.model.rowCount()):
             return
 
         from_name = self.model.data(from_index, Qt.DisplayRole)
         menu = QMenu(parent=self)
-        for to_row in range(self.model.rowCount()):
-            to_index = self.model.index(to_row, LabelListModel.ColumnID.Name)
-            to_name = self.model.data(to_index, Qt.DisplayRole)
-            action = menu.addAction( "Merge {} into {}".format( from_name, to_name ),
-                                     partial( self.mergeRequested.emit, from_index.row(), str(from_name),
-                                                                        to_row,           str(to_name)) )
-            if to_row == from_index.row():
-                action.setEnabled(False)
+        menu.addAction("Clear {}".format(from_name), partial(self.clearRequested.emit, from_index.row(), str(from_name)))
+
+        if self.support_merges and self.allowDelete:
+            for to_row in range(self.model.rowCount()):
+                to_index = self.model.index(to_row, LabelListModel.ColumnID.Name)
+                to_name = self.model.data(to_index, Qt.DisplayRole)
+                action = menu.addAction( "Merge {} into {}".format( from_name, to_name ),
+                                         partial( self.mergeRequested.emit, from_index.row(), str(from_name),
+                                                                            to_row,           str(to_name)) )
+                if to_row == from_index.row():
+                    action.setEnabled(False)
 
         menu.exec_( self.mapToGlobal(event.pos()) )
 
@@ -144,6 +145,7 @@ if __name__ == '__main__':
     w.raise_()
 
     tableView = LabelListView()
+    tableView.support_merges = True
     l.addWidget(tableView)
     tableView.setModel(model)
 

commit c3975eb3d535353579ce3b240b07e1c246451c19

LabelingGui: Fix off-by-one error introduced in 0a413b15559b47acb21c7702526e46c204e4d0fe Fixes #1196

ilastik/applets/labeling/labelingGui.py
@@ -219,7 +219,7 @@ class LabelingGui(LayerViewerGui):
                 return
 
             # This only works if the top-level operator has a 'mergeLabels' function.
-            self.topLevelOperatorView.mergeLabels( from_label+1, into_label+1 )
+            self.topLevelOperatorView.mergeLabels( from_label, into_label )
 
             names = list(self._labelingSlots.labelNames.value)
             names.pop(from_label-1)