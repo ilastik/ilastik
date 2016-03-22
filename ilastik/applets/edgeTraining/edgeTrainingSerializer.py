a2bbcd9bd4c35fc0872426c1ddecd9c8d81b084f
Stuart Berg <bergs@janelia.hhmi.org>
2016-03-21 22:52:06 -0400

    multicut: EdgeTraining: Rag is serialized to the project file.

--- ilastik/applets/edgeTraining/edgeTrainingSerializer.py
+++ ilastik/applets/edgeTraining/edgeTrainingSerializer.py
@@ -18,10 +18,57 @@
 # on the ilastik web site at:
 #           http://ilastik.org/license.html
 ###############################################################################
+import vigra
 from ilastik.applets.base.appletSerializer import AppletSerializer, SerialSlot, SerialDictSlot
+from ilastikrag import Rag
+
+class SerialRagSlot(SerialSlot):
+    def __init__(self, slot, cache, labels_slot):
+        super(SerialRagSlot, self).__init__(slot, name='Rags')
+        self.cache = cache
+        self.labels_slot = labels_slot
+        
+        # We want to bind to the INPUT, not Output:
+        # - if the input becomes dirty, we want to make sure the cache is deleted
+        # - if the input becomes dirty and then the cache is reloaded, we'll save the rag.
+        self._bind(cache.Input)
+
+    def _serialize(self, parent_group, name, multislot):
+        rags_group = parent_group.create_group( name )
+        
+        for lane_index, slot in enumerate(multislot):
+            # Is the cache up-to-date?
+            # if not, we'll just return (don't recompute the classifier just to save it)
+            if self.cache[lane_index]._dirty:
+                continue
+
+            rag = self.cache[lane_index].Output.value
+    
+            # Rag can be None if there isn't any training data yet.
+            if rag is None:
+                continue
+
+            rag_group = rags_group.create_group( "Rag_{:04}".format(lane_index) )
+            rag.serialize_hdf5( rag_group, store_labels=False )
+
+    def deserialize(self, rags_group):
+        """
+        Have to override this to ensure that dirty is always set False.
+        """
+        super(SerialRagSlot, self).deserialize(rags_group)
+        self.dirty = False
+
+    def _deserialize(self, rags_group, slot):
+        for lane_index, (_rag_groupname, rag_group) in enumerate(sorted(rags_group.items())):
+            label_img = self.labels_slot[lane_index][:].wait()
+            label_img = vigra.taggedView( label_img, self.labels_slot.meta.axistags )
+            label_img = label_img.dropChannelAxis()
+            
+            rag = Rag.deserialize_hdf5( rag_group, label_img )
+            self.cache[lane_index].forceValue( rag )
 
 class EdgeTrainingSerializer(AppletSerializer):
     def __init__(self, operator, projectFileGroupName):
-        slots = [ SerialDictSlot(operator.FeatureNames, selfdepends=True) ]
+        slots = [ SerialDictSlot(operator.FeatureNames),
+                  SerialRagSlot(operator.Rag, operator.opRagCache, operator.Superpixels) ]
         super(EdgeTrainingSerializer, self).__init__(projectFileGroupName, slots=slots)
-        
