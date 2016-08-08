1fe2304a30c1d74bc4650986cbf6c37c8c835fe4
Janez Ales <janez.ales@iwr.uni-heidelberg.de>
2016-08-08 18:35:48 +0200

    add spaces to info strings

--- ilastik/applets/tracking/annotations/annotationsGui.py
+++ ilastik/applets/tracking/annotations/annotationsGui.py
@@ -999,12 +999,12 @@ class AnnotationsGui(LayerViewerGui):
 
                 res = self._addObjectToTrack(activeTrack, uniqueLabels[0], t)
                 if res == -98:
-                    self._informationMessage("Info: Object " + str(oid) + " in time frame " + str(t) + " left the current crop time boundary." + \
+                    self._informationMessage("Info: Object " + str(oid) + " in time frame " + str(t) + " left the current crop time boundary. " + \
                                          "Stopping automatic tracking at crop boundary.")
                     self._gotoObject(uniqueLabels[0], t, keepXYZ=True)
                     return
                 elif res == -99:
-                    self._informationMessage("Info: Object " + str(oid) + " in time frame " + str(t) + " left the current crop spatial boundary." + \
+                    self._informationMessage("Info: Object " + str(oid) + " in time frame " + str(t) + " left the current crop spatial boundary. " + \
                                          "Stopping automatic tracking at crop boundary.")
                     self._gotoObject(uniqueLabels[0], t, keepXYZ=True)
                     return
