ca7ace74fb112d959b79db1da7ed7e4608f3ce51
Stuart Berg <bergs@janelia.hhmi.org>
2016-08-17 14:46:05 -0400

    logging: Hide warnings about duplicate python converter functions.

--- ilastik/ilastik_logging/default_config.py
+++ ilastik/ilastik_logging/default_config.py
@@ -22,6 +22,7 @@ import os
 import logging.config
 import warnings
 import loggingHelpers
+from ilastik.config import cfg as ilastik_config
 
 DEFAULT_LOGFILE_PATH = os.path.expanduser("~/ilastik_log.txt")
 
@@ -251,6 +252,15 @@ def init(format_prefix="", output_mode=OutputMode.LOGFILE_WITH_CONSOLE_ERRORS, l
 
     # Don't warn about pending deprecations (PyQt generates some of these)
     warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
+
+    # Don't warn about duplicate python bindings for opengm
+    # (We import opengm twice, as 'opengm' 'opengm_with_cplex'.)
+    warnings.filterwarnings("ignore", message='.*to-Python converter for .*opengm.*', category=RuntimeWarning)
+
+    # Hide all other python converter warnings unless we're in debug mode.
+    if not ilastik_config.getboolean("ilastik", "debug"):
+        warnings.filterwarnings("ignore", message='.*to-Python converter for .*second conversion method ignored.*', category=RuntimeWarning)
+        
     
     # Custom format for warnings
     def simple_warning_format(message, category, filename, lineno, line=None):
