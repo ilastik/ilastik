0b5bcd4fe065b9ef26bbccf42d0809534444e142
jaimeivancervantes <jimmycc80@hotmail.com>
2016-10-18 16:10:33 -0400

    ConservationTracking: Replaced check for Windows with check for Hytra import

--- ilastik/applets/tracking/conservation/conservationTrackingApplet.py
+++ ilastik/applets/tracking/conservation/conservationTrackingApplet.py
@@ -4,13 +4,18 @@ from ilastik.applets.tracking.base.trackingSerializer import TrackingSerializer
 import logging
 logger = logging.getLogger(__name__)
 
-import os
+try:
+    import hytra
+    WITH_HYTRA = True
+except ImportError as e:
+    WITH_HYTRA = False
 
-if os.name == 'nt': # Use old PgmLink tracking if OS is Windows
+if WITH_HYTRA:
+    from ilastik.applets.tracking.conservation.opConservationTracking import OpConservationTracking
+else:
+    # Use old PgmLink tracking operator if we can't import Hytra (When OS is Windows)
     from ilastik.applets.tracking.conservation.opConservationTrackingPgmLink import OpConservationTrackingPgmLink as OpConservationTracking
     logger.info("Using old conservation tracking workflow (PgmLink)")
-else:
-    from ilastik.applets.tracking.conservation.opConservationTracking import OpConservationTracking
 
 class ConservationTrackingApplet( StandardApplet ):
     def __init__( self, name="Tracking", workflow=None, projectFileGroupName="ConservationTracking" ):
