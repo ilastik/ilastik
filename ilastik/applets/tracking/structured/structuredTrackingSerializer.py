05ef296f5fa4ca910fd75320b83e193e69c44c09
Janez Ales <janez.ales@iwr.uni-heidelberg.de>
2017-01-18 14:57:31 +0100

    Serialization for backward compatibility (for tracking with pgmlink) Remove this when Windows supports the new pipeline (HyTra)

--- ilastik/applets/tracking/structured/structuredTrackingSerializer.py
+++ ilastik/applets/tracking/structured/structuredTrackingSerializer.py
@@ -18,28 +18,63 @@
 # on the ilastik web site at:
 #		   http://ilastik.org/license.html
 ###############################################################################
-from ilastik.applets.base.appletSerializer import AppletSerializer, SerialSlot, SerialDictSlot, SerialPickleableSlot, SerialPickledValueSlot
+from ilastik.applets.base.appletSerializer import AppletSerializer, SerialSlot, SerialDictSlot, \
+    SerialHdf5BlockSlot, SerialPickleableSlot, SerialPickledValueSlot
 
-import pgmlink
+try:
+    import hytra
+    WITH_HYTRA = True
+except ImportError as e:
+    WITH_HYTRA = False
 
 class StructuredTrackingSerializer(AppletSerializer):
 
     def __init__(self, topLevelOperator, projectFileGroupName):
-        slots = [ SerialDictSlot(topLevelOperator.Parameters, selfdepends=True),
-                  SerialDictSlot(topLevelOperator.EventsVector, transform=str, selfdepends=True),
-                  SerialDictSlot(topLevelOperator.FilteredLabels, transform=str, selfdepends=True),
-                  SerialPickledValueSlot(topLevelOperator.ExportSettings),
-                  SerialPickledValueSlot(topLevelOperator.HypothesesGraph),
-                  SerialPickledValueSlot(topLevelOperator.ResolvedMergers),
-                  SerialSlot(topLevelOperator.DivisionWeight),
-                  SerialSlot(topLevelOperator.DetectionWeight),
-                  SerialSlot(topLevelOperator.TransitionWeight),
-                  SerialSlot(topLevelOperator.AppearanceWeight),
-                  SerialSlot(topLevelOperator.DisappearanceWeight),
-                  SerialSlot(topLevelOperator.MaxNumObjOut)
-        ]
 
-        if 'CoordinateMap' in topLevelOperator.outputs:
-            slots.append(SerialPickleableSlot(topLevelOperator.CoordinateMap, 1, pgmlink.TimestepIdCoordinateMap()))
+        if WITH_HYTRA:
+            slots = [ SerialDictSlot(topLevelOperator.Parameters, selfdepends=True),
+                      SerialDictSlot(topLevelOperator.EventsVector, transform=str, selfdepends=True),
+                      SerialDictSlot(topLevelOperator.FilteredLabels, transform=str, selfdepends=True),
+                      SerialPickledValueSlot(topLevelOperator.ExportSettings),
+                      SerialPickledValueSlot(topLevelOperator.HypothesesGraph),
+                      SerialPickledValueSlot(topLevelOperator.ResolvedMergers),
+                      SerialSlot(topLevelOperator.DivisionWeight),
+                      SerialSlot(topLevelOperator.DetectionWeight),
+                      SerialSlot(topLevelOperator.TransitionWeight),
+                      SerialSlot(topLevelOperator.AppearanceWeight),
+                      SerialSlot(topLevelOperator.DisappearanceWeight),
+                      SerialSlot(topLevelOperator.MaxNumObjOut)
+            ]
+        else:
+            try:
+                import pgmlink
+            except:
+                import pgmlinkNoIlpSolver as pgmlink
+
+            slots = [ SerialDictSlot(topLevelOperator.Parameters, selfdepends=True),
+                        SerialHdf5BlockSlot(topLevelOperator.OutputHdf5,
+                                         topLevelOperator.InputHdf5,
+                                         topLevelOperator.CleanBlocks,
+                                         name="CachedOutput"),
+                        SerialDictSlot(topLevelOperator.EventsVector, transform=str, selfdepends=True),
+                        SerialDictSlot(topLevelOperator.FilteredLabels, transform=str, selfdepends=True),
+                        SerialSlot(topLevelOperator.DivisionWeight),
+                        SerialSlot(topLevelOperator.DetectionWeight),
+                        SerialSlot(topLevelOperator.TransitionWeight),
+                        SerialSlot(topLevelOperator.AppearanceWeight),
+                        SerialSlot(topLevelOperator.DisappearanceWeight),
+                        SerialSlot(topLevelOperator.MaxNumObjOut)
+            ]
+
+            if 'MergerOutput' in topLevelOperator.outputs:
+                slots.append(SerialHdf5BlockSlot(topLevelOperator.MergerOutputHdf5,
+                                         topLevelOperator.MergerInputHdf5,
+                                         topLevelOperator.MergerCleanBlocks,
+                                         name="MergerCachedOutput"),
+                              )
+
+
+            if 'CoordinateMap' in topLevelOperator.outputs:
+                slots.append(SerialPickleableSlot(topLevelOperator.CoordinateMap, 1, pgmlink.TimestepIdCoordinateMap()))
 
         super(StructuredTrackingSerializer, self ).__init__(projectFileGroupName, slots=slots, operator=topLevelOperator)
