627206ccc129098d7f51658923f4af03c2d8d032
Semion Sidorenko <semion.sidorenko@edgecom.ch>
2017-03-14 10:24:26 +0100

    slic gui

--- ilastik/workflows/voxelSegmentation/slicGui.py
+++ ilastik/workflows/voxelSegmentation/slicGui.py
@@ -1,8 +1,11 @@
+import os
+
 from ilastik.applets.layerViewer.layerViewerGui import LayerViewerGui
 
 from volumina.pixelpipeline.datasources import LazyflowSource
-
 from volumina.api import ColortableLayer, generateRandomColors
+
+from slicViewerControls import SlicViewerControls
 
 
 class SlicGui(LayerViewerGui):
@@ -19,3 +22,21 @@ class SlicGui(LayerViewerGui):
             layers.append(layer)
 
         return layers
+
+    def initViewerControlUi(self):
+        """
+        Load the viewer controls GUI, which appears below the applet bar.
+        In our case, the viewer control GUI consists mainly of a layer list.
+        Subclasses should override this if they provide their own viewer control widget.
+        """
+        localDir = os.path.split(__file__)[0]
+        self.__viewerControlWidget = SlicViewerControls()
+
+        # The editor's layerstack is in charge of which layer movement buttons are enabled
+        model = self.editor.layerStack
+
+        if self.__viewerControlWidget is not None:
+            self.__viewerControlWidget.setupConnections(model, self.topLevelOperatorView)
+
+    def viewerControlWidget(self):
+        return self.__viewerControlWidget
