1043dfcf4e0f9bb9d97d89151d6d68bd286c34f8
Stuart Berg <bergs@janelia.hhmi.org>
2017-03-14 22:37:19 -0400

    thresholding: graphcut 'beta' parameter is broadcasted to all lanes

--- ilastik/applets/thresholdTwoLevels/thresholdTwoLevelsApplet.py
+++ ilastik/applets/thresholdTwoLevels/thresholdTwoLevelsApplet.py
@@ -43,7 +43,8 @@ class ThresholdTwoLevelsApplet( StandardApplet ):
                  'LowThreshold',
                  'SmootherSigma',
                  'CurOperator',
-                 'Channel' ]
+                 'Channel',
+                 'Beta' ]
     
     @property
     def singleLaneGuiClass(self):
