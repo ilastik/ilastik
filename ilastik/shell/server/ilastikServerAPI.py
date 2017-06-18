c2d1bb803996e4a32e1140f9d8f8bdc71a56147c
kdominik <k-dominik@users.noreply.github.com>
2017-06-18 18:23:19 +0200

    added convenience method to show all get methods for the api

--- ilastik/shell/server/ilastikServerAPI.py
+++ ilastik/shell/server/ilastikServerAPI.py
@@ -1,7 +1,7 @@
 """Defines the external interface
 """
 from __future__ import division, print_function
-from flask import Blueprint, request
+from flask import Blueprint, request, url_for, jsonify
 from flask import current_app as app
 
 import logging
@@ -16,15 +16,37 @@ ilastikServerAPI = Blueprint('ilastikServerAPI', __name__)
 @ilastikServerAPI.route('/')
 def print_hello():
     logger.debug('Reached ilastikServerAPI')
-    app._ilastikServer.a
-    return app._ilastikServer.a
+    r = ['<a href=%s>api</a>' % url_for('.get_GET_map')]
+    r.append(str(app._ilastikServer))
+    return "\n".join(r)
 
 
 @ilastikServerAPI.route('/api/workflow/current_workflow_name')
 def get_current_workflow_name():
     logger.debug('getting current workflow name')
     workflow_name = app._ilastikServer.get_current_workflow_name()
-    return workflow_name
+
+
+def has_no_empty_params(rule):
+    """http://stackoverflow.com/a/13318415
+    """
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+
+@ilastikServerAPI.route('/api/map')
+def get_GET_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+
+    return "<br>".join("<a href={url:s}>{endpoint:s}</a>".format(url=url, endpoint=endpoint)
+                       for url, endpoint in links)
+
+
 
 
 def shutdown_server():
