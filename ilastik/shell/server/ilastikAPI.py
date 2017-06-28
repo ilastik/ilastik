e1b27cfdf0b336ffa932a1f9b266550c2623fac4
kdominik <k-dominik@users.noreply.github.com>
2017-06-28 17:15:44 +0200

    PythonAPI: process arrays with loaded projects

--- ilastik/shell/server/ilastikAPI.py
+++ ilastik/shell/server/ilastikAPI.py
@@ -3,7 +3,11 @@ from __future__ import print_function, division
 import logging
 import os
 
+from ilastik.applets.dataSelection.opDataSelection import DatasetInfo
 from ilastik.shell.server.ilastikServerShell import ServerShell
+from ilastik.shell.projectManager import ProjectManager
+from ilastik.applets.batchProcessing.batchProcessingApplet import BatchProcessingApplet
+import collections
 
 logger = logging.getLogger(__name__)
 
@@ -24,6 +28,14 @@ class IlastikAPI(object):
         else:
             return None
 
+    def load_project_file(self, project_file_path):
+        """Load project file from disk (local)
+
+        Args:
+          project_file_path (str): path of `.ilp` file
+        """
+        self._server_shell.openProjectFile(project_file_path)
+
     def get_workflow_status(self):
         """Collects information about all applets into a dictionary
         """
@@ -35,20 +47,87 @@ class IlastikAPI(object):
         applet_names = [applet.name for applet in workflow.applets]
         return applet_names
 
+    def get_applets(self):
+        """Get info on all applets in the current workflow"""
+        workflow = self._server_shell.workflow
+        applets = workflow.applets
+        return applets
+
     def get_applet_information(self, applet_index):
         """Generates a dict with applet-information
         """
         workflow = self._server_shell.workflow
         applet = workflow.applets[applet_index]
 
+    def process_image(self, input_dict):
+        """Process one or more image physical image files
 
-    def create_project(self, project_name, project_type='pixel_classification'):
+        Args:
+          input_dict (dict, or list of dicts): dictionary in the form
+            {
+               "role_name_a": "image_path",
+               "role_name_b": "image_path",
+               # optional:21
+            }
+
+        TODO: find out which inputs are optional; from dataSelectionApplet:
+        # As far as this parser is concerned, all roles except the first are optional.
+        # (Workflows that require the other roles are responsible for raising an error themselves.)
+        """
+        if isinstance(input_dict, collections.Sequence):
+            # do the processing of multiple images
+            pass
+        else:
+            # process a single image
+            pass
+
+    def get_batch_info(self):
+        """Information about what info needs to be supplied for batch processing
+
+        Returns an ordered dict role - dataset info pairs
+        """
+        workflow = self._server_shell.workflow
+        role_names = workflow.ROLE_NAMES
+        batch_processing_applet = self._get_batch_applet()
+        batch_data_info = batch_processing_applet._get_template_dataset_infos()
+        return collections.OrderedDict(
+            (role_names[k], v) for k, v in batch_data_info.iteritems())
+
+    def _get_batch_applet(self):
+        applets = self.get_applets()
+        batch_processing_applets = [applet for applet in applets
+                                    if isinstance(applet, BatchProcessingApplet)]
+        assert len(batch_processing_applets) == 1, (
+            "Expected only a single batch processing applet per workflow.")
+        batch_processing_applet = batch_processing_applets[0]
+        return batch_processing_applet
+
+    def process_data(self, data):
+        inputs = self.get_batch_info()
+        raw_info = inputs['Raw Data']
+        data_info = DatasetInfo(preloaded_array=data)
+        data_info.axistags = raw_info.axistags
+        batch_processing_applet = self._get_batch_applet()
+
+        role_data_dict = collections.OrderedDict({'Raw Input': [data_info]})
+        ret_data = batch_processing_applet.run_export(role_data_dict, export_to_array=True)
+
+        return ret_data
+
+    def create_project(self, project_path, project_type='pixel_classification'):
+        """Create a project at
+
+        Args:
+            project_path (TYPE): Description
+            project_type (str, optional): Description
+
+        Raises:
+            ValueError: Description
+        """
         from ilastik.workflows.pixelClassification import PixelClassificationWorkflow
         if project_type == 'pixel_classification':
             self._server_shell.createAndLoadNewProject(
-                os.path.join(
-                    CONFIG['projects_path'],
-                    "{project_name:s}.h5".format(project_name=project_name)),
+                project_path,
                 PixelClassificationWorkflow
             )
         else:
--- ilastik/shell/server/ilastikServerShell.py
+++ ilastik/shell/server/ilastikServerShell.py
@@ -41,10 +41,65 @@ class ServerShell(object):
         self.projectManager._loadProject(hdf5File, newProjectFilePath, readOnly)
         self.projectManager.saveProject()
 
-    def openProjectFile(self, projectFilePath):
+    def openProjectFile(self, projectFilePath, force_readonly=False):
         """
+        # TODO give some feedback!
         """
-        raise NotImplementedError
+        # Make sure all workflow sub-classes have been loaded,
+        #  so we can detect the workflow type in the project.
+        import ilastik.workflows
+        try:
+            # Open the project file
+            hdf5File, workflow_class, readOnly = ProjectManager.openProjectFile(projectFilePath, force_readonly)
+
+            # If there are any "creation-time" command-line args saved to the project file,
+            #  load them so that the workflow can be instantiated with the same settings
+            #  that were used when the project was first created.
+            project_creation_args = []
+            if "workflow_cmdline_args" in hdf5File.keys():
+                if len(hdf5File["workflow_cmdline_args"]) > 0:
+                    project_creation_args = map(str, hdf5File["workflow_cmdline_args"][...])
+
+            if workflow_class is None:
+                workflow_class = ilastik.workflows.pixelClassification.PixelClassificationWorkflow
+                import warnings
+                warnings.warn("Your project file ({}) does not specify a workflow type. "
+                              "Assuming Pixel Classification".format(projectFilePath))
+
+            # Create our project manager
+            # This instantiates the workflow and applies all settings from the project.
+            self.projectManager = ProjectManager(self,
+                                                 workflow_class,
+                                                 headless=True,
+                                                 workflow_cmdline_args=self.workflow_options,
+                                                 project_creation_args=project_creation_args)
+            self.projectManager._loadProject(hdf5File, projectFilePath, readOnly)
+
+        except ProjectManager.FileMissingError:
+            logger.error("Couldn't find project file: {}".format(projectFilePath))
+            raise
+        except ProjectManager.ProjectVersionError:
+            # Couldn't open project.  Try importing it.
+            oldProjectFilePath = projectFilePath
+            name, ext = os.path.splitext(oldProjectFilePath)
+
+            # Create a brand new project file.
+            projectFilePath = name + "_imported" + ext
+            logger.info("Importing project as '" + projectFilePath + "'")
+            hdf5File = ProjectManager.createBlankProjectFile(projectFilePath)
+
+            # For now, we assume that any imported projects are pixel classification workflow projects.
+            import ilastik.workflows
+            default_workflow = ilastik.workflows.pixelClassification.PixelClassificationWorkflow
+
+            # Create the project manager.
+            self.projectManager = ProjectManager(self,
+                                                 default_workflow,
+                                                 headless=True,
+                                                 workflow_cmdline_args=self.workflow_options,
+                                                 project_creation_args=self.workflow_options)
+
+            self.projectManager._importProject(oldProjectFilePath, hdf5File, projectFilePath)
 
     def setAppletEnabled(self, applet, enabled):
         pass
@@ -54,7 +109,6 @@ class ServerShell(object):
 
     def enableProjectChanges(self, enabled):
         pass
-
 
 
 assert issubclass(ServerShell, ShellABC), (
