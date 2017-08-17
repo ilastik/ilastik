80c01a0ea9549851dd0850ac436f9e6ef8bd7672
Carsten Haubold <carstenhaubold@googlemail.com>
2017-08-17 09:44:00 -0400

    Only run graphcut test if graphcut was found

--- tests/test_applets/thresholdTwoLevels/testOpLabeledThreshold.py
+++ tests/test_applets/thresholdTwoLevels/testOpLabeledThreshold.py
@@ -2,7 +2,7 @@ import numpy as np
 import vigra
 
 from lazyflow.graph import Graph
-from ilastik.applets.thresholdTwoLevels.opThresholdTwoLevels import OpLabeledThreshold, ThresholdMethod
+from ilastik.applets.thresholdTwoLevels.opThresholdTwoLevels import OpLabeledThreshold, ThresholdMethod, _has_graphcut
 
 class TestOpLabeledThreshold(object):
 
@@ -84,28 +84,29 @@ class TestOpLabeledThreshold(object):
         assert len(np.unique(result_yx[7:12, 1:5])) == 1
 
     def test_graphcut(self):
-        data = self.data
-        
-        # Core labels don't matter in this graphcut test
-        #core_binary = (data == 5)
-        core_labels = np.empty_like(data, dtype=np.uint32)
-        #core_labels[0,...,0] = vigra.analysis.labelMultiArrayWithBackground(core_binary[0,...,0].astype(np.uint8))
+        if _has_graphcut:
+            data = self.data
+            
+            # Core labels don't matter in this graphcut test
+            #core_binary = (data == 5)
+            core_labels = np.empty_like(data, dtype=np.uint32)
+            #core_labels[0,...,0] = vigra.analysis.labelMultiArrayWithBackground(core_binary[0,...,0].astype(np.uint8))
 
-        op = OpLabeledThreshold(graph=Graph())
-        op.Method.setValue(ThresholdMethod.GRAPHCUT)
-        op.FinalThreshold.setValue(0.5)
-        op.Input.setValue(data.copy())
-        op.CoreLabels.setValue(core_labels)
-        op.GraphcutBeta.setValue(0.5) # High enough to fill the hole in the third segment
-        
-        result = op.Output[:].wait()
-        result_yx = result[0,:,:,0,0]
-        #print result_yx
-        
-        label_values = np.unique(result)[1:]
-        assert len(label_values) == 3        
-        assert len(np.unique(result_yx[1:12, 1:5])) == 1 # Same as 'simple'
-        assert len(np.unique(result_yx[7:10, 6:9])) == 1 # Almost same as simple, but with the hole filled in.
+            op = OpLabeledThreshold(graph=Graph())
+            op.Method.setValue(ThresholdMethod.GRAPHCUT)
+            op.FinalThreshold.setValue(0.5)
+            op.Input.setValue(data.copy())
+            op.CoreLabels.setValue(core_labels)
+            op.GraphcutBeta.setValue(0.5) # High enough to fill the hole in the third segment
+            
+            result = op.Output[:].wait()
+            result_yx = result[0,:,:,0,0]
+            #print result_yx
+            
+            label_values = np.unique(result)[1:]
+            assert len(label_values) == 3        
+            assert len(np.unique(result_yx[1:12, 1:5])) == 1 # Same as 'simple'
+            assert len(np.unique(result_yx[7:10, 6:9])) == 1 # Almost same as simple, but with the hole filled in.
 
 if __name__ == "__main__":
     #make the program quit on Ctrl+C
