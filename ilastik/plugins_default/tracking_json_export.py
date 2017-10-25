d10531bd86a7e1a7003e04dfbd41c723f9dc50a3
Carsten Haubold <carstenhaubold@googlemail.com>
2017-10-25 12:03:17 +0200

    TrackingExport: fix JSON export for models with mergers

--- ilastik/plugins_default/tracking_json_export.py
+++ ilastik/plugins_default/tracking_json_export.py
@@ -31,6 +31,28 @@ else:
 
             :returns: True on success, False otherwise
             """
+
+            # The nodes inserted into the graph after merger resolving
+            # do not have detectionProbabilities set. We first find one node that was no merger,
+            # check how many entries are needed, and then insert 0.0 as detection probabilities there everywhere.
+
+            numStates = -1
+            for n in hypothesesGraph.nodeIterator():
+                t = hypothesesGraph._graph.node[n]['traxel']
+                if 'detProb' in t.Features:
+                    numStates = len(t.Features['detProb'])
+                    break
+
+            assert numStates > 0, "Cannot export hypotheses graph without features (e.g. only resolved mergers) to JSON"
+
+            dummyVector = np.zeros(numStates)
+            for n in hypothesesGraph.nodeIterator():
+                t = hypothesesGraph._graph.node[n]['traxel']
+                if 'detProb' not in t.Features:
+                    logger.debug(f"replacing detProb of node with ID={hypothesesGraph._graph.node[n]['id']}")
+                    t.Features['detProb'] = dummyVector
+
+            # now we can insert the energies into the graph
             hypothesesGraph.insertEnergies()
             trackingGraph = hypothesesGraph.toTrackingGraph()
 
