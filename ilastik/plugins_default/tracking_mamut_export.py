a3a28df6b11b0c55fc166120f5882a30b00b6fab
Carsten Haubold <carstenhaubold@googlemail.com>
2017-11-07 08:53:57 +0100

    TrackingExport: code cleanup and update export-docstring

--- ilastik/plugins_default/tracking_mamut_export.py
+++ ilastik/plugins_default/tracking_mamut_export.py
@@ -1,11 +1,9 @@
 from builtins import range
 import os.path
-import random
 import numpy as np
 from ilastik.plugins import TrackingExportFormatPlugin
 from mamutexport.mamutxmlbuilder import MamutXmlBuilder
 from mamutexport.bigdataviewervolumeexporter import BigDataViewerVolumeExporter
-import vigra
 import h5py
 
 def convertKeyName(key):
@@ -42,10 +40,12 @@ class TrackingMamutExportFormatPlugin(TrackingExportFormatPlugin):
         return os.path.exists(filename + '_mamut.xml') or os.path.exists(filename + '_bdv.xml') or os.path.exists(filename + '_raw.h5')
 
     def export(self, filename, hypothesesGraph, objectFeaturesSlot, labelImageSlot, rawImageSlot):
-        """Export the tracking solution stored in the hypotheses graph to two XML files so that 
-        a) the raw data can be displayed in Fiji's BigDataViewer, and b) that the tracks can be visualized in MaMuT. 
+        """Export the tracking solution stored in the hypotheses graph to three files: 
+        Firstly, a _raw.h5 file containing the data, and a _bdv.xml file
+        that allows the raw data to be be displayed in Fiji's BigDataViewer.
+        Secondly, it exports an _mamut.xml file that contains the tracks for visualization and proof-reading in MaMuT. 
 
-        :param filename: string of the FILE where to save the result (different .xml files were)
+        :param filename: string of the FILE where to save the result (different .xml files and a .h5 file with suffixes)
         :param hypothesesGraph: hytra.core.hypothesesgraph.HypothesesGraph filled with a solution
         :param objectFeaturesSlot: lazyflow.graph.InputSlot, connected to the RegionFeaturesAll output 
                of ilastik.applets.trackingFeatureExtraction.opTrackingFeatureExtraction.OpTrackingFeatureExtraction
@@ -77,13 +77,9 @@ class TrackingMamutExportFormatPlugin(TrackingExportFormatPlugin):
             frame, label = node
             # print(f"Node {node} has value={graph.node[node]['value']} and trackId={graph.node[node]['trackId']}")
             if graph.node[node]['value'] > 0:
-                t = graph.node[node]['traxel']
-
-                radius = 2*features[frame]['Standard Object Features']['RegionRadii'][label, 0]
                 for category in list(features[frame].keys()):
                     for key in list(features[frame][category].keys()):
-                        feature_string = key
-                        feature_string = convertKeyName(feature_string)
+                        feature_string = convertKeyName(key)
                         if len(feature_string) > 15:
                             shortname = getShortname(feature_string).replace('_', '')
                         else:
@@ -93,11 +89,9 @@ class TrackingMamutExportFormatPlugin(TrackingExportFormatPlugin):
 
                         if (np.asarray(features[frame][category][key])).ndim == 2:
                             if key != 'Histogram':
-                                #print key, np.asarray(features[key]).shape
                                 for column in range((np.asarray(features[frame][category][key])).shape[1]):
                                     builder.addFeatureName(feature_string + '_' + str(column), feature_string, shortname + '_' + str(column), isInt)
                         else:
-                            #print "ELSE", key, np.asarray(features[key]).shape
                             builder.addFeatureName(feature_string, feature_string, shortname, isInt)
 
                 builder.addFeatureName("LabelimageId", "LabelimageId", "labelid", False)
@@ -131,21 +125,22 @@ class TrackingMamutExportFormatPlugin(TrackingExportFormatPlugin):
                     for key in list(features[frame][category].keys()):
                         if key != 'Histogram':
                             if label != 0: # ignoring background
+                                feature_string = convertKeyName(key)
                                 if (np.asarray(features[frame][category][key])).ndim == 0:
-                                    featureDict[convertKeyName(key)] = features[frame][category][key]
+                                    featureDict[feature_string] = features[frame][category][key]
                                 if (np.asarray(features[frame][category][key])).ndim == 1:
-                                    featureDict[convertKeyName(key)] = features[frame][category][key][label]
+                                    featureDict[feature_string] = features[frame][category][key][label]
                                 if (np.asarray(features[frame][category][key])).ndim == 2:
                                     for j in range((np.asarray(features[frame][category][key])).shape[1]):
                                         try:
                                             _ = features[frame][category][key][label, 0]
                                         except IndexError:
                                             if 'SquaredDistances' in key:
-                                                featureDict[convertKeyName(key) + '_{}'.format(str(j))] = 9999.
+                                                featureDict[feature_string + '_{}'.format(str(j))] = 9999.
                                             else:
-                                                featureDict[convertKeyName(key) + '_{}'.format(str(j))] = 0.
+                                                featureDict[feature_string + '_{}'.format(str(j))] = 0.
                                             continue
-                                        featureDict[convertKeyName(key) + '_{}'.format(str(j))] = features[frame][category][key][label, j]
+                                        featureDict[feature_string + '_{}'.format(str(j))] = features[frame][category][key][label, j]
 
                 xpos = features[frame]['Standard Object Features']['RegionCenter'][label, 0]
                 ypos = features[frame]['Standard Object Features']['RegionCenter'][label, 1]
