8203a8c4f7e166a2c3b8b7f9286fe28f5a40d423
kdominik <k-dominik@users.noreply.github.com>
2017-11-30 15:49:54 +0100

    added ilastik_server_config

--- ilastik/shell/server/app.py
+++ ilastik/shell/server/app.py
@@ -1,15 +1,24 @@
 from apistar.frameworks.wsgi import WSGIApp as App
 from apistar import Route, Include
 from apistar.handlers import docs_urls, static_urls
+import os
 
-from .routes import basic
+from .routes import basic, project
 from .ilastikAPI import IlastikAPI
 from .renderer import IlastikJSONRenderer
 
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+
 routes = [
     Include('/docs', docs_urls),
-    Include('/static', static_urls)
+    Include('/static', static_urls),
+    Include('/project', project.routes)
 ]
 
 # extend here in order to add them to site root
@@ -26,8 +35,50 @@ settings = {
 
 app = App(routes=routes, settings=settings)
 
+
+class IlastikServerConfig(object):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        default_config = {
+            "projects_path": os.path.expanduser('~/ilastik_server/projects'),
+            "data_path": os.path.expanduser('~/ilastik_server/data')
+        }
+        default_config.update(kwargs)
+
+        self._config = default_config
+        self.setup_paths()
+
+    @property
+    def config(self):
+        return self._config
+
+    @property
+    def projects_path(self):
+        return self._config['projects_path']
+
+    @property
+    def data_path(self):
+        return self._config['data_path']
+
+    def setup_paths(self):
+        projects_path = self.projects_path
+        if not os.path.exists(projects_path):
+            os.makedirs(projects_path)
+            logger.info(f'Created project path {projects_path}')
+        logger.info(f'Using project path: {projects_path}')
+
+        data_path = self.data_path
+        if not os.path.exists(data_path):
+            os.makedirs(data_path)
+            logger.info(f'Created data path {data_path}.')
+        logger.info(f'Using data path: {data_path}')
+
+
+ilastik_server_config = IlastikServerConfig()
 ilastik_api = IlastikAPI()
+app._ilastik_config = ilastik_server_config
 app._ilastik_api = ilastik_api
+
 
 if __name__ == '__main__':
     app.main()
