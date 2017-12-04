5038d52b7b0f7ef642a590b1c8c9f47456c0917c
kdominik <k-dominik@users.noreply.github.com>
2017-12-04 07:26:05 +0100

    renderer: proper serialization of numpy types

--- ilastik/shell/server/renderer.py
+++ ilastik/shell/server/renderer.py
@@ -2,11 +2,23 @@ import json
 from apistar.renderers import Renderer
 from apistar import http
 
+import numpy
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 class CustomEncoder(json.JSONEncoder):
     def default(self, obj):
         # check if object has to_json() method
-        if hasattr(obj, 'to_json'):
+        logger.debug(f'Serializing object {obj}')
+        if isinstance(obj, numpy.integer):
+            return int(obj)
+        elif isinstance(obj, numpy.floating):
+            return float(obj)
+        elif isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        elif hasattr(obj, 'to_json'):
             return obj.to_json()
 
         return json.JSONEncoder.default(self, obj)
