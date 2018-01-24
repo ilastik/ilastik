646c700a43ca575de09be6ce9e8dcaa1c29b97db
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2018-01-24 11:38:08 +0100

    blockwise watersheds is working...(we just ignore the fact that there will be block borders visible in the supervoxels

--- ilastik/workflows/carving/carvingTools.py
+++ ilastik/workflows/carving/carvingTools.py
@@ -6,6 +6,9 @@ import threading
 from concurrent.futures import ThreadPoolExecutor
 
 
+
+
+
 # parallel watershed with hard block boarders
 def simple_parallel_ws(data, block_shape=None, max_workers=None):
         
@@ -18,7 +21,7 @@ def simple_parallel_ws(data, block_shape=None, max_workers=None):
     shape = data.shape
     ndim = len(shape)
     if block_shape is None:
-        block_shape  = tuple([100]*ndim)
+        block_shape  = tuple([64]*ndim)
     roi_begin = tuple([0]*ndim)
     halo = [20]*ndim
     blocking = nifty.tools.blocking(roiBegin=roi_begin, roiEnd=shape, blockShape=block_shape)
@@ -31,16 +34,24 @@ def simple_parallel_ws(data, block_shape=None, max_workers=None):
 
 
     labels = numpy.zeros(shape, dtype='uint64')
-    global_max_label = 0
-    with ThreadPoolExecutor(max_workers=8) as executor:
+
+    global_max_label = [0]
+    global_min_label = [None]
+
+
+
+
+    done_blocks = [0]
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
 
         for i in range(n_blocks):
 
 
             def per_block(block_index):
+                
                 # get block with halo
                 block_with_halo = blocking.getBlockWithHalo(blockIndex=block_index, halo=halo)
-                print(block_with_halo)
+                #print(block_with_halo)
 
                 outer_block = block_with_halo.outerBlock
                 inner_block = block_with_halo.innerBlock
@@ -65,18 +76,47 @@ def simple_parallel_ws(data, block_shape=None, max_workers=None):
 
                 # get the max
                 inner_block_max_label = inner_block_labels.max()
+                inner_block_min_label = inner_block_labels.min()
 
+
+                
 
                 with labels_lock:
-                    new_global_max_label = global_max_label + inner_block_max_label
-                    inner_block_labels += global_max_label
+
+                    gmax = global_max_label[0]
+                    gmin = global_min_label[0]
+
+                    new_global_max_label = gmax + inner_block_max_label
+                    inner_block_labels += gmax
+
+                    done_blocks[0] = done_blocks[0] +1
+                    print(done_blocks[0],n_blocks)
+
+                    min_here = inner_block_min_label + gmax
+
+                    if gmin is None:
+                        global_min_label[0] = min_here
+
+                    else:
+                        global_min_label[0] = min(gmin, min_here)
+
+
+                    global_max_label[0] = new_global_max_label + 1
+
+
+
 
                 labels[inner_slicing] = inner_block_labels
                         
 
+        
 
-            # per_block(i)
+            #per_block(i)
             future = executor.submit(per_block, block_index=i)
+
+    labels -= int(global_min_label[0]-1)
+
+    print("labels ",labels.min())
 
     return labels
         
