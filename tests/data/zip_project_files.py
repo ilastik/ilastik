9328d2c6171265e27df59d987cfb48d5604c79ec
Fynn Beuttenmueller <fynn.beuttenmueller@iwr.uni-heidelberg.de>
2018-03-10 22:39:36 +0100

    fix the absolute number of test projects, instad of the minimum number

--- tests/data/zip_project_files.py
+++ tests/data/zip_project_files.py
@@ -1,7 +1,7 @@
 import os
 import zipfile
 
-assert len([proj for proj in os.listdir('.') if proj.endswith('.ilp')]) >= \
+assert len([proj for proj in os.listdir('.') if proj.endswith('.ilp')]) == \
     25, 'unpack existing projects first in order not to overwrite them'
 
 projects = zipfile.ZipFile('test_projects.zip',
