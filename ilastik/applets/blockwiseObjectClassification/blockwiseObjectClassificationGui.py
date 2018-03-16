1283a01bab419ec099ab1c64d1dd3fc94389a29c
akreshuk <anna.kreshuk@iwr.uni-heidelberg.de>
2018-03-16 12:22:39 +0100

    make all gui classes use the colortable16 from volumina.colortables

--- ilastik/applets/blockwiseObjectClassification/blockwiseObjectClassificationGui.py
+++ ilastik/applets/blockwiseObjectClassification/blockwiseObjectClassificationGui.py
@@ -30,6 +30,7 @@ from PyQt5.QtGui import QColor
 
 from ilastik.utility.gui import threadRouted
 from volumina.api import LazyflowSource, ColortableLayer
+from volumina import colortables
 from ilastik.applets.layerViewer.layerViewerGui import LayerViewerGui
 
 logger = logging.getLogger(__name__)
@@ -38,7 +39,7 @@ class BlockwiseObjectClassificationGui( LayerViewerGui ):
     
     def __init__(self, *args, **kwargs):
         super( self.__class__, self ).__init__(*args, **kwargs)
-        self._colorTable16 = self._createDefault16ColorColorTable()
+        self._colorTable16 = colortables.default16_new
     
         # Subscribe to future changes (from serializer or whatever)
         self.topLevelOperatorView.BlockShape3dDict.notifyDirty( self._updateGuiFromOperator )
@@ -153,36 +154,3 @@ class BlockwiseObjectClassificationGui( LayerViewerGui ):
         haloPaddingDict = self.topLevelOperatorView.HaloPadding3dDict.value
         for axiskey, spinBox in list(self._haloSpinBoxes.items()):
             spinBox.setValue( haloPaddingDict[axiskey] )
-
-    def _createDefault16ColorColorTable(self):
-        colors = []
-
-        # Transparent for the zero label
-        colors.append(QColor(0,0,0,0))
-
-        # ilastik v0.5 colors
-        colors.append( QColor( Qt.red ) )
-        colors.append( QColor( Qt.green ) )
-        colors.append( QColor( Qt.yellow ) )
-        colors.append( QColor( Qt.blue ) )
-        colors.append( QColor( Qt.magenta ) )
-        colors.append( QColor( Qt.darkYellow ) )
-        colors.append( QColor( Qt.lightGray ) )
-
-        # Additional colors
-        colors.append( QColor(255, 105, 180) ) #hot pink
-        colors.append( QColor(102, 205, 170) ) #dark aquamarine
-        colors.append( QColor(165,  42,  42) ) #brown
-        colors.append( QColor(0, 0, 128) )     #navy
-        colors.append( QColor(255, 165, 0) )   #orange
-        colors.append( QColor(173, 255,  47) ) #green-yellow
-        colors.append( QColor(128,0, 128) )    #purple
-        colors.append( QColor(240, 230, 140) ) #khaki
-
-#        colors.append( QColor(192, 192, 192) ) #silver
-#        colors.append( QColor(69, 69, 69) )    # dark grey
-#        colors.append( QColor( Qt.cyan ) )
-
-        assert len(colors) == 16
-
-        return [c.rgba() for c in colors]
