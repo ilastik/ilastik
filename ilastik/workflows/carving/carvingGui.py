606cb0c66e3d3af9fdad873d40258366b8d4abf2
kdominik <k-dominik@users.noreply.github.com>
2018-03-20 10:35:24 +0100

    Superpixel layer with opacity 0.5

--- ilastik/workflows/carving/carvingGui.py
+++ ilastik/workflows/carving/carvingGui.py
@@ -729,7 +729,7 @@ class CarvingGui(LabelingGui):
                              "(undersegmentation). In this case, it will be impossible to achieve the desired " \
                              "segmentation. This layer helps you to understand these cases.</html>")
             layer.visible = False
-            layer.opacity = 1.0
+            layer.opacity = 0.5
             layers.append(layer)
 
         # Visual overlay (just for easier labeling)
