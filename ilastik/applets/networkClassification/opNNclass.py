23d9196146bc2fda2099f565bdddf3e91ed94326
Beinabih <jonas.massa@iwr.uni-heidelberg.de>
2018-07-11 10:41:45 +0200

    added broadcastings slots for batch processing

--- ilastik/applets/networkClassification/opNNclass.py
+++ ilastik/applets/networkClassification/opNNclass.py
@@ -23,7 +23,7 @@ from __future__ import print_function
 from lazyflow.graph import Operator, InputSlot, OutputSlot
 from lazyflow.operators.opBlockedArrayCache import OpBlockedArrayCache
 from lazyflow.operators.classifierOperators import OpPixelwiseClassifierPredict
-from lazyflow.operators import OpMultiArraySlicer2
+from lazyflow.operators import OpMultiArraySlicer2, OpValueCache
 from ilastik.utility.operatorSubView import OperatorSubView
 
 
