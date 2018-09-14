e52d7914d0bd4068d7fb32c25198716d647860eb
kdominik <k-dominik@users.noreply.github.com>
2018-09-14 15:38:56 +0200

    disabled printing of stack trace upon import of PyQt5 in headless mode
    vigra tries to import pyplab, which in turn tries to import PyQt5.
    This is all nice, as the exception is caught and handled - however,
    the stack trace is always printed which is too noisy.

--- ilastik/headless_dummy_modules/PyQt5/__init__.py
+++ ilastik/headless_dummy_modules/PyQt5/__init__.py
@@ -6,6 +6,9 @@ Applet GUI classes should never be imported at module scope: they should be impo
 To help us developers keep good hygiene when it comes to GUI imports, this module is used to override PyQt5 in headless mode.
 (See ilastik_main.py)
 """
-from traceback import print_stack
-print_stack()
+# you can add this for debugging purposes. Since vigra tries to import pylab,
+# which in turn tries to import PyQt5 this will be hit in every headless run and
+# therefore produce noise that is unwanted
+# from traceback import print_stack
+# print_stack()
 raise Exception("Developer error: When ilastik is running in headless mode, you aren't allowed to import PyQt5.")
