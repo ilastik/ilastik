b6a9c7e1cbf1cb59858d2e9d5edeba54cda06275
Maxim Novikov <mnovikov.work@gmail.com>
2018-11-19 15:35:53 +0100

    Migrate testObjectClassificationGui to pytest

--- tests/test_workflows/testObjectClassificationGui.py
+++ tests/test_workflows/testObjectClassificationGui.py
@@ -682,5 +682,5 @@ def try_convert_to_numeric(val):
 
 
 if __name__ == "__main__":
-    from tests.helpers.shellGuiTestCaseBase import run_shell_nosetest
-    run_shell_nosetest(__file__)
+    from tests.helpers.shellGuiTestCaseBase import run_shell_test
+    run_shell_test(__file__)
