60306a4869fd44c9a0647ce4e6274de36465fe24
fynnbe <thefynnbe@gmail.com>
2019-01-17 18:19:58 +0100

    init tiktorchLazyflowClassifier with config dict and binary data

--- lazyflow/classifiers/tiktorchLazyflowClassifier.py
+++ lazyflow/classifiers/tiktorchLazyflowClassifier.py
@@ -48,8 +48,7 @@ class TikTorchLazyflowClassifierFactory(LazyflowPixelwiseClassifierFactoryABC):
     VERSION = 1
     tikTorchServer_process = None
 
-    def __init__(self, tiktorch_config_path: str, server_config: dict, start_server=True):
-        self._filename = tiktorch_config_path  # DELETE this attribute
+    def __init__(self, *args, server_config: dict, start_server=True):
         # Privates
         self._tikTorchClient = None
         self._tikTorchClassifier = None
@@ -58,13 +57,7 @@ class TikTorchLazyflowClassifierFactory(LazyflowPixelwiseClassifierFactoryABC):
         self._opReorderAxes.AxisOrder.setValue('zcyx')
 
         # Publics
-        self.tikTorchClient = TikTorchClient(local_build_dir=tiktorch_config_path,
-                                             address=server_config['address'],
-                                             port=server_config['port'],
-                                             meta_port=server_config['meta_port'],
-                                             username=server_config['username'],
-                                             password=server_config['password'],
-                                             start_server=start_server)
+        self.tikTorchClient = TikTorchClient(*args, **server_config, start_server=start_server)
 
     @property
     def tikTorchClient(self):
