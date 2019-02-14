d35c12c4d244efed742fffa1fd80657e1ceb1a88
k-dominik <k-dominik@users.noreply.github.com>
2019-02-14 11:29:38 +0100

    fix: testObjectClassificationGui cleanup

--- tests/test_workflows/testObjectClassificationGui.py
+++ tests/test_workflows/testObjectClassificationGui.py
@@ -119,7 +119,7 @@ class TestObjectClassificationGui(ShellGuiTestCaseBase):
         super().teardown_class()
 
         # Clean up: Delete any test files we generated
-        # shutil.rmtree(cls.temp_dir)  # TODO: cleanup when dev is done
+        shutil.rmtree(cls.temp_dir)
 
     def test_00_check_preconditions(self):
         """Make sure the needed files exist"""
