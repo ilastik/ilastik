ec4adaaf300a7dc0b1b79073f62afb328715c256
Emil Melnikov <emilmelnikov@gmail.com>
2019-03-05 14:51:58 +0100

    Extract Pipeline into a public class

--- tests/testOpExportMultipageTiff.py
+++ tests/testOpExportMultipageTiff.py
@@ -20,48 +20,18 @@
 # 		   http://ilastik.org/license/
 ###############################################################################
 
-import contextlib
 import pathlib
 import tempfile
-from typing import Any, Callable
 
 import numpy
 import vigra
 
 from lazyflow.graph import Graph
-from lazyflow.operator import Operator
 from lazyflow.operators.ioOperators import OpExportMultipageTiff
 from lazyflow.operators.ioOperators.opTiffReader import OpTiffReader
 from lazyflow.operators.opArrayPiper import OpArrayPiper
 from lazyflow.operators.opReorderAxes import OpReorderAxes
-
-
-class _Pipeline:
-    def __init__(self, **op_init_kwargs):
-        self._op_init_kwargs = op_init_kwargs
-        self._ops = []
-
-    def append(self, op_callable: Callable[..., Operator], **value_slots: Any) -> None:
-        op = op_callable(**self._op_init_kwargs)
-        for name, value in value_slots.items():
-            if self and name == "Input":
-                raise ValueError(
-                    'Setting value of the input slot "Input" is allowed only for the first operator in the pipeline'
-                )
-            op.inputs[name].setValue(value)
-        if self:
-            op.Input.connect(self[-1].Output)
-        self._ops.append(op)
-
-    def __len__(self) -> int:
-        return self._ops.__len__()
-
-    def __getitem__(self, item: int) -> Operator:
-        return self._ops.__getitem__(item)
-
-    def close(self) -> None:
-        for op in reversed(self._ops):
-            op.cleanUp()
+from lazyflow.utility import Pipeline
 
 
 def test_OpExportMultipageTiff():
@@ -75,16 +45,14 @@ def test_OpExportMultipageTiff():
         filepath = str(pathlib.Path(tempdir, "multipage.tiff"))
         graph = Graph()
 
-        write_tiff = _Pipeline(graph=graph)
-        write_tiff.append(OpArrayPiper, Input=expected)
-        write_tiff.append(OpExportMultipageTiff, Filepath=filepath)
-        with contextlib.closing(write_tiff):
+        with Pipeline(graph=graph) as write_tiff:
+            write_tiff.add(OpArrayPiper, Input=expected)
+            write_tiff.add(OpExportMultipageTiff, Filepath=filepath)
             write_tiff[-1].run_export()
 
-        read_tiff = _Pipeline(graph=graph)
-        read_tiff.append(OpTiffReader, Filepath=filepath)
-        read_tiff.append(OpReorderAxes, AxisOrder=axes)
-        with contextlib.closing(read_tiff):
+        with Pipeline(graph=graph) as read_tiff:
+            read_tiff.add(OpTiffReader, Filepath=filepath)
+            read_tiff.add(OpReorderAxes, AxisOrder=axes)
             actual = read_tiff[-1].Output[:].wait().astype(dtype)
 
     numpy.testing.assert_array_equal(expected, actual)
