916a35b8a7725231c1d77f8ea4c8fd4836d814ad
fynnbe <thefynnbe@gmail.com>
2019-03-07 11:47:31 +0100

    improved lazyflowOnlineClassifier

--- lazyflow/classifiers/lazyflowClassifier.py
+++ lazyflow/classifiers/lazyflowClassifier.py
@@ -270,44 +270,30 @@ class LazyflowPixelwiseClassifierABC(with_metaclass(abc.ABCMeta, object)):
 
 class LazyflowOnlineClassifier:
     """
-    Defines an interface for online classifier objects
+    Defines an interface for online classifier objects that can be used by the lazyflow classifier operators.
+    In contrast to a "pixel-wise" classifier, this classifier gets updated and not recreated.
     """
 
-    def __new__(cls, *args, **kwargs):
-        # Force the VERSION class member to be copied to an instance member.
-        obj = object.__new__(cls)
-        obj.VERSION = cls.VERSION
-        return obj
+    def create_and_train_pixelwise(self, *args, **kwargs):
+        raise NotImplementedError("This is a 'LazyflowOnlineClassifier'. Use update instead!")
 
-    def __getstate__(self):
-        pass
-
-    @abc.abstractmethod
-    def create_and_train_pixelwise(
-        self, feature_images, label_images, axistags=None, feature_names=None, image_ids=None
+    def update(
+        self,
+        feature_images: Iterable = [],
+        label_images: Iterable = [],
+        axistags=None,
+        feature_names: Iterable = [],
+        keys: Iterable = [],
     ):
         """
-        Create a new classifier and train it with the given list of feature images and the given list of label images.
-        Generally, it is assumed that the channel dimension is the LAST axis for each image.
-        (The label image must include a singleton channel dimension.)
-        Each pair of corresponding feature and label images must have matching shapes (except for the channel dimension).
+        Update this classifier with the given lists of feature images and label images. The corresponding list of keys
         """
-        raise NotImplementedError
 
-    @abc.abstractmethod
-    def get_halo_shape(self, data_axes="zyxc"):
-        """
-        Return the halo dimensions required for optimal classifier performance.
-        For example, for a classifier that performs an internal 3D convolution with sigma=1.5 and window_size = 2.0,
-        halo_shape = (3, 3, 3, 0).
-
-        Clients are not required to provide the halo during training.
-        (For example, it may not be possible for labels near the image border.)
-
-        data_axes: A string representing the axis order of the data that will be used for training/prediction.
-                   Examples: 'yxc', 'zyxc', or 'tzyxc'.
-        """
-        raise NotImplementedError
+    # def get_halo_shape(self):
+    #     """
+    #     Return the halo dimensions required for optimal classifier performance.
+    #     """
+    #     raise NotImplementedError
 
     def description(self):
         """
@@ -322,42 +308,6 @@ class LazyflowOnlineClassifier:
         """
         return 0
 
-    @classmethod
-    def __subclasshook__(cls, C):
-        """
-        To qualify as a subclass, your class must
-        1) be an actual subclass (so our __new__ method can add the VERSION member to all instances)
-        2) override the proper instance methods
-        3) have a VERSION class member
-        """
-        if cls is LazyflowPixelwiseClassifierFactoryABC:
-            is_subclass = LazyflowPixelwiseClassifierFactoryABC in C.__mro__
-            is_subclass &= _has_attributes(C, ["create_and_train_pixelwise", "description", "get_halo_shape"])
-            is_subclass &= "VERSION" in C.__dict__
-            return is_subclass
-        return NotImplemented
-
-    def __eq__(self, other):
-        """
-        Classifier factories must be both copyable and (in)equality comparable.
-        """
-        raise NotImplementedError
-
-    def __ne__(self, other):
-        raise NotImplementedError
-
-    """
-    Defines an interface for "pixel-wise" classifier objects that can be used by the lazyflow classifier operators.
-    A "pixel-wise" classifier expects its input be given as a list of ND feature images (with M feature channels).
-    (It was trained with a list of ND label images, with 1 channel each.)
-
-    Note: It is assumed here that 'channel' is always the last axis of the image.
-
-    (This interface is typically used with classifiers that must generate their own features internally,
-    and thus require the knowledge of the image structure and context around each training/prediction point.)
-    """
-
-    @abc.abstractmethod
     def predict_probabilities_pixelwise(self, feature_image, roi, axistags=None):
         """
         For each pixel in the given feature_image, predict the probabilities that the
@@ -374,43 +324,21 @@ class LazyflowOnlineClassifier:
         """
         raise NotImplementedError
 
-    @abc.abstractproperty
     def known_classes(self):
         """
         Returns the list of label classes known to this classifier (i.e. the classes it was trained with).
         """
         raise NotImplementedError
 
-    @abc.abstractproperty
     def feature_count(self):
         """
         Return the number of features used to train this classifier.
         """
         raise NotImplementedError
 
-    @abc.abstractmethod
     def get_halo_shape(self, data_axes="zyxc"):
-        """
-        Same as LazyflowPixelwiseClassifierFactoryABC.get_halo_shape().  See that function for details.
-        """
         raise NotImplementedError
 
-    @classmethod
-    def __subclasshook__(cls, C):
-        if cls is LazyflowPixelwiseClassifierABC:
-            return _has_attributes(
-                C,
-                [
-                    "predict_probabilities_pixelwise",
-                    "known_classes",
-                    "get_halo_shape",
-                    "serialize_hdf5",
-                    "deserialize_hdf5",
-                ],
-            )
-        return NotImplemented
-
-    @abc.abstractmethod
     def serialize_hdf5(self, h5py_group):
         """
         Serialize the classifier as an hdf5 group
