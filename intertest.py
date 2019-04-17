d4186c3b913714492d5ad5d2575565d7961b7016
Tomaz Vieira <tomaz.vieira@embl.de>
2019-04-17 17:15:12 +0200

    Minor testing tweak

--- intertest.py
+++ intertest.py
@@ -18,9 +18,8 @@ scribblings = np.asarray(PilImage.open("/home/tomaz/ilastikTests/SampleData/c_ce
 raw_data1 = Array5D(raw_data1, axiskeys='yxc')
 annotation = Annotation(scribblings.astype(np.uint32), axiskeys='yx', raw_data=raw_data1)
 
-fc = FeatureCollection(GaussianSmoothing(sigma=0.3))#, HessianOfGaussian(sigma=1.2), GaussianSmoothing(sigma=1.7))
+fc = FeatureCollection(GaussianSmoothing(sigma=0.3), HessianOfGaussian(sigma=1.2), GaussianSmoothing(sigma=1.7))
 classifier = PixelClassifier(feature_collection=fc, annotations=[annotation])
 predictions = classifier.predict(raw_data1)
 
-class0 = list(list(predictions.as_uint8().images())[0].channels())[0].as_pil_image()
-class1 = list(list(predictions.as_uint8().images())[0].channels())[1].as_pil_image()
+pil_images = [c.as_pil_image() for img in predictions.as_uint8().images() for c in img.channels()]
