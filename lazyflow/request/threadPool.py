c2cc763014cf9884510a538eac6ad7e0afd7641e
Maksim Novikov <mnovikov.work@gmail.com>
2019-06-27 15:07:01 +0200

    Remove dead code

--- lazyflow/request/threadPool.py
+++ lazyflow/request/threadPool.py
@@ -243,33 +243,3 @@ class _Worker(threading.Thread):
             )  # If this fails, then your callable is some built-in that doesn't allow arbitrary
             #  members (e.g. .assigned_worker) to be "monkey-patched" onto it.  You may have to wrap it in a custom class first.
             return task
-
-    def raise_exc(self, excobj):
-        """
-        I HAVEN'T TESTED THIS YET.  (But it looks useful.)
-        http://code.activestate.com/recipes/496960-thread2-killable-threads
-
-        Debugging method.
-        Asynchronously raise an exception in this thread.
-        See docstring for _async_raise() for more details.
-        """
-        assert self.isAlive(), "thread must be started"
-        for tid, tobj in list(threading._active.items()):
-            if tobj is self:
-                _Worker._async_raise(tid, excobj)
-                return
-
-    @staticmethod
-    def _async_raise(tid, excobj):
-        """
-        Raise an exception in the thread with the given id.
-        http://code.activestate.com/recipes/496960-thread2-killable-threads
-        """
-        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(excobj))
-        if res == 0:
-            raise ValueError("nonexistent thread id")
-        elif res > 1:
-            # """if it returns a number greater than one, you're in trouble,
-            # and you should call it again with exc=NULL to revert the effect"""
-            ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
-            raise SystemError("PyThreadState_SetAsyncExc failed")
