406c769a560de91aa9cfb455c345b3f9c4607529
Emil Melnikov <emilmelnikov@gmail.com>
2019-07-31 12:56:51 +0200

    Fix determine_optimal_request_blockshape
    `blockshape -= blockshape % atomic_blockshape` resulted in shape with
    zero elements when `blockshape[i] < atomic_blockshape[i]` for some `i`.

--- lazyflow/roi.py
+++ lazyflow/roi.py
@@ -683,7 +683,7 @@ def determine_optimal_request_blockshape(
 
     # Need to stack the ideal_blockshape to come up with something larger.
     # Start with an isotropic block, clipped to the nearest multiple of the atomic_blockshape
-    blockshape = numpy.array(determineBlockShape(max_blockshape, target_block_volume_pixels))
+    blockshape = numpy.array(determineBlockShape(clipped_ideal_blockshape, target_block_volume_pixels))
     blockshape -= blockshape % atomic_blockshape
 
     while True:
