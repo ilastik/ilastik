c01b66e7bacf70c534ce2206b73adf0559b02dac
Tomaz Vieira <tomaz.vieira@embl.de>
2019-08-13 15:15:05 +0200

    ImageFileDialog: Fixes behavior on missing prefs file. Adds tests

--- ilastik/widgets/ImageFileDialog.py
+++ ilastik/widgets/ImageFileDialog.py
@@ -8,39 +8,41 @@ from volumina.utility import PreferencesManager
 from ilastik.applets.dataSelection.opDataSelection import OpDataSelection
 from ilastik.config import cfg as ilastik_config
 
+
 class ImageFileDialog(QFileDialog):
-    def __init__(self, parent_window, preferences_group : str = 'DataSelection', preferences_setting : str = 'recent image'):
+    def __init__(
+        self,
+        parent_window,
+        preferences_group: str = "DataSelection",
+        preferences_setting: str = "recent image",
+        preferences_manager: PreferencesManager = PreferencesManager(),
+    ):
         self.preferences_group = preferences_group
         self.preferences_setting = preferences_setting
+        self.preferences_manager = preferences_manager
         # Find the directory of the most recently opened image file
-        mostRecentImageFile = PreferencesManager().get(preferences_group, preferences_setting )
-        mostRecentImageFile = str(mostRecentImageFile)
-        if mostRecentImageFile is not None:
-            defaultDirectory = os.path.split(mostRecentImageFile)[0]
+        mostRecentImageFile = preferences_manager.get(preferences_group, preferences_setting)
+        if mostRecentImageFile is None:
+            defaultDirectory = os.path.expanduser("~")
         else:
-            defaultDirectory = os.path.expanduser('~')
+            defaultDirectory = os.path.split(str(mostRecentImageFile))[0]
 
         extensions = OpDataSelection.SupportedExtensions
         filter_strs = ["*." + x for x in extensions]
         filters = [f"{filt} ({filt})" for filt in filter_strs]
-        filt_all_str = "Image files (" + ' '.join(filter_strs) + ')'
+        filt_all_str = "Image files (" + " ".join(filter_strs) + ")"
 
-        super().__init__(parent_window, caption="Select Images",
-                         directory=defaultDirectory, filter=filt_all_str)
-        # use Qt dialog in debug mode (more portable?)
-        self.setOption(QFileDialog.DontUseNativeDialog, ilastik_config.getboolean("ilastik", "debug"))
+        super().__init__(parent_window, caption="Select Images", directory=defaultDirectory, filter=filt_all_str)
         self.setFileMode(QFileDialog.ExistingFiles)
 
     def getSelectedPaths(self) -> List[Path]:
         if not super().exec_():
             return []
         filePaths = [Path(selected_file) for selected_file in self.selectedFiles()]
-        PreferencesManager().set(self.preferences_group, self.preferences_setting, filePaths[0].as_posix())#FIXME: use platform style, maybe?
+        self.preferences_manager.set(self.preferences_group, self.preferences_setting, filePaths[0].as_posix())
         # For the n5 extension the attributes.json file has to be selected in the file dialog.
         # However we need just the *.n5 directory-file.
         for i, path in enumerate(filePaths):
-            if path.name.lower() == 'attributes.json' and any(p.suffix.lower() == ".n5" for p in filePath.parents):
+            if path.name.lower() == "attributes.json" and any(p.suffix.lower() == ".n5" for p in filePath.parents):
                 filePaths[i] = filePath.parent
         return filePaths
-
-
---
+++ tests/widgets/test_ImageFileDialog.py
@@ -0,0 +1,40 @@
+import pytest
+from pathlib import Path
+
+from PyQt5.QtCore import QTimer
+
+from ilastik.widgets.ImageFileDialog import ImageFileDialog
+from volumina.utility import PreferencesManager
+
+
+@pytest.fixture
+def blank_preferences_file(tmp_path) -> Path:
+    return tmp_path / "my_preferences"
+
+
+@pytest.fixture
+def blank_preferences(blank_preferences_file: Path) -> PreferencesManager:
+    return PreferencesManager(path=blank_preferences_file)
+
+
+@pytest.fixture
+def image(tmp_path) -> Path:
+    image_path = Path(tmp_path / "some_picture.png")
+    image_path.touch()
+    return image_path
+
+
+def test_default_image_directory_is_home_with_blank_preferences_file(blank_preferences: PreferencesManager):
+    dialog = ImageFileDialog(None, preferences_manager=blank_preferences)
+    assert dialog.directory().absolutePath() == Path("~").expanduser().absolute().as_posix()
+
+
+def test_picking_file_updates_default_image_directory_to_previously_used(blank_preferences_file: Path, image: Path):
+    dialog = ImageFileDialog(None, preferences_manager=PreferencesManager(blank_preferences_file))
+    assert dialog.directory().absolutePath() == Path("~").expanduser().absolute().as_posix()
+    dialog.selectFile(image.as_posix())
+    QTimer.singleShot(1000, dialog.accept)
+    assert dialog.getSelectedPaths() == [image]
+
+    dialog = ImageFileDialog(None, preferences_manager=PreferencesManager(blank_preferences_file))
+    assert Path(dialog.directory().absolutePath()) == blank_preferences_file.parent
