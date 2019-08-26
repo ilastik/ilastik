ef0ee2466062240a34341f86d64a5b007d4071da
k-dominik <k-dominik@users.noreply.github.com>
2019-08-26 16:40:49 +0200

    Re-enable and fix windows FileSelection tests
     * use mock `PreferenceManger` (wonder how this test ever worked, given that the
     config file path is hard-coded in volumina).
     * Weird enough, decreasing the time in `singleShot` made the tests pass on
     windows. Not a fan that it just works, but well...

--- tests/widgets/test_ImageFileDialog.py
+++ tests/widgets/test_ImageFileDialog.py
@@ -1,49 +1,48 @@
 import pytest
 import os
 from pathlib import Path
+from unittest import mock
 
 from PyQt5.QtCore import QTimer
 
 from ilastik.widgets.ImageFileDialog import ImageFileDialog
-from volumina.utility import PreferencesManager
 
 
 @pytest.fixture
-def blank_preferences_file(tmp_path) -> Path:
-    return tmp_path / "my_preferences"
-
-
-@pytest.fixture
-def blank_preferences(blank_preferences_file: Path) -> PreferencesManager:
-    return PreferencesManager(path=blank_preferences_file)
+def blank_preferences():
+    preference_manager = mock.Mock()
+    preference_manager.get = mock.Mock(return_value=None)
+    return preference_manager
 
 
 @pytest.fixture
 def image(tmp_path) -> Path:
-    image_path = Path(tmp_path / "some_picture.png")
+    image_path = Path(tmp_path) / "some_picture.png"
     image_path.touch()
     return image_path
 
-@pytest.mark.skipif(os.name == 'nt', reason="Unimportant tests that either hang of segfaults on windows")
-def test_default_image_directory_is_home_with_blank_preferences_file(blank_preferences: PreferencesManager):
+
+def test_default_image_directory_is_home_with_blank_preferences_file(blank_preferences):
     dialog = ImageFileDialog(None, preferences_manager=blank_preferences)
     assert dialog.directory().absolutePath() == Path("~").expanduser().absolute().as_posix()
 
 
-@pytest.mark.skipif(os.name == 'nt', reason="Unimportant tests that either hang of segfaults on windows")
-def test_picking_file_updates_default_image_directory_to_previously_used(blank_preferences_file: Path, image: Path):
-    dialog = ImageFileDialog(None, preferences_manager=PreferencesManager(blank_preferences_file))
+def test_picking_file_updates_default_image_directory_to_previously_used(blank_preferences, image: Path):
+    preferences = blank_preferences
+    dialog = ImageFileDialog(None, preferences_manager=preferences)
     assert dialog.directory().absolutePath() == Path("~").expanduser().absolute().as_posix()
     dialog.selectFile(image.as_posix())
-    QTimer.singleShot(1000, dialog.accept)
+
+    QTimer.singleShot(10, dialog.accept)
     assert dialog.getSelectedPaths() == [image]
 
-    dialog = ImageFileDialog(None, preferences_manager=PreferencesManager(blank_preferences_file))
-    assert Path(dialog.directory().absolutePath()) == blank_preferences_file.parent
+    preferences.set.assert_called_once_with(dialog.preferences_group, dialog.preferences_setting, image.as_posix())
 
-@pytest.mark.skipif(os.name == 'nt', reason="Unimportant tests that either hang of segfaults on windows")
-def test_picking_n5_json_file_returns_directory_path(tmp_n5_file: Path, blank_preferences: PreferencesManager):
+
+def test_picking_n5_json_file_returns_directory_path(tmp_n5_file: Path, blank_preferences):
     dialog = ImageFileDialog(None, preferences_manager=blank_preferences)
-    dialog.selectFile(tmp_n5_file.joinpath("attributes.json").as_posix())
-    QTimer.singleShot(1000, dialog.accept)
+    dialog.setDirectory(str(tmp_n5_file))
+    dialog.selectFile("attributes.json")
+
+    QTimer.singleShot(10, dialog.accept)
     assert dialog.getSelectedPaths() == [tmp_n5_file]
