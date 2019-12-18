a53866dc1645434c03b22741af75f944d39ff2fe
Tomaz Vieira <tomaz.vieira@embl.de>
2019-12-18 14:13:44 +0100

    TaskOrchestrator: simplifies and adds type hints

--- ilastik/distributed/TaskOrchestrator.py
+++ ilastik/distributed/TaskOrchestrator.py
@@ -1,9 +1,12 @@
 from mpi4py import MPI
 from threading import Thread
 import enum
+from typing import Generator, TypeVar, Generic, Callable
+
+TASK_DATUM = TypeVar("TASK_DATUM")
 
 
-class TaskOrchestrator(object):
+class TaskOrchestrator(Generic[TASK_DATUM]):
     @enum.unique
     class Tags(enum.IntEnum):
         TASK_DONE = 13
@@ -11,55 +14,42 @@ class TaskOrchestrator(object):
 
     END = "END"
 
-    def __init__(self, slice_iterator, producer_also_consumes=True, comm=None):
+    def __init__(self, comm=None):
         self.comm = comm or MPI.COMM_WORLD
         self.rank = self.comm.Get_rank()
-        self.slice_iterator = slice_iterator
-        self.num_workers = self.comm.size
-        if not producer_also_consumes:
-            self.num_workers -= 1
+        self.num_workers = self.comm.size - 1
 
-    def start_background_producer_if_leader(self):
-        if self.rank == 0:
-            self.start_background_producer()
-
-    def start_background_producer(self):
-        print(f"~~~~~~>> Starting a producer to manage {self.num_workers} workers")
-        Thread(target=self._orchestrate).start()
-
-    def _orchestrate(self):
-        print("Starting orchestration...")
+    def orchestrate(self, task_data: Generator[TASK_DATUM, None, None]):
+        print(f"ORCHESTRATOR: Starting orchestration of {self.num_workers}...")
         stopped_workers = self.num_workers
-        for worker_idx in range(self.num_workers):
+        for worker_idx in range(1, self.num_workers + 1):
             try:
-                next_slice = next(self.slice_iterator)
-                self.comm.send(next_slice, dest=worker_idx, tag=self.Tags.TO_WORKER)
+                datum = next(task_data)
+                self.comm.send(datum, dest=worker_idx, tag=self.Tags.TO_WORKER)
                 stopped_workers -= 1
-                print(f"Sent slice {next_slice} to worker {worker_idx}...")
+                print(f"Sent datum {datum} to worker {worker_idx}...")
             except StopIteration:
                 break
 
         while True:
             status = MPI.Status()
-            print("trying to receive........................")
             self.comm.recv(source=MPI.ANY_SOURCE, tag=self.Tags.TASK_DONE, status=status)
             worker_idx = status.Get_source()
-            print(f"==> task received from {worker_idx}.")
 
             try:
-                self.comm.send(next(self.slice_iterator), dest=worker_idx, tag=self.Tags.TO_WORKER)
-                print(f"Dispatched new job to worker {worker_idx}")
+                self.comm.send(next(task_data), dest=worker_idx, tag=self.Tags.TO_WORKER)
             except StopIteration:
                 self.comm.send(self.END, dest=worker_idx, tag=self.Tags.TO_WORKER)
                 stopped_workers += 1
                 if stopped_workers == self.num_workers:
                     break
 
-    def startForegroundWorker(self, target):
+    def start_as_worker(self, target: Callable[[TASK_DATUM, int], None]):
+        print(f"WORKER {self.rank}: Started")
         while True:
             status = MPI.Status()
-            data = self.comm.recv(source=MPI.ANY_SOURCE, tag=self.Tags.TO_WORKER, status=status)
-            if data == self.END:
+            datum = self.comm.recv(source=MPI.ANY_SOURCE, tag=self.Tags.TO_WORKER, status=status)
+            if datum == self.END:
                 break
-            self.comm.send(target(data, self.rank), dest=status.Get_source(), tag=self.Tags.TASK_DONE)
+            self.comm.send(target(datum, self.rank), dest=status.Get_source(), tag=self.Tags.TASK_DONE)
         print(f"WORKER {self.rank}: Terminated")
