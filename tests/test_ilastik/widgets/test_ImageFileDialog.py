c3c0fb0729cb653ac9bbb1e67d411c5e408e3778
Maksim Novikov <mnovikov.work@gmail.com>
2020-03-19 12:46:57 +0100

    Add some more waits

--- tests/test_ilastik/widgets/test_ImageFileDialog.py
+++ tests/test_ilastik/widgets/test_ImageFileDialog.py
@@ -5,7 +5,6 @@ from pathlib import Path
 import pytest
 from ilastik.widgets.ImageFileDialog import ImageFileDialog
 from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QApplication
 from volumina.utility import preferences
 
 
@@ -32,6 +31,8 @@ def test_default_image_directory_is_home_with_blank_preferences_file():
 
 def test_picking_file_updates_default_image_directory_to_previously_used(qtbot, image: Path, tmp_preferences):
     dialog = ImageFileDialog(None)
+    qtbot.waitExposed(dialog)
+
     dialog.selectFile(image.as_posix())
 
     qtbot.waitUntil(lambda: bool(dialog.selectedFiles()))
@@ -45,6 +46,8 @@ def test_picking_file_updates_default_image_directory_to_previously_used(qtbot, 
 
 def test_picking_n5_json_file_returns_directory_path(qtbot, tmp_n5_file: Path):
     dialog = ImageFileDialog(None)
+    qtbot.waitExposed(dialog)
+
     dialog.setDirectory(str(tmp_n5_file))
     dialog.selectFile("attributes.json")
 
