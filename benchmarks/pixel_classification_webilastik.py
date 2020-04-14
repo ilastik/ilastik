f54271b02c955643ee2d76bd67399ca475a08bf1
Tomaz Vieira <tomaz.vieira@embl.de>
2020-04-14 18:19:09 +0200

    Adds mpi and thread-pool flags to web ilastik benchmarks

--- benchmarks/pixel_classification_webilastik.py
+++ benchmarks/pixel_classification_webilastik.py
@@ -1,8 +1,8 @@
 import ast
 import time
 from pathlib import Path
+from typing import Any, Union
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
-from lazyflow.distributed.TaskOrchestrator import TaskOrchestrator
 import cProfile
 
 from ndstructs import Array5D, Slice5D, Shape5D, Point5D
@@ -17,7 +17,7 @@ from ilastik.features import (
     StructureTensorEigenvalues,
 )
 from ilastik.annotations import Annotation
-from ilastik.classifiers.pixel_classifier import PixelClassifier, ScikitLearnPixelClassifier
+from ilastik.classifiers.pixel_classifier import PixelClassifier, ScikitLearnPixelClassifier, Predictions
 from ilastik.classifiers.ilp_pixel_classifier import IlpVigraPixelClassifier
 from lazyflow.utility.timer import Timer
 import argparse
@@ -53,6 +53,11 @@ parser.add_argument(
 parser.add_argument(
     "--tile-size", required=False, type=int, default=None, help="Side of the raw data tile to use when predicting"
 )
+
+parser.add_argument(
+    "--num-workers", required=False, type=int, default=8, help="Number of process workers to run predictions"
+)
+parser.add_argument("--orchestrator", required=True, choices=["mpi", "thread-pool"])
 args = parser.parse_args()
 
 
@@ -84,16 +89,60 @@ with t:
     )
 print(f"Training {classifier.__class__.__name__} took {t.seconds()}")
 
+import pickle
+
+pickled_classifier = pickle.dumps(classifier)
+classifier = pickle.loads(pickled_classifier)
+
 predictions = classifier.allocate_predictions(datasource.roi)
+
+slc_generator = (raw_tile for raw_tile in datasource.roi.split(datasource.tile_shape))
+
 t = Timer()
-with t:
+if args.orchestrator == "mpi":
+    from lazyflow.distributed.TaskOrchestrator import TaskOrchestrator
 
-    def predict_tile(raw_tile):
-        tile_prediction = classifier.predict(raw_tile)
-        predictions.set(tile_prediction)
-        print(f"Tile {raw_tile} done! {time.time()}")
+    orchestrator = TaskOrchestrator()
+    if orchestrator.rank != 0:
 
-    for raw_tile in DataSourceSlice(datasource).split():
-        predict_tile(raw_tile)
+        def do_predict(slc: Slice5D, rank: int):
+            print(f">>>>>>>> Predicting on {slc}")
+            data_source_slice = DataSourceSlice(datasource, **slc.to_dict())
+            prediction_tile = classifier.predict(data_source_slice)
+            print(f">>>>>>>> Predicting on {slc}")
+            return prediction_tile
+
+        orchestrator.start_as_worker(do_predict)
+        exit(0)
+    with t:
+
+        def collect_predictions(prediction_tile: Predictions):
+            predictions.set(prediction_tile)
+
+        orchestrator.orchestrate(slc_generator, collector=collect_predictions)
+elif args.orchestrator == "thread-pool":
+    with t:
+
+        def do_predict(datasource_slc: DataSourceSlice):
+            print(f">>>>>>>> Predicting on {datasource_slc.roi}")
+            pred_tile = classifier.predict(datasource_slc)
+            print(f"<<<<<<<< DONE Predicting on {datasource_slc.roi}")
+            predictions.set(pred_tile)
+
+        with ThreadPoolExecutor(max_workers=args.num_workers) as executor:
+            for datasource_slc in DataSourceSlice(datasource).split():
+                executor.submit(do_predict, datasource_slc)
+else:
+    raise NotImplementedError(f"Please implement orchestraiton for {args.orchestrator}")
+
+#    executor = ProcessPoolExecutor(initializer=worker_initializer, initargs=(classifier,))
+#    #worker_initializer(classifier)
+#    futures = [executor.submit(do_prediction, raw_tile) for raw_tile in datasource.roi.split(datasource.tile_shape)]
+#    #futures = [do_prediction(raw_tile) for raw_tile in datasource.roi.split(datasource.tile_shape)]
+#    #executor.shutdown(wait=True)
+#    for f in futures:
+#        result = f.result()
+#        predictions.set(result)
+#        #predictions.set(f)
 print(f"Prediction with {classifier.__class__.__name__} took {t.seconds()}")
 predictions.as_uint8().show_channels()
