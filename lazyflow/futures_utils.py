56489c7da8d5088e78ef5ac92ed79fa222bc7365
Maksim Novikov <mnovikov.work@gmail.com>
2020-09-16 22:22:17 +0200

    Add MappableFuture util class

---
+++ lazyflow/futures_utils.py
@@ -0,0 +1,25 @@
+from concurrent.futures import CancelledError, Future
+from typing import Callable, Generic, TypeVar
+
+
+T = TypeVar("T")
+S = TypeVar("S")
+
+
+class MappableFuture(Future, Generic[T]):
+    def map(self, func: Callable[[T], S]) -> "MappableFuture[S]":
+        new_fut: S = MappableFuture()
+
+        def _do_map(f):
+            try:
+                if f.cancelled():
+                    new_fut.cancel()
+                    return
+
+                res = func(f.result())
+                new_fut.set_result(res)
+            except Exception as e:
+                new_fut.set_exception(e)
+
+        self.add_done_callback(_do_map)
+        return new_fut
