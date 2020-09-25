854236dbc7a1347716d9ca1ffbd7be7e07e794cf
Maksim Novikov <mnovikov.work@gmail.com>
2020-09-25 08:40:28 +0200

    Better enum validation

--- tests/test_ilastik/test_experimental/conftest.py
+++ tests/test_ilastik/test_experimental/conftest.py
@@ -16,12 +16,25 @@ def test_data_lookup(data_path, tmpdir_factory) -> types.ApiTestDataLookup:
         known_filenames = set(e.value for e in types.TestData)
 
         path_by_name = {}
+        unknown_names = set()
         for name in zip.namelist():
             if name not in known_filenames:
-                raise ValueError(
-                    f"Unknown file {name} in api_projects.zip. Add entry to ApiTestData or remove it from zip"
-                )
+                unknown_names.add(name)
 
+            known_filenames.discard(name)
             path_by_name[name] = str(unpacking_dir / name)
+
+        if known_filenames or unknown_names:
+            unused_names_str = "\n".join(f"\t* {name}" for name in known_filenames)
+            unknown_names_str = "\n".join(f"\t* {name}" for name in unknown_names)
+
+            fail_msg = f"ApiTest enum doesn't match contents of api_projects.zip."
+            if unknown_names_str:
+                fail_msg += f"\napi_project.zip has extra files:\n{unknown_names_str}"
+
+            if unused_names_str:
+                fail_msg += f"\nTestData enum has extra entries:\n{unused_names_str}"
+
+            pytest.fail(fail_msg)
 
         return types.ApiTestDataLookup(path_by_name)
