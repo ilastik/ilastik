74a6d188917b42b1e420275eebcd4a635b00503b
k-dominik <k-dominik@users.noreply.github.com>
2020-12-11 13:35:45 +0100

    added test for RoiRequestBuffer

---
+++ tests/test_lazyflow/test_utility/testRoiRequestBuffer.py
@@ -0,0 +1,80 @@
+import numpy
+import pytest
+
+from lazyflow.operators import OpArrayPiper
+from lazyflow.utility import RoiRequestBuffer
+from lazyflow.utility.roiRequestBuffer import _RoiIter
+import lazyflow.utility.roiRequestBuffer
+
+
+class ProcessingException(Exception):
+    pass
+
+
+class OpArrayPiperError(OpArrayPiper):
+    def __init__(self, raise_exception_on_req_no, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._raise_exception_on_req_no = raise_exception_on_req_no
+        self._execution_count = 0
+
+    def execute(self, slot, subindex, roi, result):
+        self._execution_count += 1
+        if self._execution_count == self._raise_exception_on_req_no:
+            raise ProcessingException()
+        super().execute(slot, subindex, roi, result)
+
+
+@pytest.fixture
+def raising_op(graph):
+    op = OpArrayPiperError(3, graph=graph)
+    data = numpy.ones((3, 2, 10, 16, 32))  # tczyx
+    op.Input.setValue(data)
+    return op
+
+
+def test_RoiIter():
+    shape = (200, 42, 3, 15, 27)
+    r_iter = _RoiIter(shape)
+
+    assert len(r_iter) == numpy.prod(shape[:-2])
+    assert all(x[-2:] == (0, 0) for x, _ in r_iter)
+    assert all(x[0] == y + (0, 0) for x, y in zip(r_iter, numpy.ndindex(*shape[:-2])))
+
+
+def test_in_ascending_order(graph):
+    """Make sure iterator returns data slices in ascending order"""
+    op = OpArrayPiper(graph=graph)
+    data = numpy.ones((3, 2, 10, 16, 32), dtype="uint8") * numpy.arange(32, dtype="uint8")
+
+    op.Input.setValue(data)
+
+    class RI(_RoiIter):
+        """Roi iter class that returns rois over the last dimension in descending order"""
+
+        def to_index(self, roi):
+            return roi[0][-1]
+
+        def __len__(self):
+            return self._shape[-1]
+
+        def __iter__(self):
+            for i in range(self._shape[-1], 0, -1):
+                start = (0, 0, 0, 0) + (i - 1,)
+                stop = self._shape[:-1] + (i,)
+                yield start, stop
+
+    rb = RoiRequestBuffer(op.Output, batchsize=2, roi_iter_cls=RI)
+
+    for i, item in enumerate(rb):
+        assert item.shape == (3, 2, 10, 16, 1)
+        assert item.min() == i
+        assert item.max() == i
+
+
+def test_raises(raising_op):
+    """Make sure iterator does not block indefinitely if exception occurs in thread"""
+    rb = RoiRequestBuffer(raising_op.Output, batchsize=2)
+
+    with pytest.raises(ProcessingException):
+        for item in rb:
+            assert item.shape == (1, 1, 1, 16, 32)
