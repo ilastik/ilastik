74029ea78212800198518a445e79d185893f4fc8
Tomaz Vieira <tom.fogaca@gmail.com>
2021-02-25 14:31:30 +0100

    Fixed variable-used-before-definition error on selection of bad directory

--- ilastik/widgets/stackFileSelectionWidget.py
+++ ilastik/widgets/stackFileSelectionWidget.py
@@ -139,11 +139,10 @@ class StackFileSelectionWidget(QDialog):
         self.directoryEdit.setText(directory)
         try:
             globstring = self._getGlobString(directory)
-        except StackFileSelectionWidget.DetermineStackError as e:
-            QMessageBox.warning(self, "Invalid selection", str(e))
-        if globstring:
             self.patternEdit.setText(globstring)
             self._applyPattern()
+        except StackFileSelectionWidget.DetermineStackError as e:
+            QMessageBox.warning(self, "Invalid selection", str(e))
 
     def _getGlobString(self, directory):
         all_filenames = []
