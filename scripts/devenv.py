de1a00cedc849f5182b435e856cf0ca6dd5f2d86
Emil Melnikov <emilmelnikov@gmail.com>
2021-07-29 14:20:44 +0200

    Pass-through conda create options
    All options specified for the `devenv.py create` command are passed
    directly to the `conda create` command, with the same defaults
    as before.
    
    This approach allows one to quickly create a default environment,
    while being flexible enough to support advanced use cases without
    modifying this script.

--- scripts/devenv.py
+++ scripts/devenv.py
@@ -4,42 +4,63 @@
 
 import sys  # isort: skip
 
-if sys.version_info[:2] < (3, 8):
-    sys.exit("Python 3.8 or later is required")
+if sys.version_info[:2] < (3, 7):
+    sys.exit("Python 3.7 or later is required")
 
 import argparse
 import logging
-import os
-import shlex
 import shutil
 import subprocess
 import time
 
-EXECUTABLES = "conda", "git"
-REPOSITORIES = "ilastik", "volumina"
+EXECUTABLES = ("conda", "git")
+REPOSITORIES = ("ilastik", "volumina")
+DEFAULT_CREATE_ARGS = (
+    "--channel",
+    "ilastik-forge",
+    "--channel",
+    "conda-forge",
+    "ilastik-dependencies-no-solvers",
+    "pre-commit",
+)
 
 logging.basicConfig(format="%(asctime)s %(filename)s %(levelname)-10s %(message)s", level=logging.INFO)
 
 
 def main():
-    ap = argparse.ArgumentParser(description=__doc__)
-    ap.add_argument("-n", "--name", metavar="ENV", help="environment name", default="ilastik", required=True)
-    sub_ap = ap.add_subparsers(required=True, dest="cmd", metavar="cmd")
-    sub_ap.add_parser("create", help="create a new environment").set_defaults(func=action_create)
-    sub_ap.add_parser("remove", help="remove an existing environment").set_defaults(func=action_remove)
-    args = ap.parse_args()
+    ap = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    ap.add_argument("-n", "--name", metavar="ENV", help="environment name", default="ilastik")
+
+    ap_sub = ap.add_subparsers(required=True, dest="cmd", metavar="cmd")
+
+    ap_create = ap_sub.add_parser(
+        "create",
+        help="create a new environment",
+        usage="%(prog)s [-h] [arguments ...]",
+        epilog=(
+            "All arguments will be added to the 'conda create' command.\n\n"
+            "default arguments:\n  " + " ".join(DEFAULT_CREATE_ARGS)
+        ),
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    ap_create.set_defaults(func=cmd_create)
+
+    ap_remove = ap_sub.add_parser("remove", help="remove an existing environment")
+    ap_remove.set_defaults(func=cmd_remove)
+
+    args, rest = ap.parse_known_args()
 
     if not all(map(shutil.which, EXECUTABLES)):
-        sys.exit("executables " + punctlist(EXECUTABLES) + " should exist in the system path")
+        sys.exit("the following executables should exist in the system path: " + str(EXECUTABLES)[1:-1])
 
     if not all(map(is_git_repo, REPOSITORIES)):
-        sys.exit("directories " + punctlist(REPOSITORIES) + " should be valid git repositories")
+        sys.exit("the following directories should be valid git repositories: " + str(REPOSITORIES)[1:-1])
 
     logging.info("command %r started", args.cmd)
     time_start = time.perf_counter()
 
     try:
-        args.func(args)
+        args.func(args, rest)
     except subprocess.CalledProcessError:
         logging.error("command %r failed", args.cmd)
     except KeyboardInterrupt:
@@ -51,43 +72,37 @@ def main():
     logging.info("elapsed time: %.3f seconds", time_end - time_start)
 
 
-def action_create(args):
-    run(["conda", "install", "--yes", "--name", "base", "--channel", "conda-forge", "conda-build"])
+def cmd_create(args, rest):
+    if not rest:
+        rest = list(DEFAULT_CREATE_ARGS)
 
-    chans = ["--channel", "ilastik-forge", "--channel", "conda-forge"]
-    pkgs = ["ilastik-dependencies-no-solvers", "pre-commit"]
-    run(["conda", "create", "--yes", "--name", args.name] + chans + pkgs)
+    chan_args = ["--override-channels", "--strict-channel-priority"]
+    run(["conda", "install", "--yes", "--name", "base"] + chan_args + ["--channel", "conda-forge", "conda-build"])
+    run(["conda", "create", "--yes", "--name", args.name] + chan_args + rest)
 
     for repo in REPOSITORIES:
         run(["conda", "develop", "--name", args.name, repo])
         run(["conda", "run", "--name", args.name, "pre-commit", "install"], cwd=repo)
 
 
-def action_remove(args):
+def cmd_remove(args, _rest):
     run(["conda", "env", "remove", "--yes", "--name", args.name])
 
 
 def run(cmd, cwd=None):
-    msg = "run command " + repr(shlex.join(cmd))
     if cwd:
-        msg += " in working directory " + repr(cwd)
-    logging.info(msg)
+        logging.info("run command %r in directory %r", " ".join(cmd), cwd)
+    else:
+        logging.info("run command %r", " ".join(cmd))
     subprocess.run(cmd, stdout=sys.stdout, stderr=subprocess.STDOUT, cwd=cwd, check=True)
 
 
 def is_git_repo(cwd):
-    if not os.path.isdir(cwd):
+    try:
+        subprocess.check_call(["git", "rev-parse"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, cwd=cwd)
+        return True
+    except (subprocess.CalledProcessError, FileNotFoundError):
         return False
-    proc = subprocess.run(["git", "rev-parse"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, cwd=cwd)
-    return proc.returncode == 0
-
-
-def punctlist(seq):
-    if not seq:
-        raise ValueError("empty sequence")
-    if len(seq) == 1:
-        return repr(seq[0])
-    return ", ".join(map(repr, seq[:-1])) + " and " + repr(seq[-1])
 
 
 if __name__ == "__main__":
