589f8eca62eb9311166df302e67926a7507338a0
k-dominik <k-dominik@users.noreply.github.com>
2021-11-19 17:13:26 +0100

    added channel selection

--- ilastik/applets/pixelClassificationEnhancer/opPixelClassificationEnhancer.py
+++ ilastik/applets/pixelClassificationEnhancer/opPixelClassificationEnhancer.py
@@ -1,5 +1,23 @@
 from ..pixelClassification.opPixelClassification import OpPixelClassification
 
+from lazyflow.graph import InputSlot, OutputSlot
+from ilastik.utility import OpMultiLaneWrapper
+from lazyflow.operators.generic import OpMultiChannelSelector
+
 
 class OpPixelClassificationEnhancer(OpPixelClassification):
-    pass
+    name = "OpPixelClassificationEnhancer"
+    category = "Top-level"
+    SelectedChannels = InputSlot(value=[])
+
+    EnhancerInput = OutputSlot(level=1)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.opSelectProbs = OpMultiLaneWrapper(
+            OpMultiChannelSelector, parent=self, broadcastingSlotNames=["SelectedChannels"]
+        )
+        self.opSelectProbs.SelectedChannels.connect(self.SelectedChannels)
+        self.opSelectProbs.Input.connect(self.opPredictionPipeline.PredictionProbabilities)
+        self.EnhancerInput.connect(self.opSelectProbs.Output)
