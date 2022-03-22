577639e1ce049c367bc57d53c0e842677049b86f
k-dominik <k-dominik@users.noreply.github.com>
2022-03-22 11:00:56 +0100

    replaced license dialog
    * removed old `buildem` references - `buildem` has not been used in eons
    * `QDialog` as base in order to add custom btns
    * open long licenses via `webbrowser`
    * corrected path for license when in source distribution/conda/development mode
    * remove duplicate ilastik logo
    * make logo look more smooth in dialog
    * added tests

--- ilastik/shell/gui/licenseDialog.py
+++ ilastik/shell/gui/licenseDialog.py
@@ -2,7 +2,7 @@
 ###############################################################################
 #   ilastik: interactive learning and segmentation toolkit
 #
-#       Copyright (C) 2011-2014, the ilastik developers
+#       Copyright (C) 2011-2022, the ilastik developers
 #                                <team@ilastik.org>
 #
 # This program is free software; you can redistribute it and/or
@@ -20,98 +20,119 @@
 # 		   http://ilastik.org/license.html
 ###############################################################################
 import os
-from PyQt5.QtWidgets import QMessageBox
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
+import webbrowser
+from functools import partial
+from pathlib import Path
+from textwrap import dedent
 
-import platform
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (
+    QDialog,
+    QHBoxLayout,
+    QLabel,
+    QMessageBox,
+    QPushButton,
+    QSizePolicy,
+    QSpacerItem,
+    QVBoxLayout,
+)
+
 import ilastik
 
-ilastik_path = os.path.split(ilastik.__file__)[0]
 
+class LicenseDialog(QDialog):
+    LICENSE_SHORT = dedent(
+        """
+        <p>Copyright © 2011-2022, the ilastik developers <team@ilastik.org></p>
+        <p>
+        This program is free software; you can redistribute it and/or
+        modify it under the terms of the GNU General Public License
+        as published by the Free Software Foundation; either version 2
+        of the License, or (at your option) any later version.
+        </p>
+        <p>
+        In addition, as a special exception, the copyright holders of
+        ilastik give you permission to combine ilastik with applets,
+        workflows and plugins which are not covered under the GNU
+        General Public License.
+        </p>
+        <p>See the <a href="https://ilastik.org/license.html">license page on the ilastik web site</a> for details.</p>
+        """
+    )
+    LICENSE_ERROR = dedent(
+        """
+        Cannot find LICENSE file in ilastik sources.
 
-def get_buildem_license():
-    """
-    Check if ilastik is run from a binary install created by BuildEM and
-    return path to the license file from the binary distribution.
+        Please report this problem to team@ilastik.org.
 
-    Looks up the names of the directories from the base of the binary
-    install up to the ilastik Python module source.
-    """
-    system = platform.system()
-    if system == "Windows":
-        dir_names = ["ilastik", "ilastik", "ilastik"]
-    else:
-        dir_names = ["src", "ilastik", "ilastik", "ilastik"]
-    global ilastik_path
-    path_components = ilastik_path.split(os.sep)
-    if path_components[-len(dir_names) :] == dir_names:
-        # this is safer than
-        # os.path.join(path_components[:-len(dir_names)]
-        new_components = (
-            [ilastik_path] + len(dir_names) * [".."] + ["COPYING" + (".txt" if system == "Windows" else "")]
-        )
-        license_path = os.path.join(*new_components)
-        if os.path.isfile(license_path):
-            return license_path
-    return None
+        License information is available on the the ilastik web site at https://ilastik.org/license.html.
+        """
+    )
+    LICENSE_3RD_PARTY_ERROR = dedent(
+        """
+        Cannot find third party license file. This is expected when running ilastik from source in conda.
 
-
-class LicenseDialog(QMessageBox):
-    LICENSE_SHORT = """<p>Copyright © 2011-2019, the ilastik developers <team@ilastik.org></p>
-<p>
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-</p>
-<p>
-In addition, as a special exception, the copyright holders of
-ilastik give you permission to combine ilastik with applets,
-workflows and plugins which are not covered under the GNU
-General Public License.
-</p>
-<p>See the <a href="http://ilastik.org/license.html">license page on the ilastik web site</a> for details.</p>
-"""
-    LICENSE_ERROR = """Cannot find LICENSE file in ilastik sources.
-
-Please report this problem to ilastik-devel@ilastik.org.
-
-License information is available on the the ilastik web site at http://ilastik.org/license.html.
-"""
+        If you are seeing this in a release binary, please report this problem to team@ilastik.org.
+        """
+    )
 
     def __init__(self, parent=None):
-        super(LicenseDialog, self).__init__(parent=parent)
+        super().__init__(parent=parent)
 
-        self.setText(self.LICENSE_SHORT)
+        ilastik_path = Path(ilastik.__file__).parent.parent
 
-        global ilastik_path
-        logo_path = os.path.join(ilastik_path, "shell", "gui", "ilastik-logo-alternate-colors.png")
+        # environment variable set in release binary
+        license_path = Path(os.environ.get("ILASTIK_LICENSE_PATH", ilastik_path / "LICENSE"))
+        license_3rd_party_path = Path(os.environ.get("ILASTIK_LICENSE_3RD_PARTY_PATH", "NOTFOUND"))
 
-        logo_image = QPixmap(logo_path).scaled(125, 200, Qt.KeepAspectRatio)
-        self.setIconPixmap(logo_image)
+        logo_path = ilastik_path / "ilastik" / "ilastik-fist.png"
 
-        license_path = os.path.join(ilastik_path, "..", "LICENSE")
-        # if ilastik is run from a binary
-        # use the COPYING file in the distribution
-        license_path_dist = get_buildem_license()
-        if license_path_dist:
-            license_long = open(license_path_dist, "r").read()
-        elif os.path.isfile(license_path):
-            license_long = open(license_path, "r").read()
-        else:
-            license_long = self.LICENSE_ERROR
+        logo_image = QPixmap(str(logo_path)).scaled(125, 200, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        logo = QLabel()
+        logo.setPixmap(logo_image)
 
-        self.setDetailedText(license_long)
+        short_license_label = QLabel(self.LICENSE_SHORT)
+        short_license_label.setWordWrap(True)
+        short_license_label.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+        short_license_label.setMinimumSize(600, 50)
+
+        header_layout = QHBoxLayout()
+        header_layout.addWidget(logo)
+        header_layout.addWidget(short_license_label, 1)
+
+        def show_license(_checked, license_path: Path, error_message: str):
+            if license_path.is_file():
+                webbrowser.open(license_path.as_uri())
+            else:
+                # parent, title, text
+                QMessageBox.warning(self, "License file not found!", error_message)
+
+        show_details_btn = QPushButton("Show full license")
+        show_details_btn.clicked.connect(
+            partial(show_license, license_path=license_path, error_message=self.LICENSE_ERROR)
+        )
+        self._show_details_btn = show_details_btn
+        show_3rd_party_btn = QPushButton("Show third party licenses")
+        show_3rd_party_btn.clicked.connect(
+            partial(show_license, license_path=license_3rd_party_path, error_message=self.LICENSE_3RD_PARTY_ERROR)
+        )
+        self._show_3rd_party_btn = show_3rd_party_btn
+        btnbox = QHBoxLayout()
+        btnbox.addStretch()
+        btnbox.addWidget(show_details_btn)
+        btnbox.addWidget(show_3rd_party_btn)
+        close_btn = QPushButton("Close")
+        close_btn.setDefault(True)
+        close_btn.clicked.connect(self.accept)
+        btnbox.addWidget(close_btn)
+        layout = QVBoxLayout(self)
+        layout.addLayout(header_layout)
+        layout.addLayout(btnbox)
+
+        self.setLayout(layout)
+        self.setTabOrder(close_btn, show_details_btn)
+        self.setTabOrder(show_details_btn, show_3rd_party_btn)
 
         self.setWindowModality(Qt.NonModal)
-        self.open()
-
-    def closeEvent(self, event):
-        """
-        Emulate accept signal to close the window.
-
-        Using the window close button does not close a QMessageBox.
-        """
-        self.accept()
-        super(LicenseDialog, self).closeEvent(event)
+        self.show()
---
+++ tests/test_ilastik/test_shell/test_license_dialog.py
@@ -0,0 +1,102 @@
+import os
+from unittest import mock
+
+from PyQt5.QtCore import Qt
+from pytest import fixture
+
+from ilastik.shell.gui.licenseDialog import LicenseDialog
+
+
+def get_dlg(qtbot):
+    dlg = LicenseDialog()
+    qtbot.addWidget(dlg)
+    qtbot.waitForWindowShown(dlg)
+    return dlg
+
+
+@fixture
+def mock_license(tmp_path):
+    mock_text = "this is just a test license - 42"
+    f = tmp_path / "LICENSE"
+    f.write_text(mock_text)
+    return f, mock_text
+
+
+def test_license_dlg_construct(qtbot):
+    dlg = get_dlg(qtbot)
+    assert dlg.isVisible()
+
+
+def test_license_dlg_default_license(qtbot, tmp_path):
+    with mock.patch("ilastik.__file__", new=str(tmp_path / "ilastik" / "__init__.py")):
+        dlg = get_dlg(qtbot)
+
+    license_path = tmp_path / "LICENSE"
+    license_path.touch()
+    mock_open = mock.Mock()
+    mock_error_msg = mock.Mock()
+    with mock.patch("webbrowser.open", mock_open):
+        with mock.patch("PyQt5.QtWidgets.QMessageBox.warning", mock_error_msg):
+            qtbot.mouseClick(dlg._show_details_btn, Qt.MouseButton.LeftButton)
+
+    mock_open.assert_called_once_with(license_path.as_uri())
+    mock_error_msg.assert_not_called()
+
+
+def test_license_dlg_license(qtbot, mock_license):
+    license_path, license_text = mock_license
+    with mock.patch.dict(os.environ, {"ILASTIK_LICENSE_PATH": str(license_path)}):
+        dlg = get_dlg(qtbot)
+
+    mock_open = mock.Mock()
+    mock_error_msg = mock.Mock()
+    with mock.patch("webbrowser.open", mock_open):
+        with mock.patch("PyQt5.QtWidgets.QMessageBox.warning", mock_error_msg):
+            qtbot.mouseClick(dlg._show_details_btn, Qt.MouseButton.LeftButton)
+
+    mock_open.assert_called_once_with(license_path.as_uri())
+    mock_error_msg.assert_not_called()
+
+
+def test_license_dlg_license_notfound(qtbot):
+    with mock.patch.dict(os.environ, {"ILASTIK_LICENSE_PATH": "_some_bogus_nonexistant_path_42_"}):
+        dlg = get_dlg(qtbot)
+
+    mock_open = mock.Mock()
+    mock_error_msg = mock.Mock()
+    with mock.patch("webbrowser.open", mock_open):
+        with mock.patch("PyQt5.QtWidgets.QMessageBox.warning", mock_error_msg):
+            qtbot.mouseClick(dlg._show_details_btn, Qt.MouseButton.LeftButton)
+
+    mock_open.assert_not_called()
+    mock_error_msg.assert_called_once_with(dlg, "License file not found!", LicenseDialog.LICENSE_ERROR)
+
+
+def test_license_dlg_3rdp_license(qtbot, mock_license):
+    license_path, license_text = mock_license
+    with mock.patch.dict(os.environ, {"ILASTIK_LICENSE_3RD_PARTY_PATH": str(license_path)}):
+        dlg = get_dlg(qtbot)
+
+    mock_open = mock.Mock()
+    mock_error_msg = mock.Mock()
+    with mock.patch("webbrowser.open", mock_open):
+        with mock.patch("PyQt5.QtWidgets.QMessageBox.warning", mock_error_msg):
+            qtbot.mouseClick(dlg._show_3rd_party_btn, Qt.MouseButton.LeftButton)
+
+    mock_open.assert_called_once_with(license_path.as_uri())
+    mock_error_msg.assert_not_called()
+
+
+def test_license_dlg_3rdp_license_notfound(qtbot, mock_license):
+    license_path, license_text = mock_license
+    with mock.patch.dict(os.environ, {"ILASTIK_LICENSE_3RD_PARTY_PATH": "_some_bogus_nonexistant_path_42_"}):
+        dlg = get_dlg(qtbot)
+
+    mock_open = mock.Mock()
+    mock_error_msg = mock.Mock()
+    with mock.patch("webbrowser.open", mock_open):
+        with mock.patch("PyQt5.QtWidgets.QMessageBox.warning", mock_error_msg):
+            qtbot.mouseClick(dlg._show_3rd_party_btn, Qt.MouseButton.LeftButton)
+
+    mock_open.assert_not_called()
+    mock_error_msg.assert_called_once_with(dlg, "License file not found!", LicenseDialog.LICENSE_3RD_PARTY_ERROR)
