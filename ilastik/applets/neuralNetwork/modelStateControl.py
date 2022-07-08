e64a300b7fa5da11ef14c5bd294dae6f9efb1a7d
k-dominik <k-dominik@users.noreply.github.com>
2022-07-08 11:11:31 +0200

    enable removing models, adding from file

--- ilastik/applets/neuralNetwork/modelStateControl.py
+++ ilastik/applets/neuralNetwork/modelStateControl.py
@@ -277,7 +277,11 @@ class ModelStateControl(QWidget):
             self._showErrorMessage(e)
 
     def onModelInfoRequested(self):
-        model_uri = self.modelInfoWidget.getModelSource().strip()
+        model_uri = self.modelInfoWidget.getModelSource()
+        if model_uri is None:
+            logger.debug("No model uri provided")
+            return
+        model_uri = model_uri.strip()
         if not model_uri:
             # try select file from file chooser
             model_uri = self.getModelToOpen(self)
@@ -406,14 +410,17 @@ class ModelStateControl(QWidget):
 
 class BioImageModelCombo(QComboBox):
     collections_url = "https://raw.githubusercontent.com/bioimage-io/collection-bioimage-io/gh-pages/collection.json"
-    _SELECT_FILE = object
+    _SELECT_FILE = object()
+    _REMOVE_FILE = object()
 
     modelDeleted = pyqtSignal()
+    modelOpenFromFile = pyqtSignal()
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setEditable(False)
         self.refresh()
+        self.currentIndexChanged.connect(self.onIndexChange)
 
     def getModelSource(self):
         idx = self.currentIndex()
@@ -441,6 +448,15 @@ class BioImageModelCombo(QComboBox):
             )
         )
 
+    def onIndexChange(self, idx):
+        item_data = self.itemData(idx)
+        if item_data == BioImageModelCombo._REMOVE_FILE:
+            logger.debug("model remove requested")
+            self.modelDeleted.emit()
+        elif item_data == BioImageModelCombo._SELECT_FILE:
+            logger.debug("open model from file")
+            self.modelOpenFromFile.emit()
+
     def setModelDataAvailableState(self, model_source, model_info):
         idx = self.findText(model_info.name)
         if idx == -1:
@@ -448,6 +464,8 @@ class BioImageModelCombo(QComboBox):
             self.insertItem(1, model_source, model_info)
         else:
             self.setCurrentText(self.itemText(idx))
+        self.setItemText(0, "remove model")
+        self.setItemData(0, BioImageModelCombo._REMOVE_FILE)
 
     def setReadyState(self, model_source, model_info):
         self.setModelDataAvailableState(model_source, model_info)
@@ -477,6 +495,8 @@ class EnhancerModelStateControl(ModelStateControl):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        self.modelInfoWidget.modelOpenFromFile.connect(self.onModelInfoRequested)
 
     def _setup_ui(self):
         self.threadRouter = ThreadRouter(self)
