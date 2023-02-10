52d36ebf9f291f6ec86e478d421397200f4456df
Emil Melnikov <emilmelnikov@gmail.com>
2023-02-10 14:04:43 +0100

    Refactor FilePathButton
    Move complex update logic to a single function with an optional flag for avoiding computing `abbrevs`.
    
    For a path with 6 abbreviations, the full update takes 0.5 ms, while skipping `abbrevs` takes only 0.05 ms.
    It may not be a big difference, but event handlers for resize, paint and other such events should be as fast as possible.

--- ilastik/widgets/filePathButton.py
+++ ilastik/widgets/filePathButton.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import PurePath
-from typing import Optional, Tuple, Union
+from typing import Iterable, Optional, Union
 
 from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QIcon, QResizeEvent
@@ -19,64 +19,75 @@ class FilePathButton(QPushButton):
         icon: Optional[QIcon] = None,
         parent: Optional[QWidget] = None,
     ):
-        """Create a new button with the specified path, other text, and icon.
+        """Create a new button with the specified path, subtext, and icon.
 
         If the path is too long, intermediate directories will be replaced by a placeholder.
 
-        The other text, if not empty, is displayed below the path.
+        The subtext, if not empty, is displayed below the path.
         """
         super().__init__(parent)
         self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
-
         self._path = PurePath(path)
-        self.setToolTip(str(self._path))
-
-        self._suffix = f"\n{subtext}" if subtext else ""
-
-        # Icon should be set before the text in order to be accounted for in the size hint.
+        self._subtext = subtext
         if icon is not None:
             super().setIcon(icon)
-
         self._abbrevs = [(PurePath(), 0)]
-        self._update_abbrevs()
-
-    def minimumSizeHint(self) -> QSize:
-        return QSize(self._abbrevs[-1][1], super().sizeHint().height())
+        self._update()
 
     def sizeHint(self) -> QSize:
         return QSize(self._abbrevs[0][1], super().sizeHint().height())
 
+    def minimumSizeHint(self) -> QSize:
+        return QSize(self._abbrevs[-1][1], super().sizeHint().height())
+
     def setText(self, text: str) -> None:
         self._path = PurePath(text)
         self.setToolTip(str(self._path))
-        self._update_abbrevs()
+        self._update()
+
+    def setSubtext(self, subtext: str) -> None:
+        self._subtext = subtext
+        self._update()
 
     def setIcon(self, icon: QIcon) -> None:
         super().setIcon(icon)
-        self._update_abbrevs()
+        self._update()
 
     def setIconSize(self, size: QSize) -> None:
         super().setIconSize(size)
-        self._update_abbrevs()
+        self._update()
 
     def resizeEvent(self, event: QResizeEvent) -> None:
-        self._update_current_text()
+        self._update(update_abbrevs=False)
 
-    def _update_abbrevs(self) -> None:
-        """Update abbreviations for the current widget state, then update the current text."""
-        self._abbrevs = [self._set_path(self._path)]
-        for i in range(2, len(self._path.parts)):
-            path = PurePath(self._path.parts[0], HORIZONTAL_ELLIPSIS, *self._path.parts[i:])
-            self._abbrevs.append(self._set_path(path))
-        self._update_current_text()
+    def _update(self, update_abbrevs: bool = True) -> None:
+        """Update widget text.
 
-    def _update_current_text(self) -> None:
-        """Update the current text to the shortest possible one that fits into the button."""
-        path = next((abbrev for abbrev, width in self._abbrevs if width <= self.width()), self._abbrevs[-1][0])
-        self._set_path(path)
+        If abbreviations and their corresponding widths don't need to be recomputed,
+        set ``update_abbrevs`` to ``False``.
+        """
+        if update_abbrevs:
+            self._abbrevs.clear()
+            for abbrev in abbreviated_paths(self._path):
+                # super().sizeHint() computes the proper size of the button
+                # for this abbreviation.
+                self._set_path(abbrev)
+                self._abbrevs.append((abbrev, super().sizeHint().width()))
+        self._set_path(next((p for p, w in self._abbrevs if w <= self.width()), self._abbrevs[-1][0]))
 
-    def _set_path(self, path: PurePath) -> Tuple[str, int]:
-        """Set button text from path; return the path's text, and the ideal width of this widget."""
-        text = str(path)
-        super().setText(f"{text}{self._suffix}")
-        return text, super().sizeHint().width()
+    def _set_path(self, path: PurePath) -> None:
+        """Set the current text as the specified path and (possibly) subtext on a new line."""
+        super().setText(f"{path}\n{self._subtext}" if self._subtext else str(path))
+
+
+def abbreviated_paths(path: PurePath, placeholder: str = HORIZONTAL_ELLIPSIS) -> Iterable[PurePath]:
+    """Yield the original path, and then it's consecutive abbreviations.
+
+    In an abbreviated path, one or more intermediate directories in that path
+    are replaced by a placeholder.
+
+    Directories are abbreviated starting from root, but the root itself
+    and the final path component are never abbreviated.
+    """
+    yield path
+    yield from (PurePath(path.parts[0], placeholder, *path.parts[i:]) for i in range(2, len(path.parts)))
