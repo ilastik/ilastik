9fb4f5492c5c528d7de6c7fae2a58e2c8b86d5f1
Benedikt Best <63287233+btbest@users.noreply.github.com>
2023-12-06 11:00:07 +0100

    Multiscale: Test scale combobox lock GUI-side

--- tests/test_ilastik/test_applets/dataSelection/testDataSelectionGui.py
+++ tests/test_ilastik/test_applets/dataSelection/testDataSelectionGui.py
@@ -4,7 +4,7 @@ from unittest import mock
 
 import pytest
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from PyQt5.QtWidgets import QComboBox
+from PyQt5.QtWidgets import QComboBox, QMessageBox
 
 from ilastik.applets.dataSelection.datasetDetailedInfoTableModel import DatasetColumn
 from ilastik.applets.dataSelection.datasetDetailedInfoTableView import DatasetDetailedInfoTableView
@@ -18,39 +18,48 @@ def prepare_widget(qtbot, widget):
 
 
 @pytest.fixture
+def intercept_info_popup(monkeypatch):
+    info_mock = mock.Mock()
+    monkeypatch.setattr(QMessageBox, "information", lambda *args: info_mock(*args))
+    return info_mock
+
+
+@pytest.fixture
 def dataset_table(qtbot) -> DatasetDetailedInfoTableView:
     def mock_DatasetDetailedInfoTableModel():
-        empty_model = QStandardItemModel()
-        empty_model.isEmptyRow = lambda _row: False
-        empty_model.get_scale_options = lambda _row: ["100, 100, 10", "50, 50, 10"] if _row > 0 else []
-        empty_model.is_scale_locked = lambda _row: False
-        return empty_model
-
-    data_rows = [
-        ["image", "/usr/root/image.png", "", "zyx", "(1,10,10)", "", ""],
-        ["image", "precomputed://http://localhost:8000", "", "zyx", "(1,10,10)", "", "100, 100, 10"],
-    ]
+        m = QStandardItemModel()
+        m.isEmptyRow = lambda _row: False
+        m.get_scale_options = lambda _row: ["100, 100, 10", "50, 50, 10"] if _row > 0 else []
+        m.is_scale_locked = lambda _row: _row == 2
+        data_rows = [
+            ["image", "/usr/root/image.png", "", "zyx", "(1,10,10)", "", ""],
+            ["image", "precomputed://http://localhost:8000", "", "zyx", "(1,10,10)", "", "100, 100, 10"],
+            ["image", "precomputed://http://localhost:8000", "", "zyx", "(1,10,10)", "", "50, 50, 10"],
+            [],  # Empty row because the custom setModel replaces the model's last row with an AddFileButton
+        ]
+        for i, row in enumerate(data_rows):
+            m.insertRow(i, [QStandardItem(d) for d in row])
+        return m
 
     table_view = DatasetDetailedInfoTableView(None)
     model = mock_DatasetDetailedInfoTableModel()
-    for i, row in enumerate(data_rows):
-        model.insertRow(i, [QStandardItem(d) for d in row])
     table_view.setModel(model)
+    # Make sure qtbot window is big enough to `paint` also the Scale column
+    table_view.resize(1200, 300)
     return prepare_widget(qtbot, table_view)
 
 
-def test_scale_select_dropdown(dataset_table):
+@pytest.fixture
+def mock_gui():
     dataSelectionGui = mock.Mock()
     dataSelectionGui.handleScaleSelected = mock.Mock()
-    dataset_table.scaleSelected.connect(dataSelectionGui.handleScaleSelected)
+    return dataSelectionGui
+
+
+def test_scale_select_exists_and_triggers_gui_event(dataset_table, mock_gui):
+    dataset_table.scaleSelected.connect(mock_gui.handleScaleSelected)
     scale_cell_single_scale = dataset_table.model().index(0, DatasetColumn.Scale)
     scale_cell_multiscale = dataset_table.model().index(1, DatasetColumn.Scale)
-
-    # Would have liked to assert that `paint`ing DatasetDetailedInfoTableView triggers the editor
-    # for multiscale rows, i.e. user interaction not necessary.
-    # But couldn't get qtbot to actually paint ¯\_(ツ)_/¯
-    dataset_table.edit(scale_cell_single_scale)  # Pretend-click the table cell instead
-    dataset_table.edit(scale_cell_multiscale)
 
     assert not dataset_table.isPersistentEditorOpen(scale_cell_single_scale)
     assert dataset_table.isPersistentEditorOpen(scale_cell_multiscale)
@@ -60,4 +69,14 @@ def test_scale_select_dropdown(dataset_table):
     assert editor.count() == 2
 
     editor.setCurrentIndex(1)
-    assert dataSelectionGui.handleScaleSelected.called_once_with(1, 1)
+    assert mock_gui.handleScaleSelected.called_once_with(1, 1)
+
+
+def test_locked_scale_select_does_not_trigger_gui_and_informs_user(dataset_table, mock_gui, intercept_info_popup):
+    dataset_table.scaleSelected.connect(mock_gui.handleScaleSelected)
+    scale_cell_multiscale_locked = dataset_table.model().index(2, DatasetColumn.Scale)
+    editor_locked = dataset_table.indexWidget(scale_cell_multiscale_locked)
+
+    editor_locked.setCurrentIndex(0)
+    intercept_info_popup.assert_called_once()
+    assert 0 == mock_gui.handleScaleSelected.call_count
