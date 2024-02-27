ilastik/ilastik

commit 4403bb3373f8c73f49516b9499a5f543a3f4215b

Multiscale: Use key-string instead of int-indexing for scales
This makes the project file more sane to parse for consumers other than ilastik

ilastik/applets/dataSelection/opDataSelection.py
@@ -103,7 +103,7 @@ class DatasetInfo(ABC):
         project_file: h5py.File = None,
         normalizeDisplay: bool = None,
         drange: Tuple[Number, Number] = None,
-        working_scale: int = 0,
+        working_scale: str = "",
         scale_locked: bool = False,
     ):
         if axistags and len(axistags) != len(laneShape):
@@ -126,7 +126,7 @@ class DatasetInfo(ABC):
         self.legacy_datasetId = self.generate_id()
         self.working_scale = working_scale
         self.scale_locked = scale_locked
-        self.scales = []  # list of dicts dependent on data format
+        self.scales = {}  # {scale_key: scale_object}, dependent on data format
 
     @property
     def shape5d(self) -> Shape5D:
@@ -172,7 +172,7 @@ class DatasetInfo(ABC):
             "nickname": self.nickname.encode("utf-8"),
             "normalizeDisplay": self.normalizeDisplay,
             "drange": self.drange,
-            "working_scale": self.working_scale,
+            "working_scale": self.working_scale.encode("utf-8"),
             "scale_locked": self.scale_locked,
             "location": self.legacy_location.encode("utf-8"),  # legacy support
             "filePath": self.effective_path.encode("utf-8"),  # legacy support
@@ -205,7 +205,9 @@ class DatasetInfo(ABC):
         if "display_mode" in data:
             params["display_mode"] = data["display_mode"][()].decode("utf-8")
         if "working_scale" in data:
-            params["working_scale"] = int(data["working_scale"][()])
+            # This raises if working_scale is an int,
+            # which would be the case if anyone actually created a Precomputed project with ilastik1.4.1b13
+            params["working_scale"] = data["working_scale"][()].decode("utf-8")
         if "scale_locked" in data:
             params["scale_locked"] = bool(data["scale_locked"][()])
         return cls(**params)
@@ -584,10 +586,13 @@ class UrlDatasetInfo(MultiscaleUrlDatasetInfo):
         """
         Returns a deserialized DatasetInfo dict equivalent to a dict from a MultiscaleUrlDatasetInfo
         """
+        from lazyflow.utility.io_util.RESTfulPrecomputedChunkedVolume import RESTfulPrecomputedChunkedVolume
+
+        remote_source = RESTfulPrecomputedChunkedVolume(group["filePath"][()].decode().lstrip("precomputed://"))
         deserialized = super().from_h5_group(
             group,
             {
-                "working_scale": -1,
+                "working_scale": remote_source.original_resolution_key,
                 "scale_locked": True,
             },
         )
@@ -820,7 +825,9 @@ class OpDataSelection(Operator):
             if data_provider.meta.scales:
                 datasetInfo.laneShape = data_provider.meta.shape
                 datasetInfo.scales = data_provider.meta.scales
-                datasetInfo.working_scale = self.ActiveScale.value
+                # Using "" for working_scale would also default to the lowest, but the datasetInfo property
+                # may be stored in the project file, so we want to be explicit with the key.
+                datasetInfo.working_scale = data_provider.meta.lowest_scale
 
             output_order = self._get_output_axis_order(data_provider)
             # Export applet assumes this OpReorderAxes exists.
@@ -876,7 +883,7 @@ class OpDataSelectionGroup(Operator):
 
     # Must mark as optional because not all subslots are required.
     DatasetGroup = InputSlot(stype="object", level=1, optional=True)  # "Group" as in group of slots
-    ActiveScaleGroup = InputSlot(stype="int", level=1, optional=True, value=0)
+    ActiveScaleGroup = InputSlot(stype="string", level=1, optional=True, value="")
 
     # Outputs
     ImageGroup = OutputSlot(level=1)  # "Group" as in group of slots

tests/test_ilastik/test_applets/dataSelection/testOpDataSelection.py
@@ -867,12 +867,13 @@ class TestOpDataSelection_PrecomputedChunks:
         _ = MockRemoteDataset(monkeypatch, self.MOCK_DATASET_URL, self.INFO_JSON, self.CHUNKS)
         op = OpDataSelection(graph=graph)
         op.WorkingDirectory.setValue(os.getcwd())
-        op.ActiveScale.setValue(0)
+        op.ActiveScale.setValue("")
         op.Dataset.setValue(MultiscaleUrlDatasetInfo(url=self.MOCK_DATASET_URL))
         loaded_scale0 = op.Image[:].wait()
         assert numpy.allclose(loaded_scale0, self.IMAGE_SCALED)
 
-        op.ActiveScale.setValue(1)
+        scale_keys = list(op.Image.meta.scales.keys())
+        op.ActiveScale.setValue(scale_keys[1])
         loaded_scale1 = op.Image[:].wait()
         assert numpy.allclose(loaded_scale1, self.IMAGE_ORIGINAL)
 
@@ -884,7 +885,7 @@ class TestOpDataSelection_DatasetInfo:
         "type": "image",
         "data_type": "uint16",
         "num_channels": 1,
-        "scales": [{"chunk_sizes": [[1, 1, 1]], "resolution": [""], "size": [1, 1, 1]}],
+        "scales": [{"key": "foo", "chunk_sizes": [[1, 1, 1]], "resolution": [""], "size": [1, 1, 1]}],
     }
 
     @pytest.fixture
@@ -947,7 +948,7 @@ class TestOpDataSelection_DatasetInfo:
 
         # We know that as of now, a legacy UrlDatasetInfo would be equivalent to a modern MultiscaleUrlDatasetInfo
         # with the scale set to the original resolution (the last in the list), and locked-in.
-        modern_dataset_info.working_scale = -1
+        modern_dataset_info.working_scale = self.MOCK_PRECOMPUTED_INFO["scales"][-1]["key"]
         modern_dataset_info.scale_locked = True
         assert legacy_dataset_info.to_json_data() == modern_dataset_info.to_json_data()
 

ilastik/applets/dataSelection/dataSelectionGui.py
@@ -704,5 +704,5 @@ class DataSelectionGui(QWidget):
 
         self.addLanes([MultiscaleUrlDatasetInfo(url=dvid_url, subvolume_roi=subvolume_roi)], roleIndex)
 
-    def handleScaleSelected(self, laneIndex, scale_index):
-        self.topLevelOperator.get_lane(laneIndex).ActiveScaleGroup.setValue(scale_index)
+    def handleScaleSelected(self, laneIndex, scale_key):
+        self.topLevelOperator.get_lane(laneIndex).ActiveScaleGroup.setValue(scale_key)

ilastik/applets/dataSelection/datasetDetailedInfoTableModel.py
@@ -18,7 +18,7 @@
 # on the ilastik web site at:
 # 		   http://ilastik.org/license.html
 ###############################################################################
-from typing import List
+from typing import List, Dict
 
 from PyQt5.QtCore import Qt, QAbstractItemModel, QModelIndex
 from ilastik.utility import bind
@@ -221,17 +221,20 @@ class DatasetDetailedInfoTableModel(QAbstractItemModel):
 
         raise NotImplementedError(f"Unknown column: row={index.row()}, column={index.column()}")
 
-    def get_scale_options(self, laneIndex) -> List[str]:
+    def get_scale_options(self, laneIndex) -> Dict[str, str]:
         try:
             datasetSlot = self._op.DatasetGroup[laneIndex][self._roleIndex]
         except IndexError:  # This can happen during "Save Project As"
-            return []
+            return {}
         if not datasetSlot.ready():
-            return []
+            return {}
         datasetInfo = datasetSlot.value
         if not datasetInfo.scales:
-            return []
-        return [_resolution_to_display_string(s["resolution"], datasetInfo.axiskeys) for s in datasetInfo.scales]
+            return {}
+        return {
+            key: _resolution_to_display_string(scale["resolution"], datasetInfo.axiskeys)
+            for key, scale in datasetInfo.scales.items()
+        }
 
     def is_scale_locked(self, laneIndex) -> bool:
         datasetSlot = self._op.DatasetGroup[laneIndex][self._roleIndex]

ilastik/applets/dataSelection/datasetDetailedInfoTableView.py
@@ -200,12 +200,12 @@ class ScaleComboBoxDelegate(QStyledItemDelegate):
 
     def createEditor(self, parent: "DatasetDetailedInfoTableView", option, index):
         model: "DatasetDetailedInfoTableModel" = index.model()
-        scales = model.get_scale_options(index.row())
-        if not scales:
+        scale_options = model.get_scale_options(index.row())
+        if not scale_options:
             return None
         combo = QComboBox(parent)
-        for scale_index, scale in enumerate(scales):
-            combo.addItem(scale, scale_index)
+        for scale_key, scale in scale_options.items():
+            combo.addItem(scale, scale_key)
         combo.currentIndexChanged.connect(partial(self.on_combo_selected, index))
         return combo
 
@@ -221,7 +221,7 @@ class ScaleComboBoxDelegate(QStyledItemDelegate):
             # De-focussing the combobox triggers setModelData.
             # We only want to emit when the user actually selects from the combobox.
             return
-        self.parent().scaleSelected.emit(index.row(), editor.currentIndex())
+        self.parent().scaleSelected.emit(index.row(), editor.currentData())
 
     def on_combo_selected(self, index):
         model = index.model()
@@ -246,7 +246,7 @@ class ScaleComboBoxDelegate(QStyledItemDelegate):
 
 class DatasetDetailedInfoTableView(QTableView):
     dataLaneSelected = pyqtSignal(object)  # Signature: (laneIndex)
-    scaleSelected = pyqtSignal(int, int)  # Signature: (lane_index, scale_index)
+    scaleSelected = pyqtSignal(int, str)  # Signature: (lane_index, scale_key)
 
     replaceWithFileRequested = pyqtSignal(int)  # Signature: (laneIndex), or (-1) to indicate "append requested"
     replaceWithStackRequested = pyqtSignal(int)  # Signature: (laneIndex)

ilastik/applets/dataSelection/precomputedVolumeBrowser.py
@@ -99,8 +99,8 @@ class PrecomputedVolumeBrowser(QDialog):
             f"Full URL: {self.selected_url}\n"
             f"Dataset encoding: {rv.get_encoding()}\n"
             f"Number of scales: {len(rv.scales)}\n"
-            f"Raw dataset shape: {rv.get_shape(-1)}\n"
-            f"Lowest scale shape: {rv.get_shape(0)}\n"
+            f"Raw dataset shape: {rv.get_shape(rv.original_resolution_key)}\n"
+            f"Lowest scale shape: {rv.get_shape(rv.lowest_scale_key)}\n"
         )
         # This check-button might have been triggered by pressing Enter.
         # The timer prevents triggering the now enabled OK button by the same keypress.

lazyflow/operators/ioOperators/opRESTfulPrecomputedChunkedVolumeReader.py
@@ -58,12 +58,14 @@ class OpRESTfulPrecomputedChunkedVolumeReaderNoCache(Operator):
         # Create a RESTfulPrecomputedChunkedVolume object to handle
         self._volume_object = RESTfulPrecomputedChunkedVolume(self.BaseUrl.value)
 
-        current_scale = self.Scale.value if self.Scale.ready() else 0
-        self.chunk_size = self._volume_object.get_chunk_size(current_scale)
-        self.Output.meta.shape = tuple(self._volume_object.get_shape(current_scale))
+        active_scale = self.Scale.value if self.Scale.ready() else self._volume_object.lowest_scale_key
+        self.chunk_size = self._volume_object.get_chunk_size(active_scale)
+        self.Output.meta.shape = tuple(self._volume_object.get_shape(active_scale))
         self.Output.meta.dtype = numpy.dtype(self._volume_object.dtype).type
         self.Output.meta.axistags = vigra.defaultAxistags(self._volume_object.axes)
         self.Output.meta.scales = self._volume_object.scales
+        # To feed back to DatasetInfo and hence the project file
+        self.Output.meta.lowest_scale = self._volume_object.lowest_scale_key
 
     @staticmethod
     def get_intersecting_blocks(blockshape, roi, shape):

lazyflow/utility/io_util/RESTfulPrecomputedChunkedVolume.py
@@ -73,6 +73,8 @@ class RESTfulPrecomputedChunkedVolume(object):
             n_threads (int, optional): number of concurrent downloads
         """
         self.scales = None
+        self.lowest_scale_key = None
+        self.original_resolution_key = None
         self._json_info = None
         self.tmp_data_file = tmp_data_file
         self.volume_url = volume_url
@@ -104,30 +106,37 @@ class RESTfulPrecomputedChunkedVolume(object):
 
         jsonschema.validate(self._json_info, self.info_schema)
 
-        # Precomputed spec requires scales to be ordered from highest resolution (original) to lowest (most downscaled).
-        # We want the opposite: to load the lowest resolution, i.e. smallest file size, first.
-        self.scales = list(reversed(self._json_info["scales"]))
+        # Scales are ordered from original to most-downscaled in Precomputed spec
+        self.lowest_scale_key = self._json_info["scales"][-1]["key"]
+        self.original_resolution_key = self._json_info["scales"][0]["key"]
+        # Reverse so that the ScaleComboBox shows the options ordered from most-downscaled to original
+        self.scales = {scale["key"]: scale for scale in reversed(self._json_info["scales"])}
         self.dtype = self._json_info["data_type"]
         self.n_channels = self._json_info["num_channels"]
 
-    def get_chunk_size(self, scale=0):
+    def get_chunk_size(self, scale=""):
+        scale = scale if scale else self.lowest_scale_key
         n_channels = self.n_channels
         block_shape = numpy.array([n_channels] + self.scales[scale]["chunk_sizes"][0][::-1])
         return block_shape
 
-    def get_resolution(self, scale=0):
+    def get_resolution(self, scale=""):
+        scale = scale if scale else self.lowest_scale_key
         resolution = numpy.array(self.scales[scale]["resolution"][::-1])
         return resolution
 
-    def get_voxel_offset(self, scale=0):
+    def get_voxel_offset(self, scale=""):
+        scale = scale if scale else self.lowest_scale_key
         voxel_offset = numpy.array([0] + self.scales[scale]["voxel_offset"][::-1])
         return voxel_offset
 
-    def get_encoding(self, scale=0):
+    def get_encoding(self, scale=""):
+        scale = scale if scale else self.lowest_scale_key
         encoding = self.scales[scale]["encoding"]
         return encoding
 
-    def get_shape(self, scale=0):
+    def get_shape(self, scale=""):
+        scale = scale if scale else self.lowest_scale_key
         n_channels = self.n_channels
         shape = numpy.array([n_channels] + self.scales[scale]["size"][::-1])
         return shape
@@ -142,7 +151,7 @@ class RESTfulPrecomputedChunkedVolume(object):
 
         self._json_info = json.loads(r.content)
 
-    def download_block(self, block_coordinates, scale=0):
+    def download_block(self, block_coordinates, scale=""):
         """downloads a single block at a given scale
 
         Args:
@@ -150,6 +159,7 @@ class RESTfulPrecomputedChunkedVolume(object):
               assumed
             scale (int): index of the scale to be used
         """
+        scale = scale if scale else self.lowest_scale_key
         url, block_shape = self.generate_url(block_coordinates, scale)
         try:
             content = self.downloading(url)
@@ -180,7 +190,7 @@ class RESTfulPrecomputedChunkedVolume(object):
         r = requests.get(url)
         return r.content
 
-    def generate_url(self, block_coordinates, scale=0):
+    def generate_url(self, block_coordinates, scale=""):
         """Generate url to access a specific block
 
 
@@ -192,10 +202,10 @@ class RESTfulPrecomputedChunkedVolume(object):
         Returns:
             string: URL to access the block with the given block coordinates
         """
+        scale = scale if scale else self.lowest_scale_key
         # block shape without channel
         shape = self.get_shape(scale)
         chunk_size = self.get_chunk_size(scale)
-        scale_key = self.scales[scale]["key"]
         coordinates = block_coordinates
 
         if not numpy.allclose(numpy.remainder(coordinates, chunk_size), 0):
@@ -211,7 +221,7 @@ class RESTfulPrecomputedChunkedVolume(object):
         min_z, min_y, min_x = min_values[1:]  # ignore c
         max_z, max_y, max_x = max_values[1:]
 
-        url = f"{base_url}/{scale_key}/{min_x}-{max_x}_{min_y}-{max_y}_{min_z}-{max_z}"
+        url = f"{base_url}/{scale}/{min_x}-{max_x}_{min_y}-{max_y}_{min_z}-{max_z}"
         return url, downloaded_block_shape
 
 

commit 191c1c3ef3c1d3a5e6c5d2b27a0e1a6baa57e8a7

Multiscale: Save scale
Was broken by 4403bb33

Co-authored-by: Dominik Kutra <k-dominik@users.noreply.github.com>

ilastik/applets/dataSelection/opDataSelection.py
@@ -833,6 +833,7 @@ class OpDataSelection(Operator):
             if data_provider.meta.scales:
                 datasetInfo.laneShape = data_provider.meta.shape
                 datasetInfo.scales = data_provider.meta.scales
+                datasetInfo.working_scale = self.ActiveScale.value
                 if datasetInfo.working_scale == DEFAULT_LOWEST_SCALE_KEY:
                     # datasetInfo.working_scale may be saved to the project file, so we want a real key here
                     # if we didn't already load one from the file.

tests/test_ilastik/test_applets/dataSelection/testOpDataSelection.py
@@ -863,12 +863,18 @@ class TestOpDataSelection_PrecomputedChunks:
         ],
     }
 
-    def test_load_precomputed_chunks_over_http(self, graph, monkeypatch):
+    @pytest.fixture
+    def op_and_dataset(self, graph, monkeypatch) -> (OpDataSelection, MultiscaleUrlDatasetInfo):
         _ = MockRemoteDataset(monkeypatch, self.MOCK_DATASET_URL, self.INFO_JSON, self.CHUNKS)
         op = OpDataSelection(graph=graph)
         op.WorkingDirectory.setValue(os.getcwd())
         op.ActiveScale.setValue("")
-        op.Dataset.setValue(MultiscaleUrlDatasetInfo(url=self.MOCK_DATASET_URL))
+        datasetInfo = MultiscaleUrlDatasetInfo(url=self.MOCK_DATASET_URL)
+        op.Dataset.setValue(datasetInfo)
+        return op, datasetInfo
+
+    def test_load_precomputed_chunks_over_http(self, op_and_dataset):
+        op, _ = op_and_dataset
         loaded_scale0 = op.Image[:].wait()
         assert numpy.allclose(loaded_scale0, self.IMAGE_SCALED)
 
@@ -876,6 +882,12 @@ class TestOpDataSelection_PrecomputedChunks:
         op.ActiveScale.setValue(scale_keys[1])
         loaded_scale1 = op.Image[:].wait()
         assert numpy.allclose(loaded_scale1, self.IMAGE_ORIGINAL)
+
+    def test_scale_updates_dataset_info(self, op_and_dataset):
+        op, datasetInfo = op_and_dataset
+        scale_keys = list(op.Image.meta.scales.keys())
+        op.ActiveScale.setValue(scale_keys[1])
+        assert datasetInfo.working_scale == scale_keys[1]
 
 
 class TestOpDataSelection_DatasetInfo: