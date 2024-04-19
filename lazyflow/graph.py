f606535f519ab2ca0b2ef9aaedf6288121d3ffa5
Benedikt Best <63287233+btbest@users.noreply.github.com>
2024-04-19 13:50:19 +0200

    Graph: Clean up
    The lazyflow imports are unused but left here because some scripts seem to import them from `lazyflow.graph` instead of their actual source.

--- lazyflow/graph.py
+++ lazyflow/graph.py
@@ -19,64 +19,20 @@
 # This information is also available on the ilastik web site at:
 # 		   http://ilastik.org/license/
 ###############################################################################
-"""
-This module implements the basic flow graph
-of the lazyflow module.
-
-Basic usage example:
-
----
-import numpy
-import lazyflow.graph
-from lazyflow.operators.operators import  OpArrayPiper
-
-
-g = lazyflow.graph.Graph()
-
-operator1 = OpArrayPiper(graph=g)
-operator2 = OpArrayPiper(graph=g)
-
-operator1.inputs["Input"].setValue(numpy.zeros((10,20,30), dtype=numpy.uint8))
-
-operator2.inputs["Input"].connect(operator1.outputs["Output"])
-
-result = operator2.outputs["Output"][:].wait()
----
-
-"""
-
-# Python
-import sys
-import copy
-import functools
-import collections
-import itertools
 import threading
 import logging
+from lazyflow.utility import OrderedSignal
 
-logger = logging.getLogger(__name__)
-
-# third-party
-import psutil
-
-if int(psutil.__version__.split(".")[0]) < 1 and int(psutil.__version__.split(".")[1]) < 3:
-    msg = "Lazyflow: Please install a psutil python module version of at least >= 0.3.0"
-    sys.stderr.write(msg)
-    logger.error(msg)
-    sys.exit(1)
-
-# SciPy
-import numpy
-
-# lazyflow
 from lazyflow import rtype
 from lazyflow.request import Request
 from lazyflow.stype import ArrayLike
-from lazyflow.utility import slicingtools, Tracer, OrderedSignal, Singleton
+from lazyflow.utility import slicingtools, Tracer, Singleton
 from lazyflow.slot import InputSlot, OutputSlot, Slot
 from lazyflow.operator import Operator, InputDict, OutputDict, OperatorMetaClass
 from lazyflow.operatorWrapper import OperatorWrapper
 from lazyflow.metaDict import MetaDict
+
+logger = logging.getLogger(__name__)
 
 
 class Graph:
