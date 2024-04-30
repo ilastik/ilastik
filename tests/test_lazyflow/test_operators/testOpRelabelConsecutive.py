65bc6592a1a1613a15af79006dfd5909b5f50c74
k-dominik <k-dominik@users.noreply.github.com>
2024-04-30 18:40:54 +0200

    Add Cache to OpRelabelConsecutive
    Preparation for adding an object classification workflow from label
    images.
    
    Also changed the behavior of this op to _not_ touching any zeros in
    the inputs. Zero usually has the meaning "background" in label images.

--- tests/test_lazyflow/test_operators/testOpRelabelConsecutive.py
+++ tests/test_lazyflow/test_operators/testOpRelabelConsecutive.py
@@ -1,29 +1,67 @@
-from builtins import object
 import numpy as np
 import pytest
 import vigra
-from lazyflow.graph import Graph
+
 from lazyflow.operators import OpRelabelConsecutive
 
-pandas = pytest.importorskip("pandas")
+
+@pytest.fixture
+def oprelabel(graph):
+    return OpRelabelConsecutive(graph=graph)
 
 
-class TestOpRelabelConsecutive(object):
-    def test_simple(self):
-        op = OpRelabelConsecutive(graph=Graph())
+@pytest.fixture
+def labels() -> vigra.VigraArray:
+    return vigra.taggedView(2 * np.arange(0, 100, dtype=np.uint8).reshape((10, 10)), "yx")
 
-        labels = 2 * np.arange(0, 100, dtype=np.uint8).reshape((10, 10))
-        labels = vigra.taggedView(labels, "yx")
-        op.Input.setValue(labels)
-        relabeled = op.Output[:].wait()
-        assert (relabeled == labels // 2).all()
 
-    def test_startlabel(self):
-        op = OpRelabelConsecutive(graph=Graph())
-        op.StartLabel.setValue(10)
+@pytest.mark.parametrize("axistags", ["yx", "zyx", "xyz", "xztyc"])
+def test_preserve_axistags(oprelabel, labels, axistags):
+    labels = labels.withAxes(axistags)
+    oprelabel.Input.setValue(labels)
 
-        labels = 2 * np.arange(0, 100, dtype=np.uint8).reshape((10, 10))
-        labels = vigra.taggedView(labels, "yx")
-        op.Input.setValue(labels)
-        relabeled = op.Output[:].wait()
-        assert (relabeled == 10 + labels // 2).all()
+    assert "".join(oprelabel.Output.meta.getAxisKeys()) == axistags
+
+
+def test_simple(oprelabel, labels):
+    oprelabel.Input.setValue(labels)
+    relabeled = oprelabel.Output[:].wait()
+    np.testing.assert_array_equal(relabeled, labels // 2)
+
+
+def test_startlabel(oprelabel, labels):
+    oprelabel.StartLabel.setValue(10)
+    oprelabel.Input.setValue(labels)
+    relabeled = oprelabel.Output[:].wait()
+
+    # 0 is a special label (we assume it's background), so it is unchanged
+    # the first found object will have value 10
+    # with the input data being 0, 2, 4, 6, 8 ...: 0, 10, 11, 12, 13 ...
+    expected = labels // 2
+    expected[expected > 0] += 9
+    np.testing.assert_array_equal(relabeled, expected)
+
+
+@pytest.mark.parametrize(
+    "dtype_in, expected_dtype",
+    [
+        ("uint8", "uint8"),
+        ("uint16", "uint32"),  # uint16 will be converted to uint32 (vigra cannot handle uint16)
+        ("uint32", "uint32"),
+        ("uint64", "uint64"),
+    ],
+)
+def test_input_datatypes(oprelabel, labels, dtype_in, expected_dtype):
+    labels = labels.astype(dtype_in)
+    oprelabel.Input.setValue(labels)
+    relabeled = oprelabel.Output[:].wait()
+    np.testing.assert_array_equal(relabeled, labels // 2)
+    assert relabeled.dtype == expected_dtype
+
+
+@pytest.mark.parametrize("dtype", ["float", "float16", "float32", "float64"])
+def test_unsupported_dtype_raises(oprelabel, labels, dtype):
+    labels = labels.astype(dtype)
+
+    with pytest.raises(ValueError):
+        oprelabel.Input.setValue(labels)
