f0862efc337770850d16b274887a3608bc890b50
k-dominik <k-dominik@users.noreply.github.com>
2024-07-03 10:59:19 +0200

    Allow plugins via ilastik.objectfeatures entrypoint
    this allows plugins to register themselves (via conda install / pip
    install) via an entrypoint.

--- ilastik/plugins/manager.py
+++ ilastik/plugins/manager.py
@@ -19,7 +19,13 @@
 #          http://ilastik.org/license.html
 ###############################################################################
 import os
+import sys
 from collections import namedtuple
+
+if sys.version_info < (3, 10):
+    from importlib_metadata import entry_points
+else:
+    from importlib.metadata import entry_points
 
 from yapsy.PluginManager import PluginManager
 
@@ -30,7 +36,11 @@ from .types import ObjectFeaturesPlugin, TrackingExportFormatPlugin
 # these directories are searched for plugins
 plugin_paths = cfg.get("ilastik", "plugin_directories")
 plugin_paths = list(os.path.expanduser(d) for d in plugin_paths.split(",") if len(d) > 0)
-plugin_paths.append(os.path.join(os.path.split(__file__)[0], "plugins_default"))
+plugin_paths.append(os.path.join(os.path.split(os.path.split(__file__)[0])[0], "plugins_default"))
+
+# Add directories from registered entrypoints
+for ep in entry_points(group="ilastik.objectfeatures"):
+    plugin_paths.append(os.path.split(ep.load().__file__)[0])
 
 # Helper class used to pass the necessary context information to the export plugin
 PluginExportContext = namedtuple(
