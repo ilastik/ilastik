03f6631a4dd6b269635e321705afa4822089a9dd
Benedikt Best <63287233+btbest@users.noreply.github.com>
2024-08-20 14:04:09 +0200

    OME-Zarr export: Implement and test metadata

--- lazyflow/utility/io_util/write_ome_zarr.py
+++ lazyflow/utility/io_util/write_ome_zarr.py
@@ -1,93 +1,76 @@
 import logging
-from typing import List, Literal
+from typing import List, Literal, Optional, Tuple
 
 import ngff_zarr
 import numpy
-
-from lazyflow.operators import OpReorderAxes
-from lazyflow.roi import determineBlockShape, roiToSlice, roiFromShape
-from lazyflow.slot import Slot, SlotAsNDArray
-from lazyflow.utility import BigRequestStreamer, OrderedSignal, PathComponents
+import zarr
 from zarr.storage import FSStore
 
+from lazyflow.operators import OpReorderAxes
+from lazyflow.roi import determineBlockShape
+from lazyflow.slot import Slot, SlotAsNDArray
+from lazyflow.utility import OrderedSignal, PathComponents
 from lazyflow.utility.io_util.OMEZarrStore import OME_ZARR_V_0_4_KWARGS
 
 logger = logging.getLogger(__name__)
 
 
-def write_ome_zarr(export_path: str, image_source_slot: Slot, progress_signal: OrderedSignal):
-    export_path = PathComponents(export_path)
+def _get_chunk_shape(image_source_slot) -> Tuple[int, ...]:
+    """Determine chunk shape for OME-Zarr storage based on image source slot.
+    Chunk size is 1 for t and c, and determined by ilastik default rules for zyx, with a target of 512KB per chunk."""
+    dtype = image_source_slot.meta.dtype
+    if isinstance(dtype, numpy.dtype):  # Extract raw type class
+        dtype = dtype.type
+    dtype_bytes = dtype().nbytes
+    tagged_maxshape = image_source_slot.meta.getTaggedShape()
+    tagged_maxshape["t"] = 1
+    tagged_maxshape["c"] = 1
+    chunk_shape = determineBlockShape(list(tagged_maxshape.values()), 512_000.0 / dtype_bytes)
+    return chunk_shape
+
+
+def write_ome_zarr(
+    export_path: str,
+    image_source_slot: Slot,
+    progress_signal: OrderedSignal,
+    downscale_method: Optional[ngff_zarr.methods.Methods] = None,
+):
+    pc = PathComponents(export_path)
+    external_path = pc.externalPath
+    internal_path = pc.internalPath
+
     op_reorder = OpReorderAxes(parent=image_source_slot.operator)
     op_reorder.AxisOrder.setValue("tczyx")  # OME-Zarr convention
     try:
         op_reorder.Input.connect(image_source_slot)
+        image_source = SlotAsNDArray(op_reorder.Output)
+        chunk_shape = _get_chunk_shape(op_reorder.Output)
         dims: List[Literal["t", "c", "z", "y", "x"]] = list(op_reorder.Output.meta.axistags.keys())
         scale = {k: 1.0 for k in dims}
         translation = {k: 0.0 for k in dims}
-        image_source = SlotAsNDArray(op_reorder.Output)
-        image = ngff_zarr.to_ngff_image(image_source, dims=dims, scale=scale, translation=translation)
+        image = ngff_zarr.to_ngff_image(
+            image_source, name=internal_path or "image", dims=dims, scale=scale, translation=translation
+        )
+        progress_signal(25)
+        multiscales = ngff_zarr.to_multiscales(image, scale_factors=2, chunks=chunk_shape, method=downscale_method)
         progress_signal(50)
-
-        multiscales = ngff_zarr.to_multiscales(image, scale_factors=2, chunks=64)
-        store = FSStore(export_path.externalPath, mode="w", **OME_ZARR_V_0_4_KWARGS)
-        ngff_zarr.to_ngff_zarr(store, multiscales)
-        print(export_path.externalPath)
-        print(multiscales)
+        store = FSStore(external_path, mode="w", **OME_ZARR_V_0_4_KWARGS)
+        ngff_zarr.to_ngff_zarr(store, multiscales, overwrite=False)
+        # Write ilastik metadata
+        for image in multiscales.images:
+            # ngff-zarr does not record the storage path in the image object, so we have to look it up.
+            # The only way we can know that a metadata entry corresponds to this image is the scale factor.
+            dataset = None
+            for d in multiscales.metadata.datasets:
+                scale_transforms = [t for t in d.coordinateTransformations if t.type == "scale"]
+                dataset_scale_factors = scale_transforms[0].scale  # Should only be one
+                if all(image_scale_factor in dataset_scale_factors for image_scale_factor in image.scale.values()):
+                    dataset = d
+                    break
+            assert dataset is not None, f"Could not find metadata for image, must be an error in to_ngff_zarr. {image=}"
+            za = zarr.Array(store, path=dataset.path)
+            za.attrs["axistags"] = op_reorder.Output.meta.axistags.toJSON()
+            za.attrs["display_mode"] = image_source_slot.meta.display_mode
+            za.attrs["drange"] = image_source_slot.meta.get("drange")
     finally:
         op_reorder.cleanUp()
-    return
-    # h5N5GroupName, datasetName = os.path.split(h5N5Path)
-    # if h5N5GroupName == "":
-    #     g = self.f
-    # else:
-    #     if h5N5GroupName in self.f:
-    #         g = self.f[h5N5GroupName]
-    #     else:
-    #         g = self.f.create_group(h5N5GroupName)
-
-    data_shape = image_source_slot.meta.shape
-    logger.info(f"Data shape: {data_shape}")
-
-    dtype = image_source_slot.meta.dtype
-    if isinstance(dtype, numpy.dtype):
-        # Make sure we're dealing with a type (e.g. numpy.float64),
-        # not a numpy.dtype
-        dtype = dtype.type
-    # Set up our chunk shape: Aim for a cube that's roughly 512k in size
-    dtypeBytes = dtype().nbytes
-
-    tagged_maxshape = image_source_slot.meta.getTaggedShape()
-    if "t" in tagged_maxshape:
-        # Assume that chunks should not span multiple t-slices,
-        # and channels are often handled separately, too.
-        tagged_maxshape["t"] = 1
-
-    if "c" in tagged_maxshape:
-        tagged_maxshape["c"] = 1
-
-    chunkShape = determineBlockShape(list(tagged_maxshape.values()), 512_000.0 / dtypeBytes)
-
-    # if datasetName in list(g.keys()):
-    #     del g[datasetName]
-    kwargs = {"shape": data_shape, "dtype": dtype, "chunks": chunkShape}
-
-    # self.d = g.create_dataset(datasetName, **kwargs)
-
-    progress_signal(0)
-
-    display_mode = image_source_slot.meta.display_mode
-    axistags = image_source_slot.meta.axistags.toJSON()
-    neuroglancer_axes = "".join(tag.key for tag in image_source_slot.meta.axistags)[::-1]
-    drange = image_source_slot.meta.get("drange")
-
-    def handle_block_result(roi, data):
-        slicing = roiToSlice(*roi)
-        if data.flags.c_contiguous:
-            self.d.write_direct(data.view(numpy.ndarray), dest_sel=slicing)
-        else:
-            self.d[slicing] = data
-
-    requester = BigRequestStreamer(image_source_slot, roiFromShape(data_shape))
-    requester.resultSignal.subscribe(handle_block_result)
-    requester.progressSignal.subscribe(progress_signal)
-    requester.execute()
--- tests/test_lazyflow/test_utility/test_io_util/test_write_ome_zarr.py
+++ tests/test_lazyflow/test_utility/test_io_util/test_write_ome_zarr.py
@@ -1,3 +1,4 @@
+import math
 from unittest import mock
 
 import numpy
@@ -6,22 +7,33 @@ import vigra
 import zarr
 
 from lazyflow.operators import OpArrayPiper
+from lazyflow.utility import Timer
 from lazyflow.utility.io_util.write_ome_zarr import write_ome_zarr
 
 
-@pytest.fixture(params=["ilastik default order", "2d", "3d", "2dc"])
+@pytest.fixture(params=["125MiB"])
 def data_array(request) -> vigra.VigraArray:
     shapes = {
-        "ilastik default order": (1, 128, 128, 10, 1),
-        "2d": (128, 128),
-        "3d": (10, 128, 128),
-        "2dc": (128, 128, 3),
+        "ilastik default order": (1, 128, 127, 10, 1),
+        "2d": (256, 255),
+        "3d": (10, 126, 125),
+        "2dc": (124, 123, 3),
+        "125MiB": (30, 1024, 1024),  # 4 bytes per pixel
+        "500MiB": (125, 1024, 1024),  # 4 bytes per pixel
+        "1GiB": (256, 1024, 1024),
+        "3GiB": (768, 1024, 1024),
+        "6GiB": (768, 2048, 1024),
     }
     axis_order = {
         "ilastik default order": "txyzc",
         "2d": "yx",
         "3d": "zyx",
         "2dc": "yxc",
+        "125MiB": "zyx",
+        "500MiB": "zyx",
+        "1GiB": "zyx",
+        "3GiB": "zyx",
+        "6GiB": "zyx",
     }
     shape = shapes[request.param]
     data = vigra.VigraArray(shape, axistags=vigra.defaultAxistags(axis_order[request.param]))
@@ -34,13 +46,34 @@ def test_write_new_ome_zarr_on_disc(tmp_path, graph, data_array):
     source_op = OpArrayPiper(graph=graph)
     source_op.Input.setValue(data_array)
     progress = mock.Mock()
-    write_ome_zarr(str(export_path), source_op.Output, progress)
+    with Timer() as timer:
+        write_ome_zarr(str(export_path), source_op.Output, progress)
+        duration = timer.seconds()
 
+    # Manual benchmarking
+    raw_size = math.prod(data_array.shape) * data_array.dtype.type().nbytes
+    print(";" f"{data_array.shape};" f"{data_array.dtype};" f"{raw_size};" f"{duration};" f"{duration / raw_size};")
+
+    expected_axiskeys = "tczyx"
     assert export_path.exists()
     store = zarr.open(str(export_path))
     assert "multiscales" in store.attrs
     m = store.attrs["multiscales"][0]
     assert all(key in m for key in ("datasets", "axes", "version"))
     assert m["version"] == "0.4"
-    assert [a["name"] for a in m["axes"]] == ["t", "c", "z", "y", "x"]
-    assert all(dataset["path"] in store for dataset in m["datasets"])
+    assert [a["name"] for a in m["axes"]] == list(expected_axiskeys)
+    tagged_shape = dict(zip(data_array.axistags.keys(), data_array.shape))
+    original_shape_reordered = [tagged_shape[a] if a in tagged_shape else 1 for a in expected_axiskeys]
+
+    discovered_keys = []
+    for i, dataset in enumerate(m["datasets"]):
+        assert dataset["path"] in store
+        discovered_keys.append(dataset["path"])
+        written_array = store[dataset["path"]]
+        assert "axistags" in written_array.attrs, f"no axistags for {dataset['path']}"
+        assert vigra.AxisTags.fromJSON(written_array.attrs["axistags"]) == vigra.defaultAxistags(expected_axiskeys)
+        reported_scaling = dataset["coordinateTransformations"][0]["scale"]
+        expected_shape = tuple(numpy.array(original_shape_reordered) / numpy.array(reported_scaling))
+        assert written_array.shape == expected_shape
+        assert numpy.count_nonzero(written_array) > numpy.prod(expected_shape) / 2, "did not write actual data"
+    assert all([key in discovered_keys for key in store.keys()]), "store contains undocumented subpaths"
