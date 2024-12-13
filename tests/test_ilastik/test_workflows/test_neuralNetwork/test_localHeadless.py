f91bb0967d617ca4dd29054fe1376d543f237925
k-dominik <k-dominik@users.noreply.github.com>
2024-12-13 10:29:02 +0100

    Fix NN headless test
    Upgrading bioimageio libs to v0.5 spec broke running with previous saved
    projects.
    
    * Added a test to document that breakage
    * Added new project file with a v0.5 model

--- tests/test_ilastik/test_workflows/test_neuralNetwork/test_localHeadless.py
+++ tests/test_ilastik/test_workflows/test_neuralNetwork/test_localHeadless.py
@@ -11,10 +11,24 @@ from ilastik.workflows.neuralNetwork import LocalWorkflow
 
 
 @pytest.fixture
-def project_path() -> pathlib.Path:
-    """local NN workflow created at 6b836bd112618c8ec6244c7330a711a383ef7a20"""
+def project_path_140b30v4() -> pathlib.Path:
+    """local NN workflow created at 6b836bd112618c8ec6244c7330a711a383ef7a20
+
+    on top of ilastik 1.4.0b30 with bioimage model spec version 0.4
+    """
     tests_root = pathlib.Path(__file__).parents[2]
-    project_path = tests_root / "data" / "inputdata" / "NNLocaBatch2d1c.ilp"
+    project_path = tests_root / "data" / "inputdata" / "NNLocaBatch-140b30-biov04-2d1c.ilp"
+    return project_path
+
+
+@pytest.fixture
+def project_path_141b22v5() -> pathlib.Path:
+    """local NN workflow created at f64915831ab2747c3c32194eba36712121456234
+
+    on top of ilastik 1.4.1b22 with bioimage model spec version 0.5
+    """
+    tests_root = pathlib.Path(__file__).parents[2]
+    project_path = tests_root / "data" / "inputdata" / "NNLocaBatch-141b22-biov05-2d1c.ilp"
     return project_path
 
 
@@ -23,7 +37,31 @@ def load_h5_ds(fname, dataset):
         return f[dataset][()]
 
 
-def test_nn_local_headless_prediction(project_path: pathlib.Path, data_path, tmp_path, tiktorch_executable_path):
+def test_nn_local_fails_with_old_project_file(project_path_140b30v4: pathlib.Path, tiktorch_executable_path):
+    """use an ilastik project file with a simple model to test headless op
+
+    Note: tiktorch_executable_path fixture used to ensure tiktorch can be started
+    locally. Either it's in the env or provided to pytest via
+    --tiktorch_executable
+
+    """
+    args = [
+        "--headless",
+        f"--project={project_path_140b30v4}",
+    ]
+    # Clear the existing commandline args so it looks like we're starting fresh.
+    sys.argv = ["ilastik.py"]
+    sys.argv.extend(args)
+
+    with mock.patch("ilastik.config.runtime_cfg.tiktorch_executable", new=tiktorch_executable_path):
+        with mock.patch("ilastik.workflows.WORKFLOW_CLASSES", new=[LocalWorkflow]):
+            parsed_args, workflow_cmdline_args = app.parse_known_args()
+
+            with pytest.raises(ValueError):
+                _ = app.main(parsed_args=parsed_args, workflow_cmdline_args=workflow_cmdline_args, init_logging=False)
+
+
+def test_nn_local(project_path_141b22v5: pathlib.Path, data_path, tmp_path, tiktorch_executable_path):
     """use an ilastik project file with a simple model to test headless op
 
     Note: tiktorch_executable_path fixture used to ensure tiktorch can be started
@@ -35,7 +73,7 @@ def test_nn_local_headless_prediction(project_path: pathlib.Path, data_path, tmp
     output_file = tmp_path / "output.h5"
     args = [
         "--headless",
-        f"--project={project_path}",
+        f"--project={project_path_141b22v5}",
         "--raw_data",
         f"{input_file}",
         "--input_axes",
@@ -52,12 +90,11 @@ def test_nn_local_headless_prediction(project_path: pathlib.Path, data_path, tmp
     with mock.patch("ilastik.config.runtime_cfg.tiktorch_executable", new=tiktorch_executable_path):
         with mock.patch("ilastik.workflows.WORKFLOW_CLASSES", new=[LocalWorkflow]):
             parsed_args, workflow_cmdline_args = app.parse_known_args()
-
             shell = app.main(parsed_args=parsed_args, workflow_cmdline_args=workflow_cmdline_args, init_logging=False)
 
             shell.closeCurrentProject()
 
-            # NN model does input * -1 + 1
-            output_ref = load_h5_ds(input_file, "data")[..., numpy.newaxis] * -1 + 1
+            # NN model does (normalized input (scale linear gain 1/256) * -1 + 1
+            output_ref = load_h5_ds(input_file, "data")[..., numpy.newaxis] * (1 / 256) * -1 + 1
             output = load_h5_ds(output_file, "exported_data")
             numpy.testing.assert_array_almost_equal(output, output_ref)
