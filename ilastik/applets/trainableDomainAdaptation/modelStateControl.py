1af8e4be300405568c0a4c9565e7cac4b9ef3c18
k-dominik <k-dominik@users.noreply.github.com>
2025-03-03 14:00:51 +0100

    Disable models while there is an active model
    Enforces user to consciously remove the model from the project first, before
    selecting a new one.
    
    Before there could be inconsistent states related to this.

--- ilastik/applets/trainableDomainAdaptation/modelStateControl.py
+++ ilastik/applets/trainableDomainAdaptation/modelStateControl.py
@@ -1,3 +1,23 @@
+###############################################################################
+#   ilastik: interactive learning and segmentation toolkit
+#
+#       Copyright (C) 2011-2025, the ilastik developers
+#                                <team@ilastik.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# In addition, as a special exception, the copyright holders of
+# ilastik give you permission to combine ilastik with applets,
+# workflows and plugins which are not covered under the GNU
+# General Public License.
+#
+# See the LICENSE file for details. License information is also available
+# on the ilastik web site at:
+#          http://ilastik.org/license.html
+###############################################################################
 import logging
 
 import requests
@@ -65,6 +85,12 @@ class BioImageModelCombo(QComboBox):
                 silent_self.insertItem(1, model_info.name, model_info)
         else:
             self.setCurrentText(self.itemText(idx))
+
+        model = self.model()
+        assert model
+        for idx in range(1, self.count()):
+            model.item(idx).setEnabled(False)
+
         self.setItemText(0, "remove model")
         self.setItemData(0, BioImageModelCombo._REMOVE_FILE)
 
