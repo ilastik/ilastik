c16f94b082e40a5c89f57678bc52a11f40e6aad5
Peter-Laszcz <laszczpeter@gmail.com>
2025-07-08 15:17:50 +0100

    change no. of projects to match updated zip

--- tests/test_ilastik/data/zip_project_files.py
+++ tests/test_ilastik/data/zip_project_files.py
@@ -2,7 +2,7 @@ import os
 import zipfile
 
 assert (
-    len([proj for proj in os.listdir(".") if proj.endswith(".ilp")]) == 31
+    len([proj for proj in os.listdir(".") if proj.endswith(".ilp")]) == 32
 ), "unpack existing projects first in order not to overwrite them"
 
 projects = zipfile.ZipFile("test_projects.zip", mode="w", compression=zipfile.ZIP_DEFLATED)
