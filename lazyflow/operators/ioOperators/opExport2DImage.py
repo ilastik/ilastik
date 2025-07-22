516dbadf8d07112902b5f6535686e9e67042c02d
Peter-Laszcz <laszczpeter@gmail.com>
2025-07-23 00:28:54 +0100

    write tiff pixel sizes

--- lazyflow/operators/ioOperators/opExport2DImage.py
+++ lazyflow/operators/ioOperators/opExport2DImage.py
@@ -22,6 +22,7 @@
 import os
 
 import numpy
+import tifffile
 import vigra
 
 from lazyflow.graph import Operator, InputSlot
@@ -83,6 +84,41 @@ class OpExport2DImage(Operator):
         ), "Image has shape {}, channelIndex is {}".format(data.shape, data.axistags.channelIndex)
 
         vigra.impex.writeImage(data, self.Filepath.value)
+        extension = os.path.splitext(self.Filepath.value)[1][1:]
+
+        # now write any pixel sizes
+        if (
+            extension in ["tif", "tiff"]
+            and hasattr(self.Input.meta, "axis_units")
+            and any(self.Input.meta.axis_units.items())
+        ):
+            with tifffile.TiffFile(self.Filepath.value) as tif:
+                olddata = tif.asarray()
+                data = olddata.squeeze()
+                x = None
+                y = None
+                axes = "YX"
+                if tagged_shape["c"] > 1:
+                    axes = "CYX"
+                if self.Input.meta.axis_units["x"]:
+                    x = (self.Input.meta.axis_units["x"].encode("unicode_escape").decode("ascii"),)
+                if self.Input.meta.axis_units["y"]:
+                    y = (self.Input.meta.axis_units["y"].encode("unicode_escape").decode("ascii"),)
+
+                imagej_metadata = {
+                    "spacing": 1.0,  # this is equal to the z-axis and gets handled differently in non-2d images
+                    "unit": x,
+                    "yunit": y,
+                    "axes": axes,
+                }
+
+                tifffile.imwrite(
+                    self.Filepath.value,
+                    data,
+                    imagej=True,
+                    metadata=imagej_metadata,
+                    resolution=(self.Input.meta.axistags["x"].resolution, self.Input.meta.axistags["y"].resolution),
+                )
 
 
 if __name__ == "__main__":
