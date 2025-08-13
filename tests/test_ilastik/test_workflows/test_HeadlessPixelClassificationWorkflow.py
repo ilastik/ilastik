90049a5361fcc35648a64da463856e635e4612ec
Peter-Laszcz <laszczpeter@gmail.com>
2025-08-13 19:39:34 +0100

    Add new pixel sizes preservation test

--- tests/test_ilastik/test_workflows/test_HeadlessPixelClassificationWorkflow.py
+++ tests/test_ilastik/test_workflows/test_HeadlessPixelClassificationWorkflow.py
@@ -20,6 +20,8 @@ import zipfile
 import psutil
 import zarr
 
+from lazyflow.graph import Graph
+from lazyflow.operators.ioOperators import OpInputDataReader
 from lazyflow.utility.io_util.OMEZarrStore import OME_ZARR_V_0_4_KWARGS
 
 logger = logging.getLogger(__name__)
@@ -52,6 +54,11 @@ def sample_projects_dir(tmp_path: Path) -> Path:
 @pytest.fixture
 def pixel_classification_ilp_2d3c(sample_projects_dir: Path) -> Path:
     return sample_projects_dir / "PixelClassification2d3c.ilp"
+
+
+@pytest.fixture
+def pixel_classification_ilp_2d_units(sample_projects_dir: Path) -> Path:
+    return sample_projects_dir / "PixelClassification2d_units.ilp"
 
 
 def create_h5(data: numpy.ndarray, axiskeys: str) -> Path:
@@ -364,3 +371,31 @@ def test_headless_from_ome_zarr_http_uri(
         assert exported.shape == (2, 100, 100)
         # Make sure the raw data was actually loaded (zarr fills chunks that fail to load with 0)
         assert numpy.count_nonzero(exported) > 10000
+
+
+def test_headless_pixel_size_preservation(testdir, tmp_path, sample_projects_dir):
+    """
+    Ensures pixel sizes are preserved throughout the workflow for Simple Segmentation export.
+    Based on 'test_headless_ome_zarr_multiscale_export'
+    """
+    ilp_path = sample_projects_dir / "PixelClassification2d_units.ilp"
+    raw_2d_path = str(sample_projects_dir / "inputdata" / "2d.tif")
+    output_path = tmp_path / "out_2d_units.h5"
+
+    run_headless_pixel_classification(
+        testdir,
+        project=ilp_path,
+        raw_data=raw_2d_path,
+        output_filename_format=str(output_path),
+        ignore_training_axistags=True,
+        output_format="hdf5",
+    )
+
+    assert output_path.exists()
+    opReaderResult = OpInputDataReader(graph=Graph())
+    opReaderResult.FilePath.setValue(str(output_path))
+
+    assert round(opReaderResult.Output.meta.axistags["x"].resolution) == 5.0
+    assert round(opReaderResult.Output.meta.axistags["y"].resolution) == 6.0
+    assert opReaderResult.Output.meta.axis_units["x"] == "cm"
+    assert opReaderResult.Output.meta.axis_units["y"] == "nm"
