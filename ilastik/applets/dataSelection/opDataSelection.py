0100de437fc49d92537f3db74c13afbb1c84361c
Peter-Laszcz <laszczpeter@gmail.com>
2025-08-14 11:58:49 +0100

    coverage fixes

--- ilastik/applets/dataSelection/opDataSelection.py
+++ ilastik/applets/dataSelection/opDataSelection.py
@@ -551,7 +551,7 @@ class MultiscaleUrlDatasetInfo(DatasetInfo):
         self.url = url
         op_reader = OpInputDataReader(graph=Graph(), FilePath=self.url)
         meta = op_reader.Output.meta.copy()
-        if "axis_units" in info_kwargs:
+        if "axis_units" in info_kwargs and meta.get("axis_units", None) is not None:
             meta.axis_units = info_kwargs.pop("axis_units")
         super().__init__(
             default_tags=meta.axistags,
