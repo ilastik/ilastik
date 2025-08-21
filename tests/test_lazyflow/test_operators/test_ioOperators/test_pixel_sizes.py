4c01d1c7a412e3be22de72681370a62cb4b1a590
Peter-Laszcz <laszczpeter@gmail.com>
2025-08-21 11:30:27 +0100

    Add test for tiffs with resolution dividing by zero

--- tests/test_lazyflow/test_operators/test_ioOperators/test_pixel_sizes.py
+++ tests/test_lazyflow/test_operators/test_ioOperators/test_pixel_sizes.py
@@ -221,6 +221,7 @@ def test_write_OpH5N5WriterBigDataset(graph, tmp_path, axes, shape, resolutions,
     "image_path,expected_meta",
     [
         ("/pix_res/2d.tif", {"x": (2, "cm"), "y": (7, "nm")}),
+        ("/pix_res/2d_zero_division.tif", {"x": (0, "cm"), "y": (0, "mm")}),
         ("/pix_res/2d_stringified_tuple.tif", {"x": (5, "cm"), "y": (6.000024000096, "nm")}),
         ("/pix_res/3d.tif", {"x": (11.000011000011, "cm"), "y": (6.000024000096, "mm"), "z": (2, "pm")}),
         ("/pix_res/2d_t.tif", {"x": (50, "μm"), "y": (3, "pm"), "t": (3, "min")}),
