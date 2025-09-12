ada3d94d954c5ede924384c90bbd0c5d28754ff4
Peter-Laszcz <laszczpeter@gmail.com>
2025-09-12 18:45:54 +0100

    Add encoding and conversion necessary for writing TIFFs

--- lazyflow/utility/io_util/tiff_encoding.py
+++ lazyflow/utility/io_util/tiff_encoding.py
@@ -2,3 +2,56 @@ def from_ascii(raw_string: str):  # necessary for nonstandard unit characters, e
     if raw_string == "":
         return ""
     return raw_string.encode("ascii").decode("unicode_escape")
+
+
+def to_ascii(val):  # encoding is necessary for exporting image metadata
+    if val == "":
+        return ""
+    return val.encode("unicode_escape").decode("ascii")
+
+
+def to_ome(val):
+    if val.lower() in ome_units:
+        return ome_units[val.lower()]
+    return "pixel"
+
+
+ome_units = {
+    "pixel": "pixel",
+    "": "pixel",
+    "kilometer": "km",
+    "kilometers": "km",
+    "km": "km",
+    "meter": "m",
+    "meters": "m",
+    "m": "m",
+    "centimeter": "cm",
+    "centimeters": "cm",
+    "cm": "cm",
+    "millimeter": "mm",
+    "millimeters": "mm",
+    "mm": "mm",
+    "micrometer": "µm",
+    "micrometers": "µm",
+    "um": "µm",
+    "μm": "µm",
+    "µm": "µm",
+    "micron": "µm",
+    "microns": "µm",
+    "nanometer": "nm",
+    "nanometers": "nm",
+    "nm": "nm",
+    "picometer": "pm",
+    "picometers": "pm",
+    "pm": "pm",
+    "femtometer": "fm",
+    "femtometers": "fm",
+    "fm": "fm",
+    "sec": "s",
+    "secs": "s",
+    "seconds": "s",
+    "s": "s",
+    "millisecond": "ms",
+    "milliseconds": "ms",
+    "ms": "ms",
+}
