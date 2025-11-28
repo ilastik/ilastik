857fabb2b2da66bd6bc075eba608c3dd9b77be04
Benedikt Best <63287233+btbest@users.noreply.github.com>
2025-11-28 16:43:05 +0100

    Data Selection: Unify pixel size across roles

--- tests/test_lazyflow/test_operators/test_ioOperators/test_pixel_sizes.py
+++ tests/test_lazyflow/test_operators/test_ioOperators/test_pixel_sizes.py
@@ -1,5 +1,7 @@
 import json
+import os
 import xml.etree.ElementTree as ET
+from typing import Dict, List, Tuple, Union
 
 import h5py
 import numpy as np
@@ -7,6 +9,7 @@ import pytest
 import tifffile
 import vigra
 
+from ilastik.applets.dataSelection.opDataSelection import OpDataSelectionGroup, FilesystemDatasetInfo
 from lazyflow.graph import Graph
 from lazyflow.operators import OpArrayPiper
 from lazyflow.operators.ioOperators import (
@@ -15,17 +18,20 @@ from lazyflow.operators.ioOperators import (
     OpH5N5WriterBigDataset,
     OpStreamingH5N5Reader,
 )
+from lazyflow.slot import Slot
 from ..test_ioOperators.testOpStreamingH5N5Reader import h5n5_file, data
 
 
-def get_data_op_with_pixel_size_meta(graph, axes, shape, resolutions, units):
+def get_data_op_with_pixel_size_meta(
+    graph, axes: str, shape: Tuple[int, ...], resolutions: List[float], units: Union[Dict[str, str], List[str]]
+):
     data = np.random.default_rng(1337).integers(0, 255, shape).astype(np.uint16)
     tagged_data = vigra.taggedView(data, axes)
     for axis, res in zip(axes, resolutions):
         tagged_data.axistags.setResolution(axis, res)
     op = OpArrayPiper(graph=graph)
     op.Input.setValue(tagged_data)
-    axis_units_dict = dict(zip(axes, units))
+    axis_units_dict = units if isinstance(units, dict) else dict(zip(axes, units))
     op.Output.meta.axis_units = axis_units_dict
     return op
 
@@ -423,3 +429,221 @@ def test_write_read_roundtrip_h5(
     for axis in axes:
         assert meta.axistags[axis].resolution == resolutions[axes.index(axis)]
         assert meta.axis_units[axis] == units[axes.index(axis)]
+
+
+def setup_op_data_lane(dataset_infos: List[FilesystemDatasetInfo], graph):
+    roles = ["Raw Data", "Secondary", "Tertiary"]
+    assert len(roles) == len(dataset_infos), "bad setup"
+    op_data = OpDataSelectionGroup(graph=graph)
+    op_data.WorkingDirectory.setValue(os.getcwd())
+    op_data.DatasetRoles.setValue(roles)
+    op_data.DatasetGroup.setValues(dataset_infos)
+    return op_data
+
+
+def assert_eq_spatial_pixel_size(actual_slot: Slot, expect_tags: vigra.AxisTags, expect_units: Dict[str, str]):
+    for actual_tag in actual_slot.meta.axistags:
+        if actual_tag.key == "c":
+            continue
+        assert actual_tag in expect_tags, f"axis {actual_tag.key} was not expected"
+        assert np.isclose(
+            actual_tag.resolution, expect_tags[actual_tag.key].resolution, atol=1e-15
+        ), "slot axis resolution: actual != expected"
+    assert actual_slot.meta.axis_units == expect_units, "actual slot axis units != expected"
+
+
+@pytest.fixture
+def dataset_with_pixel_size(inputdata_dir) -> FilesystemDatasetInfo:
+    return FilesystemDatasetInfo(filePath=inputdata_dir + "/pix_res/3d_t.tif")
+
+
+@pytest.fixture
+def dataset_no_pixel_size(inputdata_dir) -> FilesystemDatasetInfo:
+    info = FilesystemDatasetInfo(filePath=inputdata_dir + "/pix_res/3d_t.tif", axistags=vigra.defaultAxistags("tzyx"))
+    info.axis_units = None
+    return info
+
+
+@pytest.fixture
+def dataset_other_pixel_size(inputdata_dir) -> FilesystemDatasetInfo:
+    info = FilesystemDatasetInfo(filePath=inputdata_dir + "/pix_res/3d_t.tif")
+    for tag in info.axistags:
+        info.axistags.setResolution(tag.key, 3.1415)
+    info.axis_units = {"t": "sec", "x": "nm", "y": "cm", "z": "micron"}
+    return info
+
+
+def test_DataSelection_roles_copies_from_raw_data(graph, dataset_with_pixel_size, dataset_no_pixel_size):
+    dataset_infos = [dataset_with_pixel_size, dataset_no_pixel_size, dataset_no_pixel_size]
+    op_data = setup_op_data_lane(dataset_infos, graph)
+
+    for i in range(3):
+        assert_eq_spatial_pixel_size(
+            op_data.ImageGroup[i], dataset_with_pixel_size.axistags, dataset_with_pixel_size.axis_units
+        )
+    assert OpDataSelectionGroup.META_COPY_KEY in op_data.ImageGroup[1].meta
+    assert op_data.ImageGroup[1].meta[OpDataSelectionGroup.META_COPY_KEY] == "Raw Data"
+    assert OpDataSelectionGroup.META_COPY_KEY in op_data.ImageGroup[2].meta
+    assert op_data.ImageGroup[2].meta[OpDataSelectionGroup.META_COPY_KEY] == "Raw Data"
+
+
+def test_DataSelection_roles_copies_from_raw_data_2roles(graph, dataset_with_pixel_size, dataset_no_pixel_size):
+    dataset_infos = [dataset_with_pixel_size, dataset_no_pixel_size, None]
+    op_data = setup_op_data_lane(dataset_infos, graph)
+
+    for i in range(2):
+        assert_eq_spatial_pixel_size(
+            op_data.ImageGroup[i], dataset_with_pixel_size.axistags, dataset_with_pixel_size.axis_units
+        )
+    assert OpDataSelectionGroup.META_COPY_KEY in op_data.ImageGroup[1].meta
+    assert op_data.ImageGroup[1].meta[OpDataSelectionGroup.META_COPY_KEY] == "Raw Data"
+    assert "axistags" not in op_data.ImageGroup[2].meta
+    assert "axis_units" not in op_data.ImageGroup[2].meta
+    assert OpDataSelectionGroup.META_COPY_KEY not in op_data.ImageGroup[2].meta
+
+
+def test_DataSelection_roles_copies_from_secondary(graph, dataset_with_pixel_size, dataset_no_pixel_size):
+    dataset_infos = [dataset_no_pixel_size, dataset_with_pixel_size, dataset_no_pixel_size]
+    op_data = setup_op_data_lane(dataset_infos, graph)
+
+    for i in range(3):
+        assert_eq_spatial_pixel_size(
+            op_data.ImageGroup[i], dataset_with_pixel_size.axistags, dataset_with_pixel_size.axis_units
+        )
+    assert OpDataSelectionGroup.META_COPY_KEY in op_data.ImageGroup[0].meta
+    assert op_data.ImageGroup[0].meta[OpDataSelectionGroup.META_COPY_KEY] == "Secondary"
+    assert OpDataSelectionGroup.META_COPY_KEY in op_data.ImageGroup[2].meta
+    assert op_data.ImageGroup[2].meta[OpDataSelectionGroup.META_COPY_KEY] == "Secondary"
+
+
+def test_DataSelection_roles_detects_conflict(
+    graph, dataset_with_pixel_size, dataset_no_pixel_size, dataset_other_pixel_size
+):
+    dataset_infos = [dataset_with_pixel_size, dataset_other_pixel_size, dataset_no_pixel_size]
+    op_data = setup_op_data_lane(dataset_infos, graph)
+
+    for i in range(3):
+        assert (
+            OpDataSelectionGroup.META_COPY_KEY not in op_data.ImageGroup[i].meta
+        ), "should not have carried over pixel size"
+        assert_eq_spatial_pixel_size(op_data.ImageGroup[i], dataset_infos[i].axistags, dataset_infos[i].axis_units)
+
+
+def test_DataSelection_roles_rolls_back(
+    graph, dataset_with_pixel_size, dataset_no_pixel_size, dataset_other_pixel_size
+):
+    """
+    The first dataset has no pixel size.
+    When the second dataset is added, its pixel size is transferred to the first.
+    When the third dataset is added, it conflicts with the other two.
+    At this point, the meta carried from secondary to raw data needs to be rolled back.
+    """
+    dataset_infos = [dataset_no_pixel_size, dataset_with_pixel_size, dataset_other_pixel_size]
+    op_data = setup_op_data_lane(dataset_infos, graph)
+    for i in range(3):
+        assert (
+            OpDataSelectionGroup.META_COPY_KEY not in op_data.ImageGroup[i].meta
+        ), "should not have carried over pixel size"
+        assert_eq_spatial_pixel_size(op_data.ImageGroup[i], dataset_infos[i].axistags, dataset_infos[i].axis_units)
+
+
+@pytest.mark.parametrize(
+    "shape1, res1, units1, shape2, res2, units2",
+    [
+        ((3, 4), [0.2, 3.0], {"x": "cm", "y": "nm"}, (5, 6), [0.2, 3.0], {"x": "cm", "y": "nm"}),
+        ((3, 1), [0.2, 7.0], {"x": "cm", "y": "nm"}, (5, 6), [0.2, 3.0], {"x": "cm", "y": "nm"}),
+        ((3, 1), [0.2, 3.0], {"x": "cm", "y": "px"}, (5, 6), [0.2, 3.0], {"x": "cm", "y": "nm"}),
+        ((3, 1), [0.2, 7.0], {"x": "cm", "y": "px"}, (5, 6), [0.2, 3.0], {"x": "cm", "y": "nm"}),
+        ((3, 4), [0.2, 3.0], {"x": "cm", "y": "nm"}, (1, 6), [0.8, 3.0], {"x": "px", "y": "nm"}),
+        (
+            (3, 4, 7, 8, 9),
+            [0.2, 3.0, 0.4, 0.0, 2.0],
+            {"x": "cm", "y": "nm", "z": "mm", "c": "", "t": "m"},
+            (5, 6, 2, 3, 2),
+            [0.2, 3.0, 0.4, 0.0, 2.0],
+            {"x": "cm", "y": "nm", "z": "mm", "c": "", "t": "m"},
+        ),
+        (
+            (3, 1, 7, 8, 1),
+            [0.2, 3.0, 0.4, 0.0, 2.0],
+            {"x": "cm", "y": "px", "z": "mm", "c": "", "t": "sec"},
+            (5, 1, 1, 3, 2),
+            [0.2, 7.0, 8.0, 0.0, 5.0],
+            {"x": "cm", "y": "nm", "z": "px", "c": "", "t": "m"},
+        ),
+        (
+            (3, 4, 7, 8, 9),
+            [0.2, 7.0, 0.4, 0.0, 5.0],
+            {"x": "cm", "y": "nm", "z": "mm", "c": "", "t": "m"},
+            (5, 6, 2, 3, 2),
+            [0.2, 7.0, 0.4, 2.0, 5.0],
+            {"x": "cm", "y": "nm", "z": "mm", "c": "radians", "t": "m"},
+        ),
+    ],
+)
+def test_eq_pixel_size_ignores_singletons_and_channel(graph, shape1, res1, units1, shape2, res2, units2):
+    default_axes = "xyzct"
+    axes1 = default_axes[: len(shape1)]
+    axes2 = default_axes[: len(shape2)]
+    op_data1 = get_data_op_with_pixel_size_meta(graph, axes1, shape1, res1, units1 or [])
+    op_data2 = get_data_op_with_pixel_size_meta(graph, axes2, shape2, res2, units2 or [])
+
+    assert OpDataSelectionGroup._eq_pixel_size_spatial(op_data1.Output, op_data2.Output)
+
+
+@pytest.mark.parametrize(
+    "shape1, res1, units1, shape2, res2, units2",
+    [
+        ((3, 4), [0.2, 7.0], {"x": "cm", "y": "nm"}, (5, 6), [0.2, 3.0], {"x": "cm", "y": "nm"}),
+        ((3, 4), [0.2, 3.0], {"x": "cm", "y": "px"}, (5, 6), [0.2, 3.0], {"x": "cm", "y": "nm"}),
+        ((3, 4), [0.2, 7.0], {"x": "cm", "y": "px"}, (5, 6), [0.2, 3.0], {"x": "cm", "y": "nm"}),
+        ((3, 4), [0.2, 3.0], {"x": "cm", "y": "nm"}, (5, 6), [0.8, 3.0], {"x": "px", "y": "nm"}),
+        (
+            (3, 4, 7, 8, 9),
+            [0.2, 3.0, 0.4, 0.0, 2.0],
+            {"x": "cm", "y": "px", "z": "mm", "c": "", "t": "sec"},
+            (5, 6, 2, 3, 2),
+            [0.2, 7.0, 8.0, 0.0, 5.0],
+            {"x": "cm", "y": "nm", "z": "px", "c": "", "t": "m"},
+        ),
+    ],
+)
+def test_eq_pixel_size_true_negatives(graph, shape1, res1, units1, shape2, res2, units2):
+    default_axes = "xyzct"
+    axes1 = default_axes[: len(shape1)]
+    axes2 = default_axes[: len(shape2)]
+    op_data1 = get_data_op_with_pixel_size_meta(graph, axes1, shape1, res1, units1 or [])
+    op_data2 = get_data_op_with_pixel_size_meta(graph, axes2, shape2, res2, units2 or [])
+
+    assert not OpDataSelectionGroup._eq_pixel_size_spatial(op_data1.Output, op_data2.Output)
+
+
+@pytest.mark.parametrize(
+    "expect_eq, res1, units1, res2, units2",
+    [
+        (True, [0.0, 0.0], None, [0.0, 0.0], None),
+        (True, [0.0, 3.0], {"y": "nm"}, [0.0, 3.0], {"y": "nm"}),
+        (True, [0.0, 3.0], {"y": "nm"}, [0.0, 3.0], {"x": "", "y": "nm"}),
+        (True, [0.0, 3.0], {"x": "nm"}, [0.0, 3.0], {"x": "nm"}),
+        (True, [0.0, 0.0], None, [0.0, 0.0, 0.0, 0.0, 0.0], {"t": ""}),
+        (True, [0.0, 0.0, 0.0, 0.0, 0.0], None, [0.0, 0.0], None),
+        (False, [0.0, 3.0], {"y": "nm"}, [0.0, 0.0], None),
+        (False, [0.0, 0.0, 0.0, 0.0, 1.0], None, [0.0, 0.0], None),
+        (False, [0.0, 0.0], None, [0.0, 0.0, 0.0, 0.0, 0.0], {"t": "cm"}),
+    ],
+)
+def test_eq_pixel_size_empty(graph, expect_eq, res1, units1, res2, units2):
+    default_axes = "xyzct"
+    default_shape = (2, 3, 4, 5, 6)
+    axes1 = default_axes[: len(res1)]
+    shape1 = default_shape[: len(res1)]
+    axes2 = default_axes[: len(res2)]
+    shape2 = default_shape[: len(res2)]
+    op_data1 = get_data_op_with_pixel_size_meta(graph, axes1, shape1, res1, units1 or [])
+    op_data2 = get_data_op_with_pixel_size_meta(graph, axes2, shape2, res2, units2 or [])
+    if not units1:
+        del op_data1.Output.meta["axis_units"]
+    if not units2:
+        del op_data2.Output.meta["axis_units"]
+
+    assert OpDataSelectionGroup._eq_pixel_size_spatial(op_data1.Output, op_data2.Output) == expect_eq
