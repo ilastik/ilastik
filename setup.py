5de3d214dc8b6f7454b80761073aa504c953b076
k-dominik <k-dominik@users.noreply.github.com>
2026-04-15 11:12:16 +0200

    Remove setup.cfg, move contents to pyproject, move setup.py
    pip handles install now completely without setup.py. It is still needed
    in the conda recipe (hence it got moved) in order to allow for loading
    the dynamic version and keeping single source of truth from tags.

--- setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-import setuptools_scm
-
-_version = setuptools_scm.get_version(write_to="ilastik/_version.py")
-
-
-# need to specify version here to play well with conda
-setuptools.setup(version=_version)
